d1109844e25184d41a9d4f2775f3ef2d48ed1a8f
Xiao Bo <xiaobo@sensetime.com>
2022-08-12 16:30:49 +0800

    implement a few utilities to manipulate diopi-tensors

--- DIOPI-IMPL/cuda/functions.cu
+++ DIOPI-IMPL/cuda/functions.cu
@@ -1,34 +1,43 @@
+/**************************************************************************************************
+ * Copyright (c) 2022, SenseTime Inc.
+ * License
+ * Author
+ *
+ *************************************************************************************************/
+
 #include <diopi/functions.h>
 #include <cuda_runtime.h>
 
+#include "helper.hpp"
 
-#define dispatch_dtype(fun, dtype, gridSize, blockSize, ...)                                     \
+
+#define dispatch_dtype(fun, dtype, gridSize, blockSize, stream, ...)                             \
     if (diopi_dtype_int32 == dtype) {                                                            \
-        fun<int32_t><<<gridSize, blockSize>>>(__VA_ARGS__);                                      \
+        fun<int32_t><<<gridSize, blockSize, 0, stream>>>(__VA_ARGS__);                           \
     }                                                                                            \
     else if (diopi_dtype_uint32 == dtype) {                                                      \
-        fun<uint32_t><<<gridSize, blockSize>>>(__VA_ARGS__);                                     \
+        fun<uint32_t><<<gridSize, blockSize, 0, stream>>>(__VA_ARGS__);                          \
     }                                                                                            \
     else if (diopi_dtype_int16 == dtype) {                                                       \
-        fun<int16_t><<<gridSize, blockSize>>>(__VA_ARGS__);                                      \
+        fun<int16_t><<<gridSize, blockSize, 0, stream>>>(__VA_ARGS__);                           \
     }                                                                                            \
     else if (diopi_dtype_uint16 == dtype) {                                                      \
-        fun<uint16_t><<<gridSize, blockSize>>>(__VA_ARGS__);                                     \
+        fun<uint16_t><<<gridSize, blockSize, 0, stream>>>(__VA_ARGS__);                          \
     }                                                                                            \
     else if (diopi_dtype_int8 == dtype) {                                                        \
-        fun<int8_t><<<gridSize, blockSize>>>(__VA_ARGS__);                                       \
+        fun<int8_t><<<gridSize, blockSize, 0, stream>>>(__VA_ARGS__);                            \
     }                                                                                            \
     else if (diopi_dtype_uint8 == dtype) {                                                       \
-        fun<uint8_t><<<gridSize, blockSize>>>(__VA_ARGS__);                                      \
+        fun<uint8_t><<<gridSize, blockSize, 0, stream>>>(__VA_ARGS__);                           \
     }                                                                                            \
     else if (diopi_dtype_float32 == dtype) {                                                     \
-        fun<float><<<gridSize, blockSize>>>(__VA_ARGS__);                                        \
+        fun<float><<<gridSize, blockSize, 0, stream>>>(__VA_ARGS__);                             \
     }                                                                                            \
     else if (diopi_dtype_float64 == dtype) {                                                     \
-        fun<double><<<gridSize, blockSize>>>(__VA_ARGS__);                                       \
+        fun<double><<<gridSize, blockSize, 0, stream>>>(__VA_ARGS__);                            \
     }                                                                                            \
     else if (diopi_dtype_bool == dtype) {                                                        \
-        fun<bool><<<gridSize, blockSize>>>(__VA_ARGS__);                                         \
+        fun<bool><<<gridSize, blockSize, 0, stream>>>(__VA_ARGS__);                              \
     }                                                                                            \
     else {                                                                                       \
         fprintf(stderr, "%s:%s: %s<%s %d><<<%d,%d>>>(%s)", __FILE__, __FUNCTION__, #fun, #dtype, \
@@ -52,38 +61,15 @@ void vecAdd(const void* a, const void* b, void* c, const int numel)
 extern "C" diopiError_t add(diopiContextHandle_t ctx, diopiTensorHandle_t out,
         const diopiTensorHandle_t input, const diopiTensorHandle_t other)
 {
-    diopiDevice_t       device;
-    diopiDtype_t        dtype;
-    int64_t             numel;
-    diopiSize_t         shape;
-    diopiSize_t         stride;
-    diopiStreamHandle_t stream_handle;
-    diopiGetStream(ctx, &stream_handle);
-    diopiGetTensorDevice(input, &device);
-    diopiGetTensorDtype(input, &dtype);
-    diopiGetTensorNumel(input, &numel);
-
-    diopiGetTensorShape(input, &shape);
-    diopiGetTensorStride(input, &stride);
-
-    const void* input_tensor_data_ptr;
-    diopiGetTensorDataConst(input, &input_tensor_data_ptr);
-    const void* other_tensor_data_ptr;
-    diopiGetTensorDataConst(other, &other_tensor_data_ptr);
-
-    void* out_tensor_data_ptr;
-    diopiGetTensorData(out, &out_tensor_data_ptr);
+    auto stream  = impl::cuda::getStream(ctx);
+    auto trInput = impl::cuda::makeTensor(input);
+    auto trOther = impl::cuda::makeTensor(other);
+    auto trOut   = impl::cuda::makeTensor(out);
 
     int blockSize = 256;
-    int gridSize  = (numel + blockSize - 1) / blockSize;
-    dispatch_dtype(vecAdd,
-                dtype,
-                gridSize,
-                blockSize,
-                input_tensor_data_ptr,
-                other_tensor_data_ptr,
-                out_tensor_data_ptr,
-                numel);
+    int gridSize  = (trInput.numel() + blockSize - 1) / blockSize;
+    dispatch_dtype(vecAdd, trInput.dtype(), gridSize, blockSize, stream,
+        trInput.data(), trOther.data(), trOut.data(), trInput.numel());
     return diopiSuccess;
 }
 
@@ -100,23 +86,19 @@ void vecFill(void* a, const T value, const int numel)
 
 extern "C" diopiError_t fill(diopiContextHandle_t ctx, diopiTensorHandle_t tensor, const float value)
 {
-    diopiDevice_t device;
-    diopiDtype_t  dtype;
-    int64_t       numel;
+    auto stream = impl::cuda::getStream(ctx);
+    auto tr = impl::cuda::makeTensor(tensor);
 
-    diopiGetTensorDevice(tensor, &device);
-    diopiGetTensorDtype(tensor, &dtype);
-    diopiGetTensorNumel(tensor, &numel);
-
-    void* tensor_data_ptr;
-    diopiGetTensorData(tensor, &tensor_data_ptr);
+    diopiDevice_t device = tr.device();
+    diopiDtype_t  dtype  = tr.dtype();
+    int64_t       numel  = tr.numel();
 
     if (diopi_host == device) {
         return diopiErrorOccurred;
     } else {
         int blockSize = 256;
         int gridSize  = (numel + blockSize - 1) / blockSize;
-        dispatch_dtype(vecFill, dtype, gridSize, blockSize, tensor_data_ptr, value, numel);
+        dispatch_dtype(vecFill, dtype, gridSize, blockSize, stream, tr.data(), value, numel);
     }
 
     return diopiSuccess;
---
+++ DIOPI-IMPL/cuda/helper.hpp
@@ -0,0 +1,106 @@
+/**************************************************************************************************
+ * Copyright (c) 2022, SenseTime Inc.
+ * License
+ * Author
+ *
+ *************************************************************************************************/
+
+#ifndef _DIOPI_REFERENCE_IMPLCUDA_HELPER_HPP_
+#define _DIOPI_REFERENCE_IMPLCUDA_HELPER_HPP_
+
+#include <diopi/diopirt.h>
+#include <cuda_runtime.h>
+
+
+namespace impl {
+
+namespace cuda {
+
+template<typename T>
+struct DataType;
+
+template<>
+struct DataType<diopiTensorHandle_t> {
+    using type = void*;
+
+    static void* data(diopiTensorHandle_t& tensor) {
+        void* data;
+        diopiGetTensorData(&tensor, &data);
+        return data;
+    }
+};
+
+template<>
+struct DataType<const diopiTensorHandle_t> {
+    using type = const void*;
+
+    static const void* data(const diopiTensorHandle_t& tensor) {
+        const void* data;
+        diopiGetTensorDataConst(&tensor, &data);
+        return data;
+    }
+};
+
+template<typename T>
+class DiopiTensor final {
+public:
+    explicit DiopiTensor(T& tensor) : tensor_(tensor) {}
+
+    diopiDevice_t device() const {
+        diopiDevice_t device;
+        diopiGetTensorDevice(tensor_, &device);
+        return device;
+    }
+    diopiDtype_t dtype() const {
+        diopiDtype_t dtype;
+        diopiGetTensorDtype(tensor_, &dtype);
+        return dtype;
+    }
+
+    const diopiSize_t& shape() const {
+        diopiGetTensorShape(tensor_, &shape_);
+        return shape_;
+    }
+    const diopiSize_t& stride() const {
+        diopiGetTensorStride(tensor_, &stride_);
+        return stride_;
+    }
+
+    int64_t numel() const {
+        int64_t numel;
+        diopiGetTensorNumel(tensor_, &numel);
+        return numel;
+    }
+    int64_t elemsize() const {
+        int64_t elemsize;
+        diopiGetTensorElemSize(tensor_, &elemsize);
+        return elemsize;
+    }
+
+    typename DataType<T>::type data() {
+        return DataType<T>::data(tensor_);
+    }
+
+protected:
+    T& tensor_;
+
+    diopiSize_t shape_;
+    diopiSize_t stride_;
+};
+
+template<typename T>
+auto makeTensor(T& tensor) -> DiopiTensor<T> {
+    return DiopiTensor<T>(tensor);
+}
+
+inline cudaStream_t getStream(diopiContextHandle_t ctx) {
+    diopiStreamHandle_t stream_handle;
+    diopiGetStream(ctx, &stream_handle);
+    return static_cast<cudaStream_t>(stream_handle);
+}
+
+}  // namespace cuda
+
+}  // namespace impl
+
+#endif  // _DIOPI_REFERENCE_IMPLCUDA_HELPER_HPP_
