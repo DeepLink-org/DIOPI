8ce48c1eb5c0c0a43189f427c8a64a69410d2834
gfyts0123 <zhyanwentao@126.com>
2023-02-13 15:26:33 +0800

    fix: remove unexpected dtype

--- DIOPI-IMPL/camb_pytorch/aten_helper.hpp
+++ DIOPI-IMPL/camb_pytorch/aten_helper.hpp
@@ -130,12 +130,8 @@ caffe2::TypeMeta getATenType(diopiDtype_t dt) {
         return caffe2::TypeMeta::Make<uint16_t>();
     case diopi_dtype_int32:
         return caffe2::TypeMeta::Make<int32_t>();
-    // case  diopi_dtype_uint32: // can not find symbol for uint32_t
-    //     return caffe2::TypeMeta::Make<uint32_t>();
     case diopi_dtype_int64:
         return caffe2::TypeMeta::Make<int64_t>();
-    // case diopi_dtype_uint64:  // can not find symbol for uint64_t
-    //     return caffe2::TypeMeta::Make<uint64_t>();
     case diopi_dtype_float32:
         return caffe2::TypeMeta::Make<float>();
     case diopi_dtype_float64:
