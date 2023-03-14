8d90b0fbeeb007f2820f21b54c0c6d8e0588ec6e
段翔 <dx2077@outlook.com>
2023-03-14 13:56:20 +0800

    add randperm (#83)
    Co-authored-by: duanxiang <duanxiang@cambricon.com>

---
+++ DIOPI-IMPL/camb/functions/randperm.cpp
@@ -0,0 +1,25 @@
+#include <diopi/functions.h>
+
+#include <algorithm>
+#include <numeric>
+
+#include "../cnnl_helper.hpp"
+
+namespace impl {
+namespace camb {
+
+extern "C" DIOPI_API diopiError_t diopiRandperm(diopiContextHandle_t ctx, diopiTensorHandle_t out, int64_t n, int64_t idx) {
+    auto out_tensor = makeTensor(out);
+    long int arr[n];
+    std::iota(arr, arr + n, 0);
+    std::random_shuffle(arr, arr + n);
+    auto ret = cnrtMemcpy(out_tensor.data(), arr, sizeof(long int) * n, cnrtMemcpyHostToDev);
+    if (ret != cnrtSuccess) {
+        set_last_error_string("%s%d", "cnrt memcpy error, ret = ", ret);
+        return diopiErrorOccurred;
+    }
+    return diopiSuccess;
+}
+
+}  // namespace camb
+}  // namespace impl
