9dac833e6abf18bd2f0fe79943c0a830f8a4da06
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-03-24 11:23:38 +0800

    add ascend fill op

--- DIOPI-IMPL/ascend/functions/fill.cpp
+++ DIOPI-IMPL/ascend/functions/fill.cpp
@@ -2,16 +2,20 @@
 #include "../common/acloprunner.hpp"
 
 namespace impl {
-namespace camb {
+namespace ascend {
 
 extern "C" {
 
 diopiError_t diopiFill(diopiContextHandle_t ctx, diopiTensorHandle_t input, const diopiScalar_t* value) {
-
+    AclOpRunner<1, 1>("Fills")
+        .addInput(input)
+        .setAttr<float>("value", getValue<float>(value))
+        .addOutput(input)
+        .run(ctx);
     return diopiSuccess;
 }
 
 }  // extern "C"
 
-}  // namespace camb
+}  // namespace ascend
 }  // namespace impl
