a1ba1ee81a46a738a922a43e1faef60aeda4f418
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-03-24 11:30:53 +0800

    add conv2d for ascend

--- DIOPI-IMPL/ascend/functions/conv_2d.cpp
+++ DIOPI-IMPL/ascend/functions/conv_2d.cpp
@@ -5,7 +5,7 @@
 #include "../common/acloprunner.hpp"
 
 namespace impl {
-namespace camb {
+namespace ascend {
 extern "C" diopiError_t diopiConvolution2d(diopiContextHandle_t ctx,
                                            diopiTensorHandle_t out,
                                            diopiConstTensorHandle_t input,
@@ -15,7 +15,36 @@ extern "C" diopiError_t diopiConvolution2d(diopiContextHandle_t ctx,
                                            diopiSize_t padding,
                                            diopiSize_t dilation,
                                            int64_t groups) {
+    auto format = getAclDataFormat(input);
+    const std::string dataFormat =
+        (format == ACL_FORMAT_NHWC) ? "NHWC" : "NCHW";
+    std::vector<int64_t> strideTemp(4, 1);
+    std::vector<int64_t> dilationsTemp(4, 1);
+    if (format == ACL_FORMAT_NHWC) {
+        strideTemp[1] = stride.data[0];
+        strideTemp[2] = stride.data[1];
+        dilationsTemp[1] = dilation.data[0];
+        dilationsTemp[2] = dilation.data[1];
+    } else {
+        strideTemp[2] = stride.data[0];
+        strideTemp[3] = stride.data[1];
+        dilationsTemp[2] = dilation.data[0];
+        dilationsTemp[3] = dilation.data[1];
+    }
+    const std::vector<int64_t> paddingTemp = {padding.data[0], padding.data[2], padding.data[1], padding.data[3]};
 
+    AclOpRunner<3, 1> runner("Conv2D");
+    runner.addInput(input, weight)
+        .setAttr("strides", strideTemp)
+        .setAttr("pads", paddingTemp)
+        .setAttr("dilations", dilationsTemp)
+        .setAttr<int32_t>("groups", groups)
+        .setAttr("data_format", dataFormat)
+        .addOutput(out);
+    if (bias) {
+        runner.addInput(bias);
+    }
+    runner.run(ctx);
     return diopiSuccess;
 }
 
@@ -37,5 +66,5 @@ extern "C" diopiError_t diopiConvolution2dBackward(diopiContextHandle_t ctx,
     return diopiSuccess;
 }
 
-}  // namespace camb
+}  // namespace ascend
 }  // namespace impl
