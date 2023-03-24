38b25dbe23228f35280c45f040581c123df4e303
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-03-24 19:12:02 +0800

    support bool dtype for ascend add op

--- DIOPI-IMPL/ascend/functions/binary_op.cpp
+++ DIOPI-IMPL/ascend/functions/binary_op.cpp
@@ -11,16 +11,25 @@
 namespace impl {
 namespace ascend {
 
+namespace {
+aclDataType dtypeConvertor(diopiConstTensorHandle_t th) {
+    auto dtype = getAclDataType(th);
+    if (dtype == ACL_BOOL) {
+        return ACL_UINT8;
+    }
+    return dtype;
+}
+
+}  // namespace
+
 extern "C" DIOPI_API diopiError_t
 diopiAdd(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t other, const diopiScalar_t* alpha) {
     const float value = (alpha != nullptr) ? getValue<float>(alpha) : 1.0;
+
     if (value == 1.0) {
-        AclOpRunner<2,1> ("AddV2")
-            .addInput(input, other)
-            .addOutput(out)
-            .run(ctx);
+        AclOpRunner<2, 1, dtypeConvertor>("AddV2").addInput(input, other).addOutput(out).run(ctx);
     } else {
-        AclOpRunner<2,1> ("AxpyV2")
+        AclOpRunner<2, 1>("AxpyV2")
             .addInput(input, ACL_FORMAT_ND)
             .addInput(other, ACL_FORMAT_ND)
             .setAttr<float>("alpha", value)
@@ -38,11 +47,7 @@ extern "C" DIOPI_API diopiError_t diopiAddInp(diopiContextHandle_t ctx, diopiTen
 extern "C" DIOPI_API diopiError_t
 diopiAddScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* other, const diopiScalar_t* alpha) {
     const float value = (alpha != nullptr) ? getValue<float>(alpha) : 1.0;
-     AclOpRunner<1,1> ("Adds")
-        .addInput(input)
-        .setAttr<float>("value", value * getValue<float>(other))
-        .addOutput(out)
-        .run(ctx);
+    AclOpRunner<1, 1>("Adds").addInput(input).setAttr<float>("value", value * getValue<float>(other)).addOutput(out).run(ctx);
     return diopiSuccess;
 }
 
