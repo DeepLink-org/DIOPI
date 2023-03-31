6e46d5c08f8d422a6039c1d3b482117d1ccfcc6c
yangbofun <37532326+yangbofun@users.noreply.github.com>
2023-03-31 11:41:35 +0800

    add half (#174)

---
+++ DIOPI-IMPL/camb/common/float16.hpp
@@ -0,0 +1,6 @@
+#ifndef IMPL_CAMB_COMMON_FLOAT16_HPP_
+#define IMPL_CAMB_COMMON_FLOAT16_HPP_
+
+#include <half.hpp>
+
+#endif  // IMPL_CAMB_COMMON_FLOAT16_HPP_
--- DIOPI-IMPL/scripts/ci_script.sh
+++ DIOPI-IMPL/scripts/ci_script.sh
@@ -1,13 +1,16 @@
 # !/bin/bash
 set -e
 
+current_path=$(cd "$(dirname "$0")"; pwd)
+echo $current_path
+
 case $1 in
   cpp-lint)
     # for other cpplint version, maybe  -whitespace/indent is needed to check impl
     # --repository=.. will be deleted when repository changed.
     (echo "cpp-lint" && python scripts/cpplint.py --linelength=160 --repository=.. \
       --filter=-build/c++11,-legal/copyright,-build/include_subdir,-runtime/references,-runtime/printf,-runtime/int,-build/namespace \
-      --recursive ./) \
+      --exclude=${current_path}/../third_party --recursive ./ )  \
     || exit -1;;
     *)
     echo -e "[ERROR] Incorrect option:" $1;
