2d0753506f8930f3939b900916a7e29e4b35e215
yeyeye333 <1020402580@qq.com>
2023-04-03 18:31:20 +0800

    add cumsum (#151)
    Co-authored-by: yehongjuan <yehongjuan@cambricon.com>

---
+++ DIOPI-IMPL/camb/functions/cumsum.cpp
@@ -0,0 +1,48 @@
+#include <diopi/functions.h>
+#include "../cnnl_helper.hpp"
+#include "../common/common.hpp"
+
+namespace impl {
+namespace camb {
+namespace {
+
+int getDim(DiopiTensor tensor, int64_t dim) {
+    int shape_size = tensor.shape().size();
+    int dim_ = static_cast<int>(dim);
+    if (dim_ < 0) {
+        dim_ = dim_ + shape_size;
+    }
+    return dim_;
+}
+
+}  // namespace
+
+extern "C" {
+
+DIOPI_API diopiError_t diopiCumsum(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t dim) {
+    cnnlHandle_t handle = cnnlHandlePool.get(ctx);
+
+    auto input_tensor = DiopiTensor(input);
+    auto out_tensor = DiopiTensor(out);
+
+    std::vector<DiopiTensor*> pTensors{&input_tensor};
+    std::set<diopiDtype_t> supportedDtypes{diopi_dtype_int8, diopi_dtype_int16, diopi_dtype_int32, diopi_dtype_float16, diopi_dtype_float32};
+    autoCastTensorType(ctx, pTensors, supportedDtypes);
+    DiopiTensor input_tensor_tmp = *pTensors[0];
+    DiopiTensor out_tensor_tmp = dataTypeCast(ctx, out_tensor, input_tensor_tmp.dtype());
+
+    CnnlTensorDesc input_desc(input_tensor_tmp, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc out_desc(out_tensor_tmp, CNNL_LAYOUT_ARRAY);
+
+    int axis = getDim(input_tensor, dim);
+
+    DIOPI_CHECKCNNL(
+        cnnlCumsum(handle, input_desc.get(), input_tensor_tmp.data(), axis, false, false, CNNL_PROPAGATE_NAN, out_desc.get(), out_tensor_tmp.data()));
+    dataTypeCast(ctx, out_tensor, out_tensor_tmp);
+    return diopiSuccess;
+}
+
+}  // extern "C"
+
+}  // namespace camb
+}  // namespace impl
