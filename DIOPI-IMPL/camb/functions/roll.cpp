0f6d91237a185c8be1cb140aa3e9c919378777c3
yeyeye333 <1020402580@qq.com>
2023-04-03 18:28:32 +0800

    add roll (#147)
    * add roll
    
    ---------
    
    Co-authored-by: yehongjuan <yehongjuan@cambricon.com>

---
+++ DIOPI-IMPL/camb/functions/roll.cpp
@@ -0,0 +1,45 @@
+#include <diopi/functions.h>
+#include "../cnnl_helper.hpp"
+#include "../common/common.hpp"
+
+namespace impl {
+namespace camb {
+
+extern "C" {
+
+DIOPI_API diopiError_t diopiRoll(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t shifts, diopiSize_t dims) {
+    cnnlHandle_t handle = cnnlHandlePool.get(ctx);
+
+    auto input_tensor = DiopiTensor(input);
+    auto out_tensor = DiopiTensor(out);
+
+    CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc out_desc(out_tensor, CNNL_LAYOUT_ARRAY);
+
+    std::vector<int> shifts_{shifts.data, shifts.data + shifts.len};
+    std::vector<int> dims_{dims.data, dims.data + dims.len};
+    size_t workspace_size = 0;
+    DIOPI_CALLCNNL(cnnlGetRollWorkspaceSize(handle, input_desc.get(), &workspace_size));
+    void* workspace = nullptr;
+    if (0 != workspace_size) {
+        workspace = requiresBuffer(ctx, workspace_size).data();
+    }
+
+    DIOPI_CALLCNNL(cnnlRoll(handle,
+                            input_desc.get(),
+                            input_tensor.data(),
+                            shifts_.data(),
+                            shifts_.size(),
+                            dims_.size() > 0 ? dims_.data() : nullptr,
+                            dims_.size(),
+                            workspace,
+                            workspace_size,
+                            out_desc.get(),
+                            out_tensor.data()));
+    return diopiSuccess;
+}
+
+}  // extern "C"
+
+}  // namespace camb
+}  // namespace impl
