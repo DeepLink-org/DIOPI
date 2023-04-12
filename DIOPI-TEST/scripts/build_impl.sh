8579b14fc606a25ace4172b5ea4e0736fb59d75f
CokeDong <408244909@qq.com>
2023-04-12 11:09:57 +0800

    Add test scripts for mmcv ext ops (#62)
    * Add diopi-test for mmcv ext ops
    
    * add mmcv
    
    * Fix
    
    * Support device dispatch
    
    * Fix

--- DIOPI-TEST/scripts/build_impl.sh
+++ DIOPI-TEST/scripts/build_impl.sh
@@ -44,6 +44,10 @@ case $1 in
     (rm -rf build && mkdir build && cd build \
         &&cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DIMPL_OPT=TORCH -DHIP=ON && make -j4) \
     || exit -1;;
+  mmcv_ext)
+    (cd third_party/mmcv_diopi && rm -rf build && mkdir build \
+        && MMCV_WITH_DIOPI=1 MMCV_WITH_OPS=1 python setup.py build_ext -i) \
+    || exit -1;;
   *)
     echo -e "[ERROR] Incorrect compilation option:" $1;
 
