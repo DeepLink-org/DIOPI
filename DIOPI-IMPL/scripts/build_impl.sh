0ea4a98f03fbac2a6dd19282fcafcc80f4004dba
xintian <xintian@pjlab.org.cn>
2023-04-17 21:15:18 +0800

    fix ci

--- DIOPI-IMPL/scripts/build_impl.sh
+++ DIOPI-IMPL/scripts/build_impl.sh
@@ -25,11 +25,19 @@ case $1 in
     mkdir -p build && cd build && cmake .. -DIMPL_OPT=camb -DTEST=${DIOPI_BUILD_TESTRT} && make;;
   ascend)
     mkdir -p build && cd build && cmake .. -DIMPL_OPT=ascend -DTEST=${DIOPI_BUILD_TESTRT} && make;;
+  hip_pytorch)
+    mkdir build && cd build && cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DIMPL_OPT=TORCH -DHIP=ON && make -j4 \
+    || exit -1;;
+  mmcv_ext)
+    (cd third_party/mmcv_diopi && rm -rf build && mkdir build \
+        && MMCV_WITH_DIOPI=1 MMCV_WITH_OPS=1 python setup.py build_ext -i) \
+    || exit -1;;
   *)
     echo -e "[ERROR] Incorrect compilation option:" $1;
 esac
 
-if [[ 'x'$DIOPI_BUILD_RT == "xOn" ]] ; then
+if [[ 'x'$DIOPI_BUILD_TESTRT == "xOn" ]] ; then
+  echo ${DIOPI_TEST_PATH}
   mkdir -p ${DIOPI_TEST_PATH}/lib
   ln -sf ${CURRENT_DIR}/../lib/libdiopi_impl.so ${DIOPI_TEST_PATH}/lib
   ln -sf ${CURRENT_DIR}/../lib/libdiopirt.so ${DIOPI_TEST_PATH}/lib
