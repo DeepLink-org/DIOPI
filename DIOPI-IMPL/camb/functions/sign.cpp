0619796a2d37022fb8fc486c317dc999021ff5e2
zhangqiu <zhangqiu@sensetime.com>
2023-05-11 19:37:10 +0800

    fix format

--- DIOPI-IMPL/camb/functions/sign.cpp
+++ DIOPI-IMPL/camb/functions/sign.cpp
@@ -13,32 +13,29 @@ namespace impl {
 namespace camb {
 extern "C" {
 
-diopiError_t diopiSign(diopiContextHandle_t ctx, diopiTensorHandle_t out,
-                       diopiConstTensorHandle_t input) {
-  cnnlHandle_t handle = cnnlHandlePool.get(ctx);
-  DiopiTensor input_tensor(input);
-  DiopiTensor out_tensor(out);
+diopiError_t diopiSign(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    cnnlHandle_t handle = cnnlHandlePool.get(ctx);
+    DiopiTensor input_tensor(input);
+    DiopiTensor out_tensor(out);
 
-  std::vector<DiopiTensor *> pTensors{&input_tensor};
-  std::set<diopiDtype_t> supportedDtypes{diopi_dtype_float16,
-                                         diopi_dtype_float32};
-  DIOPI_CALL(autoCastTensorType(ctx, pTensors, supportedDtypes));
+    std::vector<DiopiTensor *> pTensors{&input_tensor};
+    std::set<diopiDtype_t> supportedDtypes{diopi_dtype_float16, diopi_dtype_float32};
+    DIOPI_CALL(autoCastTensorType(ctx, pTensors, supportedDtypes));
 
-  DiopiTensor out_tensor_temp = out_tensor;
-  if (input_tensor.dtype() != out_tensor_temp.dtype()) {
-    DIOPI_CALL(dataTypeCast(ctx, out_tensor_temp, input_tensor.dtype()));
-  }
+    DiopiTensor out_tensor_temp = out_tensor;
+    if (input_tensor.dtype() != out_tensor_temp.dtype()) {
+        DIOPI_CALL(dataTypeCast(ctx, out_tensor_temp, input_tensor.dtype()));
+    }
 
-  CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
-  CnnlTensorDesc out_desc(out_tensor_temp, CNNL_LAYOUT_ARRAY);
-  DIOPI_CALLCNNL(cnnlSign(handle, input_desc.get(), input_tensor.data(),
-                          out_desc.get(), out_tensor_temp.data()));
+    CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc out_desc(out_tensor_temp, CNNL_LAYOUT_ARRAY);
+    DIOPI_CALLCNNL(cnnlSign(handle, input_desc.get(), input_tensor.data(), out_desc.get(), out_tensor_temp.data()));
 
-  if (out_tensor_temp.dtype() != out_tensor.dtype()) {
-    DIOPI_CALL(dataTypeCast(ctx, out_tensor, out_tensor_temp));
-  }
-  return diopiSuccess;
+    if (out_tensor_temp.dtype() != out_tensor.dtype()) {
+        DIOPI_CALL(dataTypeCast(ctx, out_tensor, out_tensor_temp));
+    }
+    return diopiSuccess;
 }
 }
-} // namespace camb
-} // namespace impl
+}  // namespace camb
+}  // namespace impl
