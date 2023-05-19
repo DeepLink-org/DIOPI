1bf50e029ef4b6c0fc6e0a4d47ff5b1c42cf8101
ZhangQiu <100055343+NeosZhang@users.noreply.github.com>
2023-05-19 17:03:50 +0800

    fix reciprocal but float64 not supported (#70)
    Co-authored-by: zhangqiu <zhangqiu@sensetime.com>

--- DIOPI-IMPL/camb/device_configs.py
+++ DIOPI-IMPL/camb/device_configs.py
@@ -534,20 +534,7 @@ device_configs = {
             args=[
                 {
                     "ins": ['input'],
-                    "dtype": [Skip(Dtype.float64), Skip(Dtype.float32), Skip(Dtype.float16)],
-                },
-            ],
-        ),
-    ),
-
-    'reciprocal_int': dict(
-        name=["reciprocal"],
-        tensor_para=dict(
-            args=[
-                {
-                    "ins": ['input'],
-                    "dtype": [Skip(Dtype.int64), Skip(Dtype.int32), Skip(Dtype.int16),
-                              Skip(Dtype.int8), Skip(Dtype.uint8), Skip(Dtype.bool)],
+                    "dtype": [Skip(Dtype.float64)],
                 },
             ],
         ),
