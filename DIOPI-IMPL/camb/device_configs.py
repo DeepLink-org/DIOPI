0e7e93a0265b6d8f54ca9c092ffe4e291ec1c9f6
POI-WX <wangxing2@sensetime.com>
2023-05-22 15:49:38 +0800

    refine the code comment

--- DIOPI-IMPL/camb/device_configs.py
+++ DIOPI-IMPL/camb/device_configs.py
@@ -385,8 +385,8 @@ device_configs = {
     'embedding': dict(
         name=["embedding"],
         para=dict(
-        # The diopiEmbeddingRenorm_ function is temporarily unavailable due to the unsupported Cambrian operator.  
-        # Thus, to pass the test case, skip all non-None types of the max_norm parameter in the configuration file.
+            # The diopiEmbeddingRenorm_ function is temporarily unavailable due to the unsupported Cambrian operator.  
+            # Thus, to pass the test case, skip all non-None types of the max_norm parameter in the configuration file.
             max_norm=[Skip(1.0)],   
         ),
     ),
