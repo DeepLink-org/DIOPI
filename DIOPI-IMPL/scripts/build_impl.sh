9012f4b542b6da8e3ebda0ef1783c19d9cb6dc46
small-chen-wen <1710207933@qq.com>
2023-05-22 17:22:51 +0800

    add adaptor

--- DIOPI-IMPL/scripts/build_impl.sh
+++ DIOPI-IMPL/scripts/build_impl.sh
@@ -13,7 +13,7 @@ case $1 in
     mkdir -p build && cd build && cmake .. -DIMPL_OPT=cuda -DTEST=${DIOPI_BUILD_TESTRT} && make;;
   torch)
     mkdir -p build && cd build && cmake .. -DIMPL_OPT=torch -DDEBUG=ON -DTEST=${DIOPI_BUILD_TESTRT} \
-    # -DCMAKE_PREFIX_PATH=/nvme/share/share/xt/pytorch1.10/torch/share/cmake \
+    # -DCMAKE_PREFIX_PATH=/nvme/share/share/xt/pytorch1.10/torch/share/cmake && make;;
     -DCMAKE_PREFIX_PATH=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'` \
     && make;;
   torch_dyload)
