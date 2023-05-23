e534fb7a48fa1b7d350918440b309ecb7684a97e
yangbofun <yangbofun@163.com>
2023-05-23 15:49:17 +0800

    delete no used

--- DIOPI-IMPL/scripts/ci_script.sh
+++ DIOPI-IMPL/scripts/ci_script.sh
@@ -13,10 +13,8 @@ case $1 in
       --recursive ./ )  \
     || exit 1;;
   clang-tidy)
-    (echo "CMAKE_EXPORT_COMPILE_COMMANDS_FILE: ${CMAKE_EXPORT_COMPILE_COMMANDS_FILE}"
-    ls -l ${CMAKE_EXPORT_COMPILE_COMMANDS_FILE}
+    (
     if [ -e ${CMAKE_EXPORT_COMPILE_COMMANDS_FILE} ]; then
-      echo "111111111111111"
       python3 ${CURRENT_PATH}/../../run-clang-tidy.py -p `dirname "${CMAKE_EXPORT_COMPILE_COMMANDS_FILE}"`
     else
       echo "error: compile_commands.json not found."
