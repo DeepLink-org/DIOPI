8bbc91ea27ea1c29c7bb8d05e97301a6211d0187
yangbofun <yangbofun@163.com>
2023-06-05 16:40:33 +0800

    lint

--- DIOPI-IMPL/camb/diopi_helper.hpp
+++ DIOPI-IMPL/camb/diopi_helper.hpp
@@ -24,12 +24,12 @@
 namespace impl {
 namespace camb {
 
-#define DIOPI_CHECK(cond, fmt, args...)                                          \
-    do {                                                                         \
-        if (!(cond)) {                                                           \
+#define DIOPI_CHECK(cond, fmt, args...)                                       \
+    do {                                                                      \
+        if (!(cond)) {                                                        \
             setLastErrorString(#fmt " at %s:%d", ##args, __FILE__, __LINE__); \
-            return diopiErrorOccurred;                                           \
-        }                                                                        \
+            return diopiErrorOccurred;                                        \
+        }                                                                     \
     } while (false);
 
 #define DIOPI_CHECK_NULLPTR_ABORT(variable)                                                      \
@@ -48,13 +48,13 @@ namespace camb {
         }                                                        \
     } while (false);
 
-#define DIOPI_CALL(Expr)                                                                                                                                  \
-    do {                                                                                                                                                  \
-        diopiError_t ret = Expr;                                                                                                                          \
-        if (diopiSuccess != ret) {                                                                                                                        \
+#define DIOPI_CALL(Expr)                                                                                                                           \
+    do {                                                                                                                                           \
+        diopiError_t ret = Expr;                                                                                                                   \
+        if (diopiSuccess != ret) {                                                                                                                 \
             setLastErrorString("%s: %s called by `%s` at %s:%d\n", getDiopiErrorStr(ret), cambGetLastErrorString(), __func__, __FILE__, __LINE__); \
-            return ret;                                                                                                                                   \
-        }                                                                                                                                                 \
+            return ret;                                                                                                                            \
+        }                                                                                                                                          \
     } while (false);
 
 enum class MemoryFormat : size_t { Contiguous = 0, ChannelsLast = 1, ChannelsLast3d = 2, Preserve = 3 };
@@ -136,9 +136,7 @@ public:
         return shape_;
     }
 
-    int64_t size(int i) {
-        return shape()[i];
-    }
+    int64_t size(int i) { return shape()[i]; }
 
     const std::vector<int64_t>& stride() const {
         DIOPI_CHECK_NULLPTR_ABORT(tensor_);
