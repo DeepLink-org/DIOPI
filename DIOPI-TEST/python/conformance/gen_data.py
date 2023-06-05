351522c5a88e90a34c217d4e448f861571d2dda3
liangshinan <liangshinan@sensetime.com>
2023-06-05 10:06:59 +0800

    lint

--- DIOPI-TEST/python/conformance/gen_data.py
+++ DIOPI-TEST/python/conformance/gen_data.py
@@ -229,7 +229,7 @@ def gen_tensor(arg: dict, cfg_dict: dict) -> np.ndarray:
         elif gen_fn == Genfunc.positive:
             value = np.abs(np.array(np.random.randn(*shape)).astype(dtype))
         elif gen_fn == Genfunc.sym_mat:
-            axis = [i for i in range(len(shape)-2)] + [-1, -2]
+            axis = [i for i in range(len(shape) - 2)] + [-1, -2]
             mat = np.random.randn(*shape).astype(dtype)
             value = mat @ mat.transpose(axis) + 1e-3
         elif gen_fn == Genfunc.randn_cmplx:
