978bad724b4ba8dbca6757c44fb447fd8ea4c47f
zhangqiu <zhangqiu@sensetime.com>
2023-06-09 12:56:58 +0800

    fix lint

--- DIOPI-IMPL/camb/functions/remainder.cpp
+++ DIOPI-IMPL/camb/functions/remainder.cpp
@@ -8,7 +8,6 @@
 
 #include "../cnnl_helper.hpp"
 #include "../common/common.hpp"
-#include "../common/debug.hpp"
 
 namespace impl {
 namespace camb {
@@ -19,43 +18,15 @@ DIOPI_API diopiError_t diopiRemainderTensor(diopiContextHandle_t ctx, diopiTenso
     DiopiTensor input_tensor(input);
     DiopiTensor other_tensor(other);
     DiopiTensor out_tensor(out);
-    std::cout << "before dtype cast" << std::endl;
-    std::cout << "input.dtype = " << input_tensor.dtype() << std::endl;
-    std::cout << "other.dtype = " << other_tensor.dtype() << std::endl;
-    std::cout << "out.dtype = " << out_tensor.dtype() << std::endl;
-    std::cout << "input_tensor = ";
-    printDevData(ctx, input_tensor);
-    std::cout << "other_tensor = ";
-    printDevData(ctx, other_tensor);
-    std::cout << "out_tensor = ";
-    printDevData(ctx, out_tensor);
 
     DiopiTensor out_tensor_temp = out_tensor;
     std::vector<DiopiTensor *> pTensors{&input_tensor, &other_tensor, &out_tensor_temp};
     std::set<diopiDtype_t> supportedDtypes{diopi_dtype_float16, diopi_dtype_float32, diopi_dtype_int32};
     DIOPI_CALL(autoCastTensorType(ctx, pTensors, supportedDtypes));
 
-    // std::set<diopiDtype_t> supportedFloatDtypes{diopi_dtype_float16, diopi_dtype_float32};
-    // std::set<diopiDtype_t> supportedIntDtypes{diopi_dtype_int32};
-
-    // if (DiopiDataType::isInteger(input_tensor.dtype())) {
-    //     DIOPI_CALL(autoCastTensorType(ctx, pTensors, supportedIntDtypes));
-    // } else {
-    //     DIOPI_CALL(autoCastTensorType(ctx, pTensors, supportedFloatDtypes));
-    // }
-
-    std::cout << "after dtypecast" << std::endl;
-    std::cout << "input.dtype = " << input_tensor.dtype() << std::endl;
-    std::cout << "other.dtype = " << other_tensor.dtype() << std::endl;
-    std::cout << "out.dtype = " << out_tensor_temp.dtype() << std::endl;
-
     CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
     CnnlTensorDesc other_desc(other_tensor, CNNL_LAYOUT_ARRAY);
     CnnlTensorDesc out_desc(out_tensor_temp, CNNL_LAYOUT_ARRAY);
-    std::cout << "input_tensor = ";
-    printDevData(ctx, input_tensor);
-    std::cout << "other_tensor = ";
-    printDevData(ctx, other_tensor);
 
     size_t workspace_size = 0;
     DIOPI_CALLCNNL(cnnlGetFloorModWorkspaceSize(handle, input_desc.get(), other_desc.get(), out_desc.get(), &workspace_size));
@@ -77,9 +48,6 @@ DIOPI_API diopiError_t diopiRemainderTensor(diopiContextHandle_t ctx, diopiTenso
     if (out_tensor.dtype() != out_tensor_temp.dtype()) {
         DIOPI_CALL(dataTypeCast(ctx, out_tensor, out_tensor_temp));
     }
-    std::cout << "out_tensor = ";
-    printDevData(ctx, out_tensor);
-
     return diopiSuccess;
 }
 
