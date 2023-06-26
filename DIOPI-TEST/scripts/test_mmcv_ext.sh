ef4938a57e25da808a4e876fbc0314bb1bf8558b
dongkaixing <408244909@qq.com>
2023-06-26 17:56:06 +0800

    Fix

--- DIOPI-TEST/scripts/test_mmcv_ext.sh
+++ DIOPI-TEST/scripts/test_mmcv_ext.sh
@@ -11,12 +11,12 @@ DEVICE=${1}
 echo $MMCV_TEST_HOME
 
 if [[ $DEVICE == "CUDA" ]]; then
-    MMCV_TEST_LIST=(tests/test_ops/test_nms.py -k test_nms_allclose \
-    tests/test_ops/test_roi_align.py \
-    tests/test_ops/test_focal_loss.py -k sigmoid \
-    tests/test_ops/test_voxelization.py \
-    tests/test_ops/test_modulated_deform_conv.py::TestMdconv::test_mdconv_float \
-    tests/test_ops/test_modulated_deform_conv.py::TestMdconv::test_mdconv_double
+    MMCV_TEST_LIST=("tests/test_ops/test_nms.py -k test_nms_allclose" \
+    "tests/test_ops/test_roi_align.py" \
+    "tests/test_ops/test_focal_loss.py -k sigmoid" \
+    "tests/test_ops/test_voxelization.py" \
+    "tests/test_ops/test_modulated_deform_conv.py::TestMdconv::test_mdconv_float" \
+    "tests/test_ops/test_modulated_deform_conv.py::TestMdconv::test_mdconv_double"
     )
 elif [[ $DEVICE == "MLU" ]]; then
     MMCV_TEST_LIST=()
@@ -27,7 +27,7 @@ fi
 
 cd $MMCV_TEST_HOME
 
-for elem in ${MMCV_TEST_LIST[@]}
+for elem in "${MMCV_TEST_LIST[@]}";
 do
     python -m pytest $elem
 done
