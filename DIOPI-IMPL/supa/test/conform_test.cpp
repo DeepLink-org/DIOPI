87eb8a93cee650c9dd37dcf885958f52c51fc6b9
Aaron Wei <dswei@birentech.com>
2023-06-26 02:47:11 +0000

    merge master branch of diopi

--- DIOPI-IMPL/supa/test/conform_test.cpp
+++ DIOPI-IMPL/supa/test/conform_test.cpp
@@ -4,57 +4,43 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
+#include <conform_test.h>
+#include <diopi/diopirt.h>
+#include <supa.h>
+
+#include <cassert>
 #include <cstdio>
 #include <mutex>
-#include <cassert>
 
-#include <supa.h>
-#include <diopi/diopirt.h>
-
-#include <conform_test.h>
-
-#define SUPA_CALL(Expr)                                                               \
-    {                                                                                 \
-        suError_t ret = Expr;                                                         \
-        if (ret != suSuccess) {                                                       \
-            printf("call a supa function (%s) failed. return code=%d", #Expr, ret);   \
-        }                                                                             \
+#define SUPA_CALL(Expr)                                                             \
+    {                                                                               \
+        suError_t ret = Expr;                                                       \
+        if (ret != suSuccess) {                                                     \
+            printf("call a supa function (%s) failed. return code=%d", #Expr, ret); \
+        }                                                                           \
     }
 
-// provide a plain memory alloc/free interface as default.
-static void* plain_device_malloc(uint64_t bytes) {
+extern "C" {
+DIOPI_API void* br_device_malloc(uint64_t bytes);
+DIOPI_API void br_device_free(void* ptr);
+}
+
+void* device_malloc(uint64_t bytes) {
+    if (br_device_malloc) {
+        return br_device_malloc(bytes);
+    }
     void* ptr = nullptr;
     SUPA_CALL(suMallocDevice(&ptr, bytes));
     return ptr;
 }
 
-static void plain_device_free(void* ptr) {
+void device_free(void* ptr) {
+    if (br_device_free) {
+        br_device_free(ptr);
+        return;
+    }
     SUPA_CALL(suFree(ptr));
 }
-
-typedef void* (*Allocator)(uint64_t);
-typedef void (*Deleter)(void*);
-
-static Allocator DeviceMalloc = plain_device_malloc;
-static Deleter DeviceFree = plain_device_free;
-
-// api for register mem api.
-extern "C" DIOPI_RT_API void diopiRegisterMemApi(Allocator allocator, Deleter deleter){
-    assert(allocator);
-    assert(deleter);
-    DeviceMalloc = allocator;
-    DeviceFree = deleter;
-}
-
-
-void* device_malloc(uint64_t bytes) {
-    return DeviceMalloc(bytes);
-}
-
-void device_free(void* ptr) {
-    DeviceFree(ptr);
-}
-
 
 int32_t device_make_stream(diopiStreamHandle_t* streamHandlePtr) {
     suStream_t stream = nullptr;
