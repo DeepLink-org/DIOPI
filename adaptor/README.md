b315bc29179ad55a5605acfdc4ddac7499ba23f6
xintian-514 <121075453+xintian-514@users.noreply.github.com>
2023-07-06 12:00:52 +0800

    modify name of folders (#216)
    * modify name of folders
    
    * modify name of folders
    
    * modify name of folders
    
    * modify name of folders
    
    * modify name of folders
    
    * modify name of folders
    
    * modify name of folders
    
    * modify name of folders
    
    * modify name of folders
    
    * modify name of folders
    
    * Update README.md
    
    * Update README.md
    
    * fix cpp-linter
    
    * fix cpp-linter
    
    ---------
    
    Co-authored-by: Joyce YU <30998166+MiaoYYu@users.noreply.github.com>

---
+++ adaptor/README.md
@@ -0,0 +1,7 @@
+<div align=center>
+<img src="../img/deepLink_logo.png">
+</div>
+
+# ADAPTOR
+
+ ADAPTOR 是 DIOPI 提供的辅助工具箱，目前提供的功能包括自动类型转换、内存分布转换等，使用时在 impl 设备文件夹下添加配置问题，具体配置方法见[impl Readme](https://github.com/DeepLink-org/DIOPI/tree/main/impl#readme)。
---
+++ impl/camb/functions/where.cpp
@@ -0,0 +1,59 @@
+#include <diopi/functions.h>
+
+#include "../cnnl_helper.hpp"
+#include "../common/common.hpp"
+
+namespace impl {
+namespace camb {
+extern "C" {
+diopiError_t diopiWhere(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t condition, diopiConstTensorHandle_t input,
+                        diopiConstTensorHandle_t other) {
+    cnnlHandle_t handle = cnnlHandlePool.get(ctx);
+    DiopiTensor inputTensor(input);
+    DiopiTensor otherTensor(other);
+    DiopiTensor condTensor(condition);
+    DiopiTensor outTensor(out);
+
+    std::vector<DiopiTensor*> inputs{&inputTensor, &otherTensor, &condTensor};
+    std::set<diopiDtype_t> inputsSupportDtype{
+        diopi_dtype_int8, diopi_dtype_int16, diopi_dtype_int32, diopi_dtype_int64, diopi_dtype_float16, diopi_dtype_float32};
+    DIOPI_CALL(autoCastTensorType(ctx, inputs, inputsSupportDtype));
+    std::vector<DiopiTensor*> cond{&condTensor};
+    std::set<diopiDtype_t> condSupportDtype{diopi_dtype_uint8, diopi_dtype_bool};
+    DIOPI_CALL(autoCastTensorType(ctx, cond, condSupportDtype));
+
+    DiopiTensor outTensorTemp = outTensor;
+    if (outTensorTemp.dtype() != inputTensor.dtype()) {
+        DIOPI_CALL(dataTypeCast(ctx, outTensorTemp, inputTensor.dtype()));
+    }
+
+    CnnlTensorDesc inputDesc(inputTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc otherDesc(otherTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc condDesc(condTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc outDesc(outTensorTemp, CNNL_LAYOUT_ARRAY);
+
+    size_t workspaceSize = 0;
+    DIOPI_CALLCNNL(cnnlGetSelectV2WorkspaceSize(handle, condDesc.get(), inputDesc.get(), otherDesc.get(), &workspaceSize));
+    void* workspace = nullptr;
+    workspace = requiresBuffer(ctx, workspaceSize).data();
+
+    DIOPI_CALLCNNL(cnnlSelectV2(handle,
+                                condDesc.get(),
+                                condTensor.data(),
+                                inputDesc.get(),
+                                inputTensor.data(),
+                                otherDesc.get(),
+                                otherTensor.data(),
+                                workspace,
+                                workspaceSize,
+                                outDesc.get(),
+                                outTensorTemp.data()));
+
+    if (outTensorTemp.dtype() != outTensor.dtype()) {
+        DIOPI_CALL(dataTypeCast(ctx, outTensor, outTensorTemp));
+    }
+    return diopiSuccess;
+}
+}  // extern "C"
+}  // namespace camb
+}  // namespace impl
