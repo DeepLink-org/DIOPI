ae3264337e519578bf520393f46b07a1121d7f76
yehongjuan <yehongjuan>
2023-07-06 14:12:20 +0800

    fix unfold

--- DIOPI-IMPL/camb/functions/unfold.cpp
+++ DIOPI-IMPL/camb/functions/unfold.cpp
@@ -5,7 +5,9 @@
  */
 
 #include <diopi/functions.h>
+
 #include <vector>
+
 #include "../cnnl_helper.hpp"
 #include "../common/common.hpp"
 
@@ -13,40 +15,34 @@ namespace impl {
 namespace camb {
 namespace {
 
-int getDim(int64_t dim, int64_t input_dim) {
-    int dim_ = static_cast<int>(dim);
-    if (dim_ < 0) {
-        dim_ = dim_ + input_dim;
+int getDim(int64_t dim, int64_t inputDim) {
+    int dimTmp = static_cast<int>(dim);
+    if (dimTmp < 0) {
+        dimTmp = dimTmp + inputDim;
     }
-    return dim_;
+    return dimTmp;
 }
 
-std::vector<uint32_t> getStride(int64_t dim, int64_t input_dim, int64_t step, const int64_t* input_sizes) {
-    auto dim_ = getDim(dim, input_dim);
-    std::vector<int64_t> stride;
+std::vector<uint32_t> getStride(int64_t dim, int64_t inputDim, int64_t step, const int64_t* inputSizes) {
+    auto dimTmp = getDim(dim, inputDim);
+    std::vector<int64_t> stride(inputDim);
     // fake stride for contiguous input
     int64_t z = 1;
     stride.insert(stride.begin(), z);
-    for (int64_t d = input_dim - 1; d > 0; d--) {
-        z *= input_sizes[d];
+    for (int64_t d = inputDim - 1; d > 0; d--) {
+        z *= inputSizes[d];
         stride.insert(stride.begin(), z);
     }
 
-    std::vector<int64_t> new_stride(input_dim + 1);
-    new_stride[input_dim] = input_dim == 0 ? 1 : stride[dim_];
-    for (int64_t d = 0; d < input_dim; d++) {
-        auto input_stride = stride[d];
-        if (d == dim_) {
-            new_stride[d] = step * input_stride;
-        } else {
-            new_stride[d] = input_stride;
-        }
+    std::vector<int64_t> newStride(inputDim + 1);
+    newStride[inputDim] = inputDim == 0 ? 1 : stride[dimTmp];
+    for (int64_t d = 0; d < inputDim; d++) {
+        auto inputStride = stride[d];
+        newStride[d] = (d == dimTmp) ? (step * inputStride) : inputStride;
     }
 
     std::vector<uint32_t> strides;
-    for (auto s : new_stride) {
-        strides.emplace_back(static_cast<uint32_t>(s));
-    }
+    std::transform(newStride.begin(), newStride.end(), std::back_inserter(strides), [](int64_t s) { return static_cast<uint32_t>(s); });
     return strides;
 }
 
@@ -57,58 +53,45 @@ extern "C" {
 DIOPI_API diopiError_t diopiUnfold(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t dim, int64_t size, int64_t step) {
     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
 
-    auto input_tensor = DiopiTensor(input);
-    auto out_tensor = DiopiTensor(out);
+    auto inputTensor = DiopiTensor(input);
+    auto outTensor = DiopiTensor(out);
 
-    CnnlTensorDesc input_desc(input_tensor, CNNL_LAYOUT_ARRAY);
-    CnnlTensorDesc out_desc(out_tensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc inputDesc(inputTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc outDesc(outTensor, CNNL_LAYOUT_ARRAY);
 
-    DIOPI_CHECKCNNL(cnnlUnfold(handle,
-                               static_cast<int>(dim),
-                               static_cast<int>(size),
-                               static_cast<int>(step),
-                               input_desc.get(),
-                               input_tensor.data(),
-                               out_desc.get(),
-                               out_tensor.data()));
+    DIOPI_CHECKCNNL(cnnlUnfold(
+        handle, static_cast<int>(dim), static_cast<int>(size), static_cast<int>(step), inputDesc.get(), inputTensor.data(), outDesc.get(), outTensor.data()));
     return diopiSuccess;
 }
 
-DIOPI_API diopiError_t diopiUnfoldBackward(diopiContextHandle_t ctx, diopiTensorHandle_t grad_input, diopiConstTensorHandle_t grad_output,
-                                           diopiSize_t input_sizes, int64_t dim, int64_t size, int64_t step) {
+DIOPI_API diopiError_t diopiUnfoldBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput, diopiSize_t inputSizes,
+                                           int64_t dim, int64_t size, int64_t step) {
     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
 
-    DiopiTensor grad_output_tensor(grad_output);
-    DiopiTensor grad_input_tensor(grad_input);
+    DiopiTensor gradOutputTensor(gradOutput);
+    DiopiTensor gradInputTensor(gradInput);
 
-    std::vector<DiopiTensor*> pTensors{&grad_output_tensor};
+    std::vector<DiopiTensor*> pTensors{&gradOutputTensor};
     DIOPI_CALL(autoCastTensorType(ctx, pTensors, {diopi_dtype_float32}));
-    DiopiTensor grad_output_tensor_tmp = *pTensors[0];
-    DiopiTensor grad_input_tensor_tmp = grad_input_tensor;
-    DIOPI_CALL(dataTypeCast(ctx, grad_input_tensor_tmp, grad_output_tensor_tmp.dtype()));
+    DiopiTensor gradOutputTensorTmp = *pTensors[0];
+    DiopiTensor gradInputTensorTmp = gradInputTensor;
+    DIOPI_CALL(dataTypeCast(ctx, gradInputTensorTmp, gradOutputTensorTmp.dtype()));
 
-    CnnlTensorDesc grad_output_desc(grad_output_tensor_tmp, CNNL_LAYOUT_ARRAY);
-    CnnlTensorDesc grad_input_desc(grad_input_tensor_tmp, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc gradOutputDesc(gradOutputTensorTmp, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc gradInputDesc(gradInputTensorTmp, CNNL_LAYOUT_ARRAY);
 
-    uint32_t workspace_size = 0;
-    DIOPI_CHECKCNNL(cnnlGetAsStridedBackwardWorkspaceSize(handle, grad_input_desc.get(), &workspace_size));
+    uint32_t workspaceSize = 0;
+    DIOPI_CHECKCNNL(cnnlGetAsStridedBackwardWorkspaceSize(handle, gradInputDesc.get(), &workspaceSize));
     void* workspace = nullptr;
-    if (0 != workspace_size) {
-        workspace = requiresBuffer(ctx, workspace_size).data();
+    if (0 != workspaceSize) {
+        workspace = requiresBuffer(ctx, workspaceSize).data();
     }
 
-    auto strides = getStride(dim, input_sizes.len, step, input_sizes.data);
+    auto strides = getStride(dim, inputSizes.len, step, inputSizes.data);
 
-    DIOPI_CHECKCNNL(cnnlAsStridedBackward(handle,
-                                          grad_output_desc.get(),
-                                          grad_output_tensor_tmp.data(),
-                                          grad_input_desc.get(),
-                                          grad_input_tensor_tmp.data(),
-                                          strides.data(),
-                                          0,
-                                          workspace,
-                                          workspace_size));
-    DIOPI_CALL(dataTypeCast(ctx, grad_input_tensor, grad_input_tensor_tmp));
+    DIOPI_CHECKCNNL(cnnlAsStridedBackward(
+        handle, gradOutputDesc.get(), gradOutputTensorTmp.data(), gradInputDesc.get(), gradInputTensorTmp.data(), strides.data(), 0, workspace, workspaceSize));
+    DIOPI_CALL(dataTypeCast(ctx, gradInputTensor, gradInputTensorTmp));
 
     return diopiSuccess;
 }
