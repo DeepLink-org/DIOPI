fc7b0caa4234d970c1584603ad5f33a59e948a93
fujingguo <fujingguo@sensetime.com>
2023-07-11 17:59:06 +0800

    [fix comments]: 1. add dim=None scenario, and dim cannot be None for torch 1.9, will test in torch 2.0 2. keepdim parameter is not given explicitly, the API check up this case now. 3. no longer require additional tensor. 4. no longer need cnnlCopy.

--- impl/camb/functions/std.cpp
+++ impl/camb/functions/std.cpp
@@ -31,35 +31,56 @@ DIOPI_API diopiError_t diopiStd(diopiContextHandle_t ctx, diopiTensorHandle_t ou
     // shape of outTensor does not keep the dim.
     DiopiTensor outTensor(out);
     DiopiTensor inputTensor(input);
+    auto outDtype = outTensor.dtype();
 
-    auto outKeepDimShape = inputTensor.shape();
-    int axisNum = dim.getLen();
-    int *axis = new int[axisNum];
-    for (int i = 0; i < axisNum; i++) {
-        axis[i] = dim.data[i];
-        if (axis[i] == -1) {
-            axis[i] = inputTensor.dim() - 1;
-        }
-        outKeepDimShape[axis[i]] = 1;
+    bool keepDim = false;
+    if (outTensor.dim() == inputTensor.dim()) {
+        keepDim = true;
     }
 
-    DiopiTensor outTmpTensor = requiresTensor(ctx, outKeepDimShape, inputTensor.dtype());
+    int axisNum = 0;
+    int *axis = nullptr;
+    if (dim.getLen() == 0) {
+        axisNum = inputTensor.dim();
+        axis = new int[axisNum];
+        for (int i = 0; i < axisNum; i++) {
+            axis[i] = i;
+        }
+    } else {
+        axisNum = dim.getLen();
+        axis = new int[axisNum];
+        for (int i = 0; i < axisNum; i++) {
+            axis[i] = dim.data[i];
+        }
+    }
+
+    if (!keepDim) {
+        auto outKeepDimShape = inputTensor.shape();
+        for (int i = 0; i < axisNum; i++) {
+            outKeepDimShape[axis[i]] = 1;
+        }
+        auto outKeepDimStride = inputTensor.stride();
+        for (int i = inputTensor.dim() - 2; i >= 0; i--) {
+            outKeepDimStride[i] = outKeepDimStride[i+1] * outKeepDimShape[i+1];
+        }
+        outTensor.asStrided(outKeepDimShape, outKeepDimStride);
+    }
 
     // cast supported dtyeps for tensors.
-    // outCasted tensor will be a new object that share the data memory with out tensor.
-    std::vector<DiopiTensor *> tensorVecPtr{&outTmpTensor, &inputTensor};
+    std::vector<DiopiTensor *> tensorVecPtr{&outTensor, &inputTensor};
     std::set<diopiDtype_t> supportedDtype{diopi_dtype_float16, diopi_dtype_float32, diopi_dtype_float64};
     DIOPI_CALL(autoCastTensorType(ctx, tensorVecPtr, supportedDtype));
-    outTmpTensor = *tensorVecPtr[0];
+    outTensor = *tensorVecPtr[0];
     inputTensor = *tensorVecPtr[1];
 
     // tensor descriptor for cnnl.
-    CnnlTensorDesc outDesc(outTmpTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc outDesc(outTensor, CNNL_LAYOUT_ARRAY);
     CnnlTensorDesc inputDesc(inputTensor, CNNL_LAYOUT_ARRAY);
 
     CnnlResourceGuard<cnnlStdVarMeanDescriptor_t, cnnlCreateStdVarMeanDescriptor, cnnlDestroyStdVarMeanDescriptor> stdVarMeanObj;
     cnnlStdVarMeanDescriptor_t stdVarMeanDesc = stdVarMeanObj.get();
     DIOPI_CALLCNNL(cnnlSetStdVarMeanDescriptor(stdVarMeanDesc, CNNL_STD, axisNum, axis, unbiased));
+    delete [] axis;
 
     size_t workspaceSize = 0;
     void *workspace = nullptr;
@@ -75,15 +96,27 @@ DIOPI_API diopiError_t diopiStd(diopiContextHandle_t ctx, diopiTensorHandle_t ou
                                   workspace,
                                   workspaceSize,
                                   outDesc.get(),
-                                  outTmpTensor.data(),
+                                  outTensor.data(),
                                   nullptr,
                                   nullptr,
                                   nullptr,
                                   nullptr));
-
-    outTmpTensor.asStrided(outTensor.shape(), outTensor.stride());
-    CnnlTensorDesc outTmpDesc(outTmpTensor, CNNL_LAYOUT_ARRAY);
-    DIOPI_CALLCNNL(cnnlCopy(handle, outTmpDesc.get(), outTmpTensor.data(), outDesc.get(), outTensor.data()));
+    if (!keepDim) {
+        std::vector<int64_t> outShape;
+        for (int i = 0; i < outTensor.dim(); i++) {
+            if (outTensor.shape()[i] > 1) {
+                outShape.push_back(outTensor.shape()[i]);
+            }
+        }
+        std::vector<int64_t> outStride(outShape.size(), 1);
+        for(int i = outShape.size() - 2; i >= 0; i--) {
+            outStride[i] = outStride[i+1] * outShape[i+1];
+        }
+        outTensor.asStrided(outShape, outStride);
+    }
+    if (outTensor.dtype() != outDtype) {
+        dataTypeCast(ctx, outTensor, outDtype);
+    }
 
     return diopiSuccess;
 }
