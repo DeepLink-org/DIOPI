5aea57eef36ae1fa88826916f741adafb50e5821
yeyeye333 <1020402580@qq.com>
2023-07-11 10:55:06 +0800

    Yhj/add unfold (#214)
    * add pad
    
    * add unfold (based on cnnl1.18.1)
    
    * fix unfold
    
    * fix unfold and delete the related config
    
    * delete the related config
    
    * fix unfold
    
    ---------
    
    Co-authored-by: yehongjuan <yehongjuan>

--- impl/camb/device_configs.py
+++ impl/camb/device_configs.py
@@ -379,31 +379,6 @@ device_configs = {
         ),
     ),
 
-    'unfold': dict(
-        name=["unfold"],
-        tensor_para=dict(
-            args=[
-                {
-                    "ins": ['input'],
-                    "dtype": [Skip(Dtype.float64), Skip(Dtype.float32), Skip(Dtype.float16)],
-                },
-            ],
-        ),
-    ),
-
-    'unfold_int': dict(
-        name=["unfold"],
-        tensor_para=dict(
-            args=[
-                {
-                    "ins": ['input'],
-                    "dtype": [Skip(Dtype.int64), Skip(Dtype.int32), Skip(Dtype.int16),
-                              Skip(Dtype.int8), Skip(Dtype.uint8), Skip(Dtype.bool)],
-                },
-            ],
-        ),
-    ),
-
     'cdist': dict(
         name=['cdist'],
         para=dict(
