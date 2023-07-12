806a47a94677a4e653eaef8fecd38c08076c989b
zhangzefeng <zhang_zefeng@foxmail.com>
2023-07-12 16:50:44 +0800

    add op col2im

--- impl/camb/functions/col2im.cpp
+++ impl/camb/functions/col2im.cpp
@@ -54,54 +54,54 @@ static diopiError_t transposeInternal(diopiContextHandle_t ctx, DiopiTensor outT
     return diopiSuccess;
 }
 
-diopiError_t diopiCol2Im(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t output_size, diopiSize_t kernel_size,
+diopiError_t diopiCol2Im(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t outputSize, diopiSize_t kernelSize,
                          diopiSize_t dilation, diopiSize_t padding, diopiSize_t stride) {
     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
 
-    DiopiTensor input_tensor(input);
-    CnnlTensorDesc inputDesc(input_tensor, CNNL_LAYOUT_ARRAY);
-    DiopiTensor input_col = requiresTensor(ctx, {input_tensor.shape()[0], input_tensor.shape()[2], input_tensor.shape()[1]}, input_tensor.dtype());
-    DIOPI_CALL(transposeInternal(ctx, input_col, input_tensor, 1, 2));
-    CnnlTensorDesc input_colDesc(input_col, CNNL_LAYOUT_ARRAY);
+    DiopiTensor inputTensor(input);
+    CnnlTensorDesc inputDesc(inputTensor, CNNL_LAYOUT_ARRAY);
+    DiopiTensor inputCol = requiresTensor(ctx, {inputTensor.shape()[0], inputTensor.shape()[2], inputTensor.shape()[1]}, inputTensor.dtype());
+    DIOPI_CALL(transposeInternal(ctx, inputCol, inputTensor, 1, 2));
+    CnnlTensorDesc inputColDesc(inputCol, CNNL_LAYOUT_ARRAY);
 
     cnnlDataType_t dtype;
-    DIOPI_CALL(CnnlDataType::convertToCnnlType(&dtype, input_tensor.dtype()));
+    DIOPI_CALL(CnnlDataType::convertToCnnlType(&dtype, inputTensor.dtype()));
 
     DiopiTensor out_tensor(out);
     CnnlTensorDesc outDesc(out_tensor, CNNL_LAYOUT_NCHW);
 
-    int32_t pad_height = padding.data[0];
-    int32_t pad_width = padding.len == 2 ? padding.data[1] : padding.data[0];
-    std::vector<int32_t> v_padding = {pad_height, pad_height, pad_width, pad_width};
-    int32_t stride_height = stride.data[0];
-    int32_t stride_width = stride.len == 2 ? stride.data[1] : stride.data[0];
-    std::vector<int32_t> v_stride = {stride_height, stride_width};
-    int32_t dilation_height = dilation.data[0];
-    int32_t dilation_width = dilation.len == 2 ? dilation.data[1] : dilation.data[0];
-    std::vector<int32_t> v_dilation = {dilation_height, dilation_width};
-    int32_t kernel_size_height = kernel_size.data[0];
-    int32_t kernel_size_width = kernel_size.len == 2 ? kernel_size.data[1] : kernel_size.data[0];
-    int32_t output_size_height = output_size.data[0];
-    int32_t output_size_width = output_size.len == 2 ? output_size.data[1] : output_size.data[0];
+    int32_t padHeight = padding.data[0];
+    int32_t padWidth = padding.len == 2 ? padding.data[1] : padding.data[0];
+    std::vector<int32_t> vPadding = {padHeight, padHeight, padWidth, padWidth};
+    int32_t strideHeight = stride.data[0];
+    int32_t strideWidth = stride.len == 2 ? stride.data[1] : stride.data[0];
+    std::vector<int32_t> vStride = {strideHeight, strideWidth};
+    int32_t dilationHeight = dilation.data[0];
+    int32_t dilationWidth = dilation.len == 2 ? dilation.data[1] : dilation.data[0];
+    std::vector<int32_t> vDilation = {dilationHeight, dilationWidth};
+    int32_t kernelSizeHeight = kernelSize.data[0];
+    int32_t kernelSizeWidth = kernelSize.len == 2 ? kernelSize.data[1] : kernelSize.data[0];
+    int32_t outputSizeHeight = outputSize.data[0];
+    int32_t outputSizeWidth = outputSize.len == 2 ? outputSize.data[1] : outputSize.data[0];
 
     CnnlTensorDesc weightDesc;
     cnnlTensorDescriptor_t w_desc = weightDesc.get();
-    std::vector<int> weight_sizes = {1, 1, kernel_size_height, kernel_size_width};
-    std::vector<int> weight_strides = {1, 1, 1, 1};
-    DIOPI_CALLCNNL(cnnlSetTensorDescriptorEx(w_desc, CNNL_LAYOUT_NCHW, dtype, weight_sizes.size(), weight_sizes.data(), weight_strides.data()));
+    std::vector<int> weightSizes = {1, 1, kernelSizeHeight, kernelSizeWidth};
+    std::vector<int> weightStrides = {1, 1, 1, 1};
+    DIOPI_CALLCNNL(cnnlSetTensorDescriptorEx(w_desc, CNNL_LAYOUT_NCHW, dtype, weightSizes.size(), weightSizes.data(), weightStrides.data()));
 
-    CnnlResourceGuard<cnnlConvolutionDescriptor_t, cnnlCreateConvolutionDescriptor, cnnlDestroyConvolutionDescriptor> ConvDesc;
-    cnnlConvolutionDescriptor_t conv_desc = ConvDesc.get();
-    DIOPI_CALLCNNL(cnnlSetConvolutionDescriptor(conv_desc, 4, v_padding.data(), v_stride.data(), v_dilation.data(), 1, dtype));
+    CnnlResourceGuard<cnnlConvolutionDescriptor_t, cnnlCreateConvolutionDescriptor, cnnlDestroyConvolutionDescriptor> cnnlConvDesc;
+    cnnlConvolutionDescriptor_t convDesc = cnnlConvDesc.get();
+    DIOPI_CALLCNNL(cnnlSetConvolutionDescriptor(convDesc, 4, vPadding.data(), vStride.data(), vDilation.data(), 1, dtype));
 
     size_t workspace_size = 0;
-    DIOPI_CALLCNNL(cnnlGetCol2ImWorkspaceSize(handle, input_colDesc.get(), outDesc.get(), w_desc, &workspace_size));
+    DIOPI_CALLCNNL(cnnlGetCol2ImWorkspaceSize(handle, inputColDesc.get(), outDesc.get(), w_desc, &workspace_size));
     void* workspace = nullptr;
     if (0 != workspace_size) {
         workspace = requiresBuffer(ctx, workspace_size).data();
     }
 
-    DIOPI_CALLCNNL(cnnlCol2Im(handle, input_colDesc.get(), input_col.data(), w_desc, conv_desc, workspace, workspace_size, outDesc.get(), out_tensor.data()));
+    DIOPI_CALLCNNL(cnnlCol2Im(handle, inputColDesc.get(), inputCol.data(), w_desc, convDesc, workspace, workspace_size, outDesc.get(), out_tensor.data()));
 
     return diopiSuccess;
 }
