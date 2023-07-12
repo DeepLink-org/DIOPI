3ebe1427d991bd017fd44e2f127d2716da25ec69
gqw <994321849@qq.com>
2023-07-12 13:39:02 +0800

    bug fix

--- diopi_test/python/conformance/diopi_runtime.py
+++ diopi_test/python/conformance/diopi_runtime.py
@@ -266,7 +266,7 @@ class Tensor(diopiTensor):
     def numpy(self) -> np.ndarray:
         data = np.empty((1,), to_numpy_dtype(self.get_dtype()))
         element_size = data.itemsize
-        sumsize = int(sum([(s - 1) * st for s, st in zip(list(self.size().data), [int(stride * 8) for stride in self.get_stride().data])]) / element_size + 1)
+        sumsize = int(sum([(s - 1) * st for s, st in zip(list(self.size().data), [int(stride * element_size) for stride in self.get_stride().data])]) / element_size + 1)
         darray = np.empty(sumsize, to_numpy_dtype(self.get_dtype()))
         PyCapsule_Destructor = ctypes.CFUNCTYPE(None, ctypes.py_object)
         PyCapsule_New = ctypes.pythonapi.PyCapsule_New
