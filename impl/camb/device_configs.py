3842609ad4d74b60a1fcddd25d4cb9433c2bdbec
POI-WX <wangxing2@sensetime.com>
2023-07-17 22:09:36 +0800

    skip float16 for remainder_self_scalar_float

--- impl/camb/device_configs.py
+++ impl/camb/device_configs.py
@@ -564,20 +564,20 @@ device_configs = {
         name = ['remainder'],
         atol = 1e-1,
         rtol = 1e-2,
+        tensor_para=dict(
+            args=[
+                {
+                    "ins": ['other'],
+                    "dtype": [Skip(Dtype.float16)],
+                },
+            ],
+        ),
     ),
 
     'remainder_tensor': dict(
         name = ['remainder'],
         atol = 1e-1,
         rtol = 1e-2,
-        tensor_para=dict(
-            args=[
-                {
-                    "ins": ['input'],
-                    "dtype": [Skip(Dtype.float16), Skip(Dtype.float32)],
-                },
-            ],
-        ),
     ),
 
     'remainder_other_scalar': dict(
