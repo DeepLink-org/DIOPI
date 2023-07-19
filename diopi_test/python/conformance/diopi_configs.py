00ce564479fc40fbc058bebe7ffe85d93a133991
slc <1060314685@qq.com>
2023-07-19 19:34:35 +0800

    relax batch_norm_no_contiguous atol and rtol

--- diopi_test/python/conformance/diopi_configs.py
+++ diopi_test/python/conformance/diopi_configs.py
@@ -60,8 +60,8 @@ diopi_configs = {
     'batch_norm_no_contiguous': dict(
         name=["batch_norm"],
         dtype=[Dtype.float32, Dtype.float16, Dtype.float64],
-        atol=1e-5,
-        rtol=1e-6,
+        atol=1e-3,
+        rtol=1e-4,
         atol_half=1e-1,
         rtol_half=1e-2,
         para=dict(
