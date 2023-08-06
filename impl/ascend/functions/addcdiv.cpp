cb15c187816c631c3ec20960d6c430ca13cc436b
hellozmz <407190054@qq.com>
2023-08-06 06:58:04 +0000

    add ascend op for VIT&&YOLOv3

---
+++ impl/ascend/functions/addcdiv.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2023, DeepLink.
+ */
+
+#include <diopi/functions.h>
+
+#include "../common/acloprunner.hpp"
+
+namespace impl {
+namespace ascend {
+
+extern "C" {
+
+DIOPI_API diopiError_t diopiAddcmul(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t tensor1,
+                                    diopiConstTensorHandle_t tensor2, const diopiScalar_t* value) {
+    AclOpRunner<4, 1>("Addcdiv", ctx).addInput(input, tensor1, tensor2).addConstInput(value).addOutput(out).run();
+    return diopiSuccess;
+}
+
+}  // extern "C"
+
+}  // namespace ascend
+}  // namespace impl
