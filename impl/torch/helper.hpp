e6ec071379a1f1c62246db9c1f7f5488d298bd54
slc <1060314685@qq.com>
2023-08-08 18:56:10 +0800

    update

--- impl/torch/helper.hpp
+++ impl/torch/helper.hpp
@@ -94,7 +94,7 @@ inline void setCurCtx(diopiContextHandle_t ctx) {
     context = ctx; 
     diopiStreamHandle_t stream_handle;
     diopiGetStream(ctx, &stream_handle);
-    c10::cuda::CUDAStream cur_stream = c10::cuda::getStreamFromExternal(static_cast<cudastream_t>(stream_handle), c10::cuda::current_device());
+    c10::cuda::CUDAStream cur_stream = c10::cuda::getStreamFromExternal(static_cast<cudaStream_t>(stream_handle), c10::cuda::current_device());
     c10::cuda::setCurrentCUDAStream(cur_stream);
     //Here, we set the current stream of cuda to the stream of diopi, but when the context is unloaded, it is not restored. The main reason is that the current stream of cuda is not used. However, there may be hidden bugs, which will be optimized later.
     }
