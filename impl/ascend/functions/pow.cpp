79e60476b4ca355b6f3ea1553a431a70f54d8ebc
ding0706 <chrysantd@gmail.com>
2023-08-08 16:54:02 +0800

    Td/add ops 3 (#316)
    * add and fix ops
    
    * add end line

---
+++ impl/ascend/functions/pow.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2023, DeepLink.
+ */
+
+#include <diopi/functions.h>
+
+#include "../common/acloprunner.hpp"
+
+namespace impl {
+namespace ascend {
+
+extern "C" {
+
+diopiError_t diopiPowTensor(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t exponent) {
+    AclOpRunner<2, 1>("Pow", ctx).addInput(input, exponent).addOutput(out).run();
+    return diopiSuccess;
+}
+
+diopiError_t diopiPowInpTensor(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t exponent) {
+    return diopiPowTensor(ctx, input, input, exponent);
+}
+
+diopiError_t diopiPow(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* exponent) {
+    diopiTensorHandle_t exponentTensor;
+    diopiDtype_t dtype;
+    diopiGetTensorDtype(input, &dtype);
+    makeTensorFromScalar(ctx, exponent, &exponentTensor, dtype, diopi_device);
+    return diopiPowTensor(ctx, out, input, exponentTensor);
+}
+
+diopiError_t diopiPowInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, const diopiScalar_t* exponent) { return diopiPow(ctx, input, input, exponent); }
+
+diopiError_t diopiPowScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, const diopiScalar_t* input, diopiConstTensorHandle_t exponent) {
+    diopiTensorHandle_t inputTensor;
+    makeTensorFromScalar(ctx, input, &inputTensor, diopi_device);
+    return diopiPowTensor(ctx, out, inputTensor, exponent);
+}
+
+}  // extern "C"
+
+}  // namespace ascend
+}  // namespace impl
