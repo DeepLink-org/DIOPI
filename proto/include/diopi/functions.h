46d27208fd4be84a86eaab4df2d73212563582b6
POI-WX <131418410+POI-WX@users.noreply.github.com>
2023-08-08 20:51:06 +0800

    Update functions.h

--- proto/include/diopi/functions.h
+++ proto/include/diopi/functions.h
@@ -734,7 +734,7 @@ DIOPI_API diopiError_t diopiAdd(diopiContextHandle_t ctx, diopiTensorHandle_t ou
 DIOPI_API diopiError_t diopiAddInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t other, const diopiScalar_t* alpha);
 /**
  * @brief Add a scalar to a tensor.
- * @param[in] other  value to be added. type = [float64, float32, float16, int64, int32, int16, int8, uint8].
+ * @param[in] other the scalar value to be added. type = [float64, float32, float16, int64, int32, int16, int8, uint8].
  * @sa Other parameters refer to diopiAdd().
  */
 DIOPI_API diopiError_t diopiAddScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* other,
@@ -795,7 +795,7 @@ DIOPI_API diopiError_t diopiMul(diopiContextHandle_t ctx, diopiTensorHandle_t ou
 DIOPI_API diopiError_t diopiMulInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t other);
 /**
  * @brief Multiply tensor input with other (element-wise multiplication)
- * @param[in] other  value to be added. type = [float64, float32, float16, int64, int32, int16, int8, uint8].
+ * @param[in] other the scalar value to be added. type = [float64, float32, float16, int64, int32, int16, int8, uint8].
  * @sa Other parameters refer to diopiMul().
  */
 DIOPI_API diopiError_t diopiMulScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* other);
