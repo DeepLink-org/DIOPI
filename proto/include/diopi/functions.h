d9eda448e02939901671077609e4987aec1f27b2
yangbofun <yangbofun@163.com>
2023-08-21 20:27:55 +0800

    delete some weak attributes

--- proto/include/diopi/functions.h
+++ proto/include/diopi/functions.h
@@ -30,9 +30,9 @@ typedef enum { Contiguous = 0, ChannelsLast = 1, ChannelsLast3d = 2, Preserve = 
 /**
  * \brief get the vendor's name who implements the functions
  */
-DIOPI_RT_API const char* diopiGetVendorName();
-DIOPI_RT_API const char* diopiGetImplVersion();
-DIOPI_RT_API const char* diopiGetLastErrorString();
+DIOPI_RT_API DIOPI_ATTR_WEEK const char* diopiGetVendorName();
+DIOPI_RT_API DIOPI_ATTR_WEEK const char* diopiGetImplVersion();
+DIOPI_RT_API DIOPI_ATTR_WEEK const char* diopiGetLastErrorString();
 
 /**
  * @brief Applies a 2D convolution over an input image composed of several input planes.
