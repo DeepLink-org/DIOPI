b7bc1099609bfcc5f78eb07abc99b1f29244d5bb
jingguo-st <fujingguo@sensetime.com>
2023-08-30 10:14:14 +0800

    add ctc_loss impl for cambricon 	modified:   impl/camb/device_configs.py 	new file:   impl/camb/functions/ctc_loss.cpp

--- impl/camb/device_configs.py
+++ impl/camb/device_configs.py
@@ -1098,13 +1098,15 @@ device_configs = {
 
     'ctc_loss': dict(
         name=["ctc_loss"],
-        tensor_para=dict(
-            args=[
-                {
-                    "ins": ['log_probs'],
-                    "dtype": [Skip(Dtype.float64), Skip(Dtype.float32)],
-                },
-            ],
+        para=dict(
+            blank=[Skip(9)]
+        ),
+    ),
+
+    'ctc_loss_un_padded': dict(
+        name=["ctc_loss"],
+        para=dict(
+            blank=[Skip(9)]
         ),
     ),
 
