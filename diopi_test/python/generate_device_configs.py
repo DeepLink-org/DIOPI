64bdf62e9569c6837b5925e03d2ed9cee8ea8e33
zhangzefeng <zhang_zefeng@foxmail.com>
2023-09-05 17:59:18 +0800

    add diopi test config file for ascend

--- diopi_test/python/generate_device_configs.py
+++ diopi_test/python/generate_device_configs.py
@@ -14,34 +14,36 @@ usage example:
 if __name__ == "__main__":
     output_file = sys.argv[1]
 
+    tab = " " * 4
+
     output_str = ""
     output_str += "from .device_config_helper import Skip\n"
     output_str += "from .diopi_runtime import Dtype\n"
     output_str += "\ndevice_configs = {\n"
 
     for config_name in diopi_configs:
-        output_str += "\t'{}': dict(\n".format(config_name)
+        output_str += tab * 1 + "'{}': dict(\n".format(config_name)
         op_names = diopi_configs[config_name]["name"]
-        output_str += "\t\tname={},\n".format(str(op_names))
+        output_str += tab * 2 + "name={},\n".format(str(op_names))
 
         if "tensor_para" in diopi_configs[config_name]:
-            output_str += "\t\ttensor_para=dict(\n"
-            output_str += "\t\t\targs=[\n\t\t\t\t{\n"
+            output_str += tab * 2 + "tensor_para=dict(\n"
+            output_str += tab * 3 + "args=[\n" + tab * 4 +"{\n"
 
             if "ins" in diopi_configs[config_name]["tensor_para"]["args"][0]:
                 ins = diopi_configs[config_name]["tensor_para"]["args"][0]["ins"]
-                output_str += "\t\t\t\t\t\"ins\": {},\n".format(ins)
+                output_str += tab * 5 + "\"ins\": {},\n".format(ins)
 
                 if "dtype" in diopi_configs[config_name]["tensor_para"]["args"][0]:
                     dtype = diopi_configs[config_name]["tensor_para"]["args"][0]["dtype"]
-                    output_str += "\t\t\t\t\t\"dtype\": ["
+                    output_str += tab * 5 + "\"dtype\": ["
                     for index, value in enumerate(dtype):
                         output_str += "Skip({}),".format(value)
                     output_str += "],\n"
 
                 elif "shape" in diopi_configs[config_name]["tensor_para"]["args"][0]:
                     shape = diopi_configs[config_name]["tensor_para"]["args"][0]["shape"]
-                    output_str += "\t\t\t\t\t\"shape\": ["
+                    output_str += tab * 5 + "\"shape\": ["
                     for index, value in enumerate(shape):
                         output_str += "Skip({}),".format(value)
                     output_str += "],\n"
@@ -51,32 +53,32 @@ if __name__ == "__main__":
                         dtype = diopi_configs[config_name]["dtype"]
                     elif "dtype" in diopi_configs[config_name]["tensor_para"]:
                         dtype = diopi_configs[config_name]["tensor_para"]["dtype"]
-                    output_str += "\t\t\t\t\t\"dtype\": ["
+                    output_str += tab * 5 + "\"dtype\": ["
                     for index, value in enumerate(dtype):
                         output_str += "Skip({}),".format(value)
                     output_str += "],\n"
 
             else:
-                output_str += "\t\t\t\t\t\"ins\": ['{}'],\n".format("input")
+                output_str += tab * 5 + "\"ins\": ['{}'],\n".format("input")
                 dtype = diopi_configs[config_name]["tensor_para"]["args"][0]["dtype"]
-                output_str += "\t\t\t\t\t\"dtype\": ["
+                output_str += tab * 5 + "\"dtype\": ["
                 for index, value in enumerate(dtype):
                     output_str += "Skip({}),".format(value)
                 output_str += "],\n"
 
-            output_str += "\t\t\t\t},\n\t\t\t]\n"
-            output_str += "\t\t),\n"
+            output_str += tab * 4 + "},\n" + tab * 3 + "]\n"
+            output_str += tab * 2 + "),\n"
         else:
             para = diopi_configs[config_name]["para"]
-            output_str += "\t\tpara=dict(\n"
+            output_str += tab * 2 + "para=dict(\n"
             for item_name in para:
-                output_str += "\t\t\t{}=[".format(item_name)
+                output_str += tab * 3 + "{}=[".format(item_name)
                 for value in para[item_name]:
                     output_str += "Skip({}),".format(value)
                 output_str += "],\n"
 
-            output_str += "\t\t),\n"
-        output_str += "\t),\n\n"
+            output_str += tab * 2 + "),\n"
+        output_str += tab + "),\n\n"
     output_str += "}\n"
 
     f = open(output_file, "w")
