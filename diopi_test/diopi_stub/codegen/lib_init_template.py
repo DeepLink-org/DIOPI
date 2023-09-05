d16492433a5187e1e53591e614af1ff90e23e193
jingguo-st <fujingguo@sensetime.com>
2023-09-05 15:38:13 +0800

    fix pylint

--- diopi_test/diopi_stub/codegen/lib_init_template.py
+++ diopi_test/diopi_stub/codegen/lib_init_template.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2023, DeepLink.
-diopilib_init_tmp = \
-r'''from .export_runtime import *
+diopilib_init_tmp = r'''from .export_runtime import *
 from .export_functions import *
 '''
