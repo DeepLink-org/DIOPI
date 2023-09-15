6bc43fb566c6d1f7607ecd85469bdab5a521afd7
caikun-pjlab <caikun@pjlab.org.cn>
2023-09-15 14:28:08 +0800

    synbatch 15-1426

--- impl/camb/functions/SynBatchNorm.cpp
+++ impl/camb/functions/SynBatchNorm.cpp
@@ -1,5 +1,5 @@
 /**
- *file
+ * @file
  * @author DeepLink
  * @copyright  (c) 2023, DeepLink.
  */
@@ -114,80 +114,6 @@ diopiError_t diopiBatchNormBackwardReduce(diopiContextHandle_t ctx, diopiTensorH
 
     return diopiSuccess;
 }
-
-DIOPI_API diopiError_t diopiBatchNormElemt(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t weight,
-                                           diopiConstTensorHandle_t bias, diopiConstTensorHandle_t mean, diopiConstTensorHandle_t invstd, float eps) {
-    cnnlHandle_t handle = cnnlHandlePool.get(ctx);
-    // input
-    DiopiTensor inputTr(input);
-    DiopiTensor weightTr(weight);
-    DiopiTensor biasTr(bias);
-    DiopiTensor meanTr(mean);
-    DiopiTensor invstdTr(invstd);
-    // output
-    DiopiTensor outTr(out);
-
-    auto dim = inputTr.dim();
-    DIOPI_CHECK(dim >= 2 && dim <= 5, "Input dim is out of range");
-    DIOPI_CHECK(dim == outTr.dim(), "Input dim != out dim");
-
-    // check the input dimension
-    if (3 == dim) {
-        inputTr.unsqueeze(3);
-        outTr.view(inputTr.shape());
-    }
-    if (2 == dim) {
-        inputTr.unsqueeze(2);
-        inputTr.unsqueeze(3);
-        outTr.view(inputTr.shape());
-    }
-
-    // check the input dtype
-    std::vector<DiopiTensor*> pTensors{&inputTr, &weightTr, &biasTr, &meanTr, &invstdTr};
-    std::set<diopiDtype_t> supportedDtypes{diopi_dtype_float32};
-    DIOPI_CALL(autoCastTensorType(ctx, pTensors, supportedDtypes));
-
-    /* Transpose to channels last */
-    diopiMemoryFormat_t memoryFormat = inputTr.dim() == 4 ? diopiMemoryFormat_t::ChannelsLast : diopiMemoryFormat_t::ChannelsLast3d;
-    DIOPI_CALL(contiguous(ctx, inputTr, memoryFormat));
-    DiopiTensor outTmpTr = requiresTensor(ctx, outTr.shape(), inputTr.dtype(), memoryFormat);
-
-    // get descriptor
-    cnnlTensorLayout_t layout = inputTr.dim() == 4 ? CNNL_LAYOUT_NHWC : CNNL_LAYOUT_NDHWC;
-    CnnlTensorDesc inputDesc(inputTr, layout);
-    CnnlTensorDesc outputDesc(outTmpTr, layout);
-    CnnlTensorDesc weightDesc(weightTr, CNNL_LAYOUT_ARRAY);
-    CnnlTensorDesc biasDesc(biasTr, CNNL_LAYOUT_ARRAY);
-    CnnlTensorDesc meanDesc(meanTr, CNNL_LAYOUT_ARRAY);
-    CnnlTensorDesc invstdDesc(invstdTr, CNNL_LAYOUT_ARRAY);
-
-    DIOPI_CALLCNNL(cnnlSyncBatchNormElemt(handle,
-                                          inputDesc.get(),
-                                          inputTr.data(),
-                                          meanDesc.get(),
-                                          meanTr.data(),
-                                          invstdDesc.get(),
-                                          invstdTr.data(),
-                                          weightDesc.get(),
-                                          weightTr.data(),
-                                          biasDesc.get(),
-                                          biasTr.data(),
-                                          outputDesc.get(),
-                                          outTmpTr.data()))
-
-    // channels last -> contiguous
-    DIOPI_CALL(contiguous(ctx, outTmpTr, diopiMemoryFormat_t::Contiguous));
-    // Copy back to origin
-    DIOPI_CALL(diopiCopyInp(ctx, outTmpTr.tensorHandle(), outTr.tensorHandle()));
-
-    return diopiSuccess;
-}
-
-// DIOPI_API diopiError_t diopiBatchNormStats(diopiContextHandle_t ctx, diopiTensorHandle_t mean, diopiTensorHandle_t invstd, diopiConstTensorHandle_t input,
-//                                            double eps);{
-//     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
-
-// }
 
 }  // namespace camb
 }  // namespace impl
