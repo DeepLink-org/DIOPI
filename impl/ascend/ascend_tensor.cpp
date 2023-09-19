1069713c66407bd977b4b8f36c117ddd2b893c65
hellozmz <407190054@qq.com>
2023-09-19 16:12:45 +0800

    refactor ascend: (#412)
    1. add ascend tensor
    2. compatible with the original interface definition
    3. dynamic input

---
+++ impl/ascend/ascend_tensor.cpp
@@ -0,0 +1,202 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2023, DeepLink.
+ */
+
+#include "ascend_tensor.hpp"
+
+#include <array>
+#include <utility>
+
+#include "common/debug.hpp"
+
+namespace impl {
+namespace ascend {
+
+bool AscendTensor::isContiguous(diopiMemoryFormat_t format) const {
+    if (!defined()) {
+        return true;
+    }
+    int64_t stride = 1;
+    int64_t dim = this->dim();
+    auto strides = this->stride();
+    auto shape = this->shape();
+
+    if (format == diopiMemoryFormat_t::Contiguous) {
+        for (int64_t i = dim - 1; i >= 0; i--) {
+            const auto& shapeD = shape[i];
+            if (shapeD != 1) {
+                if (strides[i] != stride) {
+                    return false;
+                }
+            }
+            stride *= shapeD;
+        }
+
+    } else if (format == diopiMemoryFormat_t::ChannelsLast) {
+        if (strides.size() != 4) return false;
+        for (auto& i : {1, 3, 2, 0}) {
+            const auto& shapeD = shape[i];
+            if (shapeD != 1) {
+                // shape_d != 1 help dealing with shape like [2, 2048, 1, 1]
+                if (strides[i] != stride) {
+                    return false;
+                }
+            }
+            stride *= shapeD;
+        }
+    } else if (format == diopiMemoryFormat_t::ChannelsLast3d) {
+        if (strides.size() != 5) return false;
+        for (auto& i : {1, 4, 3, 2, 0}) {
+            const auto& shapeD = shape[i];
+            if (shapeD != 1) {
+                if (strides[i] != stride) {
+                    return false;
+                }
+            }
+            stride *= shape[i];
+        }
+    } else if (format == diopiMemoryFormat_t::ChannelsLast1d) {
+        if (strides.size() != 3) {
+            return false;
+        }
+        for (auto& i : {1, 2, 0}) {
+            const auto& shapeD = shape[i];
+            if (shapeD != 1) {
+                if (strides[i] != stride) {
+                    return false;
+                }
+            }
+            stride *= shapeD;
+        }
+    }
+    return true;
+}
+
+AscendTensor& AscendTensor::asStrided(const std::vector<int64_t>& shape, const std::vector<int64_t>& stride) {
+    this->shape_ = shape;
+    this->stride_ = stride;
+    return *this;
+}
+
+AscendTensor& AscendTensor::unsqueeze(int dim) {
+    // Note: `channels_last` tensor uses this will become uncontiguous
+    // which is same with pytorch
+    auto shape = this->shape();
+    auto strides = this->stride();
+    int64_t newStride = dim >= this->dim() ? 1 : shape[dim] * strides[dim];
+    std::vector<int64_t> newShape(shape.begin(), shape.end());
+    std::vector<int64_t> newStrides(strides.begin(), strides.end());
+
+    newShape.insert(newShape.begin() + dim, 1);
+    newStrides.insert(newStrides.begin() + dim, newStride);
+    this->asStrided(newShape, newStrides);
+    return *this;
+}
+
+AscendTensor& AscendTensor::view(const std::vector<int64_t> shape) {
+    // must be contiguous
+    ASCEND_CHECK_ABORT(this->isContiguous(), "now only contiguous tensor support view by shape.");
+    std::vector<int64_t> stride(shape.size());
+    this->shape_ = shape;
+    stride[shape.size() - 1] = 1;
+    for (int j = shape_.size() - 2; j >= 0; j--) {
+        stride[j] = stride[j + 1] * shape[j + 1];
+    }
+    this->stride_ = stride;
+    return *this;
+}
+
+const void* AscendTensor::data() const {
+    const void* p = nullptr;
+    diopiGetTensorDataConst(tensor_, &p);
+    return p;
+}
+
+std::vector<int64_t> AscendTensor::getAclMemShape() const {
+    std::vector<int64_t> baseShapeVec;
+    if (this->isContiguous()) {
+        if (dim() > 0) {
+            baseShapeVec.resize(dim());
+            for (int64_t i = 0; i < dim(); i++) {
+                baseShapeVec[i] = shape(i);
+            }
+        } else {
+            baseShapeVec.push_back(1);
+        }
+
+    } else {
+        int64_t maxStride = 0, maxIdx = -1;
+        for (int64_t i = 0; i < dim(); i++) {
+            if (stride(i) > maxStride) {
+                maxStride = stride(i);
+                maxIdx = i;
+            }
+        }
+        if (maxStride > 0) {
+            baseShapeVec.push_back(shape(maxIdx) * maxStride);
+        } else {
+            baseShapeVec.push_back(1);
+        }
+    }
+    return baseShapeVec;
+}
+
+int64_t AscendTensor::getAclMemBufferSize() const {
+    if (this->isContiguous()) {
+        if (dim() > 0) {
+            return this->numel() * this->elemsize();
+        } else {
+            return this->elemsize();
+        }
+    } else {
+        int64_t maxStride = 0, maxIdx = -1;
+        for (int64_t i = 0; i < dim(); i++) {
+            if (stride(i) > maxStride) {
+                maxStride = stride(i);
+                maxIdx = i;
+            }
+        }
+        if (maxStride > 0) {
+            return shape(maxIdx) * maxStride * this->elemsize();
+        } else {
+            return this->elemsize();
+        }
+    }
+}
+
+aclFormat AscendTensor::getAclDataFormat() const {
+    if (dim() == 4) {
+        std::array<int64_t, 4> thStride{stride(0), stride(1), stride(2), stride(3)};
+        {
+            std::array<int64_t, 4> nchwStride;
+            int st = 1;
+            for (auto k : {3, 2, 1, 0}) {
+                nchwStride[k] = st;
+                if (shape(k) == 0) continue;
+                if (shape(k) == -1) st = -1;
+                if (st != -1) st *= shape(k);
+            }
+            if (thStride == nchwStride) {
+                return ACL_FORMAT_NCHW;
+            }
+        }
+        std::array<int64_t, 4> nhwcStride;
+        int st = 1;
+        for (auto k : {1, 3, 2, 0}) {
+            nhwcStride[k] = st;
+            if (shape(k) == 0) continue;
+            if (shape(k) == -1) st = -1;
+            if (st != -1) st *= shape(k);
+        }
+        if (thStride == nhwcStride) {
+            return ACL_FORMAT_NHWC;
+        }
+        warning("getAclDataFormat error. Acl only support NCHW or NHWC format! but get %s", dumpTensor(tensor_).c_str());
+    }
+    return ACL_FORMAT_ND;
+}
+
+}  // namespace ascend
+}  // namespace impl
---
+++ impl/ascend/common/debug.cpp
@@ -0,0 +1,70 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2023, DeepLink.
+ */
+
+#include "debug.hpp"
+
+namespace impl {
+namespace ascend {
+
+void printContiguousTensor(diopiContextHandle_t ctx, const AscendTensor& at, char* name) {
+    printf("==========[Tensor name %s]==========\n", name);
+    if (!at.isContiguous()) {
+        printf("input tensor is not contiguous. break;");
+        return;
+    }
+    void* ptrHost;
+
+    printf("Tensor device: %s\n", (at.device() ? "diopi_device" : "diopi_host"));
+
+    if (at.device() == diopiDevice_t::diopi_device) {
+        diopiStreamHandle_t stream;
+        diopiGetStream(ctx, &stream);
+        CALL_ACLRT(aclrtMallocHost(&ptrHost, at.numel() * at.elemsize()));
+        CALL_ACLRT(aclrtMemcpyAsync(
+            ptrHost, at.numel() * at.elemsize(), at.data(), at.numel() * at.elemsize(), ACL_MEMCPY_DEVICE_TO_HOST, reinterpret_cast<aclrtStream>(stream)));
+        CALL_ACLRT(aclrtSynchronizeStream(reinterpret_cast<aclrtStream>(stream)));
+    } else {
+        const void* ptrHostCopy;
+        diopiGetTensorDataConst(at.tensorHandle(), &ptrHostCopy);
+        ptrHost = const_cast<void*>(ptrHostCopy);
+    }
+
+    for (int i = 0; i < at.dim(); ++i) {
+        std::cout << "stride(" << i << ") = " << at.stride(i) << std::endl;
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < at.dim(); ++i) {
+        std::cout << "shape(" << i << ") = " << at.shape(i) << std::endl;
+    }
+    printf("Tensor type %d \n", at.dtype());
+    printf("Tensor %s:\n\n", name);
+    for (int64_t i = 0; i < at.numel(); i++) {
+        switch (at.dtype()) {
+            case diopi_dtype_float32:
+                printf("item %ld: %f\n", i, reinterpret_cast<float*>(ptrHost)[i]);
+                break;
+            case diopi_dtype_float64:
+                printf("item %ld: %f\n", i, reinterpret_cast<double*>(ptrHost)[i]);
+                break;
+            case diopi_dtype_int32:
+                printf("item %ld: %d\n", i, reinterpret_cast<int*>(ptrHost)[i]);
+                break;
+            case diopi_dtype_int64:
+                printf("item %ld: %ld\n", i, reinterpret_cast<int64_t*>(ptrHost)[i]);
+                break;
+            case diopi_dtype_bool:
+                printf("item %ld: %d\n", i, reinterpret_cast<bool*>(ptrHost)[i]);
+                break;
+            default:
+                printf("unsupport dtype");
+                break;
+        }
+    }
+    printf("\n");
+}
+
+}  // namespace ascend
+}  // namespace impl
