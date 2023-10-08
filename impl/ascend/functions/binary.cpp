e10a706a0ae27d20482dda5818078e78a7c8e9ef
xue-yun-liang <1255450792@qq.com>
2023-10-08 10:57:56 +0000

    fix maximum op

--- impl/ascend/functions/binary.cpp
+++ impl/ascend/functions/binary.cpp
@@ -206,9 +206,8 @@ extern "C" DIOPI_API diopiError_t diopiDivInpScalar(diopiContextHandle_t ctx, di
 DIOPI_API diopiError_t diopiMaximum(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t other) {
     diopiDtype_t dtype;
     diopiGetTensorDtype(input, &dtype);
-
     // as this op do not support BOOL, UIT8, and Int16, these three data types are converted to Int32
-    if(dtype == diopi_dtype_bool || dtype == diopi_dtype_uint8 || dtype == diopi_dtype_int16){
+    if (dtype == diopi_dtype_bool || dtype == diopi_dtype_uint8 || dtype == diopi_dtype_int16) {
         diopiTensorHandle_t inputCopy;
         makeTensorLike(ctx, &inputCopy, input, diopi_dtype_int32);
         diopiCastDtype(ctx, inputCopy, input);
@@ -218,7 +217,7 @@ DIOPI_API diopiError_t diopiMaximum(diopiContextHandle_t ctx, diopiTensorHandle_
         diopiCastDtype(ctx, otherCopy, other);
 
         AclOpRunner<2, 1>("Maximum", ctx).addInput(inputCopy).addInput(otherCopy).addOutput(out).run();
-    }else{
+    } else {
         AclOpRunner<2, 1>("Maximum", ctx).addInput(input, dtype).addInput(other, dtype).addOutput(out).run();
     }
 
