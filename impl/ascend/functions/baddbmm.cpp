7b646d17fab38762d537736fa5e23789cb62dbe2
xue-yun-liang <1255450792@qq.com>
2023-10-09 10:12:49 +0000

    fix

--- impl/ascend/functions/baddbmm.cpp
+++ impl/ascend/functions/baddbmm.cpp
@@ -7,6 +7,7 @@
 #include <diopi/functions.h>
 
 #include "../ascend_tensor.hpp"
+
 #include "../common/acloprunner.hpp"
 
 namespace impl {
@@ -17,9 +18,14 @@ DIOPI_API diopiError_t diopiBaddbmm(diopiContextHandle_t ctx, diopiTensorHandle_
                                     diopiConstTensorHandle_t batch2, double beta, double alpha) {
     diopiDtype_t outDtype;
     diopiGetTensorDtype(out, &outDtype);
+    diopiDtype_t execType;
 
     // adjust the input's and output's data type
-    diopiDtype_t execType = diopi_dtype_float32;
+    if (outDtype == diopi_dtype_float64) {
+        execType = diopi_dtype_float32;
+    } else {
+        execType = outDtype;
+    }
 
     diopiTensorHandle_t inCopy;
     makeTensorLike(ctx, &inCopy, input, execType);
@@ -39,8 +45,8 @@ DIOPI_API diopiError_t diopiBaddbmm(diopiContextHandle_t ctx, diopiTensorHandle_
     diopiCastDtype(ctx, batch2Copy, batch2);
 
     // get the size of batch1 * batch2
-    AscendTensor asBatch1 = AscendTensor(batch1);
-    AscendTensor asBatch2 = AscendTensor(batch2);
+    AscendTensor asBatch1 = AscendTensor(batch1Copy);
+    AscendTensor asBatch2 = AscendTensor(batch2Copy);
     std::vector<int64_t> batch1Shape = asBatch1.shape();
     std::vector<int64_t> batch2Shape = asBatch2.shape();
     std::vector<int64_t> vectorSizeBatchMatMulTensor = {batch1Shape[0], batch1Shape[1], batch2Shape[2]};
@@ -48,14 +54,14 @@ DIOPI_API diopiError_t diopiBaddbmm(diopiContextHandle_t ctx, diopiTensorHandle_
     // init a tensor according to the size of batch1 * batch2 ;
     diopiSize_t diopiSizeBatchMatMulTensor = vectorToDiopiSize(vectorSizeBatchMatMulTensor);
     AscendTensor asBatchMatMulTensor;
-    makeTensor(ctx, asBatchMatMulTensor, &diopiSizeBatchMatMulTensor, diopi_dtype_float32, diopiDevice_t::diopi_device);
+    makeTensor(ctx, asBatchMatMulTensor, &diopiSizeBatchMatMulTensor, execType, diopiDevice_t::diopi_device);
 
     // does batch1/batch2 need to transpose?
     bool isSelfT = false;
     bool isMat2T = false;
 
     // do batch1 times batch2 -> BatchMatMulTensor
-    AclOpRunner<5, 1>("BatchMatMul", ctx)
+    AclOpRunner<2, 1>("BatchMatMul", ctx)
         .addInput(batch1Copy)
         .addInput(batch2Copy)
         .addOutput(asBatchMatMulTensor)
@@ -66,11 +72,15 @@ DIOPI_API diopiError_t diopiBaddbmm(diopiContextHandle_t ctx, diopiTensorHandle_
     // init memory based on the size of alphaMulTensor and betaMulTensor
     AscendTensor alphaMulTensor;
     AscendTensor betaMulTensor;
-    makeTensorLike(ctx, alphaMulTensor, asBatchMatMulTensor, diopi_dtype_float32);
-    makeTensorLike(ctx, betaMulTensor, asInCopy, diopi_dtype_float32);
+    makeTensorLike(ctx, alphaMulTensor, asBatchMatMulTensor, execType);
+    makeTensorLike(ctx, betaMulTensor, asInCopy, execType);
 
-    diopiScalar_t alphaScalar = {diopi_dtype_float32, alpha};
-    diopiScalar_t betaScalar = {diopi_dtype_float32, beta};
+    diopiScalar_t alphaScalar;
+    alphaScalar.stype = execType;
+    alphaScalar.fval = alpha;
+    diopiScalar_t betaScalar;
+    betaScalar.stype = execType;
+    betaScalar.fval = beta;
 
     // transform ascendTensor to diopiTensorHandle_t
     diopiTensorHandle_t diopiAlphaMulTensor = const_cast<diopiTensorHandle_t>(alphaMulTensor.tensorHandle());
@@ -84,7 +94,6 @@ DIOPI_API diopiError_t diopiBaddbmm(diopiContextHandle_t ctx, diopiTensorHandle_
     diopiScalar_t other;
     other.fval = 1;
     other.stype = outDtype;
-
     diopiAdd(ctx, outCopy, diopiAlphaMulTensor, diopiBateMulTensor, &other);
     diopiCastDtype(ctx, out, outCopy);
 
@@ -96,6 +105,7 @@ DIOPI_API diopiError_t diopiBaddbmmInp(diopiContextHandle_t ctx, diopiTensorHand
     diopiBaddbmm(ctx, input, input, batch1, batch2, beta, alpha);
     return diopiSuccess;
 }
-}
+
+}  // extern "C"
 }  // namespace ascend
 }  // namespace impl
