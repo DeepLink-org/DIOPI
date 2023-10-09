39d2f30922e9d75408d76ec85087c58223ea6031
xue-yun-liang <1255450792@qq.com>
2023-10-09 12:47:06 +0000

    fix a lint error

--- impl/ascend/functions/cumsum.cpp
+++ impl/ascend/functions/cumsum.cpp
@@ -11,25 +11,25 @@
 namespace impl {
 namespace ascend {
 extern "C" DIOPI_API diopiError_t diopiCumsum(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t dim) {
-    diopiDtype_t inputDataType;
-    diopiDtype_t outputDataType;
-    diopiGetTensorDtype(input, &inputDataType);
-    diopiGetTensorDtype(out, &outputDataType);
-    
-    if(inputDataType != outputDataType){
+    diopiDtype_t inputDtype;
+    diopiDtype_t outputDtype;
+    diopiGetTensorDtype(input, &inputDtype);
+    diopiGetTensorDtype(out, &outputDtype);
+
+    if (inputDtype != outputDtype) {
         diopiTensorHandle_t inCopy;
-        makeTensorLike(ctx, &inCopy, input, outputDataType);
+        makeTensorLike(ctx, &inCopy, input, outputDtype);
         diopiCastDtype(ctx, inCopy, input);
         AclOpRunner<2, 1>("Cumsum", ctx).addInput(inCopy).addConstInput(dim, diopi_dtype_int64).addOutput(out).run();
-    }else if(inputDataType == diopi_dtype_bool){
+    } else if (inputDtype == diopi_dtype_bool) {
         diopiTensorHandle_t inCopy;
         makeTensorLike(ctx, &inCopy, input, diopi_dtype_uint8);
         diopiCastDtype(ctx, inCopy, input);
         AclOpRunner<2, 1>("Cumsum", ctx).addInput(inCopy).addConstInput(dim, diopi_dtype_int64).addOutput(out).run();
-    }else{
+    } else {
         AclOpRunner<2, 1>("Cumsum", ctx).addInput(input).addConstInput(dim, diopi_dtype_int64).addOutput(out).run();
     }
-    
+
     return diopiSuccess;
 }
 
