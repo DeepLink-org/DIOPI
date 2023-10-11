5320868a7957cbdbcb8941916f28563b0a4b419f
gqw <994321849@qq.com>
2023-10-11 15:53:18 +0800

    test

--- impl/torch/functions_ext.cpp
+++ impl/torch/functions_ext.cpp
@@ -15,16 +15,21 @@
 
 extern "C" {
 
-diopiError_t diopiRotaryEmbedding(diopiContextHandle_t ctx, diopiTensorHandle_t out1, diopiTensorHandle_t out2, diopiConstTensorHandle_t x1,
-                                  diopiConstTensorHandle_t x2, diopiConstTensorHandle_t cos, diopiConstTensorHandle_t sin, const bool conj) {
+diopiError_t diopiRotaryEmbedding(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t x, diopiConstTensorHandle_t cos,
+                                  diopiConstTensorHandle_t sin, const bool conj) {
     impl::aten::setCurCtx(ctx);
-    auto atOut1 = impl::aten::buildATen(out1);
-    auto atOut2 = impl::aten::buildATen(out2);
-    auto atX1 = impl::aten::buildATen(x1);
-    auto atX2 = impl::aten::buildATen(x2);
+    auto atX = impl::aten::buildATen(x);
     auto atCos = impl::aten::buildATen(cos);
     auto atSin = impl::aten::buildATen(sin);
-    ext::ops::apply_rotary_cuda(atX1, atX2, atCos, atSin, atOut1, atOut2, conj);
+    auto atOut = impl::aten::buildATen(out);
+    int last_dim = atX.dim() - 1;          // 确定最后一个维度的索引
+    auto chunks = atX.chunk(2, last_dim);  // 将 atX 切分为两个部分
+    auto x1 = chunks[0];
+    auto x2 = chunks[1];
+    auto chunks_out = atOut.chunk(2, last_dim);
+    auto out1 = chunks_out[0];
+    auto out2 = chunks_out[1];
+    ext::ops::apply_rotary_cuda(x1, x2, atCos, atSin, out1, out2, conj);
     return diopiSuccess;
 }
 
