2b5d02dee9978274112da19e7b589c382551a84e
wey-code <1060314685@qq.com>
2023-10-11 15:39:22 +0800

    add rmsnorm ext op

--- impl/torch/ext_kernel.h
+++ impl/torch/ext_kernel.h
@@ -18,6 +18,10 @@ using namespace at;
 
 void apply_rotary_cuda(const Tensor x1, const Tensor x2, const Tensor cos, const Tensor sin, Tensor out1, Tensor out2, const bool conj);
 
+void rms_norm_forward(Tensor input, IntArrayRef normalized_shape, Tensor gamma, double epsilon, Tensor output, Tensor invvar);
+
+void rms_norm_backward(Tensor dout, Tensor invvar, Tensor input, IntArrayRef normalized_shape, Tensor gamma, double epsilon, Tensor grad_input, Tensor grad_gamma);
+
 }  // namespace ops
 }  // namespace ext
 #endif  // IMPL_TORCH_EXT_KERNEL_H_
--- proto/include/diopi/functions_ext.h
+++ proto/include/diopi/functions_ext.h
@@ -16,6 +16,13 @@ extern "C" {
 DIOPI_API diopiError_t diopiRotaryEmbedding(diopiContextHandle_t ctx, diopiTensorHandle_t out1, diopiTensorHandle_t out2, diopiConstTensorHandle_t x1,
                                             diopiConstTensorHandle_t x2, diopiConstTensorHandle_t cos, diopiConstTensorHandle_t sin, const bool conj);
 
+DIOPI_API diopiError_t diopiRMSNorm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiTensorHandle_t invRMS, diopiConstTensorHandle_t input, 
+                        diopiSize_t normalized_shape, diopiConstTensorHandle_t weight, diopiConstTensorHandle_t bias, double eps);
+
+DIOPI_API diopiError_t diopiRMSNormBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiTensorHandle_t gradWeight, diopiTensorHandle_t gradBias,
+                                  diopiConstTensorHandle_t gradOutput, diopiConstTensorHandle_t input, diopiConstTensorHandle_t weight, diopiConstTensorHandle_t bias,
+                                  diopiConstTensorHandle_t invRMS, diopiSize_t normalized_shape, double eps);
+
 #if defined(__cplusplus)
 }
 #endif  // __cplusplus
