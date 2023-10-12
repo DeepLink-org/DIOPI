303ec1b70c0bb9979e2bbaaa6743f04d9a87d4a6
Bonbon-Tang <tangyufeng@pjlab.org.cn>
2023-10-12 11:08:32 +0800

    deleting submodule

--- diopi_test/scripts/build_mmcv.sh
+++ diopi_test/scripts/build_mmcv.sh
@@ -2,13 +2,10 @@
 set -e
 
 current_path=$(cd "$(dirname "$0")"; pwd)
-current_path=$(cd "$(dirname "$0")"; pwd)
 
 case $1 in
   mmcv_ext)
-    (cd ${current_path}/../third_party && mkdir mmcv && git clone https://github.com/open-mmlab/mmcv.git && cd mmcv \
-        && git clone https://github.com/open-mmlab/mmengine.git \
-        && ls && rm -rf build && mkdir build \
+    (cd ${current_path}/../third_party/mmcv && ls && rm -rf build && mkdir build \
         && MMCV_WITH_DIOPI=1 MMCV_WITH_OPS=1 python setup.py build_ext -i) \
     || exit -1;;
   *)
