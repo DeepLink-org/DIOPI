e89d89752747c0cf2767777069d1e76c180767f1
Bonbon-Tang <tangyufeng@pjlab.org.cn>
2023-10-12 18:09:03 +0800

    deleting submodule

--- diopi_test/scripts/build_mmcv.sh
+++ diopi_test/scripts/build_mmcv.sh
@@ -5,11 +5,10 @@ current_path=$(cd "$(dirname "$0")"; pwd)
 
 case $1 in
   mmcv_ext)
-    (cd ${current_path}/../third_party && rm -rf mmcv\
-        && git clone https://github.com/open-mmlab/mmcv.git && cd mmcv\
-        && git checkout \
+    (mkdir third_party && cd ${current_path}/../third_party && rm -rf mmcv\
+        && git clone https://github.com/open-mmlab/mmcv.git  && cd mmcv\
         && ls && conda list && rm -rf build && mkdir build \
-        && MMCV_WITH_DIOPI=0 MMCV_WITH_OPS=1 python setup.py build_ext -i) \
+        && MMCV_WITH_DIOPI=1 MMCV_WITH_OPS=1 python setup.py build_ext -i) \
     || exit -1;;
   *)
     echo -e "[ERROR] Incorrect compilation option:" $1;
