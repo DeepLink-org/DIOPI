a550721abcddf0f91e47ab04359ce3f785fdee0a
Bonbon-Tang <tangyufeng@pjlab.org.cn>
2023-10-12 15:42:51 +0800

    deleting submodule

--- diopi_test/scripts/build_mmcv.sh
+++ diopi_test/scripts/build_mmcv.sh
@@ -5,7 +5,7 @@ current_path=$(cd "$(dirname "$0")"; pwd)
 
 case $1 in
   mmcv_ext)
-    (mkdir third_party && cd ${current_path}/../third_party && rm -rf mmcv\
+    (export PYTHONPATH=$PWD:$PYTHONPATH && mkdir third_party && cd ${current_path}/../third_party && rm -rf mmcv\
         && /mnt/lustre/share/git clone https://github.com/open-mmlab/mmcv.git && cd mmcv\
         && ls && conda list && rm -rf build && mkdir build \
         && MMCV_WITH_DIOPI=1 MMCV_WITH_OPS=1 python setup.py build_ext -i) \
