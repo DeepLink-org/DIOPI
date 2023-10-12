97cc19d74703d70621b82728872b21814a500206
xue-yun-liang <1255450792@qq.com>
2023-10-12 07:36:01 +0000

    fix a bug

--- impl/ascend/functions/bmm.cpp
+++ impl/ascend/functions/bmm.cpp
@@ -13,8 +13,7 @@ diopiError_t diopiBmm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
     AscendTensor inputCopy(input);
     AscendTensor mat2Copy(mat2);
     diopiDtype_t dtype = inputCopy.dtype();
-    if(dtype == diopi_dtype_float64)
-        dtype = diopi_dtype_float32;
+    if (dtype == diopi_dtype_float64) dtype = diopi_dtype_float32;
     printContiguousTensor(ctx, inputCopy, "input");
     printContiguousTensor(ctx, mat2Copy, "mat2");
     AclOpRunner<2, 1>("BatchMatMul", ctx).addInput(input, dtype).addInput(mat2, dtype).setAttr("adj_x1", false).setAttr("adj_x1", false).addOutput(out).run();
