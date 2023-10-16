d197f7e5655839533d91ddf47c3a549f151d0a55
xue-yun-liang <51466825+xue-yun-liang@users.noreply.github.com>
2023-10-16 16:35:14 +0800

    Update mm.cpp

--- impl/ascend/functions/mm.cpp
+++ impl/ascend/functions/mm.cpp
@@ -13,27 +13,11 @@ diopiError_t diopiMm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCon
     AscendTensor inputCopy(input);
     AscendTensor mat2Copy(mat2);
     AscendTensor outputCopy(out);
-
     if (inputCopy.numel() == 0 || mat2Copy.numel() == 0) {
         diopiScalar_t zero = {outputCopy.dtype(), 0.0};
         diopiFill(ctx, out, &zero);
         return diopiSuccess;
     }
-
-    auto inputShape = inputCopy.shape();
-    // std::cout << "input shape:" << std::endl;
-    // for (auto i : inputShape) {
-    //     std::cout << i << " ";
-    // }
-    // std::cout << std::endl;
-
-    // auto mat2Shape = mat2Copy.shape();
-    // std::cout << "mat2 shape:" << std::endl;
-    // for (auto i : mat2Shape) {
-    //     std::cout << i << " ";
-    // }
-    // std::cout << std::endl;
-
     diopiDtype_t highDType = promoteTypes(inputCopy.dtype(), mat2Copy.dtype());
     if (highDType == diopi_dtype_float64) highDType = diopi_dtype_float32;
     AclOpRunner<2, 1>("MatMul", ctx)
@@ -43,15 +27,6 @@ diopiError_t diopiMm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCon
         .setAttr("adj_x1", false)
         .addOutput(out)
         .run();
-    AscendTensor outputCopyV2(out);
-
-    auto outputShape = outputCopyV2.shape();
-    // std::cout << "output shape:" << std::endl;
-    // for (auto i : outputShape) {
-    //     std::cout << i << " ";
-    // }
-    // std::cout << std::endl;
-
     return diopiSuccess;
 }
 
