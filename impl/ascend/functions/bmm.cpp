9285f7537ea52ca34a9094b0e081d3d1ddb4c1e9
xue-yun-liang <51466825+xue-yun-liang@users.noreply.github.com>
2023-10-16 16:53:19 +0800

    Update bmm.cpp

--- impl/ascend/functions/bmm.cpp
+++ impl/ascend/functions/bmm.cpp
@@ -14,7 +14,9 @@ diopiError_t diopiBmm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
     AscendTensor mat2Copy(mat2);
     AscendTensor outputCopy(out);
     if (inputCopy.numel() == 0 || mat2Copy.numel() == 0) {
-        diopiScalar_t zero = {outputCopy.dtype(), 0.0};
+        diopiScalar_t zero;
+        zero.stype = outputCopy.dtype();
+        zero.fval = 0.0;
         diopiFill(ctx, out, &zero);
         return diopiSuccess;
     }
