70974bde55ea5725b7006aab2a94ae0e4a496b0c
Jincong Chen <112855051+ccjincong@users.noreply.github.com>
2023-10-16 10:27:24 +0800

    Fix add/cat op bugs. (#462)
    * fix add
    
    * repair gil lock and stack op
    
    * fix div op bugs: when dtype=f16 can't get result inf
    
    ---------
    
    Co-authored-by: hellozmz <407190054@qq.com>

--- impl/ascend/common/acloprunner.hpp
+++ impl/ascend/common/acloprunner.hpp
@@ -439,14 +439,18 @@ public:
      * @return A reference to the modified AclOpRunner
      */
     template <typename T>
-    AclOpRunner& addDynamicInput(const std::vector<T>& tensors) {
+    AclOpRunner& addDynamicInput(const std::vector<T>& tensors, diopiDtype_t type = diopi_dtype_unsupported) {
         ASCEND_CHECK_ABORT(hasDynamicInput_ || inputIndex_ == 0 || InputSize == 1, "only support one dynamic input");
         hasDynamicInput_ = true;
         dynamcInputSize_ = tensors.size();
         inputDescs_.resize(dynamcInputSize_);
         inputBuffers_.resize(dynamcInputSize_);
         for (int i = 0; i < dynamcInputSize_; ++i) {
-            addInput(tensors[i]);
+            if (type != diopi_dtype_unsupported) {
+                addInput(tensors[i], type);
+            } else {
+                addInput(tensors[i]);
+            }
         }
         return *this;
     }
