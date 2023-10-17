5a9fb0eb407a601d6c4e9daaab5a6e89116de88e
Lingjie Li <lilingjie@sensetime.com>
2023-10-17 15:30:10 +0800

    fix cnnl diopiRandomInp: align with torch.Tensor.random_

--- impl/camb/functions/random.cpp
+++ impl/camb/functions/random.cpp
@@ -4,7 +4,9 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
-#include <cfloat>
+#include <algorithm>
+#include <cstdint>
+#include <limits>
 #include <vector>
 
 #include "../cnnl_helper.hpp"
@@ -12,6 +14,13 @@
 
 namespace impl {
 namespace camb {
+
+namespace {
+
+// clamp x into range [-largest, largest]
+int clamp(int64_t x, int largest) { return static_cast<int>(std::min<int64_t>(std::max<int64_t>(x, -largest), largest)); }
+
+}  // namespace
 
 extern "C" diopiError_t diopiRandomInp(diopiContextHandle_t ctx, diopiTensorHandle_t inout, int64_t from, const int64_t* to, diopiGeneratorHandle_t generator) {
     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
@@ -23,18 +32,15 @@ extern "C" diopiError_t diopiRandomInp(diopiContextHandle_t ctx, diopiTensorHand
     DIOPI_CALLCNNL(cnnlRandCreateGenerator(&cnnlGenerator, CNNL_RAND_RNG_MTGP32));
 
     if (dtype == CNNL_DTYPE_FLOAT || dtype == CNNL_DTYPE_HALF) {
-        float min = from;
-        float max;
-        if (to != nullptr) {
-            max = *to - 1;
-        } else {
-            max = (dtype == CNNL_DTYPE_FLOAT ? FLT_MAX : std::numeric_limits<half_float::half>::max());
-        }
+        auto mantissa = dtype == CNNL_DTYPE_FLOAT ? std::numeric_limits<float>::digits : std::numeric_limits<half_float::half>::digits;
+        auto largestIntAsFp = 1 << mantissa;
+        int min = clamp(from, largestIntAsFp);
+        int max = to ? clamp(*to - 1, largestIntAsFp) : largestIntAsFp;
         diopiTensorHandle_t stateHandle = nullptr;
         DIOPI_CALL(diopiGeneratorGetState(ctx, generator, &stateHandle));
         void* statePtr = nullptr;
         DIOPI_CALL(diopiGetTensorData(stateHandle, &statePtr));
-        DIOPI_CALLCNNL(cnnlRandGenerateUniform(handle, cnnlGenerator, dtype, statePtr, tensor.numel(), min, max, tensor.data()));
+        DIOPI_CALLCNNL(cnnlRandGenerateDescreteUniform(handle, cnnlGenerator, dtype, statePtr, tensor.numel(), min, max, tensor.data()));
         DIOPI_CALL(diopiGeneratorSetState(generator, stateHandle));
     } else {
         setLastErrorString("%s%d", "cnnl random not support datatype: ", dtype);
