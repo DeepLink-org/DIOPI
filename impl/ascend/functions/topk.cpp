88ffb49d694c41223d66845c0bd1d3344e1809cc
ccjincong <chenjincong11@gmail.com>
2023-10-18 06:15:38 +0000

    test

--- impl/ascend/functions/topk.cpp
+++ impl/ascend/functions/topk.cpp
@@ -10,6 +10,7 @@ namespace impl {
 namespace ascend {
 diopiError_t diopiTopk(diopiContextHandle_t ctx, diopiTensorHandle_t values, diopiTensorHandle_t indices, diopiConstTensorHandle_t input, int64_t k,
                        int64_t dim, bool largest, bool sorted) {
+    // std::cout << "--------------------------------------------------------------------------------------------------------" << std::endl;
     std::vector<int64_t> kVec({k});
     diopiSize_t kSize = vectorToDiopiSize(kVec);
     diopiDtype_t outDtype, castType;
@@ -24,13 +25,18 @@ diopiError_t diopiTopk(diopiContextHandle_t ctx, diopiTensorHandle_t values, dio
     AscendTensor outA(values);
     castTensor(ctx, outA, castType);
 
-    // AscendTensor tem(input);
-    // if (tem.dim() == 0 && k == 1) {
-    //     AscendTensor outB(input);
-    //     castTensor(ctx, outB, outDtype);
-    //     diopiCastDtype(ctx, values, static_cast<diopiConstTensorHandle_t>(outB));
-    //     return diopiSuccess;
-    // }
+    AscendTensor tem(input);
+    if (tem.dim() == 0 && k == 1) {
+        // std::cout << "innnnnnnnnnnnnnnnnnnnnnnn" << std::endl;
+        AscendTensor outB(input);
+        castTensor(ctx, outB, outDtype);
+        diopiCastDtype(ctx, values, static_cast<diopiConstTensorHandle_t>(outB));
+
+        diopiScalar_t zero = constructDiopiScalarT(diopi_dtype_int32, 0);
+        diopiFill(ctx, indices, &zero);
+        return diopiSuccess;
+    }
+
     AclOpRunner<2, 2>("TopKV2", ctx)
         .addInput(input, castType)
         .addConstInput(kSize)
