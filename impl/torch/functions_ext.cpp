27a405c4ef8eed6b1602d3e1a6f64803601c2cdb
Gong-air <77915156+Gong-air@users.noreply.github.com>
2023-10-19 18:26:09 +0800

    Update functions_ext.cpp

--- impl/torch/functions_ext.cpp
+++ impl/torch/functions_ext.cpp
@@ -1,33 +1,71 @@
 /**
  * @file
- * @author OpenComputeLab
- * @copyright  (c) 2023, OpenComputeLab.
+ * @author DeepLink
+ * @copyright  (c) 2023, DeepLink.
  */
 
-#ifndef _PROJECT_DIOPERATOR_INTERFACE_FUNCTIONS_EXT_H_
-#define _PROJECT_DIOPERATOR_INTERFACE_FUNCTIONS_EXT_H_
+#include <diopi/functions.h>
+#include <diopi/functions_ext.h>
 
-#include <diopi/diopirt.h>
+#include <iostream>
 
-#if defined(__cplusplus)
+#include "context.h"
+#include "ext_kernel.h"
+#include "helper.hpp"
+
 extern "C" {
-#endif  // __cplusplus
-#define DIOPI_API
 
-DIOPI_API diopiError_t diopiRotaryEmbedding(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t x, diopiConstTensorHandle_t cos,
-                                            diopiConstTensorHandle_t sin, const bool conj);
+diopiError_t diopiRotaryEmbedding(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t x, diopiConstTensorHandle_t cos,
+                                  diopiConstTensorHandle_t sin, const bool conj) {
+    impl::aten::setCurCtx(ctx);
+    auto atX = impl::aten::buildATen(x);
+    auto atCos = impl::aten::buildATen(cos);
+    auto atSin = impl::aten::buildATen(sin);
+    auto atOut = impl::aten::buildATen(out);
+    int last_dim = atX.dim() - 1;          // 确定最后一个维度的索引
+    auto chunks = atX.chunk(2, last_dim);  // 将 atX 切分为两个部分
+    auto x1 = chunks[0];
+    auto x2 = chunks[1];
+    auto chunks_out = atOut.chunk(2, last_dim);
+    auto out1 = chunks_out[0];
+    auto out2 = chunks_out[1];
+    ext::ops::apply_rotary_cuda(x1, x2, atCos, atSin, out1, out2, conj);
+    impl::aten::unsetCurCtx();
+    std::cout << "正确return" <<std::endl;
+    return diopiSuccess;
+}
 
-DIOPI_API diopiError_t diopiRMSNorm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiTensorHandle_t invRMS, diopiConstTensorHandle_t input,
-                                    diopiSize_t normalized_shape, diopiConstTensorHandle_t weight, diopiConstTensorHandle_t bias, double eps);
+diopiError_t diopiRMSNorm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiTensorHandle_t invRMS, diopiConstTensorHandle_t input,
+                          diopiSize_t normalized_shape, diopiConstTensorHandle_t weight, diopiConstTensorHandle_t bias, double eps) {
+    impl::aten::setCurCtx(ctx);
+    auto atOut = impl::aten::buildATen(out);
+    auto atInvRMS = impl::aten::buildATen(invRMS);
+    auto atInput = impl::aten::buildATen(input);
+    auto atNormalized_shape = impl::aten::buildAtIntArray(normalized_shape);
+    auto atWeight = impl::aten::buildATen(weight);
+    auto atBias = impl::aten::buildATen(bias);  // bias在这里实际上没有使用
+    ext::ops::rms_norm_forward(atInput, atNormalized_shape, atWeight, eps, atOut, atInvRMS);
+    impl::aten::unsetCurCtx();
+    return diopiSuccess;
+}
 
 DIOPI_API diopiError_t diopiRMSNormBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiTensorHandle_t gradWeight,
                                             diopiTensorHandle_t gradBias, diopiConstTensorHandle_t gradOutput, diopiConstTensorHandle_t input,
                                             diopiConstTensorHandle_t weight, diopiConstTensorHandle_t bias, diopiConstTensorHandle_t invRMS,
-                                            diopiSize_t normalized_shape, double eps);
-#define DIOPI_API __attribute__((weak))
-
-#if defined(__cplusplus)
+                                            diopiSize_t normalized_shape, double eps) {
+    impl::aten::setCurCtx(ctx);
+    auto atGradInput = impl::aten::buildATen(gradInput);
+    auto atGradWeight = impl::aten::buildATen(gradWeight);
+    auto atGradBias = impl::aten::buildATen(gradBias);
+    auto atGradOutput = impl::aten::buildATen(gradOutput);
+    auto atInvRMS = impl::aten::buildATen(invRMS);
+    auto atInput = impl::aten::buildATen(input);
+    auto atNormalized_shape = impl::aten::buildAtIntArray(normalized_shape);
+    auto atWeight = impl::aten::buildATen(weight);
+    auto atBias = impl::aten::buildATen(bias);  // bias在这里实际上没有使用
+    ext::ops::rms_norm_backward(atGradOutput, atInvRMS, atInput, atNormalized_shape, atWeight, eps, atGradInput, atGradWeight);
+    impl::aten::unsetCurCtx();
+    return diopiSuccess;
 }
-#endif  // __cplusplus
 
-#endif  // _PROJECT_DIOPERATOR_INTERFACE_FUNCTIONS_MMCV_H_
+}  // extern "C"
