1da10fa5fad3d2278f2da34266b157e8050f1441
liangshinan <liangshinan@sensetime.com>
2023-10-23 16:29:20 +0800

    fix ci

--- diopi_test/python/main.py
+++ diopi_test/python/main.py
@@ -256,8 +256,7 @@ if __name__ == "__main__":
         if exit_code != 0:
             raise SystemExit(exit_code)
     elif args.mode == "utest":
-        call = f"python3 -m pytest -vx tests {args.pytest_args if args.pytest_args else ''}"
-        print(call)
+        call = f"python3 -m pytest -vx tests"
         exit_code = subprocess.call(shlex.split(call))  # nosec
         if exit_code != 0:
             raise SystemExit(exit_code)
