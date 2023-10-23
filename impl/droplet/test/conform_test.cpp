d192e3a639c1b362c892d6467c25176c725da1b3
lichengyuan <lichengyuan@sensetime.com>
2023-10-23 17:44:54 +0800

    fix: udpate lint check

--- impl/droplet/test/conform_test.cpp
+++ impl/droplet/test/conform_test.cpp
@@ -23,59 +23,51 @@ extern "C" {
     }}
 
 
-void* device_malloc(uint64_t bytes)
-{
+void* device_malloc(uint64_t bytes) {
     void* ptr;
     CALL_DROPLET(tangMalloc(&ptr, bytes));
     return ptr;
 }
 
-void device_free(void* ptr)
-{
+void device_free(void* ptr) {
     CALL_DROPLET(tangFree(ptr));
 }
 
-int32_t device_make_stream(diopiStreamHandle_t* stream_handle_ptr)
-{
+int32_t device_make_stream(diopiStreamHandle_t* stream_handle_ptr) {
     tangStream_t phStream;
     CALL_DROPLET(tangStreamCreate(&phStream));
     *stream_handle_ptr = (diopiStreamHandle_t)phStream;
     return diopiSuccess;
 }
 
-int32_t device_destroy_stream(diopiStreamHandle_t stream_handle)
-{
+int32_t device_destroy_stream(diopiStreamHandle_t stream_handle) {
     tangStream_t phStream = (tangStream_t)stream_handle;
     CALL_DROPLET(tangStreamDestroy(phStream));
     return diopiSuccess;
 }
 
-int32_t device_synchronize_stream(diopiStreamHandle_t stream_handle)
-{
+int32_t device_synchronize_stream(diopiStreamHandle_t stream_handle) {
     tangStream_t phStream = (tangStream_t)stream_handle;
     CALL_DROPLET(tangStreamSynchronize(phStream));
     return diopiSuccess;
 }
 
 int32_t device_memcpy_h2d_async(diopiStreamHandle_t stream_handle,
-                                void* dst, const void* src, uint64_t bytes)
-{
+                                void* dst, const void* src, uint64_t bytes) {
     tangStream_t phStream = (tangStream_t)stream_handle;
     CALL_DROPLET(tangMemcpyAsync(dst, src, bytes, tangMemcpyHostToDevice, phStream));
     return diopiSuccess;
 }
 
 int32_t device_memcpy_d2h_async(diopiStreamHandle_t stream_handle,
-                                void* dst, const void* src, uint64_t bytes)
-{
+                                void* dst, const void* src, uint64_t bytes) {
     tangStream_t phStream = (tangStream_t)stream_handle;
     CALL_DROPLET(tangMemcpyAsync(dst, src, bytes, tangMemcpyDeviceToHost, phStream));
     return diopiSuccess;
 }
 
 int32_t device_memcpy_d2d_async(diopiStreamHandle_t stream_handle,
-                                void* dst, const void* src, uint64_t bytes)
-{
+                                void* dst, const void* src, uint64_t bytes) {
     tangStream_t phStream = (tangStream_t)stream_handle;
     CALL_DROPLET(tangMemcpyAsync(dst, src, bytes, tangMemcpyDeviceToDevice, phStream));
     return diopiSuccess;
@@ -85,8 +77,7 @@ static char strLastError[8192] = {0};
 static char strLastErrorOther[4096] = {0};
 static std::mutex mtxLastError;
 
-const char* device_get_last_error_string()
-{
+const char* device_get_last_error_string() {
     tangError_t error = tangGetLastError();
     std::lock_guard<std::mutex> lock(mtxLastError);
     sprintf(strLastError, "droplet error: %s; other error: %s",
@@ -95,8 +86,7 @@ const char* device_get_last_error_string()
 }
 
 
-int32_t finalizeLibrary()
-{
+int32_t finalizeLibrary() {
     return diopiSuccess;
 }
 int32_t initLibrary() { return diopiSuccess; }
