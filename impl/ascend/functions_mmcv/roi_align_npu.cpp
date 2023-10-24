492544cea4d9aae2a55ae8bbadbc9cba992756b5
cokedong <408244909@qq.com>
2023-10-24 08:43:05 +0000

    Fix clang_format

--- impl/ascend/functions_mmcv/roi_align_npu.cpp
+++ impl/ascend/functions_mmcv/roi_align_npu.cpp
@@ -12,10 +12,10 @@ extern "C" {
 DIOPI_API diopiError_t diopiRoiAlignMmcv(diopiContextHandle_t ctx, diopiTensorHandle_t output, diopiTensorHandle_t argmaxY, diopiTensorHandle_t argmaxX,
                                          diopiConstTensorHandle_t input, diopiConstTensorHandle_t rois, int64_t alignedHeight, int64_t alignedWidth,
                                          int64_t samplingRatio, int64_t poolMode, float spatialScale, bool aligned) {
-    int64_t roi_end_mode = 2;
+    int64_t roiEndMode = 2;
     if (!aligned) {
         warning("The [aligned] attr in roi_align op is false");
-        roi_end_mode = 0;
+        roiEndMode = 0;
     }
 
     diopiDtype_t inputDtype;
@@ -23,8 +23,8 @@ DIOPI_API diopiError_t diopiRoiAlignMmcv(diopiContextHandle_t ctx, diopiTensorHa
     diopiGetTensorDtype(input, &inputDtype);
     diopiGetTensorDtype(rois, &roisDtype);
     diopiTensorHandle_t outCopy;
-    bool is_half = inputDtype == diopi_dtype_float16 || roisDtype == diopi_dtype_float16;
-    if (is_half) {
+    bool isHalf = inputDtype == diopi_dtype_float16 || roisDtype == diopi_dtype_float16;
+    if (isHalf) {
         makeTensorLike(ctx, &outCopy, output, diopi_dtype_float32);
     } else {
         outCopy = output;
@@ -37,35 +37,35 @@ DIOPI_API diopiError_t diopiRoiAlignMmcv(diopiContextHandle_t ctx, diopiTensorHa
         .setAttr<int64_t>("pooled_height", alignedHeight)
         .setAttr<int64_t>("pooled_width", alignedWidth)
         .setAttr<int64_t>("sample_num", samplingRatio)
-        .setAttr<int64_t>("roi_end_mode", roi_end_mode)
+        .setAttr<int64_t>("roi_end_mode", roiEndMode)
         .addOutput(outCopy)
         .run();
 
-    if (is_half) diopiCastDtype(ctx, output, outCopy);
+    if (isHalf) diopiCastDtype(ctx, output, outCopy);
     return diopiSuccess;
 }
 
 diopiError_t diopiRoiAlignBackwardMmcv(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput,
                                        diopiConstTensorHandle_t rois, diopiConstTensorHandle_t argmaxY, diopiConstTensorHandle_t argmaxX, int64_t alignedHeight,
                                        int64_t alignedWidth, int64_t samplingRatio, int64_t poolMode, float spatialScale, bool aligned) {
-    int64_t roi_end_mode = 2;
+    int64_t roiEndMode = 2;
     if (!aligned) {
         warning("The [aligned] attr in roi_align_grad op is false");
-        roi_end_mode = 0;
+        roiEndMode = 0;
     }
-    diopiSize_t xdiff_shape;
-    diopiGetTensorShape(gradInput, &xdiff_shape);
+    diopiSize_t xdiffShape;
+    diopiGetTensorShape(gradInput, &xdiffShape);
 
     AclOpRunner<2, 1>("ROIAlignGrad", ctx)
         .addInput(gradOutput)
         .addInput(rois)
         .addOutput(gradInput)
-        .setAttr("xdiff_shape", xdiff_shape)
+        .setAttr("xdiff_shape", xdiffShape)
         .setAttr<int64_t>("pooled_width", alignedWidth)
         .setAttr<int64_t>("pooled_height", alignedHeight)
         .setAttr<float>("spatial_scale", spatialScale)
         .setAttr<int64_t>("sample_num", samplingRatio)
-        .setAttr<int64_t>("roi_end_mode", roi_end_mode)
+        .setAttr<int64_t>("roi_end_mode", roiEndMode)
         .run();
 
     return diopiSuccess;
