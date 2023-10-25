1e8001b2e060ee8a9e1cc00275bf6d2353f3bb83
lhy <442488254@qq.com>
2023-10-25 11:34:41 +0800

    use clang-formt to fix lint errors and add "-Wl, --no-as-needed"

--- impl/droplet/test/conform_test.cpp
+++ impl/droplet/test/conform_test.cpp
@@ -5,9 +5,10 @@
  *
  *************************************************************************************************/
 #include <conform_test.h>
-#include <tang_runtime.h>
-#include <tang_compiler_api.h>
 #include <diopi/diopirt.h>
+#include <tang_compiler_api.h>
+#include <tang_runtime.h>
+
 #include <cstdio>
 #include <mutex>
 
@@ -15,12 +16,13 @@
 extern "C" {
 #endif
 
-#define CALL_DROPLET(Expr)   {                                                         \
-    tangError_t ret = Expr;                                                         \
-    if (ret != tangSuccess) {                                                       \
-        printf("call a tangrt function (%s) failed. return code=%d", #Expr, ret);   \
-    }}
-
+#define CALL_DROPLET(Expr)                                                            \
+    {                                                                                 \
+        tangError_t ret = Expr;                                                       \
+        if (ret != tangSuccess) {                                                     \
+            printf("call a tangrt function (%s) failed. return code=%d", #Expr, ret); \
+        }                                                                             \
+    }
 
 void* device_malloc(uint64_t bytes) {
     void* ptr;
@@ -28,9 +30,7 @@ void* device_malloc(uint64_t bytes) {
     return ptr;
 }
 
-void device_free(void* ptr) {
-    CALL_DROPLET(tangFree(ptr));
-}
+void device_free(void* ptr) { CALL_DROPLET(tangFree(ptr)); }
 
 int32_t device_make_stream(diopiStreamHandle_t* stream_handle_ptr) {
     tangStream_t phStream;
@@ -80,14 +80,9 @@ const char* device_get_last_error_string() {
     return strLastError;
 }
 
+int32_t finalizeLibrary() { return diopiSuccess; }
 
-int32_t finalizeLibrary() {
-    return diopiSuccess;
-}
-
-int32_t initLibrary() {
-    return diopiSuccess;
-}
+int32_t initLibrary() { return diopiSuccess; }
 
 #if defined(__cplusplus)
 }  // extern "C"
