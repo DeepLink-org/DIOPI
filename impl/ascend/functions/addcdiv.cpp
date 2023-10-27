f96461c386f9f2cbd8b960b01dff802669df15bf
hellozmz <407190054@qq.com>
2023-10-27 23:18:34 +0800

    optimise code

--- impl/ascend/functions/addcdiv.cpp
+++ impl/ascend/functions/addcdiv.cpp
@@ -11,17 +11,16 @@ namespace ascend {
 
 diopiError_t diopiAddcdiv(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t tensor1,
                           diopiConstTensorHandle_t tensor2, const diopiScalar_t* value) {
-    diopiTensorHandle_t valueTensor = nullptr;
-    AscendTensor outAt(out), valueAt(valueTensor), inAt(input), t1(tensor1), t2(tensor2), tin, t11, t22;
+    AscendTensor outAt(out), inAt(input), t1(tensor1), t2(tensor2);
     if (inAt.numel() == 0) {
         return diopiSuccess;
     }
     auto size = inferSize(t1.shape(), t2.shape());
     size = inferSize(size, inAt.shape());
-    broadcast(ctx, tin, inAt, size);
-    broadcast(ctx, t11, t1, size);
-    broadcast(ctx, t22, t2, size);
-    AclOpRunner<4, 1>("Addcdiv", ctx).addInput(tin).addInput(t11).addInput(t22).addConstInput(*value, outAt.dtype()).addOutput(out).run();
+    broadcast(ctx, inAt, inAt, size);
+    broadcast(ctx, t1, t1, size);
+    broadcast(ctx, t2, t2, size);
+    AclOpRunner<4, 1>("Addcdiv", ctx).addInput(inAt).addInput(t1).addInput(t2).addConstInput(*value, outAt.dtype()).addOutput(out).run();
     return diopiSuccess;
 }
 
