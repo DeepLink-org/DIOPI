65ff4f903f09c1f6b7f6b16cd10fb488706419d2
POI-WX <wangxing2@sensetime.com>
2023-11-09 19:37:42 +0800

    fix bug

--- impl/camb/functions/bmm.cpp
+++ impl/camb/functions/bmm.cpp
@@ -4,9 +4,6 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
-#include <numeric>
-#include <vector>
-
 #include "../cnnl_helper.hpp"
 #include "../common/common.hpp"
 
@@ -20,14 +17,6 @@ diopiError_t diopiBmm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
     DiopiTensor otherTensor(mat2);
     DiopiTensor outTensor(out);
 
-    std::set<diopiDtype_t> supportedDtypes{diopi_dtype_int8, diopi_dtype_int16, diopi_dtype_int32, diopi_dtype_float16, diopi_dtype_float32};
-    DIOPI_CALL(autoCastTensorType(ctx, {&inputTensor, &otherTensor}, supportedDtypes));
-
-    DiopiTensor outTensorTmp = outTensor;
-    if (outTensor.dtype() != inputTensor.dtype()) {
-        outTensorTmp = requiresTensor(ctx, outTensor.shape(), inputTensor.dtype());
-    }
-
     CnnlTensorDesc inputDesc(inputTensor, CNNL_LAYOUT_ARRAY);
     CnnlTensorDesc otherDesc(otherTensor, CNNL_LAYOUT_ARRAY);
     CnnlTensorDesc outDesc(outTensor, CNNL_LAYOUT_ARRAY);
@@ -35,6 +24,7 @@ diopiError_t diopiBmm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
     int32_t allowTf32Int = 1;
     CnnlDescBase<cnnlMatMulDescriptor_t, cnnlMatMulDescCreate, cnnlMatMulDescDestroy> bmmDesc;
     DIOPI_CALLCNNL(cnnlSetMatMulDescAttr(bmmDesc.get(), CNNL_MATMUL_ALLOW_TF32, &allowTf32Int, sizeof(allowTf32Int)));
+
     CnnlDescBase<cnnlMatMulAlgo_t, cnnlMatMulAlgoCreate, cnnlMatMulAlgoDestroy> bmmAlgo;
     CnnlDescBase<cnnlMatMulHeuristicResult_t, cnnlCreateMatMulHeuristicResult, cnnlDestroyMatMulHeuristicResult> bmmHeuristicResult;
     int returnAlgoCount = 0;
@@ -48,8 +38,8 @@ diopiError_t diopiBmm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
         workspace = requiresBuffer(ctx, workspaceSize).data();
     }
 
-    int alpha = 1;
-    int beta = 0;
+    float alpha = 1;
+    float beta = 0;
     DIOPI_CALLCNNL(cnnlBatchMatMulBCast_v2(handle,
                                            bmmDesc.get(),
                                            bmmAlgo.get(),
@@ -63,9 +53,6 @@ diopiError_t diopiBmm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
                                            outTensor.data(),
                                            workspace,
                                            workspaceSize));
-    if (outTensor.dtype() != outTensorTmp.dtype()) {
-        DIOPI_CALL(dataTypeCast(ctx, outTensor, outTensorTmp));
-    }
     return diopiSuccess;
 }
 
