1cd219ad5124885d97862a43d6d8e30649545496
POI-WX <wangxing2@sensetime.com>
2023-11-09 17:21:36 +0800

    wx optimize bmm op on camb

--- impl/camb/functions/bmm.cpp
+++ impl/camb/functions/bmm.cpp
@@ -16,24 +16,56 @@ namespace camb {
 diopiError_t diopiBmm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t mat2) {
     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
 
-    DiopiTensor mat1Tensor = DiopiTensor(input);
-    DiopiTensor mat2Tensor = DiopiTensor(mat2);
-    DiopiTensor outputTensor = DiopiTensor(out);
+    DiopiTensor inputTensor(input);
+    DiopiTensor otherTensor(mat2);
+    DiopiTensor outTensor(out);
 
-    DiopiTensor mat1Casted = mat1Tensor;
-    DiopiTensor mat2Casted = mat2Tensor;
-    DiopiTensor outputCasted = outputTensor;
+    std::set<diopiDtype_t> supportedDtypes{diopi_dtype_int8, diopi_dtype_int16, diopi_dtype_int32, diopi_dtype_float16, diopi_dtype_float32};
+    DIOPI_CALL(autoCastTensorType(ctx, {&inputTensor, &otherTensor}, supportedDtypes));
 
-    std::vector<DiopiTensor*> tensors{&mat1Casted, &mat2Casted, &outputCasted};
-    DIOPI_CALL(autoCastTensorType(ctx, tensors, {diopi_dtype_float16, diopi_dtype_float32}));
+    DiopiTensor outTensorTmp = outTensor;
+    if (outTensor.dtype() != inputTensor.dtype()) {
+        outTensorTmp = requiresTensor(ctx, outTensor.shape(), inputTensor.dtype());
+    }
 
-    CnnlTensorDesc mat1Desc(mat1Casted, CNNL_LAYOUT_ARRAY);
-    CnnlTensorDesc mat2Desc(mat2Casted, CNNL_LAYOUT_ARRAY);
-    CnnlTensorDesc outDesc(outputCasted, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc inputDesc(inputTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc otherDesc(otherTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc outDesc(outTensor, CNNL_LAYOUT_ARRAY);
 
-    DIOPI_CALLCNNL(
-        cnnlBatchMatMul(handle, false, false, mat1Desc.get(), mat1Casted.data(), mat2Desc.get(), mat2Casted.data(), outDesc.get(), outputCasted.data()));
-    DIOPI_CALL(dataTypeCast(ctx, outputTensor, outputCasted));
+    int32_t allowTf32Int = 1;
+    CnnlDescBase<cnnlMatMulDescriptor_t, cnnlMatMulDescCreate, cnnlMatMulDescDestroy> bmmDesc;
+    DIOPI_CALLCNNL(cnnlSetMatMulDescAttr(bmmDesc.get(), CNNL_MATMUL_ALLOW_TF32, &allowTf32Int, sizeof(allowTf32Int)));
+    CnnlDescBase<cnnlMatMulAlgo_t, cnnlMatMulAlgoCreate, cnnlMatMulAlgoDestroy> bmmAlgo;
+    CnnlDescBase<cnnlMatMulHeuristicResult_t, cnnlCreateMatMulHeuristicResult, cnnlDestroyMatMulHeuristicResult> bmmHeuristicResult;
+    int returnAlgoCount = 0;
+    DIOPI_CALLCNNL(cnnlGetBatchMatMulAlgoHeuristic(
+        handle, bmmDesc.get(), inputDesc.get(), otherDesc.get(), outDesc.get(), nullptr, 1, &(bmmHeuristicResult.get()), &returnAlgoCount));
+
+    size_t workspaceSize = 0;
+    DIOPI_CALLCNNL(cnnlGetBatchMatMulHeuristicResult(bmmHeuristicResult.get(), bmmAlgo.get(), &workspaceSize));
+    void* workspace = nullptr;
+    if (workspaceSize != 0) {
+        workspace = requiresBuffer(ctx, workspaceSize).data();
+    }
+
+    int alpha = 1;
+    int beta = 0;
+    DIOPI_CALLCNNL(cnnlBatchMatMulBCast_v2(handle,
+                                           bmmDesc.get(),
+                                           bmmAlgo.get(),
+                                           &alpha,
+                                           inputDesc.get(),
+                                           inputTensor.data(),
+                                           otherDesc.get(),
+                                           otherTensor.data(),
+                                           &beta,
+                                           outDesc.get(),
+                                           outTensor.data(),
+                                           workspace,
+                                           workspaceSize));
+    if (outTensor.dtype() != outTensorTmp.dtype()) {
+        DIOPI_CALL(dataTypeCast(ctx, outTensor, outTensorTmp));
+    }
     return diopiSuccess;
 }
 
