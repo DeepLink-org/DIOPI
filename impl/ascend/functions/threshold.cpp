f3d66b36b201ec04494e2d27c1188ddea596dc80
ccjincong <chenjincong11@gmail.com>
2023-11-15 08:00:29 +0000

    fix threshold

--- impl/ascend/functions/threshold.cpp
+++ impl/ascend/functions/threshold.cpp
@@ -13,18 +13,18 @@ diopiError_t diopiThreshold(diopiContextHandle_t ctx, diopiTensorHandle_t out, d
                             const diopiScalar_t* value) {
     diopiDtype_t dtype;
     diopiGetTensorDtype(input, &dtype);
-    float InputThreshold = getValue<float>(threshold);
-    float InputValue = getValue<float>(value);
+    float inputThreshold = getValue<float>(threshold);
+    float inputValue = getValue<float>(value);
 
     if (dtype == diopi_dtype_uint8) {
-        InputThreshold = static_cast<float>(static_cast<uint8_t>(InputThreshold));
-        InputValue = static_cast<float>(static_cast<uint8_t>(InputValue));
+        inputThreshold = static_cast<float>(static_cast<uint8_t>(inputThreshold));
+        inputValue = static_cast<float>(static_cast<uint8_t>(inputValue));
     }
     if (dtype == diopi_dtype_int32) {
-        InputThreshold = static_cast<float>(getValue<int>(threshold));
-        InputValue = static_cast<float>(getValue<int>(value));
+        inputThreshold = static_cast<float>(getValue<int>(threshold));
+        inputValue = static_cast<float>(getValue<int>(value));
     }
-    AclOpRunner<1, 1>("ThresholdV2D", ctx).addInput(input).setAttr("threshold", InputThreshold).setAttr("value", InputValue).addOutput(out).run();
+    AclOpRunner<1, 1>("ThresholdV2D", ctx).addInput(input).setAttr("threshold", inputThreshold).setAttr("value", inputValue).addOutput(out).run();
     return diopiSuccess;
 }
 
