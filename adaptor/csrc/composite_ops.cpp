9802f7dfec00051efaf851a096db6981ab256590
yewentao <zhyanwentao@126.com>
2023-11-16 19:40:45 +0800

    adding an example for composite ops

--- adaptor/csrc/composite_ops.cpp
+++ adaptor/csrc/composite_ops.cpp
@@ -2,9 +2,16 @@
 
 namespace impl {
 namespace composite {
-diopiError_t diopiLinear(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t weight,
-                         diopiConstTensorHandle_t bias) {
-    return diopiSuccess;
-}
+
+// This is an example for developing composite ops
+// diopiError_t diopiLinear(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t weight,
+//                          diopiConstTensorHandle_t bias) {
+//     // Your logic here...
+//     DIOPI_CALL(diopiMatmul(ctx, out, input, weight));
+//     // Your logic here...
+//     DIOPI_CALL(diopiAddInp(ctx, input, bias, {1}));
+//     // Your logic here...
+//     return diopiSuccess;
+// }
 }  // namespace composite
 }  // namespace impl
