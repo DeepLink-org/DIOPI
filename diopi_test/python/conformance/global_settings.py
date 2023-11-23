6e8d9c51c21e656d293eb3b526478484c5c6fe57
zhousike <zsksmhq@outlook.com>
2023-11-23 09:47:59 +0800

    update

--- diopi_test/python/conformance/global_settings.py
+++ diopi_test/python/conformance/global_settings.py
@@ -1,14 +1,13 @@
 # Copyright (c) 2023, DeepLink.
-from diopilib import Dtype
-
+import numpy as np
 
 class glob_var(object):
     def __init__(self, nhwc=False, nhwc_min_dim=3, four_bytes=False):
         self.nhwc = nhwc
         self.nhwc_min_dim = nhwc_min_dim
         self.four_bytes = four_bytes
-        self.int_type = Dtype.int64
-        self.float_type = Dtype.float64
+        self.int_type = np.int64
+        self.float_type = np.float64
         self._cur_test_func = ''
         self._func_status = {}
         self._debug_level = 0
@@ -25,8 +24,8 @@ class glob_var(object):
 
     def set_four_bytes(self):
         self.four_bytes = True
-        self.int_type = Dtype.int32
-        self.float_type = Dtype.float32
+        self.int_type = np.int32
+        self.float_type = np.float32
 
     def get_four_bytes(self):
         return self.four_bytes
