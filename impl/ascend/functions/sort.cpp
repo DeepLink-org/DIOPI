4d6a24826b3f465783b0c6b5e302a38883479ae1
ccjincong <chenjincong11@gmail.com>
2023-11-24 03:39:06 +0000

    fix sort

--- impl/ascend/functions/sort.cpp
+++ impl/ascend/functions/sort.cpp
@@ -10,7 +10,19 @@ namespace impl {
 namespace ascend {
 diopiError_t diopiSort(diopiContextHandle_t ctx, diopiTensorHandle_t values, diopiTensorHandle_t indices, diopiConstTensorHandle_t input, int64_t dim,
                        bool descending, const bool* stable) {
-    AclOpRunner<1, 2>("Sort", ctx).addInput(input).setAttr("axis", dim).setAttr("descending", descending).addOutput(values).addOutput(indices).run();
+    bool tem = &stable;
+    AclOpRunner<1, 2>("Sort", ctx).addInput(input).setAttr("axis", dim).setAttr("descending", descending).setAttr("stable", tem).addOutput(values).addOutput(indices).run();
+
+    // AscendTensor tem(input);
+    // AclOpRunner<2, 2>("TopKV2", ctx)
+    //     .addInput(input)
+    //     .addConstInput(tem.shape())
+    //     .setAttr("dim", dim)
+    //     .setAttr("largest", descending)
+    //     .setAttr("sorted", (bool)1)
+    //     .addOutput(values)
+    //     .addOutput(indices)
+    //     .run();
     return diopiSuccess;
 }
 
