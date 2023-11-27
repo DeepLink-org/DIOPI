ff1894c7acf0a6c6b2ffc36779830a25f159d05b
Lxz@lmdeploy_container <1293750327@qq.com>
2023-11-27 12:23:53 +0000

    optimize plusscalarinp

--- impl/torch/functions_lmdeploy.cpp
+++ impl/torch/functions_lmdeploy.cpp
@@ -12,54 +12,46 @@
 
 extern "C" {
 
+/**
+ * @brief PlusScalar. if 0 < index < size, add val.
+ * @param[in] ctx The diopi context.
+ * @param[inout] inoutput : Output tensor.shape=[len].type = [int64, int32]
+ * @param[in] val : Val for add.type = [int64, int32]
+ * @param[in] size : Size or maxindex.type = [int64, int32]
+ */
 DIOPI_API diopiError_t diopiPlusScalarInp(diopiContextHandle_t ctx, diopiTensorHandle_t inoutput, const int64_t val, const int64_t size) {
     diopiSize_t in_shape;
     diopiGetTensorShape(inoutput, &in_shape);
     if (in_shape.len != 1) {
         return diopiErrorOccurred;
     }
-    int64_t input_len = in_shape.data[0];
 
     diopiDtype_t in_type;
-    diopiSize_t in_stride;
     diopiDevice_t in_device;
     diopiGetTensorDtype(inoutput, &in_type);
-    diopiGetTensorStride(inoutput, &in_stride);
     diopiGetTensorDevice(inoutput, &in_device);
 
+    int64_t front_len = (size <= in_shape.data[0]) ? size : in_shape.data[0];
     diopiSize_t front_shape;
-    front_shape.data = &size;
+    front_shape.data = &front_len;
     front_shape.len = 1;
-    diopiTensorHandle_t tmp[2];
-    diopiRequireTensor(ctx, tmp, &front_shape, &in_stride, in_type, in_device);
+    diopiSize_t front_stride;
 
-    diopiScalar_t front_scalar;
-    front_scalar.stype = diopi_dtype_int64;
-    front_scalar.ival = val;
-    diopiFill(ctx, tmp[0], &front_scalar);
+    void *input_data;
+    diopiGetTensorData(inoutput, &input_data);
+    front_stride.data = reinterpret_cast<const int64_t *>(input_data);
+    front_stride.len = -1;
+    diopiTensorHandle_t front;
+    diopiRequireTensor(ctx, &front, &front_shape, &front_stride, in_type, in_device);
 
-    diopiTensorHandle_t added = tmp[0];
-
-    if (size < input_len) {
-        int64_t latter_len = input_len - size;
-        diopiSize_t latter_shape;
-        latter_shape.data = &latter_len;
-        latter_shape.len = 1;
-        diopiRequireTensor(ctx, &tmp[1], &latter_shape, &in_stride, in_type, in_device);
-
-        diopiScalar_t latter_scalar;
-        latter_scalar.stype = diopi_dtype_int64;
-        latter_scalar.ival = 0;
-        diopiFill(ctx, tmp[1], &latter_scalar);
-
-        diopiRequireTensor(ctx, &added, &in_shape, &in_stride, in_type, in_device);
-        diopiCat(ctx, added, const_cast<diopiConstTensorHandle_t*>(&tmp[0]), 2, 0);
-    }
+    diopiScalar_t val_scalar;
+    val_scalar.stype = diopi_dtype_int64;
+    val_scalar.ival = val;
 
     diopiScalar_t one;
     one.stype = diopi_dtype_int64;
     one.ival = 1;
-    diopiAddInp(ctx, inoutput, added, &one);
+    diopiAddInpScalar(ctx, front, &val_scalar, &one);
     return diopiSuccess;
 }
 
