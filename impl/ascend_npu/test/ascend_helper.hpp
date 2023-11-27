9fa77f2636782645f230a24df7bec3655e546867
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-11-27 13:31:49 +0800

    support diopi_test

---
+++ impl/ascend_npu/test/ascend_helper.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <acl/acl.h>
+#include <diopi/diopirt.h>
+
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace impl {
+namespace ascend {
+
+#define TRACK_ACL(x)                                                    \
+    do {                                                                \
+        static bool enable = std::getenv("DIOPI_TRACK_ACL") != nullptr; \
+        if (enable) {                                                   \
+            printf("[%s: %d]:%s\n", __FILE__, __LINE__, x);             \
+        }                                                               \
+    } while (0);
+
+#define CALL_ACLRT(Expr)                                                                          \
+    do {                                                                                          \
+        TRACK_ACL(#Expr);                                                                         \
+        ::aclError ret = Expr;                                                                    \
+        if (ret != ::ACL_SUCCESS) {                                                               \
+            throw std::runtime_error(std::string("ascend device error:") + aclGetRecentErrMsg()); \
+        }                                                                                         \
+    } while (0);
+
+#define CHECK_ASCENDRT(Expr)                                                \
+    do {                                                                    \
+        TRACK_ACL(#Expr);                                                   \
+        ::diopiError_t ret = Expr;                                          \
+        if (ret != ::diopiSuccess) {                                        \
+            throw std::runtime_error(std::string("call function failed.")); \
+        }                                                                   \
+    } while (0);
+
+#define ASCEND_CHECK_ABORT(condition, ...)                            \
+    do {                                                              \
+        if (!(condition)) {                                           \
+            printf("[%s:%s:%d]: ", __FILE__, __FUNCTION__, __LINE__); \
+            printf(__VA_ARGS__);                                      \
+            printf("\n");                                             \
+            std::abort();                                             \
+        }                                                             \
+    } while (0);
+
+#define ASCEND_CHECK_NULLPTR_ABORT(ptr) ASCEND_CHECK_ABORT(ptr, "Variable is nullptr, pls check.")
+
+#define error(...)                               \
+    printf("[%s:%d]: ", __FUNCTION__, __LINE__); \
+    printf(__VA_ARGS__);                         \
+    printf("\n");                                \
+    std::abort();
+
+#define warning(...)                             \
+    printf("[%s:%d]: ", __FUNCTION__, __LINE__); \
+    printf(__VA_ARGS__);                         \
+    printf("\n");
+
+#define info(...)                                \
+    printf("[%s:%d]: ", __FUNCTION__, __LINE__); \
+    printf(__VA_ARGS__);                         \
+    printf("\n");
+
+}  // namespace ascend
+}  // namespace impl
+
---
+++ impl/ascend_npu/test/conform_test.cpp
@@ -0,0 +1,94 @@
+/**************************************************************************************************
+ * Copyright (c) 2022, SenseTime Inc.
+ * License
+ * Author
+ *
+ *************************************************************************************************/
+
+#include <acl/acl_base.h>
+#include <conform_test.h>
+#include <diopi/diopirt.h>
+
+#include <cstdio>
+
+#include "ascend_helper.hpp"
+#include "litert.hpp"
+namespace impl {
+namespace ascend {
+
+extern "C" {
+void* device_malloc(uint64_t bytes) {
+    void* ptr = nullptr;
+    if (bytes > 0) {
+        CALL_ACLRT(::aclrtMalloc(&ptr, bytes, ACL_MEM_MALLOC_HUGE_FIRST));
+    }
+    return ptr;
+}
+
+void device_free(void* ptr) {
+    if (ptr) {
+        CALL_ACLRT(aclrtFree(ptr));
+    }
+}
+
+diopiError_t device_make_stream(diopiStreamHandle_t* streamHandlePtr) {
+    CALL_ACLRT(aclrtCreateStream(reinterpret_cast<aclrtStream*>(streamHandlePtr)));
+    return diopiSuccess;
+}
+
+diopiError_t device_destroy_stream(diopiStreamHandle_t streamHandle) {
+    CALL_ACLRT(aclrtDestroyStream(reinterpret_cast<aclrtStream>(streamHandle)));
+    return diopiSuccess;
+}
+
+diopiError_t device_synchronize_stream(diopiStreamHandle_t streamHandle) {
+    CALL_ACLRT(aclrtSynchronizeStream(reinterpret_cast<aclrtStream>(streamHandle)));
+    return diopiSuccess;
+}
+
+diopiError_t device_memcpy_h2d_async(diopiStreamHandle_t streamHandle, void* dst, const void* src, uint64_t bytes) {
+    if (nullptr != dst && nullptr != src) {
+        CALL_ACLRT(aclrtMemcpyAsync(dst, bytes, src, bytes, ACL_MEMCPY_HOST_TO_DEVICE, reinterpret_cast<aclrtStream>(streamHandle)));
+    }
+    return diopiSuccess;
+}
+
+diopiError_t device_memcpy_d2h_async(diopiStreamHandle_t streamHandle, void* dst, const void* src, uint64_t bytes) {
+    if (nullptr != dst && nullptr != src) {
+        CALL_ACLRT(aclrtMemcpyAsync(dst, bytes, src, bytes, ACL_MEMCPY_DEVICE_TO_HOST, reinterpret_cast<aclrtStream>(streamHandle)));
+    }
+    return diopiSuccess;
+}
+
+diopiError_t device_memcpy_d2d_async(diopiStreamHandle_t streamHandle, void* dst, const void* src, uint64_t bytes) {
+    CALL_ACLRT(aclrtMemcpyAsync(dst, bytes, src, bytes, ACL_MEMCPY_DEVICE_TO_DEVICE, reinterpret_cast<aclrtStream>(streamHandle)));
+    return diopiSuccess;
+}
+
+diopiError_t initLibrary() {
+    CALL_ACLRT(aclInit(nullptr));
+    CALL_ACLRT(aclrtSetDevice(0));
+    aclrtContext context;
+    CALL_ACLRT(aclrtCreateContext(&context, 0));
+    return diopiSuccess;
+}
+
+diopiError_t finalizeLibrary() {
+    CALL_ACLRT(aclFinalize());
+    return diopiSuccess;
+}
+
+diopiError_t buildGeneratorState(diopiContextHandle_t ctx, diopiTensorHandle_t out) {
+    // state size = seed size + offset size
+    std::vector<int64_t> vec{sizeof(uint64_t) + sizeof(int64_t)};
+    diopiSize_t size{vec.data(), static_cast<int64_t>(vec.size())};
+    diopiTensorHandle_t tensor = nullptr;
+    diopiRequireTensor(ctx, &tensor, &size, nullptr, diopi_dtype_uint8, diopi_host);
+    *out = *tensor;
+    return diopiSuccess;
+}
+
+}  // extern "C"
+
+}  // namespace ascend
+}  // namespace impl
