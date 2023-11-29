7c00fe4cb73eb423f800ba4d1b242f0e8763ae2b
ZhangQiu <100055343+NeosZhang@users.noreply.github.com>
2023-11-29 10:12:21 +0800

    Update slice.cpp

--- impl/ascend/functions/slice.cpp
+++ impl/ascend/functions/slice.cpp
@@ -8,47 +8,43 @@
 
 namespace impl {
 namespace ascend {
-diopiError_t diopiSlice(diopiContextHandle_t ctx, diopiTensorHandle_t null_out, diopiConstTensorHandle_t input, int64_t dim, int64_t start, int64_t end,
+diopiError_t diopiSlice(diopiContextHandle_t ctx, diopiTensorHandle_t nullOut, diopiConstTensorHandle_t input, int64_t dim, int64_t start, int64_t end,
                         int64_t step) {
     std::vector<int64_t> index;
     for (int64_t i = start; i < end; i += step) {
         index.push_back(i);
     }
-    std::vector<int64_t> dim_vec{dim};
-    bool negative_index_support = true;
+    std::vector<int64_t> dimVec{dim};
+    bool negativeIndexSupport = true;
     AclOpRunner<3, 1>("GatherV2", ctx)
         .addInput(input)
         .addConstInput(vectorToDiopiSize(index))
-        .addConstInput(vectorToDiopiSize(dim_vec))
-        .setAttr("negative_index_support", negative_index_support)
-        .addOutput(null_out)
+        .addConstInput(vectorToDiopiSize(dimVec))
+        .setAttr("negative_index_support", negativeIndexSupport)
+        .addOutput(nullOut)
         .run();
     return diopiSuccess;
 }
 
-diopiError_t diopiSliceBackward(diopiContextHandle_t ctx, diopiTensorHandle_t grad_input, diopiConstTensorHandle_t grad_output, diopiSize_t input_sizes,
+diopiError_t diopiSliceBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput, diopiSize_t inputSizes,
                                 int64_t dim, int64_t start, int64_t end, int64_t step) {
     if (dim < 0) {
-        dim += input_sizes.len;
+        dim += inputSizes.len;
     }
-    std::vector<int64_t> index;
-    for (int64_t i = start; i < end; i += step) {
-        index.push_back(i);
-    }
-
-    std::vector<int64_t> start64(input_sizes.len, 0);
-    std::vector<int64_t> step64(input_sizes.len, 1);
-    std::vector<int64_t> end64(input_sizes.data, input_sizes.data + input_sizes.len);
+ 
+    std::vector<int64_t> start64(inputSizes.len, 0);
+    std::vector<int64_t> step64(inputSizes.len, 1);
+    std::vector<int64_t> end64(inputSizes.data, inputSizes.data + inputSizes.len);
     start64[dim] = start;
     step64[dim] = step;
     end64[dim] = end;
     AclOpRunner<5, 1>("StridedSliceGrad", ctx)
-        .addConstInput(input_sizes)
+        .addConstInput(inputSizes)
         .addConstInput(vectorToDiopiSize(start64))
         .addConstInput(vectorToDiopiSize(end64))
         .addConstInput(vectorToDiopiSize(step64))
-        .addInput(grad_output)
-        .addOutput(grad_input)
+        .addInput(gradOutput)
+        .addOutput(gradInput)
         .run();
 
     return diopiSuccess;
