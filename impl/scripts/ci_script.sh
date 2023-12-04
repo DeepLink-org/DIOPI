5739e46439c1e459bebbeb6ace9a0dff7ba2d15c
yangbofun <yangbofun@163.com>
2023-12-04 19:40:31 +0800

    add scripts for clang-tidy-ascend

--- impl/scripts/ci_script.sh
+++ impl/scripts/ci_script.sh
@@ -1,26 +1,49 @@
 # !/bin/bash
+
 set -e
 
-CURRENT_PATH=$(cd "$(dirname "$0")"; pwd)
-CMAKE_EXPORT_COMPILE_COMMANDS_FILE=${CURRENT_PATH}/../build/compile_commands.json
+CURRENT_PATH=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+
+CMAKE_EXPORT_COMPILE_COMMANDS_FILE=$(readlink -f ${CURRENT_PATH}/../build/compile_commands.json)
+IMPL_PATH=$(readlink -f ${CURRENT_PATH}/../)
+
 case $1 in
-  cpp-lint)
-    # for other cpplint version, maybe  -whitespace/indent is needed to check impl
-    # --repository=.. will be deleted when repository changed.
-    (echo "cpp-lint" && python scripts/cpplint.py --linelength=160 --repository=.. \
-      --filter=-build/c++11,-legal/copyright,-build/include_subdir,-runtime/references,-runtime/printf,-runtime/int,-build/namespace \
-      --exclude=${CURRENT_PATH}/../third_party --exclude=${CURRENT_PATH}/../ascend_npu/third_party  --exclude=${CURRENT_PATH}/../build \
-      --recursive ./ )  \
-    || exit 1;;
-  clang-tidy)
-    (
+cpp-lint)
+  # for other cpplint version, maybe  -whitespace/indent is needed to check impl
+  # --repository=.. will be deleted when repository changed.
+  (
+    echo "cpp-lint" && python3 scripts/cpplint.py --linelength=160 --repository=..
+    --filter=-build/c++11,-legal/copyright,-build/include_subdir,-runtime/references,-runtime/printf,-runtime/int,-build/namespace
+    --exclude=${IMPL_PATH}/third_party --exclude=${IMPL_PATH}/ascend_npu/third_party --exclude=${IMPL_PATH}/build
+    --recursive ./
+  ) ||
+    exit 1
+  ;;
+clang-tidy)
+  (
     if [ -e ${CMAKE_EXPORT_COMPILE_COMMANDS_FILE} ]; then
-      python3 ${CURRENT_PATH}/../../run-clang-tidy.py -p `dirname "${CMAKE_EXPORT_COMPILE_COMMANDS_FILE}"`
+      python3 ${IMPL_PATH}/../run-clang-tidy.py -p $(dirname "${CMAKE_EXPORT_COMPILE_COMMANDS_FILE}")
     else
       echo "error: compile_commands.json not found."
       exit 1
-    fi);;
-  *)
-    echo -e "[ERROR] Incorrect option:" $1 && exit 1;
+    fi
+  )
+  ;;
+clang-tidy-ascend)
+  (
+    if [ -e ${CMAKE_EXPORT_COMPILE_COMMANDS_FILE} ]; then
+      python3 ${IMPL_PATH}/../run-clang-tidy.py ${IMPL_PATH}/ascend ${IMPL_PATH}/ascend_npu -p $(dirname ${CMAKE_EXPORT_COMPILE_COMMANDS_FILE})
+    else
+      echo "error: compile_commands.json not found."
+      exit 1
+    fi
+  )
+  ;;
+*)
+  echo -e "[ERROR] Incorrect option:" $1 && exit 1
+  ;;
 esac
 exit 0
