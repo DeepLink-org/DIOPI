21dd9ef0c9cdc58127d195ffdc8e619e0818a4d8
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-12-06 18:38:17 +0800

    fix lint

--- impl/ascend_npu/diopi_impl/norm.cpp
+++ impl/ascend_npu/diopi_impl/norm.cpp
@@ -12,10 +12,10 @@ using npu_utils = at_npu::native::NpuUtils;
 using npu_compile_type = at_npu::native::CompileType;
 using npu_preparation = at_npu::native::OpPreparation;
 
-at::Tensor& normal_out_npu_nocheck(at::Tensor& result, c10::optional<at::Generator> gen) {
-    auto gen_default = gen.value().get<at_npu::NPUGeneratorImpl>();
+at::Tensor& normalOutNpuNocheck(at::Tensor& result, c10::optional<at::Generator> gen) {
+    auto genDefault = gen.value().get<at_npu::NPUGeneratorImpl>();
 
-    auto pair = gen_default->philox_engine_inputs(10);
+    auto pair = genDefault->philox_engine_inputs(10);
     const int64_t seed = pair.first;
     const int64_t offset = pair.second;
 
@@ -41,7 +41,7 @@ namespace OP_IMPL_NS {
 diopiError_t diopiNormal(diopiContextHandle_t ctx, diopiTensorHandle_t out, double mean, double std, diopiGeneratorHandle_t generator) {
     BEGIN_CALL_ACL_OP(out, generator);
     if (outAt.numel() > 0) {
-        normal_out_npu_nocheck(outAt, c10::make_optional(std::move(generatorAt)));
+        normalOutNpuNocheck(outAt, c10::make_optional(std::move(generatorAt)));
         acl_op::mul_(outAt, std);
         acl_op::add_(outAt, mean);
     }
