4a6a68709800b2bb9c30859bce6e97decca1d1ee
NeosZhang <zhangqiu@sensetime.com>
2023-12-06 16:15:23 +0800

    fix ascend_config.yaml

--- impl/ascend/functions/repeat.cpp
+++ impl/ascend/functions/repeat.cpp
@@ -4,15 +4,15 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
-// #include "../common/acloprunner.hpp"
+#include "../common/acloprunner.hpp"
 
-// namespace impl {
-// namespace ascend {
+namespace impl {
+namespace ascend {
 
-// diopiError_t diopiRepeat(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t repeatsSize) {
-//     AclOpRunner<2, 1>("Tile", ctx).addInput(input).addConstInput(repeatsSize).addOutput(out).run();
-//     return diopiSuccess;
-// }
+diopiError_t diopiRepeat(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t repeatsSize) {
+    AclOpRunner<2, 1>("Tile", ctx).addInput(input).addConstInput(repeatsSize).addOutput(out).run();
+    return diopiSuccess;
+}
 
-// }  // namespace ascend
-// }  // namespace impl
+}  // namespace ascend
+}  // namespace impl
