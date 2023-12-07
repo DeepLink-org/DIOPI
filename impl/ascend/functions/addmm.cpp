cebae98f6b5eea00f43d3c4fe0a796b30389a316
NeosZhang <zhangqiu@sensetime.com>
2023-12-07 16:49:08 +0800

    fix convert_config

--- impl/ascend/functions/addmm.cpp
+++ impl/ascend/functions/addmm.cpp
@@ -10,28 +10,31 @@ namespace impl {
 namespace ascend {
 diopiError_t diopiAddmm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t mat1,
                         diopiConstTensorHandle_t mat2, const diopiScalar_t* beta, const diopiScalar_t* alpha) {
-    std::cout << "ready to run diopiAddmm" << std::endl;
     AscendTensor asInput(input);
-    AscendTensor asOut(out);
-    std::cout << "FLAG1" << std::endl;
+    AscendTensor asMat1(mat1);
+    AscendTensor asMat2(mat2);
+
     // calculate (mat1 @ mat2)
-    std::vector<int64_t> outShape = asOut.shape();
-    diopiSize_t outShape_ = vectorToDiopiSize(outShape);
-    diopiTensorHandle_t matResult;
-    makeTensorFromSize(ctx, &outShape_, &matResult, asOut.dtype());
+    std::vector<int64_t> tempShape(2);
+    tempShape[0] = asMat1.shape(0);
+    tempShape[1] = asMat2.shape(1);
+    AscendTensor asMatResult;
+    diopiDtype_t highDType = promoteTypes(asMat1.dtype(), asMat2.dtype());
+    makeTensor(ctx, asMatResult, tempShape, highDType);
+    diopiTensorHandle_t matResult = const_cast<diopiTensorHandle_t>(asMatResult.tensorHandle());
     diopiMm(ctx, matResult, mat1, mat2);
 
-    std::cout << "FLAG2" << std::endl;
     // calculate beta x input;
     std::vector<int64_t> inputShape = asInput.shape();
-    diopiSize_t inputShape_ = vectorToDiopiSize(inputShape);
-    diopiTensorHandle_t bXinput;
-    makeTensorFromSize(ctx, &inputShape_, &bXinput, asOut.dtype());
+    AscendTensor asBXinput;
+    diopiDtype_t highDType2 = promoteTypes(asInput.dtype(), beta->stype);
+    makeTensor(ctx, asBXinput, inputShape, highDType2);
+    diopiTensorHandle_t bXinput = const_cast<diopiTensorHandle_t>(asBXinput.tensorHandle());
     diopiMulScalar(ctx, bXinput, input, beta);
 
-    std::cout << "FLAG3" << std::endl;
     // calculate beta x input + alpha x (mat1 @ mat2);
     diopiAdd(ctx, out, bXinput, matResult, alpha);
+    return diopiSuccess;
 }
 
 }  // namespace ascend
