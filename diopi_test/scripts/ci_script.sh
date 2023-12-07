2a71713502eba6ce62a5dfb9efaad9d18f76da5b
liwenjian.vendor <liwenjian.vendor@sensetime.com>
2023-12-07 11:16:55 +0800

    add diopi_test cpp-lint

--- diopi_test/scripts/ci_script.sh
+++ diopi_test/scripts/ci_script.sh
@@ -8,6 +8,13 @@ DIOPI_TEST_DIR=$(readlink -f $CURRENT_DIR/../)
 case $1 in
   py-lint)
     echo "py-lint" && flake8 $DIOPI_TEST_DIR || exit -1;;
+  cpp-lint)
+    # for other cpplint version, maybe  -whitespace/indent is needed to check impl
+    (echo "cpp-lint" && python scripts/cpplint.py --exclude=impl/third_party/ --linelength=160 \
+      --filter=-build/c++11,-legal/copyright,-build/include_subdir,-runtime/references,-runtime/printf,-runtime/int,-build/namespace \
+      --recursive impl/ \
+      && python scripts/cpplint.py --linelength=240 --filter=-build/header_guard --recursive diopirt/ ) \
+    || exit -1;;
   *)
     echo -e "[ERROR] Incorrect option:" $1;
 
