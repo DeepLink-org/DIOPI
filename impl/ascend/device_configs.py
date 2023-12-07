77375253e9e690d91ae0afb992323b30ba56e4fb
jfxu-st <143591296+jfxu-st@users.noreply.github.com>
2023-12-07 19:46:44 +0800

    fix dropout for Ascend (#696)
    * fix dropout for Ascend
    
    * remove redundant copy for diopiDropoutInp in test mode

--- impl/ascend/device_configs.py
+++ impl/ascend/device_configs.py
@@ -1044,42 +1044,6 @@ device_configs = {
         ),
     ),
 
-    'dropout': dict(
-        name=['dropout'],
-        tensor_para=dict(
-            args=[
-                {
-                    "ins": ['input'],
-                    "dtype": [Skip(np.float32),Skip(np.float64),],
-                },
-            ]
-        ),
-    ),
-
-    'dropout_training': dict(
-        name=['dropout'],
-        tensor_para=dict(
-            args=[
-                {
-                    "ins": ['input'],
-                    "dtype": [Skip(np.float32),Skip(np.float64),],
-                },
-            ]
-        ),
-    ),
-
-    'dropout2d': dict(
-        name=['dropout2d'],
-        tensor_para=dict(
-            args=[
-                {
-                    "ins": ['input'],
-                    "dtype": [Skip(np.float32),Skip(np.float64),],
-                },
-            ]
-        ),
-    ),
-
     'leaky_relu': dict(
         name=['leaky_relu'],
         tensor_para=dict(
