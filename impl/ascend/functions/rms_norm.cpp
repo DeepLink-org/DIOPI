c1cf808f8f25ec10288b62a472cb5499fd333d10
zhangzefeng92 <zhang_zefeng@foxmail.com>
2023-12-08 14:17:45 +0800

    add rms norm

---
+++ impl/ascend/functions/rms_norm.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2023, DeepLink.
+ */
+
+#include "../common/acloprunner.hpp"
+
+namespace impl {
+namespace ascend {
+
+diopiError_t diopiRMSNorm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiTensorHandle_t inv_rms, diopiConstTensorHandle_t input,
+                          diopiSize_t normalized_shape, diopiConstTensorHandle_t weight, diopiConstTensorHandle_t bias, double eps) {
+    AclOpRunner<2, 2>("RmsNorm", ctx).addInput(input).addInput(weight).addOutput(out).addOutput(inv_rms).setAttr("epsilon", static_cast<float>(eps)).run();
+    return diopiSuccess;
+}
+
+diopiError_t diopiRMSNormBackward(diopiContextHandle_t ctx, diopiTensorHandle_t grad_input, diopiTensorHandle_t grad_weight, diopiTensorHandle_t grad_bias,
+                                  diopiConstTensorHandle_t grad_output, diopiConstTensorHandle_t input, diopiConstTensorHandle_t weight,
+                                  diopiConstTensorHandle_t bias, diopiConstTensorHandle_t inv_rms, diopiSize_t normalized_shape, double eps) {
+    AclOpRunner<4, 2>("RmsNorm", ctx).addInput(grad_output).addInput(input).addInput(inv_rms).addInput(weight).addOutput(grad_input).addOutput(grad_weight).run();
+    return diopiSuccess;
+}
+
+}  // namespace ascend
+}  // namespace impl
