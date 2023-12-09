29486123337b387154bd55866566e2ad4f21e6ce
yangbofun <yangbofun@163.com>
2023-12-09 23:20:54 +0800

    lint

--- diopi_test/scripts/build_mmcv.sh
+++ diopi_test/scripts/build_mmcv.sh
@@ -7,13 +7,13 @@ current_path=$(
 )
 
 case $1 in
-mmcv_ext)
-  cd ${current_path}/../third_party/mmcv_diopi
-  rm -rf build && mkdir build
-  MMCV_WITH_DIOPI=1 MMCV_WITH_OPS=1 python setup.py build_ext -i
-  ;;
-*)
-  echo -e "[ERROR] Incorrect compilation option:" $1 && exit 1
-  ;;
+  mmcv_ext)
+    cd ${current_path}/../third_party/mmcv_diopi
+    rm -rf build && mkdir build
+    MMCV_WITH_DIOPI=1 MMCV_WITH_OPS=1 python setup.py build_ext -i
+    ;;
+  *)
+    echo -e "[ERROR] Incorrect compilation option:" $1 && exit 1
+    ;;
 
 esac
