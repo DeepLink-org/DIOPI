df61ddf09b1c872237e5c8268df219f44f692dbf
yangbofun <yangbofun@163.com>
2023-12-09 23:52:19 +0800

    add cpp-lint in diopi_test

--- diopi_test/scripts/ci_script.sh
+++ diopi_test/scripts/ci_script.sh
@@ -12,8 +12,8 @@ case $1 in
   cpp-lint)
     # for other cpplint version, maybe  -whitespace/indent is needed to check impl
     echo "cpp-lint"
-    python scripts/cpplint.py --exclude=impl/third_party/ --linelength=160 --filter=-build/c++11,-legal/copyright,-build/include_subdir, -runtime/references,-runtime/printf,-runtime/int,-build/namespace --recursive impl/
-    python scripts/cpplint.py --linelength=240 --filter=-build/header_guard --recursive diopirt/
+    python scripts/cpplint.py --exclude=impl/third_party/ --linelength=160 --filter=-build/c++11,-legal/copyright,-build/include_subdir,-runtime/references,-runtime/printf,-runtime/int,-build/namespace \
+      --recursive $DIOPI_TEST_DIR/diopi_stub/csrc $DIOPI_TEST_DIR/diopi_stub/include
     ;;
   *)
     echo -e "[ERROR] Incorrect option:" $1 || exit 1
