d1d8cf4dccdb9d08c67d972413fc4c0cbdfab440
zhangzefeng92 <zhang_zefeng@foxmail.com>
2023-12-11 14:41:20 +0800

    add linspace op

--- impl/ascend/functions/linspace.cpp
+++ impl/ascend/functions/linspace.cpp
@@ -12,10 +12,13 @@ namespace impl {
 namespace ascend {
 
 diopiError_t diopiLinspace(diopiContextHandle_t ctx, diopiTensorHandle_t out, const diopiScalar_t* start, const diopiScalar_t* end, int64_t steps) {
-
-    AclOpRunner<3, 1>("LinSpace", ctx).addConstInput(*start, diopi_dtype_float32).addConstInput(*end, diopi_dtype_float32).addConstInput(steps, diopi_dtype_int32).addOutput(out).run();
+    AclOpRunner<3, 1>("LinSpace", ctx)
+        .addConstInput(*start, diopi_dtype_float32)
+        .addConstInput(*end, diopi_dtype_float32)
+        .addConstInput(steps, diopi_dtype_int32)
+        .addOutput(out)
+        .run();
     return diopiSuccess;
-
 }
 
 }  // namespace ascend
