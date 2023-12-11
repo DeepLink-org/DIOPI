46b62e4efeb7e9dcb881e4bdfcfef504b620b345
yangbo <yangbofun@163.com>
2023-12-11 19:25:32 +0800

    Yb/fix ci exit (#710)
    * fix the exit problem

--- impl/scripts/build_impl.sh
+++ impl/scripts/build_impl.sh
@@ -10,20 +10,24 @@ DIOPI_TEST_PATH=${CURRENT_DIR}/../../diopi_test
 case $1 in
   tops)
     mkdir -p build && cd build
+    # do not use && between cmake and make -j, because the cmake error will not incur error exit (return code is 0) in switch case statementts.
     cmake -DPYTHON_EXECUTABLE=$(python3 -c "import sys; print(sys.executable)") .. -DIMPL_OPT=tops -DTEST=ON -DCMAKE_BUILD_TYPE=Debug \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DTOPS_HOME=/home/cse/src/install/usr && make -j8
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DTOPS_HOME=/home/cse/src/install/usr
+    make -j8
     ;;
   clean)
     rm -rf build
     ;;
   cuda)
     mkdir -p build && cd build
-    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=cuda -DTEST=ON && make -j8
+    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=cuda -DTEST=ON
+    make -j8
     ;;
   torch)
     mkdir -p build && cd build
     cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=torch -DCMAKE_BUILD_TYPE=Release -DTEST=ON \
-      -DENABLE_COVERAGE=${USE_COVERAGE} -DCMAKE_PREFIX_PATH=$(python -c 'import torch;print(torch.utils.cmake_prefix_path)') && make -j8
+      -DENABLE_COVERAGE=${USE_COVERAGE} -DCMAKE_PREFIX_PATH=$(python -c 'import torch;print(torch.utils.cmake_prefix_path)')
+    make -j8
     ;;
   torch_dyload)
     mkdir -p build && cd build
@@ -34,19 +38,23 @@ case $1 in
   camb_pytorch)
     mkdir -p build && cd build
     cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=camb_pytorch -DCMAKE_BUILD_TYPE=Release -DTEST=ON \
-      -DCMAKE_PREFIX_PATH=$(python -c 'import torch;print(torch.utils.cmake_prefix_path)') && make -j8
+      -DCMAKE_PREFIX_PATH=$(python -c 'import torch;print(torch.utils.cmake_prefix_path)')
+    make -j8
     ;;
   camb)
     mkdir -p build && cd build
-    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=camb -DCMAKE_BUILD_TYPE=Release -DTEST=ON -DENABLE_COVERAGE=${USE_COVERAGE} && make -j8
+    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=camb -DCMAKE_BUILD_TYPE=Release -DTEST=ON -DENABLE_COVERAGE=${USE_COVERAGE}
+    make -j8
     ;;
   ascend)
     mkdir -p build && cd build
-    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=ascend -DCMAKE_BUILD_TYPE=Release -DTEST=ON && make -j16
+    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=ascend -DCMAKE_BUILD_TYPE=Release -DTEST=ON
+    make -j16
     ;;
   hip_pytorch)
     mkdir build && cd build
-    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=TORCH -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DHIP=ON && make -j8
+    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=TORCH -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DHIP=ON
+    make -j8
     ;;
   mmcv_ext)
     cd third_party/mmcv_diopi && rm -rf build && mkdir build
@@ -54,11 +62,13 @@ case $1 in
     ;;
   droplet)
     mkdir build && cd build
-    cmake .. -DIMPL_OPT=droplet -DTEST=ON -DCMAKE_BUILD_TYPE=Release && make -j8
+    cmake .. -DIMPL_OPT=droplet -DTEST=ON -DCMAKE_BUILD_TYPE=Release
+    make -j8
     ;;
   supa)
     mkdir -p build && cd build
-    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=supa -DCMAKE_BUILD_TYPE=Release -DTEST=ON && make -j8
+    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DIMPL_OPT=supa -DCMAKE_BUILD_TYPE=Release -DTEST=ON
+    make -j8
     ;;
   *)
     echo -e "[ERROR] Incorrect compilation option:" $1 || exit 1
