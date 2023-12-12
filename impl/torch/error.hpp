597feb404b550789f94e769b6e0e008d4982caf8
Peter Ye <44945378+yewentao256@users.noreply.github.com>
2023-12-12 14:49:14 +0800

    Ywt/remove redundant copy (#702)
    * feature: remove redundant copy
    
    * fix: getlaststringerror segmentfault

--- impl/torch/error.hpp
+++ impl/torch/error.hpp
@@ -4,10 +4,28 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
+#include <iostream>
+#include <utility>
+
 #ifndef IMPL_TORCH_ERROR_HPP_
 #define IMPL_TORCH_ERROR_HPP_
 
 void _set_last_error_string(const char* err);
 const char* cuda_get_last_error_string();
+
+inline void logError() { std::cerr << std::endl; }
+
+template <typename First, typename... Rest>
+void logError(First&& first, Rest&&... rest) {
+    std::cerr << std::forward<First>(first);
+    logError(std::forward<Rest>(rest)...);
+}
+
+template <typename... Types>
+inline void set_last_error_string(const char* szFmt, Types&&... args) {
+    char szBuf[4096] = {0};
+    sprintf(szBuf, szFmt, std::forward<Types>(args)...);
+    _set_last_error_string(szBuf);
+}
 
 #endif  // IMPL_TORCH_ERROR_HPP_
