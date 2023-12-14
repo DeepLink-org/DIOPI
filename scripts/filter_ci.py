d0d372e728458eb3aa3aef3411eaf0f44603b0c3
wugeshui <wugeshui@sensetime.com>
2023-12-14 17:04:42 +0800

    test gen data

--- scripts/filter_ci.py
+++ scripts/filter_ci.py
@@ -20,7 +20,7 @@ def get_run_result(pr_number):
     response = requests.get(api_url, headers=headers)
     if response.status_code == 200:
         pr_files = response.json()
-        if "diopi_configs.py" in pr_files:
+        if "diopi_configs.py" in str(pr_files):
             run_result['GENDATA'] = True
         norunpaths = ["impl/camb_pytorch","impl/cuda"]
         for file in pr_files:
