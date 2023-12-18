c2244debe509a0e929820efe010477ca35641408
yangbofun <yangbofun@163.com>
2023-12-18 19:38:10 +0800

    lint

--- impl/ascend_npu/torch_npu/csrc/DIOPIAdapter.cpp
+++ impl/ascend_npu/torch_npu/csrc/DIOPIAdapter.cpp
@@ -878,7 +878,7 @@ public:
         size_t outputDataBuffArrLen = outputNum * sizeof(uintptr_t);
 
         size_t totalMemLen = inputTensorDescArrLen + inputDataBuffArrLen + outputTensorDescArrLen + outputDataBuffArrLen;
-        std::unique_ptr<char, void(*)(void*)> basePtrUnique((char*)malloc(totalMemLen), free);
+        std::unique_ptr<char, void (*)(void*)> basePtrUnique((char*)malloc(totalMemLen), free);
         char* basePtr = basePtrUnique.get();
         AT_ASSERT(basePtr != nullptr);
         const aclTensorDesc** aclTensorInputDescArr = reinterpret_cast<const aclTensorDesc**>(basePtr);
