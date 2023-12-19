be0bebb391b1dd49aa4045e4191db9b6ccdc7054
yangbo <yangbofun@163.com>
2023-12-19 17:42:23 +0800

    yb/fix mem leak (#746)
    * fix mem leak

--- impl/ascend_npu/torch_npu/csrc/DIOPIAdapter.cpp
+++ impl/ascend_npu/torch_npu/csrc/DIOPIAdapter.cpp
@@ -1795,8 +1795,8 @@ public:
         size_t outputDataBuffArrLen = outputNum * sizeof(uintptr_t);
 
         size_t totalMemLen = inputTensorDescArrLen + inputDataBuffArrLen + outputTensorDescArrLen + outputDataBuffArrLen;
-
-        char* basePtr = static_cast<char*>(malloc(totalMemLen));
+        std::unique_ptr<char, void (*)(void*)> basePtrUnique((char*)malloc(totalMemLen), free);
+        char* basePtr = basePtrUnique.get();
         AT_ASSERT(basePtr != nullptr);
         const aclTensorDesc** aclTensorInputDescArr = reinterpret_cast<const aclTensorDesc**>(basePtr);
         basePtr += inputTensorDescArrLen;
