f6ef35d5bb16480306a866e22d1d032bbc2e3a89
zhumingzhu <407190054@qq.com>
2023-12-19 10:54:28 +0000

    try

--- impl/ascend_npu/diopi_impl/token_attention.cpp
+++ impl/ascend_npu/diopi_impl/token_attention.cpp
@@ -9,15 +9,17 @@
 #include <c10/core/TensorImpl.h>
 // #include <torch/csrc/autograd/generated/variable_type.h>
 // #include <torch/csrc/autograd/generated/variable_type.h>
-#include <torch/torch.h>
 #include <acl/acl.h>
+#include <torch/torch.h>
 
 namespace OP_IMPL_NS {
 // at::Tensor toDevice(at::Tensor tensor) {
 //     int devId_ = 0;
 //     ::aclrtGetDevice(&devId_);
-//     auto options = at::TensorOptions(c10::Device(at::DeviceType::XLA, devId_)).dtype(tensor.dtype());
-//     return fromPreAllocated(tensor.data_ptr(), tensor.dim(), tensor.strides(), options);
+//     auto options = at::TensorOptions(c10::Device(at::DeviceType::XLA,
+//     devId_)).dtype(tensor.dtype()); return
+//     fromPreAllocated(tensor.data_ptr(), tensor.dim(), tensor.strides(),
+//     options);
 // }
 
 diopiError_t diopiTokenAttentionInference(
@@ -45,40 +47,55 @@ diopiError_t diopiTokenAttentionInference(
   for (int i = 0; i < batch; ++i) {
     std::cout << "i=" << i << std::endl;
     int curSeqLen = atBSeqLen.cpu()[i].item<int>();
-    std::cout << "curSeqLen=" << curSeqLen << std::endl;
     int curSeqStartLoc = atBStartLoc.cpu()[i].item<int>();
-    std::cout << "curSeqStartLoc=" << curSeqStartLoc << std::endl;
     at::Tensor kLoc = atBLoc.cpu()[i].index_select(
         0, at::arange(maxInputLen - curSeqLen, maxInputLen));
     // std::cout << "kLoc0=" << kLoc0 << std::endl;
     // std::cout << "atQ.device()=" << atQ.device() << std::endl;
     // at::Tensor kLoc = kLoc0.to(atQ.device());
-    std::cout << "kLoc=" << "kLoc value is too more." << std::endl;
+    std::cout << "kLoc="
+              << "kLoc value is too more." << std::endl;
     // at::Tensor key = atK.index({kLoc}).view({1, curSeqLen, head,
     // dim}).transpose(1, 2);
     torch::List<c10::optional<at::Tensor>> indicesAtList;
     auto kLoc1 = kLoc.to(at::ScalarType::Long);
     auto kLoc2 = kLoc1.to(atBSeqLen.device());
     auto kLoc3 = aten::toDevice(kLoc1);
-    std::cout << "atBSeqLen.device()=" << atBSeqLen.device() << ", kLoc2.device()=" << kLoc2.device() << ", kLoc1.device()=" << kLoc1.device() << ", kLoc3.device()=" << kLoc3.device() << std::endl;
+    std::cout << "atBSeqLen.device()=" << atBSeqLen.device()
+              << ", kLoc2.device()=" << kLoc2.device()
+              << ", kLoc1.device()=" << kLoc1.device()
+              << ", kLoc3.device()=" << kLoc3.device() << std::endl;
     // auto kLoc3 = kLoc1.to(at::Device(at::DeviceType::XLA, 0));
     // auto kLoc2 = kLoc1.to();
-    indicesAtList.push_back(kLoc3);
+    indicesAtList.push_back(kLoc1);
     for (auto item : indicesAtList) {
       std::cout << "item=";
       std::cout << item.get() << std::endl;
     }
-    std::cout << "atK.device()=" << atK.device() << ", kLoc1.device()=" << kLoc1.device() << std::endl;
+    std::cout << "atK.device()=" << atK.device()
+              << ", kLoc1.device()=" << kLoc1.device() << std::endl;
     auto cpu = atK.cpu();
-    at::Tensor key = acl_op::index(atK, indicesAtList);
-    std::cout << "key=" << key << std::endl;
+    // std::cout << "======atK=" << atK << std::endl;
+    // std::cout << "======kLoc3=" << kLoc3 << std::endl;
+    // at::Tensor key = acl_op::index(cpu, indicesAtList);
+    at::Tensor key = at::index(cpu, indicesAtList);
+    std::cout << "======key=" << "key is too more." << std::endl;
+    at::Tensor key1 = key.view({1, curSeqLen, head, dim}).transpose(1, 2);
     at::Tensor outLoc = at::arange(curSeqStartLoc, curSeqStartLoc + curSeqLen);
     std::cout << "outLoc=" << outLoc << std::endl;
+    std::cout << "Before transpose, key shape: " << key.sizes() << std::endl;
+
+    auto a = atQ.cpu().index({i});
+    std::cout << "a.device()=" << a.device() << std::endl;
+    a = aten::toDevice(a);
+    std::cout << "========a.device()=" << a.device() << std::endl;
+    auto b = aten::toDevice(key1.transpose(2, 3));
+    std::cout << "========b.device()=" << b.device() << std::endl;
     at::Tensor values =
-        (at::matmul(atQ.index({i}), key.transpose(2, 3)) / std::sqrt(dim))
+        (at::matmul(a, b) / std::sqrt(dim))
             .view({head, curSeqLen});
     std::cout << "values=" << values << std::endl;
-    atAttentionOut.index_put_({torch::indexing::Slice(), outLoc}, values);
+    atAttentionOut.index_put_({torch::indexing::Slice(), outLoc.cpu()}, values);
     std::cout << "finish i=" << i << std::endl;
   }
   impl::aten::unsetCurCtx();
