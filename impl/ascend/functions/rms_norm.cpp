ecff4e2f8fe6037d5e1ea9955302e5dec2dca681
zhangzefeng92 <zhang_zefeng@foxmail.com>
2023-12-19 17:40:23 +0800

    fix rms norm

--- impl/ascend/functions/rms_norm.cpp
+++ impl/ascend/functions/rms_norm.cpp
@@ -18,7 +18,14 @@ diopiError_t diopiRMSNorm(diopiContextHandle_t ctx, diopiTensorHandle_t out, dio
 diopiError_t diopiRMSNormBackward(diopiContextHandle_t ctx, diopiTensorHandle_t grad_input, diopiTensorHandle_t grad_weight, diopiTensorHandle_t grad_bias,
                                   diopiConstTensorHandle_t grad_output, diopiConstTensorHandle_t input, diopiConstTensorHandle_t weight,
                                   diopiConstTensorHandle_t bias, diopiConstTensorHandle_t inv_rms, diopiSize_t normalized_shape, double eps) {
-    AclOpRunner<4, 2>("RmsNorm", ctx).addInput(grad_output).addInput(input).addInput(inv_rms).addInput(weight).addOutput(grad_input).addOutput(grad_weight).run();
+    AclOpRunner<4, 2>("RmsNorm", ctx)
+        .addInput(grad_output)
+        .addInput(input)
+        .addInput(inv_rms)
+        .addInput(weight)
+        .addOutput(grad_input)
+        .addOutput(grad_weight)
+        .run();
     return diopiSuccess;
 }
 
