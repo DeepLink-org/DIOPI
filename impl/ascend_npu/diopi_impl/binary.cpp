917f710a5cb73392a1c55025acf23a88c69ab7ac
zhaoguochun1995 <zhaoguochun1995@sensetime.com>
2023-12-19 11:03:08 +0800

    enhance diopiAdd in op-plugin

--- impl/ascend_npu/diopi_impl/binary.cpp
+++ impl/ascend_npu/diopi_impl/binary.cpp
@@ -12,12 +12,19 @@ namespace OP_IMPL_NS {
 diopiError_t diopiAdd(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t other,
                       const diopiScalar_t* alpha) {
     BEGIN_CALL_ACL_OP(input, out, alpha, other);
+    if (!outAt.defined() || outAt.numel() <= 0) {
+        return diopiSuccess;
+    }
+
     acl_op::add_out(inputAt, otherAt, alphaAt, outAt);
     END_CALL_ACL_OP();
 }
 
 diopiError_t diopiAddInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t other, const diopiScalar_t* alpha) {
     BEGIN_CALL_ACL_OP(input, other, alpha);
+    if (!inputAt.defined() || inputAt.numel() <= 0) {
+        return diopiSuccess;
+    }
     acl_op::add_out(inputAt, otherAt, alphaAt, inputAt);
     END_CALL_ACL_OP();
 }
@@ -25,12 +32,18 @@ diopiError_t diopiAddInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, di
 diopiError_t diopiAddScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* other,
                             const diopiScalar_t* alpha) {
     BEGIN_CALL_ACL_OP(input, other, alpha, out);
+    if (!outAt.defined() || outAt.numel() <= 0) {
+        return diopiSuccess;
+    }
     acl_op::add_out(inputAt, at::scalar_to_tensor(otherAt).to(inputAt.dtype()), alphaAt, outAt);
     END_CALL_ACL_OP();
 }
 
 diopiError_t diopiAddInpScalar(diopiContextHandle_t ctx, diopiTensorHandle_t input, const diopiScalar_t* other, const diopiScalar_t* alpha) {
     BEGIN_CALL_ACL_OP(input, other, alpha);
+    if (!inputAt.defined() || inputAt.numel() <= 0) {
+        return diopiSuccess;
+    }
     acl_op::add_(inputAt, at::scalar_to_tensor(otherAt).to(inputAt.dtype()), alphaAt);
     END_CALL_ACL_OP();
 }
