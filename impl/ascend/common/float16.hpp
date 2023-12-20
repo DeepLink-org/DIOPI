48aa5a9c9cbed2744342fc5230adc0376b510de3
yangbofun <yangbofun@163.com>
2023-12-20 16:23:30 +0800

    add fp16

---
+++ impl/ascend/common/float16.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2023, DeepLink.
+ */
+
+#ifndef IMPL_ASCEND_COMMON_FLOAT16_HPP_
+#define IMPL_ASCEND_COMMON_FLOAT16_HPP_
+
+#include <half.hpp>
+
+#endif  // IMPL_ASCEND_COMMON_FLOAT16_HPP_
