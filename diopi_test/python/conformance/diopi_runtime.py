84f18c77cea13148ce1ddedbe54eba4e8446e02e
Peter Ye <44945378+yewentao256@users.noreply.github.com>
2023-12-22 13:49:37 +0800

    fix: numel error (#774)

--- diopi_test/python/conformance/diopi_runtime.py
+++ diopi_test/python/conformance/diopi_runtime.py
@@ -310,7 +310,7 @@ class Tensor(diopiTensor):
         return tr
 
     def numpy(self) -> np.ndarray:
-        if all(x == 0 for x in self.size().data) and self.numel == 0:
+        if all(x == 0 for x in self.size().data) and self.numel() == 0:
             # cases when shape all 0, but not include the scalar tensor
             return np.empty(self.size().data, to_numpy_dtype(self.get_dtype()))
         data = np.empty((1,), to_numpy_dtype(self.get_dtype()))
