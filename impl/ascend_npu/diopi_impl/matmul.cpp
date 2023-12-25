a5a7b8a69fc1d750124a25a77a95fa6ec343917d
zhumingzhu <407190054@qq.com>
2023-12-25 02:53:40 +0000

    format

--- impl/ascend_npu/diopi_impl/matmul.cpp
+++ impl/ascend_npu/diopi_impl/matmul.cpp
@@ -11,7 +11,8 @@ namespace OP_IMPL_NS {
 
 diopiError_t diopiMatmul(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t other) {
     BEGIN_CALL_ACL_OP(input, out, other);
-    std::cout << "input.size()=" << inputAt.sizes() << ", other.size()=" << otherAt.sizes() << ", out.size()=" << outAt.sizes() << ", input.device()=" << inputAt.device() << ", other.device()=" << otherAt.device() << ", out.device()=" << outAt.device() << std::endl;
+    std::cout << "input.size()=" << inputAt.sizes() << ", other.size()=" << otherAt.sizes() << ", out.size()=" << outAt.sizes()
+              << ", input.device()=" << inputAt.device() << ", other.device()=" << otherAt.device() << ", out.device()=" << outAt.device() << std::endl;
     acl_op::matmul_out(inputAt, otherAt, outAt);
     END_CALL_ACL_OP();
 }
