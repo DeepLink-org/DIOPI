1f453c29f4994f36a0f2f6b0707caffaa121c262
wangqing <2917021186@qq.com>
2023-12-25 08:37:19 +0000

    wq/refactor variable names

--- impl/ascend_npu/diopi_impl/col2im.cpp
+++ impl/ascend_npu/diopi_impl/col2im.cpp
@@ -9,10 +9,10 @@
 
 namespace OP_IMPL_NS {
 
-diopiError_t diopiCol2Im(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t output_size, diopiSize_t kernel_size,
+diopiError_t diopiCol2Im(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t outputSize, diopiSize_t kernelSize,
                          diopiSize_t dilation, diopiSize_t padding, diopiSize_t stride) {
-    BEGIN_CALL_ACL_OP(input, output_size, kernel_size, dilation, padding, stride, out);
-    acl_op::col2im_out(inputAt, output_sizeAt, kernel_sizeAt, dilationAt, paddingAt, strideAt, outAt);
+    BEGIN_CALL_ACL_OP(input, outputSize, kernelSize, dilation, padding, stride, out);
+    acl_op::col2im_out(inputAt, outputSizeAt, kernelSizeAt, dilationAt, paddingAt, strideAt, outAt);
     END_CALL_ACL_OP();
 }
 
