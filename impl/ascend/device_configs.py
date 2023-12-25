27f82ab4ab5bf981a72b1154e8ea0c22c8839998
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-12-25 16:57:12 +0800

    skip cat double test case

--- impl/ascend/device_configs.py
+++ impl/ascend/device_configs.py
@@ -29,6 +29,18 @@ device_configs = {
         ),
     ),
 
+    'cat_diff_size': dict(
+        name=['cat'],
+        tensor_para=dict(
+            args=[
+                {
+                    "ins": ['tensors'],
+                    "dtype": [Skip(np.float64),],
+                },
+            ],
+        ),
+    ),
+
     # temp for 910B
     'topk_nonzero': dict(
         name=['topk'],
