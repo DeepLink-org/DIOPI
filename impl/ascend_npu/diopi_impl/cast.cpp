e0dcc77bbad58e022942bb5068f06656589c6bb5
zhaoguochun1995 <zhaoguochun1995@163.com>
2023-12-26 21:03:44 +0800

    fallback cast

--- impl/ascend_npu/diopi_impl/cast.cpp
+++ impl/ascend_npu/diopi_impl/cast.cpp
@@ -14,7 +14,9 @@ diopiError_t diopiCastDtype(diopiContextHandle_t ctx, diopiTensorHandle_t out, d
     if (out == nullptr || input == nullptr || !inputAt.defined() || !outAt.defined() || inputAt.numel() <= 0 || outAt.numel() <= 0) {
         return diopiSuccess;
     }
-    outAt.copy_(inputAt);
+    // TODO(zhaoguochun): This must be repaired
+    // outAt.copy_(inputAt);
+    outAt.copy_(inputAt.cpu().to(outAt.scalar_type()));
     END_CALL_ACL_OP();
 }
 
