db7413bbc1c1ed5186a187af151f087a2175c9bb
unknown <liwenjian.vendor@sensetime.com>
2023-12-28 17:22:07 +0800

    fix

--- scripts/increment_coverage.sh
+++ scripts/increment_coverage.sh
@@ -14,11 +14,23 @@ remote_count=$(git remote | wc -l)
 if [ "$remote_count" -eq 1 ]; then echo "Not from dev repository" && exit 0 ;fi
 gcovr --csv --gcov-ignore-errors=no_working_dir_found > coverage/coverage.csv
 sed -i '1d' coverage/coverage.csv
-newcommit=$(git rev-parse HEAD)
-oldcommit=$(git merge-base ${newcommit} main)
-if [ -z $oldcommit ]; then echo "is not Pull request" && exit 0; fi
-git diff $oldcommit $newcommit --name-only  >coverage/gitdiff.txt 2>/dev/null || echo "error can be ignored"
 
+commit_hash=$(git rev-parse HEAD)
+parent_count=$(git log --pretty=%P -n 1 $commit_hash | wc -w)
+if [ $parent_count -eq 1 ]; then echo "is not Pull request" && exit 0; fi
+rsync -a ${ROOT_DIR}/../source coverage/ || (echo "cannot find the source dir" && exit 1)
+cd coverage/source && git log main --pretty=format:"%H" -n 100 >../gitdiff_main.txt
+git reset --hard HEAD~1 &&newcommit=$(git rev-parse HEAD) &&git log  --pretty=format:"%H" -n 100 >../gitdiff_merge.txt
+while read -r commit; do
+    if grep -Fxq "$commit" ../gitdiff_main.txt; then
+        echo $commit
+        oldcommit=$commit
+        break
+    fi
+done < ../gitdiff_merge.txt
+if [ -z $oldcommit ]; then echo "The Pull request opening time is too long" && exit 0; fi
+git diff $oldcommit $newcommit --name-only  > gitdiff.txt 2>/dev/null || echo "error can be ignored"
+cd $ROOT_DIR
 cat coverage/gitdiff.txt |egrep '\.(cpp|hpp|h)$'|grep "$include/" >coverage/gitdiff_screen.txt || true
 if [ ! -s coverage/gitdiff_screen.txt ]; then echo "No C/C++ in incremental code" && exit 0;fi
 rm -rf coverage/gitdiff.txt
