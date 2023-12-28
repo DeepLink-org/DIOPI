8e20b0587246302b6925602f9244160e551e175f
unknown <liwenjian.vendor@sensetime.com>
2023-12-28 17:45:01 +0800

    fix

--- scripts/increment_coverage.sh
+++ scripts/increment_coverage.sh
@@ -12,24 +12,22 @@ require_coverage=$1
 
 remote_count=$(git remote | wc -l)
 if [ "$remote_count" -eq 1 ]; then echo "Not from dev repository" && exit 0 ;fi
-gcovr --csv --gcov-ignore-errors=no_working_dir_found > coverage/coverage.csv
-sed -i '1d' coverage/coverage.csv
 
 commit_hash=$(git rev-parse HEAD)
 parent_count=$(git log --pretty=%P -n 1 $commit_hash | wc -w)
 if [ $parent_count -eq 1 ]; then echo "is not Pull request" && exit 0; fi
 rsync -a ${ROOT_DIR}/../source coverage/ || (echo "cannot find the source dir" && exit 1)
-cd coverage/source && git log main --pretty=format:"%H" -n 100 >../gitdiff_main.txt
-git reset --hard HEAD~1 &&newcommit=$(git rev-parse HEAD) &&git log  --pretty=format:"%H" -n 100 >../gitdiff_merge.txt
+cd coverage/source && git log main --pretty=format:"%H" -n 100 >../commit_main.txt
+git reset --hard HEAD~1 &&newcommit=$(git rev-parse HEAD) &&git log  --pretty=format:"%H" -n 100 >../commit_merge.txt
 while read -r commit; do
-    if grep -Fxq "$commit" ../gitdiff_main.txt; then
+    if grep -Fxq "$commit" ../commit_main.txt; then
         echo $commit
         oldcommit=$commit
         break
     fi
-done < ../gitdiff_merge.txt
+done < ../commit_merge.txt
 if [ -z $oldcommit ]; then echo "The Pull request opening time is too long" && exit 0; fi
-git diff $oldcommit $newcommit --name-only  > gitdiff.txt 2>/dev/null || echo "error can be ignored"
+git diff $oldcommit $newcommit --name-only  > ../gitdiff.txt 2>/dev/null || echo "error can be ignored"
 cd $ROOT_DIR
 cat coverage/gitdiff.txt |egrep '\.(cpp|hpp|h)$'|grep "$include/" >coverage/gitdiff_screen.txt || true
 if [ ! -s coverage/gitdiff_screen.txt ]; then echo "No C/C++ in incremental code" && exit 0;fi
@@ -41,6 +39,8 @@ while IFS= read -r line; do
 done < "coverage/gitdiff_screen.txt"
 
 echo "export IS_cover=True" >coverage/IS_cover.txt
+gcovr --csv --gcov-ignore-errors=no_working_dir_found > coverage/coverage.csv
+sed -i '1d' coverage/coverage.csv
 mkdir coverage/html
 gcovr -r . --html --html-details --gcov-ignore-errors=no_working_dir_found -o coverage/html/index.html
 python scripts/increment_coverage.py $ROOT_DIR/coverage/ $require_coverage
