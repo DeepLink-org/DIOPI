89ad0fdb914ec2e13618a81f8da1f0b33d451910
Bonbon-Tang <928081866@qq.com>
2024-01-04 16:29:59 +0800

    change add

--- impl/camb/functions/add.cpp
+++ impl/camb/functions/add.cpp
@@ -6,7 +6,6 @@
 
 #include "../cnnl_helper.hpp"
 #include "../common/common.hpp"
-
 namespace impl {
 namespace camb {
 
@@ -15,15 +14,20 @@ diopiError_t diopiAdd(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
     DiopiTensor inputTensor(input);
     DiopiTensor otherTensor(other);
     DiopiTensor outputTensor(out);
-    bool inputContiguous = inputTensor.isContiguous();
-    bool otherContiguous = otherTensor.isContiguous();
-    if (inputContiguous && (!otherContiguous)) {
-        DIOPI_CALL(contiguous(ctx, otherTensor, diopiMemoryFormat_t::Contiguous));
-    } else if ((!inputContiguous) && otherContiguous) {
-        if (inputTensor.dim() == 4) {
-            DIOPI_CALL(contiguous(ctx, otherTensor, diopiMemoryFormat_t::ChannelsLast));
-        } else {
-            DIOPI_CALL(contiguous(ctx, otherTensor, diopiMemoryFormat_t::ChannelsLast1d));
+    bool isInputContiguous = inputTensor.isContiguous();
+    bool isOtherContiguous = otherTensor.isContiguous();
+    if (isInputContiguous && (!isOtherContiguous))
+    {
+        DIOPI_CALL(contiguous(ctx,otherTensor,diopiMemoryFormat_t::Contiguous));
+    }else if((!isInputContiguous) && isOtherContiguous){
+        if(inputTensor.dim() == 5){
+            DIOPI_CALL(contiguous(ctx,otherTensor,diopiMemoryFormat_t::ChannelsLast3d));
+        }else if(inputTensor.dim() == 4){
+            DIOPI_CALL(contiguous(ctx,otherTensor,diopiMemoryFormat_t::ChannelsLast));
+        }else if(inputTensor.dim() == 3){
+            DIOPI_CALL(contiguous(ctx,otherTensor,diopiMemoryFormat_t::ChannelsLast1d));
+        }else if(inputTensor.dim() == 2){
+            DIOPI_CALL(contiguous(ctx,inputTensor,diopiMemoryFormat_t::Contiguous));
         }
     }
     DIOPI_CALL(cnnlOpTensor(
@@ -34,18 +38,24 @@ diopiError_t diopiAdd(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
 diopiError_t diopiAddInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t other, const diopiScalar_t* alpha) {
     DiopiTensor inputTensor(input);
     DiopiTensor otherTensor(other);
-    DiopiTensor outputTensor(input);
     bool inputContiguous = inputTensor.isContiguous();
     bool otherContiguous = otherTensor.isContiguous();
-    if (inputContiguous && (!otherContiguous)) {
-        DIOPI_CALL(contiguous(ctx, otherTensor, diopiMemoryFormat_t::Contiguous));
-    } else if ((!inputContiguous) && otherContiguous) {
-        if (inputTensor.dim() == 4) {
-            DIOPI_CALL(contiguous(ctx, otherTensor, diopiMemoryFormat_t::ChannelsLast));
-        } else {
-            DIOPI_CALL(contiguous(ctx, otherTensor, diopiMemoryFormat_t::ChannelsLast1d));
+    if (isInputContiguous && (!isOtherContiguous))
+    {
+        DIOPI_CALL(contiguous(ctx,otherTensor,diopiMemoryFormat_t::Contiguous));
+    }else if((!isInputContiguous) && isOtherContiguous){
+        if(inputTensor.dim() == 5){
+            DIOPI_CALL(contiguous(ctx,otherTensor,diopiMemoryFormat_t::ChannelsLast3d));
+        }else if(inputTensor.dim() == 4){
+            DIOPI_CALL(contiguous(ctx,otherTensor,diopiMemoryFormat_t::ChannelsLast));
+        }else if(inputTensor.dim() == 3){
+            DIOPI_CALL(contiguous(ctx,otherTensor,diopiMemoryFormat_t::ChannelsLast1d));
+        }else if(inputTensor.dim() == 2){
+            DIOPI_CALL(contiguous(ctx,inputTensor,diopiMemoryFormat_t::Contiguous));
         }
     }
+    DiopiTensor outputTensor(input);
+
     DIOPI_CALL(cnnlOpTensor(
         ctx, inputTensor, otherTensor, outputTensor, CNNL_OP_TENSOR_ADD, 1.0, DiopiDataType::isFloatPoint(alpha->stype) ? alpha->fval : alpha->ival));
     return diopiSuccess;
