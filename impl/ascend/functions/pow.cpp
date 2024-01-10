346659a8fede5f5cf312c789a0133309f4b0443c
zhangqiu <zhangqiu1994@outlook.com>
2024-01-10 17:38:15 +0800

    fix lint

--- impl/ascend/functions/pow.cpp
+++ impl/ascend/functions/pow.cpp
@@ -29,11 +29,9 @@ diopiError_t diopiPow(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
     diopiTensorHandle_t exponentTensor;
     AscendTensor inputAt(input);
 
-    if (isFloatingType(inputAt.dtype()) && isFloatingType(exponent->stype)) {
+    if ((isFloatingType(inputAt.dtype()) && isFloatingType(exponent->stype)) ||
+        ((isIntegralTypeWithBool(inputAt.dtype()) && isIntegralTypeWithBool(inputAt.dtype())))) {
         makeTensorFromScalar(ctx, exponent, &exponentTensor, inputAt.dtype(), diopi_device);
-    } else if (isIntegralTypeWithBool(inputAt.dtype()) && isIntegralTypeWithBool(inputAt.dtype())) {
-        makeTensorFromScalar(ctx, exponent, &exponentTensor, inputAt.dtype(), diopi_device);
-
     } else if (isIntegralTypeWithBool(inputAt.dtype()) && isFloatingType(exponent->stype)) {
         makeTensorFromScalar(ctx, exponent, &exponentTensor, diopi_dtype_float32, diopi_device);
     } else if (isFloatingType(inputAt.dtype()) && isIntegralTypeWithBool(inputAt.dtype())) {
@@ -49,11 +47,8 @@ diopiError_t diopiPowScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, c
     diopiTensorHandle_t inputTensor;
     AscendTensor expAt(exponent);
 
-    if (isFloatingType(input->stype) && isFloatingType(expAt.dtype())) {
+    if ((isFloatingType(input->stype) && isFloatingType(expAt.dtype())) || ((isIntegralTypeWithBool(input->stype) && isIntegralTypeWithBool(expAt.dtype())))) {
         makeTensorFromScalar(ctx, input, &inputTensor, expAt.dtype(), diopi_device);
-    } else if (isIntegralTypeWithBool(input->stype) && isIntegralTypeWithBool(expAt.dtype())) {
-        makeTensorFromScalar(ctx, input, &inputTensor, expAt.dtype(), diopi_device);
-
     } else if (isIntegralTypeWithBool(input->stype) && isFloatingType(expAt.dtype())) {
         makeTensorFromScalar(ctx, input, &inputTensor, expAt.dtype(), diopi_device);
     } else if (isFloatingType(input->stype) && isIntegralTypeWithBool(expAt.dtype())) {
