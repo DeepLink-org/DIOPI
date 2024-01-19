10d0740b48c4af7d537166e63e5b482c11ec5b2f
liangshinan <liangshinan@sensetime.com>
2024-01-19 06:33:55 +0000

    debug

--- impl/camb/device_configs.py
+++ impl/camb/device_configs.py
@@ -574,19 +574,6 @@ device_configs = {
         ),
     ),
 
-    'index_int': dict(
-        name=["index"],
-        tensor_para=dict(
-            args=[
-                {
-                    "ins": ['input'],
-                    "dtype": [Skip(np.int64), Skip(np.int32), Skip(np.int16),
-                              Skip(np.int8), Skip(np.uint8), Skip(np.bool_)],
-                },
-            ],
-        ),
-    ),
-
     'embedding': dict(
         name=["embedding"],
         tensor_para=dict(
@@ -630,7 +617,8 @@ device_configs = {
             args=[
                 {
                     "ins": ['input'],
-                    "dtype": [Skip(np.int64), Skip(np.bool_)],
+                    "dtype": [Skip(np.int64), Skip(np.int32), Skip(np.int16),
+                              Skip(np.int8), Skip(np.uint8), Skip(np.bool_)],
                 },
             ],
         ),
