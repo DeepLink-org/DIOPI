c27b629241ca7eef52a4fa3bcfd664a759116345
zhangqiu <zhangqiu1994@outlook.com>
2024-01-24 17:16:37 +0800

    fix lint

--- diopi_test/python/conformance/diopi_functions.py
+++ diopi_test/python/conformance/diopi_functions.py
@@ -5152,7 +5152,7 @@ def rms_norm(input, normalized_shape, weight, bias, eps):
     size = list(input.size().data)
     out = Tensor(size, input.get_dtype())
     normalized_dim = list(range(-(len(normalized_shape)), 0))
-    inv_rms_size = size[:len(size)-len(normalized_dim)]
+    inv_rms_size = size[:len(size) - len(normalized_dim)]
     inv_rms = Tensor(inv_rms_size, input.get_dtype())
     normalized_shape = Sizes(list(normalized_shape))
     ret = func(
