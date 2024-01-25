521679a0f0130ab0cb4dd8a258f546f13b7fc4e1
yewentao <zhyanwentao@126.com>
2024-01-25 17:01:07 +0800

    have some positive max_norm

--- diopi_test/python/configs/diopi_configs.py
+++ diopi_test/python/configs/diopi_configs.py
@@ -3862,7 +3862,7 @@ diopi_configs = {
             padding_idx=[None, None, 92, -20, 0, -15, 19, 2, 0],
             # Do not set max_norm to negative number
             # causing undefined precision error
-            max_norm=[None, 1.0, None, None, None, 2, None, 9, None],
+            max_norm=[None, 1.0, None, None, 2, 2, None, 9, 0.5],
             norm_type=[2.0, 0, 1, 2, 0.5, 1.2, float('inf'), -2, -0.5],
             scale_grad_by_freq=[False, True, False, True, False, True, False, True, True],
             # sparse=[True, False, True, False, True, False, True, False, False],
