90b0cd264418765c491348bf7fa41cef4ff0df9b
POI-WX <wangxing2@sensetime.com>
2024-02-01 23:03:34 +0800

    fix some bug

--- impl/ascend/functions/addcmul.cpp
+++ impl/ascend/functions/addcmul.cpp
@@ -13,7 +13,7 @@ diopiError_t diopiAddcmul(diopiContextHandle_t ctx, diopiTensorHandle_t out, dio
                           diopiConstTensorHandle_t tensor2, const diopiScalar_t* value) {
     diopiTensorHandle_t trOther = nullptr;
     diopiDtype_t dtype;
-    diopiGetTensorDtype(out, &dtype);
+    diopiGetTensorDtype(input, &dtype);
     makeTensorFromScalar(ctx, value, &trOther, dtype, diopiDevice_t::diopi_device);
     AclOpRunner<4, 1>("Addcmul", ctx).addInput(input).addInput(tensor1).addInput(tensor2).addInput(trOther).addOutput(out).run();
     return diopiSuccess;
