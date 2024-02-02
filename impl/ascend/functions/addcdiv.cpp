8f20f36f5010d2cc9098e54a200c73613680ea6a
POI-WX <wangxing2@sensetime.com>
2024-02-02 13:29:01 +0800

    fix bug

--- impl/ascend/functions/addcdiv.cpp
+++ impl/ascend/functions/addcdiv.cpp
@@ -20,11 +20,11 @@ diopiError_t diopiAddcdiv(diopiContextHandle_t ctx, diopiTensorHandle_t out, dio
     broadcast(ctx, inAt, inAt, size);
     broadcast(ctx, t1, t1, size);
     broadcast(ctx, t2, t2, size);
-    diopiTensorHandle_t trOther = nullptr;
+    diopiTensorHandle_t valueTensor = nullptr;
     diopiDtype_t dtype;
     diopiGetTensorDtype(input, &dtype);
-    makeTensorFromScalar(ctx, value, &trOther, dtype, diopiDevice_t::diopi_device);
-    AclOpRunner<4, 1>("Addcdiv", ctx).addInput(inAt).addInput(t1).addInput(t2).addInput(trOther).addOutput(out).run();
+    makeTensorFromScalar(ctx, value, &valueTensor, dtype, diopiDevice_t::diopi_device);
+    AclOpRunner<4, 1>("Addcdiv", ctx).addInput(inAt).addInput(t1).addInput(t2).addInput(valueTensor).addOutput(out).run();
     return diopiSuccess;
 }
 
