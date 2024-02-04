00e5b2a8561ebdb5ca9a6d6eb1c89898a6ef2ebb
yangbofun <yangbofun@163.com>
2024-02-04 21:57:51 +0800

    fix

--- impl/ascend/functions/arange.cpp
+++ impl/ascend/functions/arange.cpp
@@ -10,43 +10,46 @@ namespace impl {
 namespace ascend {
 
 diopiError_t diopiArange(diopiContextHandle_t ctx, diopiTensorHandle_t out, const diopiScalar_t* start, const diopiScalar_t* end, const diopiScalar_t* step) {
-    diopiScalar_t* startTmp = const_cast<diopiScalar_t*>(start);
-    diopiScalar_t* endTmp = const_cast<diopiScalar_t*>(end);
-    diopiScalar_t* stepTmp = const_cast<diopiScalar_t*>(step);
+    diopiScalar_t startTmp;
+    diopiScalar_t endTmp;
+    diopiScalar_t stepTmp;
     diopiDtype_t outDtype;
     diopiGetTensorDtype(out, &outDtype);
     // convert to integer
     if (isIntegralTypeWithBool(outDtype)) {
         if (!isIntegralTypeWithBool(start->stype)) {
-            *startTmp = constructDiopiScalarT(outDtype, start->fval);
+            startTmp = constructDiopiScalarT(outDtype, start->fval);
         } else {
+            startTmp.ival = start->ival;
             startTmp->stype = outDtype;
         }
         if (!isIntegralTypeWithBool(end->stype)) {
             *endTmp = constructDiopiScalarT(outDtype, end->fval);
         } else {
+            endTmp.ival = end->ival;
             endTmp->stype = outDtype;
         }
         if (!isIntegralTypeWithBool(step->stype)) {
             *stepTmp = constructDiopiScalarT(outDtype, step->fval);
         } else {
+            stepTmp.ival = step->ival;
             stepTmp->stype = outDtype;
         }
     } else {
         if (isIntegralTypeWithBool(start->stype)) {
             *startTmp = constructDiopiScalarT(outDtype, start->ival);
         } else {
-            startTmp->stype = outDtype;
+            startTmp.ival = start->ival startTmp->stype = outDtype;
         }
         if (isIntegralTypeWithBool(end->stype)) {
             *endTmp = constructDiopiScalarT(outDtype, end->ival);
         } else {
-            endTmp->stype = outDtype;
+            endTmp.ival = end->ival endTmp->stype = outDtype;
         }
         if (isIntegralTypeWithBool(step->stype)) {
             *stepTmp = constructDiopiScalarT(outDtype, step->ival);
         } else {
-            stepTmp->stype = outDtype;
+            stepTmp.ival = step->ival stepTmp->stype = outDtype;
         }
     }
     AclOpRunner<3, 1>("Range", ctx).addConstInput(*startTmp).addConstInput(*endTmp).addConstInput(*stepTmp).addOutput(out).run();
