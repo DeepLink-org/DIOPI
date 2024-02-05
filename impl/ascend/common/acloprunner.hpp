63b30428232756c2b912bd740010674112275e27
jingguo-st <fujingguo@sensetime.com>
2024-02-05 23:36:11 +0800

    fix fill

--- impl/ascend/common/acloprunner.hpp
+++ impl/ascend/common/acloprunner.hpp
@@ -397,6 +397,16 @@ public:
         return *this;
     }
 
+    AclOpRunner& addInput(const std::vector<int64_t>& vec) {
+        std::vector<int64_t> dims(1, vec.size());
+        aclTensorDesc* desc = aclCreateTensorDesc(ACL_INT64, 1, dims.data(), ACL_FORMAT_ND);
+        aclDataBuffer* buffer = aclCreateDataBuffer((void*)(vec.data()), sizeof(int64_t) * vec.size());
+        inputBuffers_[inputIndex_] = buffer;
+        inputDescs_[inputIndex_] = desc;
+        inputIndex_++;
+        return *this;
+    }
+
     AclOpRunner& addInput(const AscendTensor& at) {
         if (at.isContiguous()) {
             return addInput(at, at.getAclDataFormat());
--- impl/ascend/functions/fill.cpp
+++ impl/ascend/functions/fill.cpp
@@ -15,15 +15,16 @@ namespace ascend {
 
 diopiError_t diopiFill(diopiContextHandle_t ctx, diopiTensorHandle_t input, const diopiScalar_t* value) {
     int64_t numel = 0;
+    AscendTensor inputTr(input);
     diopiGetTensorNumel(input, &numel);
     if (numel <= 0) {
         return diopiSuccess;
     }
-
     float val = getValue<float>(value);
 
     AscendTensor inputAt(input);
-    AclOpRunner<1, 1>("Fill", ctx).addInput(input).setAttr<float>("value", val).addOutput(input).run();
+    std::vector<int64_t> shape = inputTr.shape();
+    AclOpRunner<1, 1>("Fill", ctx).addInput(shape).addConstInput(*value).addOutput(input).run();
     return diopiSuccess;
 }
 
