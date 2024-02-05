2fef361f3c0d5d488c1d1fcc80c39d589b60e9df
wangxing <131418410+POI-WX@users.noreply.github.com>
2024-02-05 23:38:21 +0800

    [Ascend] Wx/fix redundant dtype cast of pow op (#933)
    fix redundant dtype cast of pow op

--- impl/ascend/functions/pow.cpp
+++ impl/ascend/functions/pow.cpp
@@ -10,11 +10,8 @@ namespace impl {
 namespace ascend {
 
 diopiError_t diopiPowTensor(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t exponent) {
-    AscendTensor inputAt(input), expAt(exponent);
-    auto dtype = promoteTypes(inputAt.dtype(), expAt.dtype());
-    castTensor(ctx, inputAt, dtype);
-    castTensor(ctx, expAt, dtype);
-    AclOpRunner<2, 1>("Pow", ctx).addInput(inputAt).addInput(expAt).addOutput(out).run();
+    AscendTensor outTensor(out);
+    AclOpRunner<2, 1>("Pow", ctx).addInput(input, outTensor.dtype()).addInput(exponent, outTensor.dtype()).addOutput(out).run();
     return diopiSuccess;
 }
 
@@ -23,17 +20,21 @@ diopiError_t diopiPowInpTensor(diopiContextHandle_t ctx, diopiTensorHandle_t inp
 }
 
 diopiError_t diopiPow(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* exponent) {
+    AscendTensor outTensor(out);
     diopiTensorHandle_t exponentTensor;
-    makeTensorFromScalar(ctx, exponent, &exponentTensor, diopi_device);
-    return diopiPowTensor(ctx, out, input, exponentTensor);
+    makeTensorFromScalar(ctx, exponent, &exponentTensor, outTensor.dtype(), diopi_device);
+    AclOpRunner<2, 1>("Pow", ctx).addInput(input, outTensor.dtype()).addInput(exponentTensor).addOutput(out).run();
+    return diopiSuccess;
 }
 
 diopiError_t diopiPowInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, const diopiScalar_t* exponent) { return diopiPow(ctx, input, input, exponent); }
 
 diopiError_t diopiPowScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, const diopiScalar_t* input, diopiConstTensorHandle_t exponent) {
+    AscendTensor outTensor(out);
     diopiTensorHandle_t inputTensor;
-    makeTensorFromScalar(ctx, input, &inputTensor, diopi_device);
-    return diopiPowTensor(ctx, out, inputTensor, exponent);
+    makeTensorFromScalar(ctx, input, &inputTensor, outTensor.dtype(), diopi_device);
+    AclOpRunner<2, 1>("Pow", ctx).addInput(inputTensor).addInput(exponent, outTensor.dtype()).addOutput(out).run();
+    return diopiSuccess;
 }
 
 }  // namespace ascend
