52562b9cb14e9f02c45696cafffdffdc34d0722a
jingguo-st <fujingguo@sensetime.com>
2024-02-05 22:06:44 +0800

    fix fill

--- impl/ascend/functions/fill.cpp
+++ impl/ascend/functions/fill.cpp
@@ -20,22 +20,11 @@ diopiError_t diopiFill(diopiContextHandle_t ctx, diopiTensorHandle_t input, cons
         return diopiSuccess;
     }
 
-    bool divByZero = true;
     float val = getValue<float>(value);
-    if (val == INFINITY) {
-        val = 1;
-    } else if (val == -INFINITY) {
-        val = -1;
-    } else if (std::isnan(val)) {
-        val = 0;
-    } else {
-        divByZero = false;
-    }
 
     AscendTensor inputAt(input);
     AclOpRunner<1, 1>("Fills", ctx).addInput(input).setAttr<float>("value", val).addOutput(input).run();
     diopiScalar_t zeroValueScalar = constructDiopiScalarT(inputAt.dtype(), 0.0);
-    if (divByZero) diopiDivInpScalar(ctx, input, &zeroValueScalar, diopiRoundMode_t::RoundModeNone);
 
     return diopiSuccess;
 }
