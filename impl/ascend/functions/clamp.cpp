529dc31cfb588b37bf99a644aa094d8b36009935
zhangqiu <zhangqiu1994@outlook.com>
2024-02-05 21:17:00 +0800

    fix clamp

--- impl/ascend/functions/clamp.cpp
+++ impl/ascend/functions/clamp.cpp
@@ -16,37 +16,6 @@ namespace impl {
 namespace ascend {
 
 // to get the limit value according to diopiDtype
-std::pair<double, double> getMinMaxFromDtype(diopiDtype_t tensorDtype) {
-    switch (tensorDtype) {
-        case diopi_dtype_int8:
-            return std::make_pair(std::numeric_limits<int8_t>::lowest(), std::numeric_limits<int8_t>::max());
-        case diopi_dtype_uint8:
-            return std::make_pair(std::numeric_limits<uint8_t>::lowest(), std::numeric_limits<uint8_t>::max());
-        case diopi_dtype_int16:
-            return std::make_pair(std::numeric_limits<int16_t>::lowest(), std::numeric_limits<int16_t>::max());
-        case diopi_dtype_uint16:
-            return std::make_pair(std::numeric_limits<uint16_t>::lowest(), std::numeric_limits<uint16_t>::max());
-        case diopi_dtype_int32:
-            return std::make_pair(std::numeric_limits<int32_t>::lowest(), std::numeric_limits<int32_t>::max());
-        case diopi_dtype_uint32:
-            return std::make_pair(std::numeric_limits<uint32_t>::lowest(), std::numeric_limits<uint32_t>::max());
-        case diopi_dtype_int64:
-            return std::make_pair(std::numeric_limits<int64_t>::lowest(), std::numeric_limits<int64_t>::max());
-        case diopi_dtype_uint64:
-            return std::make_pair(std::numeric_limits<uint64_t>::lowest(), std::numeric_limits<uint64_t>::max());
-        case diopi_dtype_float16:
-            return std::make_pair(std::numeric_limits<half_float::half>::lowest(), std::numeric_limits<half_float::half>::max());
-        case diopi_dtype_float32:
-            return std::make_pair(std::numeric_limits<float>::lowest(), std::numeric_limits<float>::max());
-        case diopi_dtype_float64:
-            return std::make_pair(std::numeric_limits<double>::lowest(), std::numeric_limits<double>::max());
-        case diopi_dtype_bool:
-            return std::make_pair(std::numeric_limits<bool>::lowest(), std::numeric_limits<bool>::max());
-        default:
-            break;
-    }
-}
-
 std::pair<double, double> getFloatMinMaxFromDtype(diopiDtype_t tensorDtype) {
     switch (tensorDtype) {
         case diopi_dtype_float16:
@@ -89,6 +58,7 @@ diopiError_t diopiClamp(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopi
                         diopiConstTensorHandle_t max) {
     diopiDtype_t highDtype, inputDtype, minDtype, maxDtype;
     diopiTensorHandle_t minTmp, maxTmp, boolOut;
+    diopiScalar_t minScalar, maxScalar;
 
     AscendTensor inputAt(input);
     const std::vector<int64_t>& sizes = inputAt.shape();
@@ -100,8 +70,14 @@ diopiError_t diopiClamp(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopi
         broadcast(ctx, minTmp, min, sizes);
     } else {
         makeTensorLike(ctx, &minTmp, input, inputDtype);
-        double minVal = getMinMaxFromDtype(inputDtype).first;
-        fillTensor(ctx, minTmp, minVal);
+        if (isFloatingType(inputDtype)) {
+            double minVal = getFloatMinMaxFromDtype(inputDtype).first;
+            minScalar = constructDiopiScalarT(inputDtype, minVal);
+        } else {
+            int64_t minVal = getIntMinMaxFromDtype(inputDtype).first;
+            minScalar = constructDiopiScalarT(inputDtype, minVal);
+        }
+        diopiFill(ctx, minTmp, &minScalar);
         minDtype = inputDtype;
     }
 
@@ -111,8 +87,14 @@ diopiError_t diopiClamp(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopi
         broadcast(ctx, maxTmp, max, sizes);
     } else {
         makeTensorLike(ctx, &maxTmp, input, inputDtype);
-        double maxVal = getMinMaxFromDtype(inputDtype).second;
-        fillTensor(ctx, maxTmp, maxVal);
+        if (isFloatingType(inputDtype)) {
+            double maxVal = getFloatMinMaxFromDtype(inputDtype).second;
+            maxScalar = constructDiopiScalarT(inputDtype, maxVal);
+        } else {
+            int64_t maxVal = getIntMinMaxFromDtype(inputDtype).second;
+            maxScalar = constructDiopiScalarT(inputDtype, maxVal);
+        }
+        diopiFill(ctx, maxTmp, &maxScalar);
         maxDtype = inputDtype;
     }
 
