060a0a978429823d8c7f4ac3d3f4eda0cb43970e
POI-WX <wangxing2@sensetime.com>
2024-02-06 13:26:36 +0800

    fix addcdiv op

--- impl/ascend/functions/addcmul.cpp
+++ impl/ascend/functions/addcmul.cpp
@@ -11,7 +11,6 @@ namespace ascend {
 
 diopiError_t diopiAddcmul(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t tensor1,
                           diopiConstTensorHandle_t tensor2, const diopiScalar_t* value) {
-    diopiTensorHandle_t valueTensor = nullptr;
     diopiDtype_t dtype;
     diopiGetTensorDtype(input, &dtype);
     AclOpRunner<4, 1>("Addcmul", ctx).addInput(input).addInput(tensor1).addInput(tensor2).addConstInput(*value, dtype).addOutput(out).run();
