7996d84c71a8e35a90c37d6fc355a27f4d24c92b
POI-WX <wangxing2@sensetime.com>
2024-02-19 21:26:03 +0800

    update

--- impl/ascend/functions/binary.cpp
+++ impl/ascend/functions/binary.cpp
@@ -21,8 +21,6 @@ aclDataType dtypeConvertor(diopiDtype_t type) {
     return dtype;
 }
 
-}  // namespace
-
 bool isScalarOne(const diopiScalar_t* alpha) {
     if (alpha == nullptr) {
         return true;
@@ -30,6 +28,7 @@ bool isScalarOne(const diopiScalar_t* alpha) {
     float val = getValue<float>(alpha);
     return fabs(val - 1.0) < 1e-6;
 }
+}  // namespace
 
 diopiError_t diopiAdd(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t other,
                       const diopiScalar_t* alpha) {
