0b16832d918534e282ef24bf28d40088397e1eb6
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-02-20 15:02:17 +0800

    use new copy impl

--- impl/ascend/functions/copy.cpp
+++ impl/ascend/functions/copy.cpp
@@ -4,38 +4,17 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
-#include "../common/acloprunner.hpp"
+#include <diopi/diopirt.h>
 
 namespace impl {
+
+namespace ascend_npu {
+extern diopiError_t diopiCopyInp(diopiContextHandle_t ctx, diopiConstTensorHandle_t src, diopiTensorHandle_t dest);
+}  // namespace ascend_npu
+
 namespace ascend {
 
-diopiError_t diopiCopyInp(diopiContextHandle_t ctx, diopiConstTensorHandle_t src, diopiTensorHandle_t dest) {
-    int64_t numel = 0;
-    diopiGetTensorNumel(src, &numel);
-    if (0 == numel) {
-        return diopiSuccess;
-    }
-
-    AscendTensor inputAt(src), outAt(dest);
-    if (src != dest) {
-        if (inputAt.dtype() == outAt.dtype() && 1 != numel) {
-            AclOpRunner<8, 1>("ViewCopy", ctx)
-                .addInputWithoutContiguous(dest)
-                .addConstInput(outAt.shape())
-                .addConstInput(outAt.stride())
-                .addConstInput(0, diopi_dtype_int64)
-                .addInputWithoutContiguous(src)
-                .addConstInput(inputAt.shape())
-                .addConstInput(inputAt.stride())
-                .addConstInput(0, diopi_dtype_int64)
-                .addOutputWithoutContiguous(dest)
-                .run();
-        } else {
-            diopiCastDtype(ctx, dest, src);
-        }
-    }
-    return diopiSuccess;
-}
+diopiError_t diopiCopyInp(diopiContextHandle_t ctx, diopiConstTensorHandle_t src, diopiTensorHandle_t dest) { return ascend_npu::diopiCopyInp(ctx, src, dest); }
 
 }  // namespace ascend
 }  // namespace impl
