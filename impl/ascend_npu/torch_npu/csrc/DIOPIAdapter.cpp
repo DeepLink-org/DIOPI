8ce0d71f060a2cbc48d6ea12408f363b86b6baec
jfxu-st <143591296+jfxu-st@users.noreply.github.com>
2024-02-21 15:35:02 +0800

    Remove unnecessary sync (#956)
    * remove unnecessary sync
    
    * remove calling the redundant Sync() function
    
    ---------
    
    Co-authored-by: wangxing <131418410+POI-WX@users.noreply.github.com>

--- impl/ascend_npu/torch_npu/csrc/DIOPIAdapter.cpp
+++ impl/ascend_npu/torch_npu/csrc/DIOPIAdapter.cpp
@@ -2617,9 +2617,6 @@ void OpCommand::Run() {
     aclCmd->SetEnginePriority();
     const string& op_name = aclCmd->GetName();
     aclCmd->Run(sync, sync_index, outputTensor);
-    if (sync) {
-        Sync();
-    }
     aclCmd->releaseSource();
 }
 
@@ -2628,7 +2625,6 @@ OpCommand& OpCommand::Sync(c10::SmallVector<int64_t, N>& index) {
     if (!index.empty()) {
         sync = true;
     }
-    Sync();
     return *this;
 }
 
