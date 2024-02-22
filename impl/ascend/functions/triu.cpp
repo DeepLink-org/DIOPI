f2213c28ab46f33c19a5098b31c8ed26b7efcc5e
POI-WX <wangxing2@sensetime.com>
2024-02-22 12:19:07 +0800

    modify triu op

--- impl/ascend/functions/triu.cpp
+++ impl/ascend/functions/triu.cpp
@@ -10,23 +10,32 @@ namespace impl {
 namespace ascend {
 
 diopiError_t diopiTriu(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t diagonal) {
-    AclTensor inputAcl(input), outAcl(out);
-    if (!inputAcl.defined() || inputAcl.numel() == 0) {
+    if (useAclnn()) {
+        AclTensor inputAcl(input), outAcl(out);
+        if (!inputAcl.defined() || inputAcl.numel() == 0) {
+            return diopiSuccess;
+        }
+
+        ACLNN_ADAPTOR(aclnnTriu, ctx, inputAcl, diagonal, outAcl);
+        return diopiSuccess;
+    } else {
+        AclOpRunner<1, 1>("Triu", ctx).addInput(input).setAttr("diagonal", diagonal).addOutput(out).run();
         return diopiSuccess;
     }
-
-    ACLNN_ADAPTOR(aclnnTriu, ctx, inputAcl, diagonal, outAcl);
-    return diopiSuccess;
 }
 
 diopiError_t diopiTriuInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, int64_t diagonal) {
-    AclTensor inputAcl(input);
-    if (!inputAcl.defined() || inputAcl.numel() == 0) {
-        return diopiSuccess;
-    }
+    if (useAclnn()) {
+        AclTensor inputAcl(input);
+        if (!inputAcl.defined() || inputAcl.numel() == 0) {
+            return diopiSuccess;
+        }
 
-    ACLNN_ADAPTOR(aclnnInplaceTriu, ctx, inputAcl, diagonal);
-    return diopiSuccess;
+        ACLNN_ADAPTOR(aclnnInplaceTriu, ctx, inputAcl, diagonal);
+        return diopiSuccess;
+    } else {
+        return diopiTriu(ctx, input, input, diagonal);
+    }
 }
 
 }  // namespace ascend
