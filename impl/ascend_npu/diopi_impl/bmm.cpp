0dbad4be6a22c2f3daba46b715ab7f69c855f163
jingguo-st <fujingguo@sensetime.com>
2024-02-22 12:02:34 +0800

    impl bmm with op_api api

--- impl/ascend_npu/diopi_impl/bmm.cpp
+++ impl/ascend_npu/diopi_impl/bmm.cpp
@@ -6,12 +6,17 @@
 
 #include "helper.hpp"
 #include "op_plugin/AclOpsInterface.h"
+#include "op_plugin/OpApiInterface.h"
 
 namespace OP_IMPL_NS {
 
 diopiError_t diopiBmm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t mat2) {
-    BEGIN_CALL_ACL_OP(input, mat2, out);
-    acl_op::bmm_out(inputAt, mat2At, outAt);
+    BEGIN_CALL_ACL_OP(out);
+    if (outAt.numel() == 0) {
+        return diopiSuccess;
+    }
+    BEGIN_CALL_ACL_OP(input, mat2);
+    op_api::bmm_out(inputAt, mat2At, outAt);
     END_CALL_ACL_OP();
 }
 
