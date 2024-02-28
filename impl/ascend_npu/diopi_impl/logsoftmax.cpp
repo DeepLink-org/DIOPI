912af18062bb5fb5c8e831f073074b9efd6fe9cd
zhangqiu <zhangqiu1994@outlook.com>
2024-02-28 21:34:06 +0800

    fix

--- impl/ascend_npu/diopi_impl/logsoftmax.cpp
+++ impl/ascend_npu/diopi_impl/logsoftmax.cpp
@@ -7,27 +7,29 @@
 #include "helper.hpp"
 #include "op_plugin/AclOpsInterface.h"
 #include "op_plugin/OpApiInterface.h"
-
+#include "op_plugin/utils/op_api_common.h"
 namespace OP_IMPL_NS {
 diopiError_t diopiLogSoftmax(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t dim) {
     BEGIN_CALL_ACL_OP(input, out);
+    if (inputAt.numel() == 1) {
+        op_api::fill_(outAt, 0);
+        return diopiSuccess;
+    }
     if (dim < 0) {
         dim += inputAt.sizes().size();
     }
-    at::Tensor result;
-    if (inputAt.scalar_type() == at::kHalf) {
-        result = op_api::_log_softmax(inputAt, dim, true);
 
-    } else {
-        result = op_api::_log_softmax(inputAt, dim, false);
-    }
-    outAt.copy_(result);
+    EXEC_NPU_CMD(aclnnLogSoftmax, inputAt, dim, outAt);
     END_CALL_ACL_OP();
 }
 
 diopiError_t diopiLogSoftmaxBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput,
                                      diopiConstTensorHandle_t output, int64_t dim) {
     BEGIN_CALL_ACL_OP(gradInput, gradOutput, output);
+    if (gradInputAt.numel() == 1) {
+        op_api::fill_(gradInputAt, 0);
+        return diopiSuccess;
+    }
     if (dim < 0) {
         dim += gradInputAt.sizes().size();
     }
