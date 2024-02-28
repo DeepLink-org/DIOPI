a0f96f79c9664269023c32a621ce271d17c0486d
zhangqiu <zhangqiu1994@outlook.com>
2024-02-28 10:51:06 +0800

    reimpl softmax

--- impl/ascend_npu/diopi_impl/logsoftmax.cpp
+++ impl/ascend_npu/diopi_impl/logsoftmax.cpp
@@ -10,6 +10,26 @@
 
 namespace OP_IMPL_NS {
 
+diopiError_t diopiSoftmax(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t dim) {
+    BEGIN_CALL_ACL_OP(input, out);
+    at::Tensor result;
+    if (inputAt.scalar_type() == at::kHalf) {
+        result = op_api::_softmax(inputAt, dim, true);
+    } else {
+        result = op_api::_softmax(inputAt, dim, false);
+    }
+    outAt.copy_(result);
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiSoftmaxBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput, diopiConstTensorHandle_t output,
+                                  int64_t dim) {
+    BEGIN_CALL_ACL_OP(gradInput, gradOutput, output);
+    at::ScalarType inputDtype = gradInputAt.scalar_type();
+    op_api::_softmax_backward_data_out(gradOutputAt, outputAt, dim, inputDtype, gradInputAt);
+    END_CALL_ACL_OP();
+}
+
 diopiError_t diopiLogSoftmax(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t dim) {
     BEGIN_CALL_ACL_OP(input, out);
     at::Tensor result;
@@ -20,7 +40,6 @@ diopiError_t diopiLogSoftmax(diopiContextHandle_t ctx, diopiTensorHandle_t out, 
     }
     outAt.copy_(result);
     END_CALL_ACL_OP();
-    std::vector<int64_t> dimList = {dim};
 }
 
 diopiError_t diopiLogSoftmaxBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput,
@@ -29,7 +48,6 @@ diopiError_t diopiLogSoftmaxBackward(diopiContextHandle_t ctx, diopiTensorHandle
     at::ScalarType inputDtype = gradInputAt.scalar_type();
     op_api::_log_softmax_backward_data_out(gradOutputAt, outputAt, dim, inputDtype, gradInputAt);
     END_CALL_ACL_OP();
-    std::vector<int64_t> dimList = {dim};
 }
 
 }  // namespace OP_IMPL_NS
