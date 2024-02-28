1dc7391014384b5567b01dcc6153e189fe228e01
jingguo-st <fujingguo@sensetime.com>
2024-02-28 15:20:07 +0800

    reimpl diopiMean with opapi for ascend

--- impl/ascend_npu/diopi_impl/reduce.cpp
+++ impl/ascend_npu/diopi_impl/reduce.cpp
@@ -32,11 +32,34 @@ diopiError_t diopiSum(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
 
     at::ArrayRef<int64_t> rdim(dim.data, dim.len);
 
-    if (impl::aten::isIntegralTypeWithBool(impl::aten::getDIOPITensorType(inputAt.scalar_type()))) {
-        op_api::sum_out(inputAt.to(at::kLong), rdim, keepdim, inputAt.scalar_type(), outAt);
-    } else {
-        op_api::sum_out(inputAt, rdim, keepdim, inputAt.scalar_type(), outAt);
+    op_api::sum_out(inputAt, rdim, keepdim, inputAt.scalar_type(), outAt);
+
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiMean(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t dim) {
+    BEGIN_CALL_ACL_OP(input, out);
+    if (inputAt.numel() == 0) {
+        return diopiSuccess;
     }
+
+    if (inputAt.dim() == 0) {
+        diopiCopyInp(ctx, input, out);
+        return diopiSuccess;
+    }
+
+    bool keepdim = true;
+    diopiSize_t inS, outS;
+    diopiGetTensorShape(input, &inS);
+    diopiGetTensorShape(out, &outS);
+
+    if (inS.len != outS.len) {
+        keepdim = false;
+    }
+
+    at::ArrayRef<int64_t> rdim(dim.data, dim.len);
+
+    op_api::mean_out(inputAt, rdim, keepdim, inputAt.scalar_type(), outAt);
 
     END_CALL_ACL_OP();
 }
