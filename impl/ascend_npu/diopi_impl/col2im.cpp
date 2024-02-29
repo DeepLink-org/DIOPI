7afc25b7e1df5929d799affc0d248ba0fc3993c8
yangbofun <yangbofun@163.com>
2024-02-29 10:01:22 +0800

    fix

--- impl/ascend_npu/diopi_impl/col2im.cpp
+++ impl/ascend_npu/diopi_impl/col2im.cpp
@@ -13,6 +13,9 @@ namespace OP_IMPL_NS {
 diopiError_t diopiCol2Im(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t outputSize, diopiSize_t kernelSize,
                          diopiSize_t dilation, diopiSize_t padding, diopiSize_t stride) {
     BEGIN_CALL_ACL_OP(input, outputSize, kernelSize, dilation, padding, stride, out);
+#if 0
+    acl_op::col2im_out(inputAt, outputSizeAt, kernelSizeAt, dilationAt, paddingAt, strideAt, outAt);
+#endif
     op_api::col2im_out(inputAt, outputSizeAt, kernelSizeAt, dilationAt, paddingAt, strideAt, outAt);
     END_CALL_ACL_OP();
 }
