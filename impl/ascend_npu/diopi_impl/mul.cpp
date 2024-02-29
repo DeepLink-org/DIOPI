f04c5f3f2bdca8f4f30f976124b2807b63fe43a9
POI-WX <wangxing2@sensetime.com>
2024-02-29 14:02:37 +0800

    reimpl mul op on ascend using aclnn

--- impl/ascend_npu/diopi_impl/mul.cpp
+++ impl/ascend_npu/diopi_impl/mul.cpp
@@ -5,8 +5,8 @@
  */
 
 #include "helper.hpp"
-#include "op_plugin/AclOpsInterface.h"
 #include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
 
 namespace OP_IMPL_NS {
 
@@ -31,16 +31,16 @@ diopiError_t diopiMulInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, di
     END_CALL_ACL_OP();
 }
 
-// diopiError_t diopiMulScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* other) {
-//     BEGIN_CALL_ACL_OP(input, other);
-//     if (!inputAt.defined() || inputAt.numel() == 0) {
-//         return diopiSuccess;
-//     }
+diopiError_t diopiMulScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* other) {
+    BEGIN_CALL_ACL_OP(input, other);
+    if (!inputAt.defined() || inputAt.numel() == 0) {
+        return diopiSuccess;
+    }
 
-//     BEGIN_CALL_ACL_OP(out);
-//     op_api::mul_out(inputAt, otherAt, outAt);
-//     END_CALL_ACL_OP();
-// }
+    BEGIN_CALL_ACL_OP(out);
+    EXEC_NPU_CMD(aclnnMuls, inputAt, otherAt, outAt);
+    END_CALL_ACL_OP();
+}
 
 diopiError_t diopiMulInpScalar(diopiContextHandle_t ctx, diopiTensorHandle_t input, const diopiScalar_t* other) {
     BEGIN_CALL_ACL_OP(input, other);
