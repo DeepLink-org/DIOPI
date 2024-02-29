e7a7e9630b627d16618d00039c6faa4c529b3f91
zhangzefeng92 <zhang_zefeng@foxmail.com>
2024-02-29 17:15:58 +0800

    modify implementation of bitwise and logical to op_api

--- impl/ascend_npu/diopi_impl/logical.cpp
+++ impl/ascend_npu/diopi_impl/logical.cpp
@@ -25,7 +25,8 @@ diopiError_t diopiLogicalOr(diopiContextHandle_t ctx, diopiTensorHandle_t out, d
 diopiError_t diopiLogicalAndInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t other) {
     BEGIN_CALL_ACL_OP(input, other);
     op_api::logical_and_out(inputAt, otherAt, inputAt);
-    END_CALL_ACL_OP();}
+    END_CALL_ACL_OP();
+}
 
 diopiError_t diopiLogicalOrInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t other) {
     BEGIN_CALL_ACL_OP(input, other);
