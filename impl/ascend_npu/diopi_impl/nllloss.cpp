9256f0a2ad5900efd6bca0396d6230780361a95e
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-02-29 13:50:52 +0800

    add nll_loss relate

--- impl/ascend_npu/diopi_impl/nllloss.cpp
+++ impl/ascend_npu/diopi_impl/nllloss.cpp
@@ -11,10 +11,6 @@
 
 namespace OP_IMPL_NS {
 
-//: std::tuple<at::Tensor &,at::Tensor &> nll_loss2d_forward_out(const at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> & weight,
-//: int64_t reduction, int64_t ignore_index, at::Tensor & output, at::Tensor & total_weight); :std::tuple<at::Tensor &,at::Tensor &> nll_loss_forward_out(const
-//: at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> & weight, int64_t reduction, int64_t ignore_index, at::Tensor & output,
-//: at::Tensor & total_weight);
 diopiError_t diopiNLLLoss(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t target,
                           diopiConstTensorHandle_t weight, diopiReduction_t reduction, int64_t ignoreIndex) {
     BEGIN_CALL_ACL_OP(out, input, target, weight);
@@ -48,30 +44,61 @@ diopiError_t diopiNLLLoss(diopiContextHandle_t ctx, diopiTensorHandle_t out, dio
     }
     END_CALL_ACL_OP();
 }
-#if 0
-// at::Tensor nll_loss2d_backward(const at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> & weight, int64_t reduction, int64_t ignore_index, const at::Tensor & total_weight);
-// at::Tensor nll_loss_backward(const at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> & weight, int64_t reduction, int64_t ignore_index, const at::Tensor & total_weight);
+
+// at::Tensor & nll_loss2d_backward_out(const at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> &
+// weight, int64_t reduction, int64_t ignoreIndex, const at::Tensor & total_weight, at::Tensor & grad_input); at::Tensor & nll_loss_backward_out(const
+// at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> & weight, int64_t reduction, int64_t
+// ignoreIndex, const at::Tensor & total_weight, at::Tensor & grad_input);
 diopiError_t diopiNLLLossBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput, diopiConstTensorHandle_t input,
                                   diopiConstTensorHandle_t target, diopiConstTensorHandle_t weight, diopiReduction_t reduction, int64_t ignoreIndex) {
     BEGIN_CALL_ACL_OP(input, target, weight, gradInput, gradOutput);
     if (input == nullptr) {
         return diopiSuccess;
     }
+    /*
+     * A tensor representing the sum of weights for each element considered in the NLL loss computation.
+     * In case a weight tensor is provided, total_weight represents the sum of weights for all the non-ignored indices in the target tensor.
+     * When no weight tensor is provided, total_weight corresponds to the count of all non-ignored indices.
+     */
     at::Tensor totalWeightAt;
-    if (weight != nullptr) {
-        totalWeightAt = at_npu::native::empty_npu(weightAt.sizes(), weightAt.options());
+    // Flatten the target tensor for easier processing
+    auto flatTarget = targetAt.view(-1);
+
+    // Create a mask corresponding to ignoreIndex if it's provided
+    // auto mask = (ignoreIndex >= 0) ? (flatTarget != ignoreIndex) : at::ones(flatTarget.sizes(), flatTarget.options()).to(at::kBool);
+    // auto mask = (ignoreIndex >= 0) ? (flatTarget != ignoreIndex) : at_npu::native::empty_npu(flatTarget.sizes(), flatTarget.options()).fill_(true);
+    auto mask = at_npu::native::empty_npu(flatTarget.sizes(), flatTarget.options().dtype(at::kBool));
+    if (ignoreIndex >= 0) {
+        op_api::ne_out(flatTarget, at::Scalar(ignoreIndex), mask);
+    } else {
+        mask.fill_(true);
+    }
+
+    if (weightAt.defined()) {
+        // Filter out the targets using the mask and compute total weight using index_select
+        // totalWeightAt = weightAt.index_select(0, flatTarget.masked_select(mask)).sum();
+        auto selectedTargetAt = op_api::masked_select(flatTarget, mask);
+        auto selectedWeightAt = op_api::index_select(flatTarget, 0, selectedTargetAt);
+        totalWeightAt = op_api::sum(selectedWeightAt, weightAt.scalar_type());
+    } else {
+        // If weight is not defined, compute total weight by counting the valid targets
+        totalWeightAt = at::scalar_tensor(mask.sum().item<float>(), inputAt.options());
     }
 
     if (inputAt.dim() <= 2) {
-        op_api::nll_loss_backward(gradOutputAt, inputAt, targetAt, weightAt, reduction, ignoreIndex, totalWeightAt);
+        op_api::nll_loss_backward_out(gradOutputAt, inputAt, targetAt, weightAt, reduction, ignoreIndex, totalWeightAt, gradInputAt);
     } else if (inputAt.dim() == 4) {
-        op_api::nll_loss2d_backward(gradOutputAt, inputAt, targetAt, weightAt, reduction, ignoreIndex,
-        totalWeightAt);
+        op_api::nll_loss2d_backward_out(gradOutputAt, inputAt, targetAt, weightAt, reduction, ignoreIndex, totalWeightAt, gradInputAt);
     } else {
-        TORCH_CHECK(false, "diopiNLLLossBackward: invalid input dim");
+        auto veiwedInputAt = impl::aten::viewStorage(inputAt, {inputAt.size(0), inputAt.size(1), inputAt.numel() / inputAt.size(0) / inputAt.size(1), 1});
+        auto veiwedGradOutAt = (gradOutputAt.numel() > 1)
+                                   ? impl::aten::viewStorage(gradOutputAt, {gradOutputAt.size(0), gradOutputAt.numel() / gradOutputAt.size(0), 1})
+                                   : gradOutputAt;
+        auto veiwedTargetAt = impl::aten::viewStorage(targetAt, {targetAt.size(0), targetAt.numel() / targetAt.size(0), 1});
+        op_api::nll_loss2d_backward_out(veiwedGradOutAt, veiwedInputAt, veiwedTargetAt, weightAt, reduction, ignoreIndex, totalWeightAt, gradInputAt);
     }
+
     END_CALL_ACL_OP();
 }
-#endif
 
 }  // namespace OP_IMPL_NS
