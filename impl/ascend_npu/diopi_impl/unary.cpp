3695b70dec3d0b1f51ce6d735ac0f0b1bbd2e117
SHshenhao <shen.hao.1985@gmail.com>
2024-02-29 17:02:51 +0800

    update_absabsinp_toaclnn

--- impl/ascend_npu/diopi_impl/unary.cpp
+++ impl/ascend_npu/diopi_impl/unary.cpp
@@ -5,7 +5,9 @@
  */
 
 #include "helper.hpp"
+#include "op_plugin/AclOpsInterface.h"
 #include "op_plugin/OpApiInterface.h"
+#include "op_plugin/OpInterface.h"
 
 namespace OP_IMPL_NS {
 
@@ -15,6 +17,34 @@ diopiError_t diopiFill(diopiContextHandle_t ctx, diopiTensorHandle_t input, cons
         return diopiSuccess;
     }
     op_api::fill_(inputAt, valueAt);
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiAbs(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    BEGIN_CALL_ACL_OP(input, out);
+    if (inputAt.numel() <= 0) {
+        return diopiSuccess;
+    }
+
+    if (false) {
+        acl_op::abs_out(inputAt, outAt);
+    } else {
+        op_api::abs_out(inputAt, outAt);
+    }
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiAbsInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    BEGIN_CALL_ACL_OP(input);
+    if (inputAt.numel() <= 0) {
+        return diopiSuccess;
+    }
+
+    if (false) {
+        acl_op::abs_(inputAt);
+    } else {
+        op_api::abs_(inputAt);
+    }
     END_CALL_ACL_OP();
 }
 
