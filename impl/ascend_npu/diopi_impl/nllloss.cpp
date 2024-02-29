18531715b96c208074c751de04084d1b5cc99c52
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-02-29 14:46:29 +0800

    add ascend nll_loss: use aclnn impl and delete no need op,such as transpose and memcopy

--- impl/ascend_npu/diopi_impl/nllloss.cpp
+++ impl/ascend_npu/diopi_impl/nllloss.cpp
@@ -27,9 +27,9 @@ diopiError_t diopiNLLLoss(diopiContextHandle_t ctx, diopiTensorHandle_t out, dio
     }
 
     at::Tensor totalWeightAt = at_npu::native::empty_npu({1}, inputAt.options());
-    const int64_t C = inputAt.dim() >= 4 ? inputAt.size(1) : inputAt.size(-1);
+    const int64_t channel = inputAt.dim() >= 4 ? inputAt.size(1) : inputAt.size(-1);
     if (weight == nullptr) {
-        weightAt = at_npu::native::empty_npu({C}, inputAt.options());
+        weightAt = at_npu::native::empty_npu({channel}, inputAt.options());
         op_api::fill_(weightAt, c10::Scalar(1.0f));
     }
     if (inputAt.dim() <= 2) {
@@ -45,14 +45,10 @@ diopiError_t diopiNLLLoss(diopiContextHandle_t ctx, diopiTensorHandle_t out, dio
     END_CALL_ACL_OP();
 }
 
-// at::Tensor & nll_loss2d_backward_out(const at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> &
-// weight, int64_t reduction, int64_t ignoreIndex, const at::Tensor & total_weight, at::Tensor & grad_input); at::Tensor & nll_loss_backward_out(const
-// at::Tensor & grad_output, const at::Tensor & self, const at::Tensor & target, const c10::optional<at::Tensor> & weight, int64_t reduction, int64_t
-// ignoreIndex, const at::Tensor & total_weight, at::Tensor & grad_input);
 diopiError_t diopiNLLLossBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput, diopiConstTensorHandle_t input,
                                   diopiConstTensorHandle_t target, diopiConstTensorHandle_t weight, diopiReduction_t reduction, int64_t ignoreIndex) {
     BEGIN_CALL_ACL_OP(input, target, weight, gradInput, gradOutput);
-    if (input == nullptr) {
+    if (input == nullptr || gradInput == nullptr || inputAt.numel() <= 0 || gradInputAt.numel() <= 0) {
         return diopiSuccess;
     }
     /*
@@ -64,25 +60,21 @@ diopiError_t diopiNLLLossBackward(diopiContextHandle_t ctx, diopiTensorHandle_t 
     // Flatten the target tensor for easier processing
     auto flatTarget = targetAt.view(-1);
 
+    // calculate totalWeight
     // Create a mask corresponding to ignoreIndex if it's provided
-    // auto mask = (ignoreIndex >= 0) ? (flatTarget != ignoreIndex) : at::ones(flatTarget.sizes(), flatTarget.options()).to(at::kBool);
-    // auto mask = (ignoreIndex >= 0) ? (flatTarget != ignoreIndex) : at_npu::native::empty_npu(flatTarget.sizes(), flatTarget.options()).fill_(true);
     auto mask = at_npu::native::empty_npu(flatTarget.sizes(), flatTarget.options().dtype(at::kBool));
     if (ignoreIndex >= 0) {
         op_api::ne_out(flatTarget, at::Scalar(ignoreIndex), mask);
     } else {
         mask.fill_(true);
     }
-
     if (weightAt.defined()) {
         // Filter out the targets using the mask and compute total weight using index_select
-        // totalWeightAt = weightAt.index_select(0, flatTarget.masked_select(mask)).sum();
         auto selectedTargetAt = op_api::masked_select(flatTarget, mask);
         auto selectedWeightAt = op_api::index_select(flatTarget, 0, selectedTargetAt);
         totalWeightAt = op_api::sum(selectedWeightAt, weightAt.scalar_type());
     } else {
         // If weight is not defined, compute total weight by counting the valid targets
-        // totalWeightAt = at::scalar_tensor(mask.sum().item<float>(), inputAt.options());
         totalWeightAt = op_api::sum(mask, inputAt.scalar_type());
     }
 
@@ -92,13 +84,14 @@ diopiError_t diopiNLLLossBackward(diopiContextHandle_t ctx, diopiTensorHandle_t 
         op_api::nll_loss2d_backward_out(gradOutputAt, inputAt, targetAt, weightAt, reduction, ignoreIndex, totalWeightAt, gradInputAt);
     } else {
         auto veiwedInputAt = impl::aten::viewStorage(inputAt, {inputAt.size(0), inputAt.size(1), inputAt.numel() / inputAt.size(0) / inputAt.size(1), 1});
+        auto veiwedGradInputAt = impl::aten::viewStorage(
+            gradInputAt, {gradInputAt.size(0), gradInputAt.size(1), gradInputAt.numel() / gradInputAt.size(0) / gradInputAt.size(1), 1});
         auto veiwedGradOutAt = (gradOutputAt.numel() > 1)
                                    ? impl::aten::viewStorage(gradOutputAt, {gradOutputAt.size(0), gradOutputAt.numel() / gradOutputAt.size(0), 1})
                                    : gradOutputAt;
         auto veiwedTargetAt = impl::aten::viewStorage(targetAt, {targetAt.size(0), targetAt.numel() / targetAt.size(0), 1});
-        op_api::nll_loss2d_backward_out(veiwedGradOutAt, veiwedInputAt, veiwedTargetAt, weightAt, reduction, ignoreIndex, totalWeightAt, gradInputAt);
+        op_api::nll_loss2d_backward_out(veiwedGradOutAt, veiwedInputAt, veiwedTargetAt, weightAt, reduction, ignoreIndex, totalWeightAt, veiwedGradInputAt);
     }
-
     END_CALL_ACL_OP();
 }
 
