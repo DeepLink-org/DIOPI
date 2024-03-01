a3ecfdeeb021e5f0c8c0c0dbf8d2cf2f4eb0440b
POI-WX <wangxing2@sensetime.com>
2024-03-01 11:36:09 +0800

    reimpl adaptive pool2d op on ascend

---
+++ impl/ascend_npu/diopi_impl/pool.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2024, DeepLink.
+ */
+#include "helper.hpp"
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace OP_IMPL_NS {
+
+diopiError_t diopiAdaptiveAvgPool2d(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t outputSize) {
+    BEGIN_CALL_ACL_OP(input, outputSize, out);
+    EXEC_NPU_CMD(aclnnAdaptiveAvgPool2d, inputAt, outputSizeAt, outAt);
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiAdaptiveAvgPool2dBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput,
+                                            diopiConstTensorHandle_t input) {
+    BEGIN_CALL_ACL_OP(input, gradOutput, gradInput);
+    EXEC_NPU_CMD(aclnnAdaptiveAvgPool2dBackward, gradOutputAt, inputAt, gradInputAt);
+    END_CALL_ACL_OP();
+}
+
+}  // namespace OP_IMPL_NS
