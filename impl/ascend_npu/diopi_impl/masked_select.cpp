aab36436b722ac0f25d6a5451e27d0510f4aa822
zhangzefeng92 <zhang_zefeng@foxmail.com>
2024-03-01 15:05:23 +0800

    fix masked select

--- impl/ascend_npu/diopi_impl/masked_select.cpp
+++ impl/ascend_npu/diopi_impl/masked_select.cpp
@@ -23,11 +23,11 @@ DIOPI_API diopiError_t diopiMaskedSelectBackward(diopiContextHandle_t ctx, diopi
         at::Tensor gradInputNew = op_api::zeros_like(maskAt, gradInputAt.scalar_type(), maskAt.layout(), maskAt.device(), false);
         op_api::masked_scatter_(gradInputNew, maskAt, gradOutputAt);
         std::vector<int64_t> dims;
-        for (int i = 0; i< maskAt.dim() - inputAt.dim(); i++) {
+        for (int i = 0; i < maskAt.dim() - inputAt.dim(); i++) {
             dims.push_back(i);
         }
         at::OptionalIntArrayRef sumDim(dims);
-        op_api::sum_out(gradInputNew, sumDim, false, gradInputAt.scalar_type(),  gradInputAt);
+        op_api::sum_out(gradInputNew, sumDim, false, gradInputAt.scalar_type(), gradInputAt);
     } else {
         at::Scalar zero = 0;
         op_api::fill_(gradInputAt, zero);
