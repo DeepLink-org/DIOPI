66ed1321a6462014f4f727cfa1aea24ea302311e
zhangzefeng92 <zhang_zefeng@foxmail.com>
2024-03-01 14:20:21 +0800

    fix masked select

--- impl/ascend_npu/diopi_impl/masked_select.cpp
+++ impl/ascend_npu/diopi_impl/masked_select.cpp
@@ -6,11 +6,12 @@
 
 #include "helper.hpp"
 #include "op_plugin/AclOpsInterface.h"
+#include "op_plugin/OpApiInterface.h"
 
 namespace OP_IMPL_NS {
 diopiError_t diopiMaskedSelect(diopiContextHandle_t ctx, diopiTensorHandle_t* out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t mask) {
     BEGIN_CALL_ACL_OP(input, mask);
-    at::Tensor outAt = acl_op::masked_select(inputAt, maskAt);
+    at::Tensor outAt = op_api::masked_select(inputAt, maskAt);
     impl::aten::buildDiopiTensor(ctx, outAt, out);
     END_CALL_ACL_OP();
 }
@@ -18,9 +19,15 @@ diopiError_t diopiMaskedSelect(diopiContextHandle_t ctx, diopiTensorHandle_t* ou
 DIOPI_API diopiError_t diopiMaskedSelectBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput,
                                                  diopiConstTensorHandle_t input, diopiConstTensorHandle_t mask) {
     BEGIN_CALL_ACL_OP(input, mask, gradInput, gradOutput);
-    at::Scalar zero = 0;
-    acl_op::fill_(gradInputAt, zero);
-    acl_op::masked_scatter_(gradInputAt, maskAt, gradOutputAt);
+    if (inputAt.dim() < maskAt.dim()) {
+        at::Tensor gradInputNew = op_api::zeros_like(maskAt, gradInputAt.scalar_type(), maskAt.layout(), maskAt.device(), false);
+        op_api::masked_scatter_(gradInputNew, maskAt, gradOutputAt);
+        op_api::sum_out(gradInputNew, 0, false, gradInputAt.scalar_type(),  gradInputAt);
+    } else {
+        at::Scalar zero = 0;
+        op_api::fill_(gradInputAt, zero);
+        op_api::masked_scatter_(gradInputAt, maskAt, gradOutputAt);
+    }
     END_CALL_ACL_OP();
 }
 
