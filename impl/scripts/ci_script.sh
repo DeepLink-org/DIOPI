17344b193a930894a76d34e484ce713186c97a18
yangbofun <yangbofun@163.com>
2024-03-02 15:32:54 +0800

    replace clang-tidy with clangd-tidy

--- impl/scripts/ci_script.sh
+++ impl/scripts/ci_script.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Required tools.
+[ -x "$(command -v git)" ] || { echo "::error::Missing git tool" && exit 1; }
+[ -x "$(command -v clangd)" ] || { echo "::error::Missing clangd tool" && exit 1; }
+
+
 CURRENT_PATH=$(
   cd "$(dirname "$0")"
   pwd
@@ -9,6 +14,12 @@ CURRENT_PATH=$(
 IMPL_PATH=$(readlink -f ${CURRENT_PATH}/..)
 
 CMAKE_EXPORT_COMPILE_COMMANDS_FILE=${IMPL_PATH}/build/compile_commands.json
+
+function down_clangd_tidy {
+  [ -d "$CURRENT_PATH/clangd-tidy" ] ||
+  git -c advice.detachedHead=false clone --depth 1 -b v0.1.2 https://github.com/lljbash/clangd-tidy.git "$CURRENT_PATH/clangd-tidy"
+}
+
 
 case $1 in
   cpp-lint)
@@ -22,14 +33,19 @@ case $1 in
     ;;
   clang-tidy)
     if [ -e ${CMAKE_EXPORT_COMPILE_COMMANDS_FILE} ]; then
-      python3 ${IMPL_PATH}/../run-clang-tidy.py -p $(dirname "${CMAKE_EXPORT_COMPILE_COMMANDS_FILE}")
+      down_clangd_tidy
+      find camb ../adaptor/csrc \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) |
+      xargs "$CURRENT_PATH/clangd-tidy/clangd-tidy" -j8 -v -p $(dirname "${CMAKE_EXPORT_COMPILE_COMMANDS_FILE}")
     else
       echo "error: compile_commands.json not found." && exit 1
     fi
     ;;
   clang-tidy-ascend)
     if [ -e ${CMAKE_EXPORT_COMPILE_COMMANDS_FILE} ]; then
-      python3 ${IMPL_PATH}/../run-clang-tidy.py 'impl/ascend(?!_npu)' 'impl/ascend_npu/diopi_impl' -p $(dirname ${CMAKE_EXPORT_COMPILE_COMMANDS_FILE})
+      down_clangd_tidy
+      # Collect source files and run tidy.
+      find ascend ascend_npu/diopi_impl ../adaptor/csrc \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) |
+      xargs "$CURRENT_PATH/clangd-tidy/clangd-tidy" -j8 -v -p $(dirname "${CMAKE_EXPORT_COMPILE_COMMANDS_FILE}")
     else
       echo "error: compile_commands.json not found." && exit 1
     fi
