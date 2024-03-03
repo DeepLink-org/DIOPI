62aa049d85e547b521cb3139df9303703bf2c5e9
POI-WX <wangxing2@sensetime.com>
2024-03-03 13:12:19 +0800

    reimpl relu, sigmoid op using aclnn on ascend

--- impl/ascend_npu/diopi_impl/activation.cpp
+++ impl/ascend_npu/diopi_impl/activation.cpp
@@ -5,28 +5,39 @@
  */
 
 #include "helper.hpp"
-#include "op_plugin/AclOpsInterface.h"
 #include "op_plugin/OpApiInterface.h"
-#include "op_plugin/OpInterface.h"
+#include "op_plugin/utils/op_api_common.h"
 
 namespace OP_IMPL_NS {
 
 diopiError_t diopiRelu(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
     BEGIN_CALL_ACL_OP(input, out);
-    auto result = acl_op::relu(inputAt);
-    outAt.copy_(result);
+    EXEC_NPU_CMD(aclnnRelu, inputAt, outAt);
     END_CALL_ACL_OP();
 }
 
 diopiError_t diopiReluInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
     BEGIN_CALL_ACL_OP(input);
-    acl_op::relu_(inputAt);
+    EXEC_NPU_CMD(aclnnInplaceRelu, inputAt);
     END_CALL_ACL_OP();
 }
 
 diopiError_t diopiSigmoid(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
     BEGIN_CALL_ACL_OP(input, out);
-    acl_op::sigmoid_out(inputAt, outAt);
+    op_api::sigmoid_out(inputAt, outAt);
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiSigmoidInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    BEGIN_CALL_ACL_OP(input);
+    op_api::sigmoid_(inputAt);
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiSigmoidBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput,
+                                  diopiConstTensorHandle_t output) {
+    BEGIN_CALL_ACL_OP(gradInput, gradOutput, output);
+    op_api::sigmoid_backward_out(gradOutputAt, outputAt, gradInputAt);
     END_CALL_ACL_OP();
 }
 
