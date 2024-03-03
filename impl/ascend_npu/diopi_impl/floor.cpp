0e704202221c1ca89a7e8aa0f2ee33fa6ca819cc
POI-WX <wangxing2@sensetime.com>
2024-03-03 20:52:24 +0800

    reimpl floor op

---
+++ impl/ascend_npu/diopi_impl/floor.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2024, DeepLink.
+ */
+
+#include "helper.hpp"
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace OP_IMPL_NS {
+
+diopiError_t diopiFloor(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    BEGIN_CALL_ACL_OP(input, out);
+    op_api::floor_out(inputAt, outAt);
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiFloorInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    BEGIN_CALL_ACL_OP(input);
+    op_api::floor_(inputAt);
+    END_CALL_ACL_OP();
+}
+
+}  // namespace OP_IMPL_NS
