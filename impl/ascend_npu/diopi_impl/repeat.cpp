14cbe5599cc4d5fd09364614d38a946f17354870
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-03-04 15:26:16 +0800

    add ascend repeate

--- impl/ascend_npu/diopi_impl/repeat.cpp
+++ impl/ascend_npu/diopi_impl/repeat.cpp
@@ -6,22 +6,17 @@
 
 #include "helper.hpp"
 #include "op_plugin/AclOpsInterface.h"
+#include "op_plugin/OpApiInterface.h"
 
 extern "C" {
 diopiError_t diopiRepeat(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t repeatSize) {
     BEGIN_CALL_ACL_OP(out, input, repeatSize);
-    std::vector<int64_t> inputShape(inputAt.sizes().cbegin(), inputAt.sizes().cend());
-
-    if (inputShape.size() < repeatSize.len) {
-        while (inputShape.size() < repeatSize.len) {
-            inputShape.insert(inputShape.begin(), 1);
-        }
-
-        inputAt = impl::aten::viewStorage(inputAt, inputShape);
+    if (repeatSizeAt.size() <= 0) {
+        outAt.copy_(inputAt);
+        return diopiSuccess;
     }
-
     at_npu::native::OpPreparation::markAsOutputForApplyTensor(outAt);
-    outAt = acl_op::repeat(inputAt, repeatSizeAt);
+    op_api::repeat(inputAt, repeatSizeAt);
     END_CALL_ACL_OP();
 }
 
