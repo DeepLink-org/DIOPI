bffbc3485d7ad526ffb47e9e07a46666167c809c
Fu Jingguo <fujingguo@sensetime.com>
2024-03-05 16:28:41 +0800

    [Ascend] fuj/ reimpl any, mm, fix sum and mean impl with opapi (#987)
    * fix sum and mean impl with opapi for ascend
    
    * reimpl any
    
    * move sum and any to ascend

--- impl/ascend_npu/diopi_impl/reduce.cpp
+++ impl/ascend_npu/diopi_impl/reduce.cpp
@@ -13,26 +13,23 @@ namespace OP_IMPL_NS {
 diopiError_t diopiSum(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t dim) {
     BEGIN_CALL_ACL_OP(input, out);
     if (inputAt.numel() == 0) {
-        return diopiSuccess;
+        op_api::fill_(outAt, c10::Scalar(0.0));
+        END_CALL_ACL_OP();
     }
 
     if (inputAt.dim() == 0) {
         diopiCopyInp(ctx, input, out);
-        return diopiSuccess;
+        END_CALL_ACL_OP();
     }
 
     bool keepdim = true;
-    diopiSize_t inS, outS;
-    diopiGetTensorShape(input, &inS);
-    diopiGetTensorShape(out, &outS);
-
-    if (inS.len != outS.len) {
+    if (inputAt.dim() != outAt.dim()) {
         keepdim = false;
     }
 
     at::ArrayRef<int64_t> rdim(dim.data, dim.len);
 
-    op_api::sum_out(inputAt, rdim, keepdim, inputAt.scalar_type(), outAt);
+    op_api::sum_out(inputAt, rdim, keepdim, outAt.scalar_type(), outAt);
 
     END_CALL_ACL_OP();
 }
@@ -40,26 +37,44 @@ diopiError_t diopiSum(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiCo
 diopiError_t diopiMean(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t dim) {
     BEGIN_CALL_ACL_OP(input, out);
     if (inputAt.numel() == 0) {
-        return diopiSuccess;
+        op_api::fill_(outAt, c10::Scalar(std::nanf("")));
+        END_CALL_ACL_OP();
     }
 
     if (inputAt.dim() == 0) {
         diopiCopyInp(ctx, input, out);
-        return diopiSuccess;
+        END_CALL_ACL_OP();
     }
 
     bool keepdim = true;
-    diopiSize_t inS, outS;
-    diopiGetTensorShape(input, &inS);
-    diopiGetTensorShape(out, &outS);
-
-    if (inS.len != outS.len) {
+    if (inputAt.dim() != outAt.dim()) {
         keepdim = false;
     }
 
     at::ArrayRef<int64_t> rdim(dim.data, dim.len);
 
-    op_api::mean_out(inputAt, rdim, keepdim, inputAt.scalar_type(), outAt);
+    op_api::mean_out(inputAt, rdim, keepdim, outAt.scalar_type(), outAt);
+
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiAny(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const int64_t* dim) {
+    BEGIN_CALL_ACL_OP(input, out);
+
+    if (inputAt.numel() == 0) {
+        op_api::fill_(outAt, c10::Scalar(0.0));
+        END_CALL_ACL_OP();
+    }
+
+    if (dim == nullptr) {
+        op_api::any_out(inputAt, outAt);
+    } else {
+        bool keepdim = false;
+        if (inputAt.dim() == outAt.dim()) {
+            keepdim = true;
+        }
+        op_api::any_out(inputAt, *dim, keepdim, outAt);
+    }
 
     END_CALL_ACL_OP();
 }
