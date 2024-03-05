8fc3d440dc8a80528182bb05cfaa989f1e8323c2
zhangzefeng92 <zhang_zefeng@foxmail.com>
2024-03-05 15:58:14 +0800

    aclnn onehot

--- impl/ascend_npu/diopi_impl/onehot.cpp
+++ impl/ascend_npu/diopi_impl/onehot.cpp
@@ -11,29 +11,29 @@
 
 namespace OP_IMPL_NS {
 
-static const int64_t MIN_DEPTH = 1;
-static const int64_t AUTO_DEPTH = -1;
-static const int64_t MIN_NUM_CLASSES = 0;
+static const int64_t minDepth = 1;
+static const int64_t authDepth = -1;
+static const int64_t minNumClass = 0;
 
 diopiError_t diopiOneHot(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t numClasses) {
     BEGIN_CALL_ACL_OP(input, out);
     int64_t depth = numClasses;
-    TORCH_CHECK(depth >= AUTO_DEPTH, "NPU error, not yet support negative num_classes, when num_classes less than -1");
-    TORCH_CHECK(inputAt.numel() != 0 || numClasses > MIN_NUM_CLASSES, "NPU error, can not infer total number of classes from empty tensor.");
-    if (depth == AUTO_DEPTH) {
+    TORCH_CHECK(depth >= authDepth, "NPU error, not yet support negative num_classes, when num_classes less than -1");
+    TORCH_CHECK(inputAt.numel() != 0 || numClasses > minNumClass, "NPU error, can not infer total number of classes from empty tensor.");
+    if (depth == authDepth) {
         depth = inputAt.max().item().toLong() + 1;
-        if (depth < MIN_DEPTH) {
-            depth = MIN_DEPTH;
+        if (depth < minDepth) {
+            depth = minDepth;
         }
     }
-    at::Tensor on_value_tensor = at_npu::native::OpPreparation::apply_tensor_without_format({1}, inputAt.options());
-    op_api::fill_(on_value_tensor, 1);
-    at::Tensor off_value_tensor = at_npu::native::OpPreparation::apply_tensor_without_format({1}, inputAt.options());
-    op_api::fill_(off_value_tensor, 0);
+    at::Tensor onValueTensor = at_npu::native::OpPreparation::apply_tensor_without_format({1}, inputAt.options());
+    op_api::fill_(onValueTensor, 1);
+    at::Tensor offValueTensor = at_npu::native::OpPreparation::apply_tensor_without_format({1}, inputAt.options());
+    op_api::fill_(offValueTensor, 0);
     auto output_size = op_infer::array_to_small_vector(inputAt.sizes());
     output_size.emplace_back(depth);
     int64_t axis = -1;
-    EXEC_NPU_CMD(aclnnOneHot, inputAt, depth, on_value_tensor, off_value_tensor, axis, outAt);
+    EXEC_NPU_CMD(aclnnOneHot, inputAt, depth, onValueTensor, offValueTensor, axis, outAt);
     END_CALL_ACL_OP();
 }
 
