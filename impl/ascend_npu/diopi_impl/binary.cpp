3f181fe3d937b7f8af8f6eec7d0cac49f3bb3173
zhangzefeng92 <zhang_zefeng@foxmail.com>
2024-03-06 14:56:48 +0800

    fix add

--- impl/ascend_npu/diopi_impl/binary.cpp
+++ impl/ascend_npu/diopi_impl/binary.cpp
@@ -71,39 +71,26 @@ diopiError_t diopiDivInpScalar(diopiContextHandle_t ctx, diopiTensorHandle_t inp
 diopiError_t diopiAdd(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t other,
                       const diopiScalar_t* alpha) {
     BEGIN_CALL_ACL_OP(input, out, alpha, other);
-    if (!outAt.defined() || outAt.numel() <= 0) {
-        return diopiSuccess;
-    }
-
-    acl_op::add_out(inputAt, otherAt, alphaAt, outAt);
+    op_api::add_out(inputAt, otherAt, alphaAt, outAt);
     END_CALL_ACL_OP();
 }
 
 diopiError_t diopiAddInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t other, const diopiScalar_t* alpha) {
     BEGIN_CALL_ACL_OP(input, other, alpha);
-    if (!inputAt.defined() || inputAt.numel() <= 0) {
-        return diopiSuccess;
-    }
-    acl_op::add_out(inputAt, otherAt, alphaAt, inputAt);
+    op_api::add_(inputAt, otherAt, alphaAt);
     END_CALL_ACL_OP();
 }
 
 diopiError_t diopiAddScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* other,
                             const diopiScalar_t* alpha) {
     BEGIN_CALL_ACL_OP(input, other, alpha, out);
-    if (!outAt.defined() || outAt.numel() <= 0) {
-        return diopiSuccess;
-    }
-    acl_op::add_out(inputAt, at::scalar_to_tensor(otherAt).to(inputAt.dtype()), alphaAt, outAt);
+    EXEC_NPU_CMD(aclnnAdds, inputAt, otherAt, alpha, outAt);
     END_CALL_ACL_OP();
 }
 
 diopiError_t diopiAddInpScalar(diopiContextHandle_t ctx, diopiTensorHandle_t input, const diopiScalar_t* other, const diopiScalar_t* alpha) {
     BEGIN_CALL_ACL_OP(input, other, alpha);
-    if (!inputAt.defined() || inputAt.numel() <= 0) {
-        return diopiSuccess;
-    }
-    acl_op::add_(inputAt, at::scalar_to_tensor(otherAt).to(inputAt.dtype()), alphaAt);
+    acl_op::add_(inputAt, otherAt, alphaAt);
     END_CALL_ACL_OP();
 }
 
