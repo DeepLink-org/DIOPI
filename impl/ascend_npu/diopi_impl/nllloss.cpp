babf8c437f7ec5661132b6ee4d0129633982ecc4
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-03-06 13:27:31 +0800

    optimize nll_loss

--- impl/ascend_npu/diopi_impl/nllloss.cpp
+++ impl/ascend_npu/diopi_impl/nllloss.cpp
@@ -45,6 +45,31 @@ diopiError_t diopiNLLLoss(diopiContextHandle_t ctx, diopiTensorHandle_t out, dio
     END_CALL_ACL_OP();
 }
 
+inline at::Tensor calTotalWeight(const at::Tensor& input, const at::Tensor& weight, const at::Tensor& target, const int64_t ignoreIndex) {
+    at::Tensor totalWeight;
+    // Flatten the target tensor for easier processing
+    auto flatTarget = target.view(-1);
+
+    // calculate totalWeight
+    // Create a mask corresponding to ignoreIndex if it's provided
+    auto mask = at_npu::native::empty_npu(flatTarget.sizes(), flatTarget.options().dtype(at::kBool));
+    if (ignoreIndex >= 0) {
+        op_api::ne_out(flatTarget, at::Scalar(ignoreIndex), mask);
+    } else {
+        mask.fill_(true);
+    }
+    if (weight.defined()) {
+        // Filter out the targets using the mask and compute total weight using index_select
+        auto selectedTargetAt = op_api::masked_select(flatTarget, mask);
+        auto selectedWeightAt = op_api::index_select(weight, 0, selectedTargetAt);
+        totalWeight = op_api::sum(selectedWeightAt, weight.scalar_type());
+    } else {
+        // If weight is not defined, compute total weight by counting the valid targets
+        totalWeight = op_api::sum(mask, input.scalar_type());
+    }
+    return totalWeight;
+}
+
 diopiError_t diopiNLLLossBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput, diopiConstTensorHandle_t input,
                                   diopiConstTensorHandle_t target, diopiConstTensorHandle_t weight, diopiReduction_t reduction, int64_t ignoreIndex) {
     BEGIN_CALL_ACL_OP(input, target, weight, gradInput, gradOutput);
@@ -56,27 +81,7 @@ diopiError_t diopiNLLLossBackward(diopiContextHandle_t ctx, diopiTensorHandle_t 
      * In case a weight tensor is provided, total_weight represents the sum of weights for all the non-ignored indices in the target tensor.
      * When no weight tensor is provided, total_weight corresponds to the count of all non-ignored indices.
      */
-    at::Tensor totalWeightAt;
-    // Flatten the target tensor for easier processing
-    auto flatTarget = targetAt.view(-1);
-
-    // calculate totalWeight
-    // Create a mask corresponding to ignoreIndex if it's provided
-    auto mask = at_npu::native::empty_npu(flatTarget.sizes(), flatTarget.options().dtype(at::kBool));
-    if (ignoreIndex >= 0) {
-        op_api::ne_out(flatTarget, at::Scalar(ignoreIndex), mask);
-    } else {
-        mask.fill_(true);
-    }
-    if (weightAt.defined()) {
-        // Filter out the targets using the mask and compute total weight using index_select
-        auto selectedTargetAt = op_api::masked_select(flatTarget, mask);
-        auto selectedWeightAt = op_api::index_select(flatTarget, 0, selectedTargetAt);
-        totalWeightAt = op_api::sum(selectedWeightAt, weightAt.scalar_type());
-    } else {
-        // If weight is not defined, compute total weight by counting the valid targets
-        totalWeightAt = op_api::sum(mask, inputAt.scalar_type());
-    }
+    at::Tensor totalWeightAt = calTotalWeight(inputAt, weightAt, targetAt, ignoreIndex);
 
     if (inputAt.dim() <= 2) {
         op_api::nll_loss_backward_out(gradOutputAt, inputAt, targetAt, weightAt, reduction, ignoreIndex, totalWeightAt, gradInputAt);
