4d94daf8a57bbf738bb197ea653c3dbcd5ebab46
Zhangzefeng <zhang_zefeng@foxmail.com>
2024-03-06 10:06:55 +0800

    Update logical.cpp

--- impl/ascend_npu/diopi_impl/logical.cpp
+++ impl/ascend_npu/diopi_impl/logical.cpp
@@ -24,13 +24,13 @@ diopiError_t diopiLogicalOr(diopiContextHandle_t ctx, diopiTensorHandle_t out, d
 
 diopiError_t diopiLogicalAndInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t other) {
     BEGIN_CALL_ACL_OP(input, other);
-    op_api::logical_and_out(inputAt, otherAt, inputAt);
+    op_api::logical_and_(inputAt, otherAt);
     END_CALL_ACL_OP();
 }
 
 diopiError_t diopiLogicalOrInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t other) {
     BEGIN_CALL_ACL_OP(input, other);
-    op_api::logical_or_out(inputAt, otherAt, inputAt);
+    op_api::logical_or_(inputAt, otherAt);
     END_CALL_ACL_OP();
 }
 
@@ -42,7 +42,7 @@ diopiError_t diopiLogicalNot(diopiContextHandle_t ctx, diopiTensorHandle_t out, 
 
 diopiError_t diopiLogicalNotInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
     BEGIN_CALL_ACL_OP(input);
-    op_api::logical_not_out(inputAt, inputAt);
+    op_api::logical_not_(inputAt);
     END_CALL_ACL_OP();
 }
 
