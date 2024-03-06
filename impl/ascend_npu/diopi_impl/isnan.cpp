80ecf6c226d6c0e3547201edefe4f1af1de5cca2
Zhangzefeng <zhang_zefeng@foxmail.com>
2024-03-06 10:12:07 +0800

    Update isnan.cpp

--- impl/ascend_npu/diopi_impl/isnan.cpp
+++ impl/ascend_npu/diopi_impl/isnan.cpp
@@ -12,8 +12,7 @@ namespace OP_IMPL_NS {
 
 DIOPI_API diopiError_t diopiIsNan(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
     BEGIN_CALL_ACL_OP(input, out);
-    at::Tensor outTemp = op_api::isclose(inputAt, inputAt);
-    op_api::logical_not_out(outTemp, outAt);
+    op_api::ne_out(inputAt, inputAt, outAt);
     END_CALL_ACL_OP();
 }
 
