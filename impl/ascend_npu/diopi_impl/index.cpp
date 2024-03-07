2372b7bf451c57fd9ec1d6ac9d3c1c9b06efa3e3
POI-WX <wangxing2@sensetime.com>
2024-03-07 17:23:38 +0800

    modify codes of index and index_put op

--- impl/ascend_npu/diopi_impl/index.cpp
+++ impl/ascend_npu/diopi_impl/index.cpp
@@ -6,21 +6,13 @@
 
 #include "helper.hpp"
 #include "op_plugin/OpApiInterface.h"
+#include "torch_npu/csrc/framework/DIOPIAdapter.h"
+
+namespace impl::aten {
+c10::List<c10::optional<at::Tensor>> castIntIndicesToLongIndices(const c10::List<c10::optional<at::Tensor>>& indices);
+};
 
 namespace OP_IMPL_NS {
-
-static c10::List<c10::optional<at::Tensor>> castIntIndicesToLongIndices(const c10::List<c10::optional<at::Tensor>>& indices) {
-    c10::List<c10::optional<at::Tensor>> result;
-    for (c10::optional<at::Tensor> indexOpt : indices) {
-        if (!indexOpt.has_value()) {
-            result.emplace_back();
-        } else {
-            at::Tensor index = std::move(*indexOpt);
-            result.emplace_back(index.scalar_type() == at::kInt ? index.toType(at::kLong) : index);
-        }
-    }
-    return result;
-}
 
 diopiError_t diopiIndex(diopiContextHandle_t ctx, diopiTensorHandle_t* out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t* indices, int64_t nums) {
     BEGIN_CALL_ACL_OP(input);
@@ -30,7 +22,7 @@ diopiError_t diopiIndex(diopiContextHandle_t ctx, diopiTensorHandle_t* out, diop
         indicesAtList.emplace_back(impl::aten::buildATen(indices[i]));
     }
 
-    auto indicesCast = castIntIndicesToLongIndices(indicesAtList);
+    auto indicesCast = impl::aten::castIntIndicesToLongIndices(indicesAtList);
     at::Tensor outAt = op_api::index(inputAt, indicesCast);
     impl::aten::buildDiopiTensor(ctx, outAt, out);
     END_CALL_ACL_OP();
@@ -45,7 +37,7 @@ diopiError_t diopiIndexBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gr
         indicesAtList.emplace_back(impl::aten::buildATen(indices[i]));
     }
 
-    auto indicesCast = castIntIndicesToLongIndices(indicesAtList);
+    auto indicesCast = impl::aten::castIntIndicesToLongIndices(indicesAtList);
     op_api::_index_put_impl_(zerosLikeInputAt, indicesCast, gradOutputAt, true, false);
     gradInputAt.copy_(zerosLikeInputAt);
     END_CALL_ACL_OP();
--- impl/ascend_npu/diopi_impl/index_put.cpp
+++ impl/ascend_npu/diopi_impl/index_put.cpp
@@ -6,21 +6,13 @@
 
 #include "helper.hpp"
 #include "op_plugin/OpApiInterface.h"
+#include "torch_npu/csrc/framework/DIOPIAdapter.h"
+
+namespace impl::aten {
+c10::List<c10::optional<at::Tensor>> castIntIndicesToLongIndices(const c10::List<c10::optional<at::Tensor>>& indices);
+};
 
 namespace OP_IMPL_NS {
-
-static c10::List<c10::optional<at::Tensor>> castIntIndicesToLongIndices(const c10::List<c10::optional<at::Tensor>>& indices) {
-    c10::List<c10::optional<at::Tensor>> result;
-    for (c10::optional<at::Tensor> indexOpt : indices) {
-        if (!indexOpt.has_value()) {
-            result.emplace_back();
-        } else {
-            at::Tensor index = std::move(*indexOpt);
-            result.emplace_back(index.scalar_type() == at::kInt ? index.toType(at::kLong) : index);
-        }
-    }
-    return result;
-}
 
 diopiError_t diopiIndexPut(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t values,
                            diopiConstTensorHandle_t* indices, int64_t indicesCounts, bool accumulate) {
@@ -32,7 +24,7 @@ diopiError_t diopiIndexPut(diopiContextHandle_t ctx, diopiTensorHandle_t out, di
     }
 
     outAt.copy_(inputAt);
-    auto indicesCast = castIntIndicesToLongIndices(indicesAtList);
+    auto indicesCast = impl::aten::castIntIndicesToLongIndices(indicesAtList);
     op_api::_index_put_impl_(outAt, indicesCast, valuesAt, accumulate, false);
     END_CALL_ACL_OP();
 }
@@ -46,7 +38,7 @@ diopiError_t diopiIndexPutInp(diopiContextHandle_t ctx, diopiTensorHandle_t inpu
         indicesAtList.emplace_back(impl::aten::buildATen(indices[i]));
     }
 
-    auto indicesCast = castIntIndicesToLongIndices(indicesAtList);
+    auto indicesCast = impl::aten::castIntIndicesToLongIndices(indicesAtList);
     op_api::_index_put_impl_(inputAt, indicesCast, valuesAt, accumulate, false);
     END_CALL_ACL_OP();
 }
