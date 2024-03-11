a2bd9a03bdaa65a47763ff44da491679799ca2d3
POI-WX <wangxing2@sensetime.com>
2024-03-11 11:49:46 +0800

    temporarily impl diopiFlashAttentionBackward

--- impl/ascend_npu/diopi_impl/functions_ext/flash_attention.cpp
+++ impl/ascend_npu/diopi_impl/functions_ext/flash_attention.cpp
@@ -37,11 +37,11 @@ diopiError_t diopiFlashAttention(diopiContextHandle_t ctx, diopiTensorHandle_t a
     DIOPI_CHECK(pDropout >= 0 && pDropout <= 1, "The p_dropout value must be in range of [0, 1]");
 
     std::string inputLayout = "BSND";
-    int64_t B = qAt.size(0);
-    int64_t S0 = qAt.size(1);  // S for query
-    int64_t S1 = kAt.size(1);  // S for key & value
-    int64_t N = qAt.size(2);
-    int64_t D = qAt.size(3);
+    int64_t b = qAt.size(0);
+    int64_t s0 = qAt.size(1);  // S for query
+    int64_t s1 = kAt.size(1);  // S for key & value
+    int64_t n = qAt.size(2);
+    int64_t d = qAt.size(3);
     double keepProb = 1 - pDropout;
 
     at::Tensor pseAt = at::Tensor();
@@ -69,9 +69,9 @@ diopiError_t diopiFlashAttention(diopiContextHandle_t ctx, diopiTensorHandle_t a
     at::Tensor softmaxSumAt;
     at::Tensor softmaxOutAt;
 
-    softmaxMaxAt = at_npu::native::OpPreparation::apply_tensor_without_format({B, N, S0, 8},
+    softmaxMaxAt = at_npu::native::OpPreparation::apply_tensor_without_format({b, n, s0, 8},
                                                                               qAt.options().dtype(at::kFloat));  // [B, N, S0, 8]
-    softmaxSumAt = at_npu::native::OpPreparation::apply_tensor_without_format({B, N, S0, 8},
+    softmaxSumAt = at_npu::native::OpPreparation::apply_tensor_without_format({b, n, s0, 8},
                                                                               qAt.options().dtype(at::kFloat));  // [B, N, S0, 8]
     softmaxOutAt = at::empty({0}, qAt.options());
 
@@ -88,7 +88,7 @@ diopiError_t diopiFlashAttention(diopiContextHandle_t ctx, diopiTensorHandle_t a
                                  keepProb,
                                  preTokens,
                                  nextTokens,
-                                 N,
+                                 n,
                                  inputLayoutPtr,
                                  innerPrecise,
                                  sparseMode,
@@ -100,6 +100,77 @@ diopiError_t diopiFlashAttention(diopiContextHandle_t ctx, diopiTensorHandle_t a
     impl::aten::buildDiopiTensor(ctx, softmaxMaxAt, softmaxMax);
     impl::aten::buildDiopiTensor(ctx, softmaxSumAt, softmaxSum);
     impl::aten::buildDiopiTensor(ctx, softmaxOutAt, softmaxOut);
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiFlashAttentionBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradQ, diopiTensorHandle_t gradK, diopiTensorHandle_t gradV,
+                                         diopiConstTensorHandle_t gradOut, diopiConstTensorHandle_t q, diopiConstTensorHandle_t k, diopiConstTensorHandle_t v,
+                                         diopiConstTensorHandle_t attentionOut, diopiConstTensorHandle_t softmaxMax, diopiConstTensorHandle_t softmaxSum,
+                                         diopiConstTensorHandle_t softmaxOut, diopiGeneratorHandle_t gen, double pDropout, double softmaxScale, bool isCausal) {
+    BEGIN_CALL_ACL_OP(q, k, v, attentionOut, softmaxMax, softmaxSum, softmaxOut, gen, gradQ, gradK, gradV, gradOut);
+
+    DIOPI_CHECK(qAt.dim() == 4, "The shapes of the input query should be 4-dimensional");
+    DIOPI_CHECK(kAt.dim() == 4, "The shapes of the input key should be 4-dimensional");
+    DIOPI_CHECK(vAt.dim() == 4, "The shapes of the input value should be 4-dimensional");
+    DIOPI_CHECK(pDropout >= 0 && pDropout <= 1, "The p_dropout value must be in range of [0, 1]");
+
+    std::string inputLayout = "BSND";
+    int64_t b = qAt.size(0);
+    int64_t s0 = qAt.size(1);  // S for query
+    int64_t s1 = kAt.size(1);  // S for key & value
+    int64_t n = qAt.size(2);
+    int64_t d = qAt.size(3);
+    double keepProb = 1 - pDropout;
+
+    at::Tensor pseAt = at::Tensor();
+    at::Tensor gradPseAt = at::empty({0}, qAt.options());
+    at::IntArrayRef prefixN = at::IntArrayRef{};
+
+    at::Tensor paddingMaskAt = at::Tensor();
+
+    at::Tensor dropMaskAt = at::Tensor();
+    if (pDropout != 0) {
+        // int64_t length = (input.numel() + bitNumber - 1) / bitNumber * bitNumber / uInt8BitNumber;
+        // dropMaskAt = at_npu::native::OpPreparation::apply_tensor_without_format({length}, input.options().dtype(at::kByte));
+    }
+
+    at::Tensor attentionMaskAt = at::Tensor();
+    if (isCausal) {
+    }
+
+    char* inputLayoutPtr = const_cast<char*>(inputLayout.c_str());
+    int64_t preTokens = kAt.size(1);
+    int64_t nextTokens = 0;
+    int64_t innerPrecise = 0;  // 保留参数，暂未使用。0, fp16 high precision. 1, high performance.
+    int64_t sparseMode = 0;
+
+    EXEC_NPU_NO_FORMAT_CHECK_CMD(aclnnFlashAttentionScoreGrad,
+                                 qAt,
+                                 kAt,
+                                 vAt,
+                                 gradOutAt,
+                                 pseAt,
+                                 dropMaskAt,
+                                 paddingMaskAt,
+                                 attentionMaskAt,
+                                 softmaxMaxAt,
+                                 softmaxSumAt,
+                                 softmaxOutAt,
+                                 attentionOutAt,
+                                 prefixN,
+                                 softmaxScale,
+                                 keepProb,
+                                 preTokens,
+                                 nextTokens,
+                                 n,
+                                 inputLayoutPtr,
+                                 innerPrecise,
+                                 sparseMode,
+                                 gradQAt,
+                                 gradKAt,
+                                 gradVAt,
+                                 gradPseAt);
+
     END_CALL_ACL_OP();
 }
 
