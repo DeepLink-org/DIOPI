f064b9e17a4af68d8777c8d9d0e4840827635513
jingguo-st <fujingguo@sensetime.com>
2024-03-13 16:46:12 +0800

    reimpl triu with op api for ascend

---
+++ impl/ascend_npu/diopi_impl/triu.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2024, DeepLink.
+ */
+
+#include "helper.hpp"
+#include "op_plugin/OpApiInterface.h"
+
+namespace OP_IMPL_NS {
+
+diopiError_t diopiTriu(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t diagonal) {
+    BEGIN_CALL_ACL_OP(input, out);
+    op_api::triu_out(inputAt, diagonal, outAt);
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiTriuInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, int64_t diagonal) {
+    BEGIN_CALL_ACL_OP(input);
+    op_api::triu_(inputAt, diagonal);
+    END_CALL_ACL_OP();
+}
+
+}  // namespace OP_IMPL_NS
