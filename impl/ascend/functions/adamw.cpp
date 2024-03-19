a0b35a21d6cd3df0a66905133400b64f17f2ade1
NeosZhang <zhangqiu1994@outlook.com>
2024-03-19 15:27:35 +0800

    fix lint

--- impl/ascend/functions/adamw.cpp
+++ impl/ascend/functions/adamw.cpp
@@ -12,24 +12,22 @@
 
 namespace impl {
 namespace ascend {
-diopiError_t diopiAdamW(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiTensorHandle_t grad, diopiTensorHandle_t exp_avg,
-                        diopiTensorHandle_t exp_avg_sq, diopiTensorHandle_t max_exp_avg_sq, float lr, float beta1, float beta2, float eps, float weight_decay,
-                        int64_t step, bool amsgrad) {
-    std::cout << "calling ascend diopiAdamw" << std::endl;
+diopiError_t diopiAdamW(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiTensorHandle_t grad, diopiTensorHandle_t expAvg, diopiTensorHandle_t expAvgSq,
+                        diopiTensorHandle_t maxExpAvgSq, float lr, float beta1, float beta2, float eps, float weightDecay, int64_t step, bool amsgrad) {
     AscendTensor inputAt(input);
-    if (!inputAt.defined() || inputAt.nueml() == 0) {
+    if (!inputAt.defined() || inputAt.numel() == 0) {
         return diopiSuccess;
     }
     AscendTensor gradAt(grad);
-    AscendTensor expAvgAt(exp_avg);
-    AscendTensor expAvgSqAt(exp_avg_sq);
-    AscendTensor maxExpAvgSqAt(max_exp_avg_sq);
+    AscendTensor expAvgAt(expAvg);
+    AscendTensor expAvgSqAt(expAvgSq);
+    AscendTensor maxExpAvgSqAt(maxExpAvgSq);
     diopiScalar_t beta1Scalar = constructDiopiScalarT(inputAt.dtype(), beta1);
     diopiScalar_t beta2Scalar = constructDiopiScalarT(inputAt.dtype(), beta2);
     diopiScalar_t beta1PowerScalar = constructDiopiScalarT(inputAt.dtype(), pow(beta1, step));
     diopiScalar_t beta2PowerScalar = constructDiopiScalarT(inputAt.dtype(), pow(beta2, step));
     diopiScalar_t lrScalar = constructDiopiScalarT(inputAt.dtype(), lr);
-    diopiScalar_t weightDecayScalar = constructDiopiScalarT(inputAt.dtype(), weight_decay);
+    diopiScalar_t weightDecayScalar = constructDiopiScalarT(inputAt.dtype(), weightDecay);
     diopiScalar_t epsScalar = constructDiopiScalarT(inputAt.dtype(), eps);
 
     AclOpRunner<12, 3>("ApplyAdamW", ctx) adamwRunner.addInput(inputAt)
