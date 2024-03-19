47f3edfde4496cddabd9b46ee1b40b2ad430c2f7
zhaoguochun1995 <zhaoguochun1995@sensetime.com>
2024-03-19 09:02:20 +0000

    add cos sin use op-plugin impl

---
+++ impl/ascend_npu/diopi_impl/cos.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2024, DeepLink.
+ */
+
+#include "helper.hpp"
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace OP_IMPL_NS {
+
+
+diopiError_t diopiCosInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    BEGIN_CALL_ACL_OP(input);
+    if (input == nullptr || inputAt.numel() <= 0) {
+        return diopiSuccess;
+    }
+    op_api::cos_(inputAt);
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiCos(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    BEGIN_CALL_ACL_OP(input, out);
+    if (input == nullptr || inputAt.numel() <= 0) {
+        return diopiSuccess;
+    }
+    op_api::cos_out(inputAt, outAt);
+    END_CALL_ACL_OP();
+}
+
+}  // namespace OP_IMPL_NS
---
+++ impl/ascend_npu/diopi_impl/sin.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2024, DeepLink.
+ */
+
+#include "helper.hpp"
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace OP_IMPL_NS {
+
+diopiError_t diopiSinInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
+    BEGIN_CALL_ACL_OP(input);
+    if (input == nullptr || inputAt.numel() <= 0) {
+        return diopiSuccess;
+    }
+    op_api::sin_(inputAt);
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiSin(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
+    BEGIN_CALL_ACL_OP(input, out);
+    if (input == nullptr || inputAt.numel() <= 0) {
+        return diopiSuccess;
+    }
+    op_api::sin_out(inputAt, outAt);
+    END_CALL_ACL_OP();
+}
+
+}  // namespace OP_IMPL_NS
