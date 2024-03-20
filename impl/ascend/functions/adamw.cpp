d52748068cc9baa2a52032b0487ff5adbbb4332f
NeosZhang <zhangqiu1994@outlook.com>
2024-03-20 21:04:28 +0800

    tiny fix

--- impl/ascend/functions/adamw.cpp
+++ impl/ascend/functions/adamw.cpp
@@ -24,26 +24,19 @@ diopiError_t diopiAdamW(diopiContextHandle_t ctx, diopiTensorHandle_t input, dio
     AscendTensor expAvgAt(expAvg);
     AscendTensor expAvgSqAt(expAvgSq);
     AscendTensor maxExpAvgSqAt(maxExpAvgSq);
-    diopiScalar_t beta1Scalar = constructDiopiScalarT(inputAt.dtype(), beta1);
-    diopiScalar_t beta2Scalar = constructDiopiScalarT(inputAt.dtype(), beta2);
-    diopiScalar_t beta1PowerScalar = constructDiopiScalarT(inputAt.dtype(), pow(beta1, step));
-    diopiScalar_t beta2PowerScalar = constructDiopiScalarT(inputAt.dtype(), pow(beta2, step));
-    diopiScalar_t lrScalar = constructDiopiScalarT(inputAt.dtype(), lr);
-    diopiScalar_t weightDecayScalar = constructDiopiScalarT(inputAt.dtype(), weightDecay);
-    diopiScalar_t epsScalar = constructDiopiScalarT(inputAt.dtype(), eps);
     diopiDtype_t inputDtype = inputAt.dtype();
 
     AclOpRunner<12, 3> adamwRunner("ApplyAdamW", ctx);
     adamwRunner.addInput(inputAt)
         .addInput(expAvgAt)
         .addInput(expAvgSqAt)
-        .addConstInput(beta1PowerScalar, inputDtype)
-        .addConstInput(beta2PowerScalar, inputDtype)
-        .addConstInput(lrScalar, inputDtype)
-        .addConstInput(weightDecayScalar, inputDtype)
-        .addConstInput(beta1Scalar, inputDtype)
-        .addConstInput(beta2Scalar, inputDtype)
-        .addConstInput(epsScalar, inputDtype)
+        .addConstInput(pow(beta1, step), inputDtype)
+        .addConstInput(pow(beta2, step), inputDtype)
+        .addConstInput(lr, inputDtype)
+        .addConstInput(weightDecay, inputDtype)
+        .addConstInput(beta1, inputDtype)
+        .addConstInput(beta2, inputDtype)
+        .addConstInput(eps, inputDtype)
         .addInput(gradAt);
 
     // at present, ApplyAdamW only supports amsgrad false.
