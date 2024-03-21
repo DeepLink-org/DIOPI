0857aba17f0aa5b4f5808528d334dd6433103a56
zhangzefeng92 <zhang_zefeng@foxmail.com>
2024-03-21 10:42:32 +0800

    fix sin and cos to aclnn impl

--- impl/ascend_npu/diopi_impl/sin.cpp
+++ impl/ascend_npu/diopi_impl/sin.cpp
@@ -6,6 +6,7 @@
 
 #include "helper.hpp"
 #include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
 
 namespace OP_IMPL_NS {
 diopiError_t diopiSin(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
