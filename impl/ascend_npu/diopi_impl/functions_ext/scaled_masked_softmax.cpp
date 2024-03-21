100b283fa716ae78c5124dd6be031f0094e063d2
POI-WX <wangxing2@sensetime.com>
2024-03-21 18:56:13 +0800

    fix bug

--- impl/ascend_npu/diopi_impl/functions_ext/scaled_masked_softmax.cpp
+++ impl/ascend_npu/diopi_impl/functions_ext/scaled_masked_softmax.cpp
@@ -10,17 +10,17 @@
 
 namespace OP_IMPL_NS {
 
-namespace {
-
-using npu_preparation = at_npu::native::OpPreparation;
-
-}  // namespace
-
 diopiError_t diopiScaledMaskedSoftmax(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t mask,
                                       double scale, bool fixedTriuMask) {
     BEGIN_CALL_ACL_OP(input, mask, out);
     at_npu::native::OpCommand cmd;
-    cmd.Name("ScaledMaskedSoftmax").Input(inputAt).Input(maskAt).Output(outAt).Attr("scale", scale).Attr("fixed_triu_mask", fixedTriuMask).Run();
+    cmd.Name("ScaledMaskedSoftmax")
+        .Input(inputAt)
+        .Input(maskAt)
+        .Output(outAt)
+        .Attr<c10::Scalar>("scale", scale)
+        .Attr<bool>("fixed_triu_mask", fixedTriuMask)
+        .Run();
     END_CALL_ACL_OP();
 }
 
@@ -33,8 +33,8 @@ diopiError_t diopiScaledMaskedSoftmaxBackward(diopiContextHandle_t ctx, diopiTen
         .Input(outAt)
         .Input(maskAt)
         .Output(gradInputAt)
-        .Attr("scale", scale)
-        .Attr("fixed_triu_mask", fixedTriuMask)
+        .Attr<c10::Scalar>("scale", scale)
+        .Attr<bool>("fixed_triu_mask", fixedTriuMask)
         .Run();
     END_CALL_ACL_OP();
 }
