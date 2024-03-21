c69416690e29d5514e1486ffcd99532250a45b76
POI-WX <wangxing2@sensetime.com>
2024-03-21 17:52:19 +0800

    add diopiScaledMaskedSoftmaxBackward op

---
+++ impl/ascend_npu/diopi_impl/functions_ext/scaled_masked_softmax.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file
+ * @author DeepLink
+ * @copyright  (c) 2024, DeepLink.
+ */
+
+#include "../helper.hpp"
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace OP_IMPL_NS {
+
+namespace {
+
+using npu_preparation = at_npu::native::OpPreparation;
+
+}  // namespace
+
+diopiError_t diopiScaledMaskedSoftmax(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t mask,
+                                      double scale, bool fixedTriuMask) {
+    BEGIN_CALL_ACL_OP(input, mask, out);
+    at_npu::native::OpCommand cmd;
+    cmd.Name("ScaledMaskedSoftmax").Input(inputAt).Input(maskAt).Output(outAt).Attr("scale", scale).Attr("fixed_triu_mask", fixedTriuMask).Run();
+    END_CALL_ACL_OP();
+}
+
+diopiError_t diopiScaledMaskedSoftmaxBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput,
+                                              diopiConstTensorHandle_t out, diopiConstTensorHandle_t mask, double scale, bool fixedTriuMask) {
+    BEGIN_CALL_ACL_OP(gradOutput, out, mask, gradInput);
+    at_npu::native::OpCommand cmd;
+    cmd.Name("ScaledMaskedSoftmaxGrad")
+        .Input(gradOutputAt)
+        .Input(outAt)
+        .Input(maskAt)
+        .Output(gradInputAt)
+        .Attr("scale", scale)
+        .Attr("fixed_triu_mask", fixedTriuMask)
+        .Run();
+    END_CALL_ACL_OP();
+}
+
+}  // namespace OP_IMPL_NS
