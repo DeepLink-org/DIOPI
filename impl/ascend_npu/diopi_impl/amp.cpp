16387182d4a0f01a8e9256127baa0ea5f260404e
YPOI-WX <wangxing2@sensetime.com>
2024-03-25 17:35:44 +0800

    optimize according to pytorch

--- impl/ascend_npu/diopi_impl/amp.cpp
+++ impl/ascend_npu/diopi_impl/amp.cpp
@@ -6,6 +6,7 @@
 
 #include "helper.hpp"
 #include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
 
 namespace OP_IMPL_NS {
 
@@ -22,19 +23,18 @@ diopiError_t diopiAmpForeachNonFiniteCheckAndUnscaleInp(diopiContextHandle_t ctx
     DIOPI_CHECK(foundInfAt.numel() == 1, "found_inf must be a 1-element tensor");
     DIOPI_CHECK(invScaleAt.scalar_type() == at::ScalarType::Float, "inv_scale must be a float tensor");
     DIOPI_CHECK(foundInfAt.scalar_type() == at::ScalarType::Float, "found_inf must be a float tensor");
-
+    // reference to: https://github.com/pytorch/pytorch/blob/069270db60112704c6387cf994cf9b7298731699/aten/src/ATen/native/cuda/AmpKernels.cu#L64
     bool isFinite = true;
-    for (const auto& scaledGrad : scaledGradsList) {
-        if (!op_api::all(op_api::isfinite(scaledGrad)).item<bool>()) {
-            isFinite = false;
-            op_api::ones_out(1, foundInfAt);
-            break;
-        }
-    }
-
     for (auto& scaledGrad : scaledGradsList) {
+        if (isFinite && !op_api::all(op_api::isfinite(scaledGrad)).item<bool>()) {
+            isFinite = false;
+        }
         op_api::mul_(scaledGrad, invScaleAt);
     }
+    if (!isFinite) {
+        EXEC_NPU_CMD(aclnnInplaceOne, foundInfAt);
+    }
+
     END_CALL_ACL_OP();
 }
 
