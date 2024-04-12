cba3e45629cf89a6cea10224b3c7154437163200
YPOI-WX <wangxing2@sensetime.com>
2024-04-12 17:49:27 +0800

    skip some test cases for camb

--- impl/camb/device_configs.py
+++ impl/camb/device_configs.py
@@ -1966,17 +1966,22 @@ device_configs = {
         ),
     ),
 
+    # multi-dimensional normalized_shape is currently skipped on camb
+    'rms_norm_with_normalized_shape': dict(
+        name=['rms_norm'],
+        dtype=[Skip(np.float16), Skip(np.float32), Skip(np.float64)],
+    ),
+
+    # bias is currently skipped on camb
+    'rms_norm_with_bias': dict(
+        name=['rms_norm'],
+        dtype=[Skip(np.float16), Skip(np.float32), Skip(np.float64)],
+    ),
+    
+    # multi-dimensional normalized_shape and bias is currently skipped on camb
     'rms_norm': dict(
-        name=["rms_norm"],
-        tensor_para=dict(
-            args=[
-                {
-                    "ins": ['input'],
-                    # 寒武纪计算正确，gen-data生成的没考虑bias，暂时屏蔽
-                    "dtype": [Skip(np.float16),Skip(np.float32),Skip(np.float64)],
-                },
-            ],
-        ),
+        name=['rms_norm'],
+        dtype=[Skip(np.float16), Skip(np.float32), Skip(np.float64)],
     ),
 
     'multihead_attention': dict(
