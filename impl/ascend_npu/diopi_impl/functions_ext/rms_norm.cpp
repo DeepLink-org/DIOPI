0eda6b2cc0cac966e29d2b051f69ed920945f087
YPOI-WX <wangxing2@sensetime.com>
2024-04-16 14:45:41 +0800

    fix

--- impl/ascend_npu/diopi_impl/functions_ext/rms_norm.cpp
+++ impl/ascend_npu/diopi_impl/functions_ext/rms_norm.cpp
@@ -15,24 +15,16 @@ namespace OP_IMPL_NS {
 diopiError_t diopiRMSNorm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiTensorHandle_t invRms, diopiConstTensorHandle_t input,
                           diopiSize_t normalizedShape, diopiConstTensorHandle_t weight, diopiConstTensorHandle_t bias, double eps) {
     BEGIN_CALL_ACL_OP(out, invRms, input, weight);
-    TORCH_CHECK(
-        1 == normalizedShape.len && normalizedShape.data[0] == inputAt.size(inputAt.dim() - 1) || normalizedShape.len == 0 || normalizedShape.data == nullptr,
-        "normalized shape is not supported on ascend!");
+    TORCH_CHECK(1 == normalizedShape.len && normalizedShape.data[0] == inputAt.size(inputAt.dim() - 1),
+                "normalized shape currently must be the size of the last dimension on ascend!");
 
-    std::tuple<at::Tensor, at::Tensor> result;
     if (false) {
+        std::tuple<at::Tensor, at::Tensor> result;
         result = acl_op::npu_rms_norm(inputAt, weightAt, eps);
         invRmsAt.copy_(std::get<1>(result));
         outAt.copy_(std::get<0>(result));
     } else {
-        if (invRmsAt.scalar_type() != at::kFloat) {
-            at::Tensor invRmsTempAt = at_npu::native::OpPreparation::apply_tensor_with_format(
-                op_infer::rms_norm_npu_output_size(inputAt, weightAt)[1], invRmsAt.options().dtype(at::kFloat), ACL_FORMAT_ND);
-            EXEC_NPU_CMD(aclnnRmsNorm, inputAt, weightAt, eps, outAt, invRmsTempAt);
-            invRmsAt.copy_(invRmsTempAt);
-        } else {
-            EXEC_NPU_CMD(aclnnRmsNorm, inputAt, weightAt, eps, outAt, invRmsAt);
-        }
+        EXEC_NPU_CMD(aclnnRmsNorm, inputAt, weightAt, eps, outAt, invRmsAt);
     }
     END_CALL_ACL_OP();
 }
@@ -41,27 +33,22 @@ diopiError_t diopiRMSNormBackward(diopiContextHandle_t ctx, diopiTensorHandle_t 
                                   diopiConstTensorHandle_t gradOutput, diopiConstTensorHandle_t input, diopiConstTensorHandle_t weight,
                                   diopiConstTensorHandle_t bias, diopiConstTensorHandle_t invRms, diopiSize_t normalizedShape, double eps) {
     BEGIN_CALL_ACL_OP(gradInput, gradWeight, gradOutput, input, weight, invRms);
-    TORCH_CHECK(
-        1 == normalizedShape.len && normalizedShape.data[0] == inputAt.size(inputAt.dim() - 1) || normalizedShape.len == 0 || normalizedShape.data == nullptr,
-        "normalized shape is not supported on ascend!");
-    at::Tensor invRmsTempAt = invRmsAt;
-    if (invRmsAt.scalar_type() != at::kFloat) {
-        invRmsTempAt = invRmsAt.to(at::kFloat);
-    }
+    TORCH_CHECK(1 == normalizedShape.len && normalizedShape.data[0] == inputAt.size(inputAt.dim() - 1),
+                "normalized shape currently must be the size of the last dimension on ascend!");
 
     if (false) {
         std::tuple<at::Tensor, at::Tensor> result;
-        result = acl_op::npu_rms_norm_backward(gradOutputAt, inputAt, weightAt, invRmsTempAt);
+        result = acl_op::npu_rms_norm_backward(gradOutputAt, inputAt, weightAt, invRmsAt);
         gradInputAt.copy_(std::get<0>(result));
         gradWeightAt.copy_(std::get<1>(result));
     } else {
         if (gradWeightAt.scalar_type() != at::kFloat) {
             at::Tensor gradWeightTempAt = at_npu::native::OpPreparation::apply_tensor_with_format(
                 op_infer::rms_norm_grad_npu_output_size(inputAt, weightAt)[1], gradWeightAt.options().dtype(at::kFloat), ACL_FORMAT_ND);
-            EXEC_NPU_CMD(aclnnRmsNormGrad, gradOutputAt, inputAt, invRmsTempAt, weightAt, gradInputAt, gradWeightTempAt);
+            EXEC_NPU_CMD(aclnnRmsNormGrad, gradOutputAt, inputAt, invRmsAt, weightAt, gradInputAt, gradWeightTempAt);
             gradWeightAt.copy_(gradWeightTempAt);
         } else {
-            EXEC_NPU_CMD(aclnnRmsNormGrad, gradOutputAt, inputAt, invRmsTempAt, weightAt, gradInputAt, gradWeightAt);
+            EXEC_NPU_CMD(aclnnRmsNormGrad, gradOutputAt, inputAt, invRmsAt, weightAt, gradInputAt, gradWeightAt);
         }
     }
     END_CALL_ACL_OP();
