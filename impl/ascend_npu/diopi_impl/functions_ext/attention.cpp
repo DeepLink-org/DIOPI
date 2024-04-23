63fb60074962a43e506e8acc38c647b5ffce7219
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-04-23 17:29:53 +0800

    optimize attention args desc and delete atten_mask in backward

--- impl/ascend_npu/diopi_impl/functions_ext/attention.cpp
+++ impl/ascend_npu/diopi_impl/functions_ext/attention.cpp
@@ -21,9 +21,9 @@ diopiError_t diopiAttention(diopiContextHandle_t ctx, diopiTensorHandle_t attent
                             diopiConstTensorHandle_t q, diopiConstTensorHandle_t k, diopiConstTensorHandle_t v, diopiConstTensorHandle_t attention_mask,
                             double p_dropout, diopiGeneratorHandle_t gen_dropout, double softmax_scale, bool is_causal, const char* attention_type) {
     BEGIN_CALL_ACL_OP(attention_out, q, k, v, attention_mask, gen_dropout);
-    TORCH_CHECK(qAt.dim() == 4, "The shapes of the input query should be 4 dimensional, but got ", qAt.dim(), "-dimensional");
-    TORCH_CHECK(kAt.dim() == 4, "The shapes of the input key should be 4 dimensional, but got ", kAt.dim(), "-dimensional");
-    TORCH_CHECK(vAt.dim() == 4, "The shapes of the input value should be 4 dimensional, but got ", vAt.dim(), "-dimensional");
+    TORCH_CHECK(qAt.dim() == 4 || qAt.dim() == 3, "The shapes of the input query should be 4 or 3 dimensional, but got ", qAt.dim(), "-dimensional");
+    TORCH_CHECK(kAt.dim() == 4 || kAt.dim() == 3, "The shapes of the input key should be 4 or 3 dimensional, but got ", kAt.dim(), "-dimensional");
+    TORCH_CHECK(vAt.dim() == 4 || vAt.dim() == 3, "The shapes of the input value should be 4 or 3 dimensional, but got ", vAt.dim(), "-dimensional");
     at::Tensor realShiftOptional;
     at::Tensor dropMaskOptional;
     at::Tensor paddingMaskOptional;
@@ -32,14 +32,15 @@ diopiError_t diopiAttention(diopiContextHandle_t ctx, diopiTensorHandle_t attent
     double scaleValueOptional = softmax_scale;
     double keepProbOptional = 1 - p_dropout;
     int64_t nextTockensOptional = 0;
-    const char* inputLayout = "BNSD";
     const int64_t innerPreciseOptional = 0;
     const int64_t sparseModeOptional = 0;
+    const bool is_4d_input = qAt.sizes().size() == 4;
+    const char* inputLayout = is_4d_input ? "BNSD" : "BSH";
+    const int64_t headNum = is_4d_input ? qAt.size(1) : 1;
     const int64_t B = qAt.size(0);
-    const int64_t Sq = qAt.size(2);
-    const int64_t Sk = kAt.size(2);
-    const int64_t N = qAt.size(1);
-    const int64_t headNum = qAt.size(1);
+    const int64_t Sq = is_4d_input ? qAt.size(2) : qAt.size(1);
+    const int64_t Sk = is_4d_input ? kAt.size(2) : kAt.size(1);
+    const int64_t N = headNum;
     const int64_t preTockensOptional = Sk;
 
     const auto qShape = qAt.sizes();
@@ -48,15 +49,11 @@ diopiError_t diopiAttention(diopiContextHandle_t ctx, diopiTensorHandle_t attent
     at::Tensor softmaxMaxOut = at_npu::native::empty_npu(softmaxMaxShape, attention_outAt.options().dtype(at::kFloat));
     at::Tensor softmaxSumOut = at_npu::native::empty_npu(softmaxMaxShape, attention_outAt.options().dtype(at::kFloat));
     at::Tensor softmaxOutOut = at_npu::native::empty_npu({0}, attention_outAt.options().dtype(at::kFloat));
-    DEBUG_ARGS(softmaxMaxOut);
-    DEBUG_ARGS(softmaxSumOut);
-    DEBUG_ARGS(softmaxOutOut);
     if (is_causal) {
         attentionMaskOptional = at_npu::native::empty_npu({Sq, Sk}, qAt.options().dtype(at::kBool));  // [Sq, Sk]
         EXEC_NPU_CMD(aclnnInplaceOne, attentionMaskOptional);
         int64_t diagonal = 1;
         EXEC_NPU_CMD(aclnnInplaceTriu, attentionMaskOptional, diagonal);
-        DEBUG_ARGS(attentionMaskOptional);
     }
 
     TORCH_CHECK(keepProbOptional >= 0 && keepProbOptional <= 1, "The keep_prob value must be in range of [0, 1], but got ", keepProbOptional);
@@ -85,16 +82,16 @@ diopiError_t diopiAttention(diopiContextHandle_t ctx, diopiTensorHandle_t attent
     save_for_backward[0] = torch_npu::NPUBridge::GetNpuStorageImpl(softmaxMaxOut)->npu_desc_.diopi_tensor_;
     save_for_backward[1] = torch_npu::NPUBridge::GetNpuStorageImpl(softmaxSumOut)->npu_desc_.diopi_tensor_;
     save_for_backward[2] = torch_npu::NPUBridge::GetNpuStorageImpl(softmaxOutOut)->npu_desc_.diopi_tensor_;
-    *save_tensor_num = 3;
+    save_for_backward[3] = attentionMaskOptional.defined() ? torch_npu::NPUBridge::GetNpuStorageImpl(attentionMaskOptional)->npu_desc_.diopi_tensor_ : nullptr;
+    *save_tensor_num = 4;
     END_CALL_ACL_OP();
 }
 
 diopiError_t diopiAttentionBackward(diopiContextHandle_t ctx, diopiTensorHandle_t grad_q, diopiTensorHandle_t grad_k, diopiTensorHandle_t grad_v,
                                     diopiConstTensorHandle_t grad_out, diopiConstTensorHandle_t q, diopiConstTensorHandle_t k, diopiConstTensorHandle_t v,
-                                    diopiConstTensorHandle_t attention_out, diopiConstTensorHandle_t attention_mask,
-                                    diopiConstTensorHandle_t* saved_for_backward, int64_t saved_tensor_num, double p_dropout,
-                                    diopiGeneratorHandle_t gen_dropout, double softmax_scale, bool is_causal, const char* attention_type) {
-    BEGIN_CALL_ACL_OP(attention_out, q, k, v, grad_q, grad_k, grad_v, grad_out, attention_mask, gen_dropout);
+                                    diopiConstTensorHandle_t attention_out, diopiConstTensorHandle_t* saved_for_backward, int64_t saved_tensor_num,
+                                    double p_dropout, diopiGeneratorHandle_t gen_dropout, double softmax_scale, bool is_causal, const char* attention_type) {
+    BEGIN_CALL_ACL_OP(attention_out, q, k, v, grad_q, grad_k, grad_v, grad_out, gen_dropout);
     at::Tensor pseShiftOptional;
     at::Tensor dropMaskOptional;
     at::Tensor paddingMaskOptional;
@@ -102,6 +99,8 @@ diopiError_t diopiAttentionBackward(diopiContextHandle_t ctx, diopiTensorHandle_
     at::Tensor softmaxMaxOptional = impl::aten::buildATen(saved_for_backward[0]);
     at::Tensor softmaxSumOptional = impl::aten::buildATen(saved_for_backward[1]);
     at::Tensor softmaxInOptional = impl::aten::buildATen(saved_for_backward[2]);
+    at::Tensor attentionMaskOptional = impl::aten::buildATen(saved_for_backward[2]);
+    ;
     at::Tensor prefixOptional;
     double scaleValueOptional = softmax_scale;
     double keepProbOptional = 1 - p_dropout;
@@ -120,7 +119,7 @@ diopiError_t diopiAttentionBackward(diopiContextHandle_t ctx, diopiTensorHandle_
                  pseShiftOptional,
                  dropMaskOptional,
                  paddingMaskOptional,
-                 attention_maskAt,
+                 attentionMaskOptional,
                  attention_outAt,
                  prefixOptional,
                  scaleValueOptional,
--- proto/include/diopi/functions_ext.h
+++ proto/include/diopi/functions_ext.h
@@ -250,17 +250,17 @@ DIOPI_API diopiError_t diopiFlashAttentionBackward(diopiContextHandle_t ctx, dio
  and applying dropout if a probability greater than 0.0 is specified.
  * @param[in] ctx The diopi context.
  * @param[out] attention_out Tensor storing the result after applying flash attention. shape = [batch_size, q_head_num, q_seq_len, head_dim] or [batch_size,
- q_seq_len, q_head_num * head_dim]. type = [bfloat16, float16, float32].
+ q_seq_len, embedding_dim]. type = [bfloat16, float16, float32].
  * @param[out] save_for_backward The intermediate variables that need to be saved for backward
  * @param[out] save_tensor_num The number of the intermediate variables that need to be saved for backward
  * @param[in] attention_mask (optional Tensor) – Attention mask. shape (batch_size, q_seq_len, kv_seq_len). Two types of masks are supported. A boolean mask
  where a value of True indicates that the element should take part in attention. A float mask of the same type as query, key, value that is added to the
  attention score.
- * @param[in] q Query tensor. shape = [batch_size, q_head_num, q_seq_len, head_dim] or [batch_size, q_seq_len, q_head_num * head_dim]. type = [bfloat16,
+ * @param[in] q Query tensor. shape = [batch_size, q_head_num, q_seq_len, head_dim] or [batch_size, q_seq_len, embedding_dim]. type = [bfloat16,
  float16, float32].
- * @param[in] k Key tensor. shape = [batch_size, kv_head_num, kv_seq_len, head_dim] or [batch_size, kv_seq_len, kv_head_num * head_dim]. type = [bfloat16,
+ * @param[in] k Key tensor. shape = [batch_size, kv_head_num, kv_seq_len, head_dim] or [batch_size, kv_seq_len, embedding_dim]. type = [bfloat16,
  float16, float32].
- * @param[in] v Value tensor. shape = [batch_size, kv_head_num, kv_seq_len, head_dim] or [batch_size, kv_seq_len, kv_head_num * head_dim]. type = [bfloat16,
+ * @param[in] v Value tensor. shape = [batch_size, kv_head_num, kv_seq_len, head_dim] or [batch_size, kv_seq_len, embedding_dim]. type = [bfloat16,
  float16, float32].
  * @param[in] p_dropout Dropout probability; if greater than 0.0, dropout is applied.
  * @param[in] gen_dropout Handle for the random number generator used in dropout op.
@@ -275,19 +275,21 @@ DIOPI_API diopiError_t diopiAttention(diopiContextHandle_t ctx, diopiTensorHandl
 /**
  * @brief Compute the backward pass for Attention.
  * @param[in] ctx The diopi context.
- * @param[out] grad_q The gradient of the query tensor. shape = [batch_size, q_head_num, q_seq_len, head_dim] or [batch_size, q_seq_len, q_head_num * head_dim].
+ * @param[out] grad_q The gradient of the query tensor. shape = [batch_size, q_head_num, q_seq_len, head_dim] or [batch_size, q_seq_len, embedding_dim].
  * type = [bfloat16, float16, float32].
  * @param[out] grad_k The gradient of the key tensor. shape = [batch_size, kv_head_num, kv_seq_len, head_dim] or [batch_size, kv_seq_len, kv_head_num *
  * head_dim]. type = [bfloat16, float16, float32].
  * @param[out] grad_v The gradient of the value tensor. shape = [batch_size, kv_seq_len,  head_dim]. type = [bfloat16, float16, float32].
  * @param[in] grad_out The gradient of the output tensor. shape = [batch_size, q_head_num, q_seq_len, head_dim] or [batch_size, q_seq_len, q_head_num *
  * head_dim].  type = [bfloat16, float16, float32].
- * @param[in] k k Key tensor. shape = [batch_size, kv_head_num, kv_seq_len, head_dim] or [batch_size, kv_seq_len, kv_head_num * head_dim]. type = [bfloat16,
+ * @param[in] q Query tensor. shape = [batch_size, q_head_num, q_seq_len, head_dim] or [batch_size, q_seq_len, embedding_dim]. type = [bfloat16,
+ float16, float32].
+ * @param[in] k k Key tensor. shape = [batch_size, kv_head_num, kv_seq_len, head_dim] or [batch_size, kv_seq_len, embedding_dim]. type = [bfloat16,
  * float16, float32].
- * @param[in] v Value tensor. shape = [batch_size, kv_head_num, kv_seq_len, head_dim] or [batch_size, kv_seq_len, kv_head_num * head_dim]. type = [bfloat16,
+ * @param[in] v Value tensor. shape = [batch_size, kv_head_num, kv_seq_len, head_dim] or [batch_size, kv_seq_len, embedding_dim]. type = [bfloat16,
  * float16, float32].
  * @param[in] attention_out Tensor storing the result after applying flash attention. shape = [batch_size, q_head_num, q_seq_len, head_dim] or [batch_size,
- * q_seq_len, q_head_num * head_dim]. type = [bfloat16, float16, float32].
+ * q_seq_len, embedding_dim]. type = [bfloat16, float16, float32].
  * @param[in] attention_mask (optional Tensor) – Attention mask. shape (batch_size, q_seq_len, kv_seq_len)
  * @param[in] save_for_backward The intermediate variables saved in forward
  * @param[out] save_tensor_num The number of the intermediate variables saved in forward
@@ -299,9 +301,9 @@ DIOPI_API diopiError_t diopiAttention(diopiContextHandle_t ctx, diopiTensorHandl
  */
 DIOPI_API diopiError_t diopiAttentionBackward(diopiContextHandle_t ctx, diopiTensorHandle_t grad_q, diopiTensorHandle_t grad_k, diopiTensorHandle_t grad_v,
                                               diopiConstTensorHandle_t grad_out, diopiConstTensorHandle_t q, diopiConstTensorHandle_t k,
-                                              diopiConstTensorHandle_t v, diopiConstTensorHandle_t attention_out, diopiConstTensorHandle_t attention_mask,
-                                              diopiConstTensorHandle_t* saved_for_backward, int64_t saved_tensor_num, double p_dropout,
-                                              diopiGeneratorHandle_t gen_dropout, double softmax_scale, bool is_causal, const char* attention_type);
+                                              diopiConstTensorHandle_t v, diopiConstTensorHandle_t attention_out, diopiConstTensorHandle_t* saved_for_backward,
+                                              int64_t saved_tensor_num, double p_dropout, diopiGeneratorHandle_t gen_dropout, double softmax_scale,
+                                              bool is_causal, const char* attention_type);
 
 /**
  * @brief Computes attention on var Len query, key and value tensors, using an optional attention mask if passed,
