bff24458ec8cb0753b69733719cb7ff57babf41e
root <zsksmhq>
2024-04-26 16:17:18 +0800

    reimpl repeat and fix for corner case

--- impl/ascend_npu/diopi_impl/repeat.cpp
+++ impl/ascend_npu/diopi_impl/repeat.cpp
@@ -5,23 +5,24 @@
  */
 
 #include "helper.hpp"
-#include "op_plugin/AclOpsInterface.h"
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
 
 extern "C" {
 diopiError_t diopiRepeat(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t repeatSize) {
     BEGIN_CALL_ACL_OP(out, input, repeatSize);
-    std::vector<int64_t> inputShape(inputAt.sizes().cbegin(), inputAt.sizes().cend());
-
-    if (inputShape.size() < repeatSize.len) {
-        while (inputShape.size() < repeatSize.len) {
-            inputShape.insert(inputShape.begin(), 1);
-        }
-
-        inputAt = impl::aten::viewStorage(inputAt, inputShape);
+    TORCH_CHECK(inputAt.dim() <= repeatSize.len, "repeats size should not be smaller than input tensor dim on ascend!");
+    // When repeatSize.len is equal to 0, out is the same as input.
+    if (repeatSize.len == 0) {
+        outAt.copy_(inputAt);
+        END_CALL_ACL_OP();
     }
 
-    at_npu::native::OpPreparation::markAsOutputForApplyTensor(outAt);
-    outAt = acl_op::repeat(inputAt, repeatSizeAt);
+    std::vector<int64_t> inputShape = inputAt.sizes().vec();
+    inputShape.insert(inputShape.begin(), repeatSize.len - inputAt.dim(), 1);
+    inputAt = impl::aten::viewStorage(inputAt, inputShape);
+
+    EXEC_NPU_CMD(aclnnRepeat, inputAt, repeatSizeAt, outAt);
     END_CALL_ACL_OP();
 }
 
