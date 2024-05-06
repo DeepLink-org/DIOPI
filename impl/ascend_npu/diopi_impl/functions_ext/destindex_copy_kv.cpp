019f8c5408065e4c3969174fb00d4e3cb2915d60
jinminxi104 <jinminxi104@hotmail.com>
2024-05-06 23:38:33 +0800

    use scatterndupdate in destindex_copy_kv

--- impl/ascend_npu/diopi_impl/functions_ext/destindex_copy_kv.cpp
+++ impl/ascend_npu/diopi_impl/functions_ext/destindex_copy_kv.cpp
@@ -12,7 +12,16 @@ namespace OP_IMPL_NS {
 
 diopiError_t diopiDestIndexCopyKV(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t k, diopiConstTensorHandle_t destLoc) {
     BEGIN_CALL_ACL_OP(out, k, destLoc);
-    at::index_put_(outAt, {destLocAt}, kAt, false);
+    auto shape = at::DimVector();
+    auto orig_shape = destLocAt.sizes();
+    if (destLocAt.sizes().size() != 1) {
+        set_last_error_string("only support destLoc.rank == 1");
+        return diopiNoImplement;
+    }
+    shape.append(orig_shape.begin(),orig_shape.end());
+    shape.append(shape.size(),1);
+    auto destLocReshapeAt = destLocAt.reshape(shape);
+    EXEC_NPU_CMD(aclnnScatterNd, outAt, destLocReshapeAt, kAt, outAt);
     END_CALL_ACL_OP();
 }
 }  // namespace OP_IMPL_NS
