8d71fc7c1278d35351689dbac4e48a11f73bd747
POI-WX <wangxing2@sensetime.com>
2024-05-10 19:50:00 +0800

    reimpl log op

--- impl/ascend/functions/unary.cpp
+++ impl/ascend/functions/unary.cpp
@@ -50,41 +50,33 @@ diopiError_t diopiErfInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
     return diopiSuccess;
 }
 
-void logInternal(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, float base) {
-    AclOpRunner<1, 1>("Log", ctx).addInput(input).setAttr<float>("base", base).setAttr<float>("scale", 1).setAttr<float>("shift", 0).addOutput(out).run();
-}
-
-void logInpInternal(diopiContextHandle_t ctx, diopiTensorHandle_t input, float base) {
-    AclOpRunner<1, 1>("Log", ctx).addInput(input).setAttr<float>("base", base).setAttr<float>("scale", 1).setAttr<float>("shift", 0).addOutput(input).run();
-}
-
 diopiError_t diopiLog(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
-    logInternal(ctx, out, input, -1);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnLog, ctx, input, out);
     return diopiSuccess;
 }
 
 diopiError_t diopiLogInp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
-    logInpInternal(ctx, input, -1);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceLog, ctx, input);
     return diopiSuccess;
 }
 
 diopiError_t diopiLog2(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
-    logInternal(ctx, out, input, 2);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnLog2, ctx, input, out);
     return diopiSuccess;
 }
 
 diopiError_t diopiLog2Inp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
-    logInpInternal(ctx, input, 2);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceLog2, ctx, input);
     return diopiSuccess;
 }
 
 diopiError_t diopiLog10(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input) {
-    logInternal(ctx, out, input, 10);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnLog10, ctx, input, out);
     return diopiSuccess;
 }
 
 diopiError_t diopiLog10Inp(diopiContextHandle_t ctx, diopiTensorHandle_t input) {
-    logInpInternal(ctx, input, 10);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceLog10, ctx, input);
     return diopiSuccess;
 }
 
