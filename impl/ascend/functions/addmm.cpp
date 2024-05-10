ad665befbb6787467e8e6c749b3ab3f8510cd5d6
NeosZhang <zhangqiu1994@outlook.com>
2024-05-10 15:02:38 +0800

    add todo

--- impl/ascend/functions/addmm.cpp
+++ impl/ascend/functions/addmm.cpp
@@ -13,6 +13,7 @@ namespace ascend {
 
 diopiError_t diopiAddmm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t mat1,
                         diopiConstTensorHandle_t mat2, const diopiScalar_t* beta, const diopiScalar_t* alpha) {
+    // TODO(zhangqiu) impl int8_t CalcuOpUtil::GetCubeMathType(bool allowHf32)
     int8_t cubeMathType = 0;
     DIOPI_ASCEND_CALL_ACLNN(aclnnAddmm, ctx, input, mat1, mat2, beta, alpha, out, cubeMathType);
     return diopiSuccess;
