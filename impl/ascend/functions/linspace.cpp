89b6c19475b7b189d093345a39d88156ca1623cb
POI-WX <wangxing2@sensetime.com>
2024-05-13 17:00:40 +0800

    reimpl linspace op

--- impl/ascend/functions/linspace.cpp
+++ impl/ascend/functions/linspace.cpp
@@ -1,21 +1,17 @@
 /**
  * @file
  * @author DeepLink
- * @copyright  (c) 2023, DeepLink.
+ * @copyright  (c) 2024, DeepLink.
  */
 
-#include "../common/acloprunner.hpp"
+#include "../aclnn/acl_scalar.hpp"
+#include "../aclnn/adaptor.hpp"
 
 namespace impl {
 namespace ascend {
 
 diopiError_t diopiLinspace(diopiContextHandle_t ctx, diopiTensorHandle_t out, const diopiScalar_t* start, const diopiScalar_t* end, int64_t steps) {
-    AclOpRunner<3, 1>("LinSpace", ctx)
-        .addConstInput(*start, diopi_dtype_float32)
-        .addConstInput(*end, diopi_dtype_float32)
-        .addConstInput(steps, diopi_dtype_int32)
-        .addOutput(out)
-        .run();
+    DIOPI_ASCEND_CALL_ACLNN(aclnnLinspace, ctx, start, end, steps, out);
     return diopiSuccess;
 }
 
