a8990cb92423ba5e38fe9a87d87ae4d59c664fe9
NeosZhang <zhangqiu1994@outlook.com>
2024-05-14 17:22:26 +0800

    fix

--- impl/ascend/functions/addcmul.cpp
+++ impl/ascend/functions/addcmul.cpp
@@ -13,10 +13,13 @@ namespace ascend {
 
 diopiError_t diopiAddcmul(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t tensor1,
                           diopiConstTensorHandle_t tensor2, const diopiScalar_t* value) {
-    int64_t inputNumel = 0;
-    diopiGetTensorNumel(input, &inputNumel);
-    if (inputNumel != 0) {
-        DIOPI_ASCEND_CALL_ACLNN(aclnnAddcmul, ctx, input, tensor1, tensor2, value, out);
+    AscendTensor inputAt(input);
+    AscendTensor tensor1At(tensor1);
+    AscendTensor tensor2At(tensor2);
+    AscendTensor outAt(out);
+
+    if (inputAt.numel() != 0) {
+        DIOPI_ASCEND_CALL_ACLNN(aclnnAddcmul, ctx, inputAt, tensor1At, tensor2At, value, outAt);
     }
     return diopiSuccess;
 }
