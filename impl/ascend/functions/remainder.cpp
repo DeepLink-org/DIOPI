ee422c2319a226cc232fc7f532c8e96226805634
NeosZhang <zhangqiu1994@outlook.com>
2024-05-14 14:01:46 +0800

    fix

--- impl/ascend/functions/remainder.cpp
+++ impl/ascend/functions/remainder.cpp
@@ -12,26 +12,30 @@ namespace impl {
 namespace ascend {
 
 diopiError_t diopiRemainderTensor(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t other) {
-    int64_t inputNumel = 0;
-    int64_t otherNumel = 0;
-    diopiGetTensorNumel(input, &inputNumel);
-    diopiGetTensorNumel(other, &otherNumel);
-    if (input == nullptr || inputNumel == 0 || other == nullptr || otherNumel == 0) {
+    AscendTensor inputAt(input);
+    AscendTensor otherAt(other);
+
+    diopiDtype_t promotedType = promoteTypes(inputAt.dtype(), otherAt.dtype());
+    if (input == nullptr || inputAt.numel() == 0 || other == nullptr || otherAt.numel() == 0) {
         return diopiSuccess;
     }
 
-    DIOPI_ASCEND_CALL_ACLNN(aclnnRemainderTensorTensorf, ctx, input, other, out);
+    castTensor(ctx, inputAt, promotedType);
+    castTensor(ctx, otherAt, promotedType);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnRemainderTensorTensor, ctx, inputAt, otherAt, out);
     return diopiSuccess;
 }
 
 diopiError_t diopiRemainderScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* other) {
-    int64_t inputNumel = 0;
-    diopiGetTensorNumel(input, &inputNumel);
-    if (input == nullptr || inputNumel == 0) {
+    AscendTensor inputAt(input);
+    AscendTensor outAt(out);
+    if (input == nullptr || inputAt.numel() == 0) {
         return diopiSuccess;
     }
 
-    DIOPI_ASCEND_CALL_ACLNN(aclnnRemainderTensorScalar, ctx, input, other, out);
+    diopiDtype_t promotedType = promoteTypes(inputAt.dtype(), other->stype);
+    castTensor(ctx, inputAt, promotedType);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnRemainderTensorScalar, ctx, inputAt, other, out);
     return diopiSuccess;
 }
 
