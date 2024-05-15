a53628567a1ce05f75d9dedd8df0191935768e6f
wangxing <131418410+POI-WX@users.noreply.github.com>
2024-05-15 21:15:21 +0800

    [Ascend] Wx/reimpl some ops (#1180)
    * Skip float64 test cases for some ops[batch_norm, adaptive_avg_pool2d, interpolate], as other ops are implemented using DIOPI_ASCEND_CALL_ACLNN.
    * Reimpl activation, cast, atan, sin, cos, fill, floor, isnan, lerp, linalg_vec_norm, linspace, remainder, sgn, sort, threshold, neg, sqrt, rsqrt, erf, log, log2, log10, exp, reciprocal, rms_norm using DIOPI_ASCEND_CALL_ACLNN.
    * Fix permute.
    * Remove redundant dtype cast.

--- impl/ascend/functions/threshold.cpp
+++ impl/ascend/functions/threshold.cpp
@@ -4,36 +4,25 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
-#include "../common/acloprunner.hpp"
+#include "../aclnn/acl_scalar.hpp"
+#include "../aclnn/adaptor.hpp"
 
 namespace impl {
 namespace ascend {
 diopiError_t diopiThreshold(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* threshold,
                             const diopiScalar_t* value) {
-    diopiDtype_t dtype;
-    diopiGetTensorDtype(input, &dtype);
-    float inputThreshold = getValue<float>(threshold);
-    float inputValue = getValue<float>(value);
-
-    if (dtype == diopi_dtype_uint8) {
-        // Converting a float type directly to uint is undefined behavior, so it is necessary to first convert it to an int type.
-        inputThreshold = static_cast<float>(static_cast<uint8_t>(static_cast<int>(inputThreshold)));
-        inputValue = static_cast<float>(static_cast<uint8_t>(static_cast<int>(inputValue)));
-    } else if (isIntegralType(dtype)) {
-        inputThreshold = static_cast<float>(getValue<int>(threshold));
-        inputValue = static_cast<float>(getValue<int>(value));
-    }
-    AclOpRunner<1, 1>("ThresholdV2D", ctx).addInput(input).setAttr("threshold", inputThreshold).setAttr("value", inputValue).addOutput(out).run();
+    DIOPI_ASCEND_CALL_ACLNN(aclnnThreshold, ctx, input, threshold, value, out);
     return diopiSuccess;
 }
 
 diopiError_t diopiThresholdInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, const diopiScalar_t* threshold, const diopiScalar_t* value) {
-    return diopiThreshold(ctx, input, input, threshold, value);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceThreshold, ctx, input, threshold, value);
+    return diopiSuccess;
 }
 
 diopiError_t diopiThresholdBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput,
                                     diopiConstTensorHandle_t input, const diopiScalar_t* threshold) {
-    AclOpRunner<2, 1>("ThresholdGradV2D", ctx).addInput(gradOutput).addInput(input).setAttr("threshold", getValue<float>(threshold)).addOutput(gradInput).run();
+    DIOPI_ASCEND_CALL_ACLNN(aclnnThresholdBackward, ctx, gradOutput, input, threshold, gradInput);
     return diopiSuccess;
 }
 
