30b814fef40eaba6437bc2dc25912abc1a3056eb
NeosZhang <zhangqiu1994@outlook.com>
2024-05-16 11:34:27 +0800

    fix

--- impl/ascend/functions/addcmul.cpp
+++ impl/ascend/functions/addcmul.cpp
@@ -6,7 +6,6 @@
 
 #include "../aclnn/acl_scalar.hpp"
 #include "../aclnn/adaptor.hpp"
-#include "../ascend_tensor.hpp"
 
 namespace impl {
 namespace ascend {
@@ -14,21 +13,16 @@ namespace ascend {
 diopiError_t diopiAddcmul(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t tensor1,
                           diopiConstTensorHandle_t tensor2, const diopiScalar_t* value) {
     AscendTensor inputAt(input);
-    AscendTensor tensor1At(tensor1);
-    AscendTensor tensor2At(tensor2);
-    AscendTensor outAt(out);
-
     if (inputAt.numel() != 0) {
-        DIOPI_ASCEND_CALL_ACLNN(aclnnAddcmul, ctx, inputAt, tensor1At, tensor2At, value, outAt);
+        DIOPI_ASCEND_CALL_ACLNN(aclnnAddcmul, ctx, inputAt, tensor1, tensor2, value, out);
     }
     return diopiSuccess;
 }
 
 diopiError_t diopiAddcmulInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t tensor1, diopiConstTensorHandle_t tensor2,
                              const diopiScalar_t* value) {
-    int64_t inputNumel = 0;
-    diopiGetTensorNumel(input, &inputNumel);
-    if (inputNumel != 0) {
+    AscendTensor inputAt(input);
+    if (inputAt.numel() != 0) {
         DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceAddcmul, ctx, input, tensor1, tensor2, value);
     }
 
