01510023d66e1f607e7cfeb4160281c8cdeb2f37
NeosZhang <zhangqiu1994@outlook.com>
2024-05-17 16:16:11 +0800

    fix

--- impl/ascend/functions/split.cpp
+++ impl/ascend/functions/split.cpp
@@ -17,11 +17,10 @@ diopiError_t diopiSplitWithSizes(diopiContextHandle_t ctx, diopiTensorHandle_t* 
     }
 
     // build the dynamicOutput vector
-    std::vector<AscendTensor> dynamicOutput;
+    std::vector<diopiTensorHandle_t> dynamicOutput;
 
     for (int64_t i = 0; i < numOuts; i++) {
-        AscendTensor outputTensorI(outs[i]);
-        dynamicOutput.push_back(outputTensorI);
+        dynamicOutput.push_back(outs[i]);
     }
 
     DIOPI_ASCEND_CALL_ACLNN(aclnnSplitWithSize, ctx, input, splitSizes, dim, dynamicOutput);
