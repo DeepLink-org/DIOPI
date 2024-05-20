7541914283d99f905bae9516c1d499043965946d
POI-WX <wangxing2@sensetime.com>
2024-05-20 15:17:39 +0800

    reimpl norm op by aclnn

--- impl/ascend/functions/norm.cpp
+++ impl/ascend/functions/norm.cpp
@@ -4,32 +4,21 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
-#include <vector>
-
-#include "../common/acloprunner.hpp"
+#include "../aclnn/adaptor.hpp"
 
 namespace impl {
 namespace ascend {
 
 diopiError_t diopiNorm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, const diopiScalar_t* p, diopiSize_t dim) {
-    diopiSize_t size;
-    diopiGetTensorShape(out, &size);
-    diopiDtype_t dtype;
-    diopiGetTensorDtype(input, &dtype);
-    diopiTensorHandle_t resultTmp;
-    diopiRequireTensor(ctx, &resultTmp, &size, nullptr, dtype, diopi_device);
-    AclOpRunner<1, 1> runner("LpNormReduce", ctx);
-    runner.addInput(input);
-    std::vector<int> dimVec;
-    for (size_t i = 0; i < dim.len; ++i) {
-        dimVec.emplace_back(dim.data[i]);
+    AscendTensor outTensor(out);
+    if (!outTensor.defined() || outTensor.numel() == 0) {
+        return diopiSuccess;
     }
-    // default value is 2
-    int64_t pValue = (p ? getValue<int64_t>(p) : 2);
-    runner.addOutput(resultTmp).setAttr("p", pValue).setAttr("axes", dimVec).setAttr("keepdim", false).setAttr<float>("epsilon", 0.0).run();
 
-    AclOpRunner<1, 1>("LpNormUpdate", ctx).addInput(resultTmp).addOutput(out).setAttr("p", pValue).setAttr<float>("epsilon", 0.0).run();
-
+    float pValue = getValue<float>(p);
+    ASCEND_CHECK_ABORT(pValue == 0.0 || pValue == 1.0 || pValue == 2.0 || pValue == 3.0, "aclnnNorm currently only supports p=0,1,2,3!");
+    bool keepDim = false;
+    DIOPI_ASCEND_CALL_ACLNN(aclnnNorm, ctx, input, p, dim, keepDim, out);
     return diopiSuccess;
 }
 
