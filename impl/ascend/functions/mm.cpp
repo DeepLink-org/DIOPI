1c419cc84f3fc65ba34bdfce939f9e67cd75d177
jingguo-st <fujingguo@sensetime.com>
2024-05-21 13:19:24 +0000

    replace mm with aclnn

--- impl/ascend/functions/mm.cpp
+++ impl/ascend/functions/mm.cpp
@@ -4,27 +4,24 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
-#include "../common/acloprunner.hpp"
+#include "../aclnn/acl_scalar.hpp"
+#include "../aclnn/adaptor.hpp"
 
 namespace impl {
 namespace ascend {
 
 diopiError_t diopiMm(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t mat2) {
-    AscendTensor inputTr(input);
-    AscendTensor mat2Tr(mat2);
-    AscendTensor outputTr(out);
-    if (inputTr.numel() == 0 || mat2Tr.numel() == 0) {
-        diopiScalar_t zero = constructDiopiScalarT(outputTr.dtype(), 0.0);
-        diopiFill(ctx, out, &zero);
+    AscendTensor inputAt(input);
+    AscendTensor mat2At(mat2);
+    AscendTensor outAt(out);
+
+    if (inputAt.numel() == 0 || mat2At.numel() == 0) {
+        DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceZero, ctx, out);
         return diopiSuccess;
     }
-    AclOpRunner<2, 1>("MatMul", ctx)
-        .addInput(input, inputTr.dtype())
-        .addInput(mat2, mat2Tr.dtype())
-        .setAttr("adj_x1", false)
-        .setAttr("adj_x1", false)
-        .addOutput(out)
-        .run();
+
+    int cubeMathType = 0;
+    DIOPI_ASCEND_CALL_ACLNN(aclnnMm, ctx, input, mat2, out, cubeMathType);
     return diopiSuccess;
 }
 
