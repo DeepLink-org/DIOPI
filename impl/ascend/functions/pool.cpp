33d30dbc9e54ef889c688389ff57d2e7d3530e28
NeosZhang <zhangqiu1994@outlook.com>
2024-05-22 16:54:42 +0800

    fix

--- impl/ascend/functions/pool.cpp
+++ impl/ascend/functions/pool.cpp
@@ -4,18 +4,25 @@
  * @copyright  (c) 2024, DeepLink.
  */
 
+#include <algorithm>
+
 #include "../aclnn/acl_scalar.hpp"
 #include "../aclnn/adaptor.hpp"
-#include "../common/acloprunner.hpp"
 namespace impl {
 namespace ascend {
 
 diopiError_t diopiAdaptiveAvgPool2d(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t outputSize) {
-    int64_t inputNumel = 0;
-    diopiGetTensorNumel(input, &inputNumel);
-    if (input == nullptr || inputNumel == 0) {
+    // the shape of input: (N, C, H, W) or (C, H, W)
+    // the shape of output: (N, C, S0, S1) or (C, S0, S1), where S = output_size
+
+    AscendTensor inputAt(input);
+    AscendTensor outAt(out);
+    ASCEND_CHECK_ABORT((inputAt.dim() == 3 || inputAt.dim() == 4), "The input of adaptive_avg_pool2d should be 3_dimensional or 4_dimensional.");
+
+    if ((outputSize.len == 1 && outputSize.data[0] == 0) || (outputSize.len == 2 && outputSize.data[0] == 0 && outputSize.data[1] == 0)) {
         return diopiSuccess;
     }
+
     DIOPI_ASCEND_CALL_ACLNN(aclnnAdaptiveAvgPool2d, ctx, input, outputSize, out);
     return diopiSuccess;
 }
