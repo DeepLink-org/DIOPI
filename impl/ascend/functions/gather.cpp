d2a265a83b2890f74194221687678f7c2aa78022
NeosZhang <zhangqiu1994@outlook.com>
2024-05-23 16:35:46 +0800

    fix

--- impl/ascend/functions/gather.cpp
+++ impl/ascend/functions/gather.cpp
@@ -4,21 +4,69 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
-#include "../common/acloprunner.hpp"
+#include "../aclnn/adaptor.hpp"
 
 namespace impl {
 namespace ascend {
 diopiError_t diopiGather(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t dim, diopiConstTensorHandle_t index) {
-    AclOpRunner<2, 1>("GatherElements", ctx).addInput(input).addInput(index).setAttr("dim", dim).addOutput(out).run();
+    AscendTensor inputAt(input);
+    AscendTensor indexAt(index);
+    AscendTensor outAt(out);
+
+    if (inputAt.numel() == 0 || indexAt.numel() == 0) {
+        return diopiSuccess;
+    }
+
+    // alcnnGather requires that the dimensions of input, output, and index match.
+    while (inputAt.dim() < indexAt.dim()) {
+        inputAt.unsqueeze(0);
+    }
+
+    while (indexAt.dim() < inputAt.dim()) {
+        indexAt.unsqueeze(0);
+    }
+
+    while (outAt.dim() < inputAt.dim()) {
+        outAt.unsqueeze(0);
+    }
+
+    for (int64_t i = 0; i < inputAt.dim(); i++) {
+        if (inputAt.shape(i) == 0 || indexAt.shape(i) == 0) {
+            return diopiSuccess;
+        }
+    }
+
+    DIOPI_ASCEND_CALL_ACLNN(aclnnGather, ctx, inputAt, dim, indexAt, outAt);
     return diopiSuccess;
 }
 
 diopiError_t diopiGatherBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput, diopiConstTensorHandle_t input,
                                  int64_t dim, diopiConstTensorHandle_t index) {
+    AscendTensor inputAt(input);
     AscendTensor gradInputAt(gradInput);
-    diopiScalar_t scalarZero = constructDiopiScalarT(gradInputAt.dtype(), 0);
-    diopiFill(ctx, gradInput, &scalarZero);
-    AclOpRunner<3, 1>("ScatterAddWithAxis", ctx).addInput(gradInput).addInput(index).addInput(gradOutput).setAttr("axis", dim).addOutput(gradInput).run();
+    AscendTensor gradOutputAt(gradOutput);
+    AscendTensor indexAt(index);
+
+    if (inputAt.numel() == 0 || indexAt.numel() == 0) {
+        return diopiSuccess;
+    }
+
+    // alcnnScatter requires that the dimensions of input, output, and index match.
+    while (indexAt.dim() < gradOutputAt.dim()) {
+        indexAt.unsqueeze(0);
+    }
+
+    while (gradOutputAt.dim() < indexAt.dim()) {
+        gradOutputAt.unsqueeze(0);
+    }
+
+    while (gradInputAt.dim() < gradOutputAt.dim()) {
+        gradInputAt.unsqueeze(0);
+    }
+
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceZero, ctx, gradInputAt);
+    // the gradOutput will scatter_accumulates according to the number of times an element is indexed in the input.
+    DIOPI_ASCEND_CALL_ACLNN(aclnnAddScatter, ctx, gradInputAt, dim, indexAt, gradOutputAt, gradInput);
     return diopiSuccess;
 }
 
