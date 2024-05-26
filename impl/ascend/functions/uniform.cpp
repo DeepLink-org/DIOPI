043dc8f55ab70533838b625f5c29caef5673e3ea
jingguo-st <fujingguo@sensetime.com>
2024-05-26 15:04:20 +0000

    fix generator with getSeedAndOffset

--- impl/ascend/functions/uniform.cpp
+++ impl/ascend/functions/uniform.cpp
@@ -11,9 +11,9 @@ namespace impl {
 namespace ascend {
 
 diopiError_t diopiUniformInp(diopiContextHandle_t ctx, diopiTensorHandle_t inout, double from, double to, diopiGeneratorHandle_t generator) {
-    uint64_t seed = 0;
-    uint64_t offset = 0;
-    diopiGeneratorGetSeedAndOffset(generator, &seed, &offset);
+    const std::pair<uint64_t, uint64_t> gen = getSeedAndOffset(ctx, generator, 10);
+    const uint64_t seed = gen.first;
+    const uint64_t offset = gen.second;
     DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceUniform, ctx, inout, from, to, seed, offset);
     return diopiSuccess;
 }
