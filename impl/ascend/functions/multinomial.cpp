310018325b0e343e5d0b6609267396a726292c74
POI-WX <wangxing2@sensetime.com>
2024-05-27 18:52:00 +0800

    fix

--- impl/ascend/functions/multinomial.cpp
+++ impl/ascend/functions/multinomial.cpp
@@ -11,8 +11,8 @@ namespace impl {
 namespace ascend {
 diopiError_t diopiMultinomial(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t numSamples, bool replacement,
                               diopiGeneratorHandle_t generator) {
-    auto pair = getSeedAndOffset(ctx, generator, 10);
-    DIOPI_ASCEND_CALL_ACLNN(aclnnMultinomial, ctx, input, numSamples, replacement, pair.first, pair.second, out);
+    std::pair<uint64_t, int64_t> pair = getSeedAndOffset(ctx, generator, 10);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnMultinomial, ctx, input, numSamples, replacement, static_cast<int64_t>(pair.first), pair.second, out);
     return diopiSuccess;
 }
 
