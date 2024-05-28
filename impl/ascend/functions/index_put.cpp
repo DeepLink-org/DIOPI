bd1093b143b4ed89405d8045d0e1e403fdd148f4
POI-WX <wangxing2@sensetime.com>
2024-05-28 11:07:46 +0800

    update

--- impl/ascend/functions/index_put.cpp
+++ impl/ascend/functions/index_put.cpp
@@ -9,6 +9,34 @@
 namespace impl {
 namespace ascend {
 
+diopiError_t diopiIndexPut(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiConstTensorHandle_t values,
+                           diopiConstTensorHandle_t* indices, int64_t indicesCounts, bool accumulate) {
+    AscendTensor inputTensor(input);
+    AscendTensor valuesTensor(values);
+    if (inputTensor.numel() == 0 || valuesTensor.numel() == 0) {
+        return diopiSuccess;
+    }
+
+    std::vector<diopiConstTensorHandle_t> indicesVec;
+    indicesVec.reserve(indicesCounts);
+
+    for (int i = 0; i < indicesCounts; i++) {
+        if (indices[i] != nullptr) {
+            indicesVec[i] = indices[i];
+        } else {
+            int64_t array[1] = {0};
+            diopiSize_t size = {array, 1};
+            diopiTensorHandle_t emptyTensor = nullptr;
+            diopiRequireTensor(ctx, &emptyTensor, &size, nullptr, inputTensor.dtype(), diopi_device);
+            indicesVec[i] = emptyTensor;
+        }
+    }
+
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceCopy, ctx, out, input);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnIndexPutImpl, ctx, out, indicesVec, values, accumulate, false);
+    return diopiSuccess;
+}
+
 diopiError_t diopiIndexPutInp(diopiContextHandle_t ctx, diopiTensorHandle_t input, diopiConstTensorHandle_t values, diopiConstTensorHandle_t* indices,
                               int64_t indicesCounts, bool accumulate) {
     AscendTensor inputTensor(input);
