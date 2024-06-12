b64e08a2a6ef87ae8d1e91650d24a424871c877b
wangxing <131418410+POI-WX@users.noreply.github.com>
2024-06-12 18:06:11 +0800

    [Ascend] Wx/reimpl index select, cat and stack op (#1211)

--- impl/ascend/functions/index_select.cpp
+++ impl/ascend/functions/index_select.cpp
@@ -4,28 +4,21 @@
  * @copyright  (c) 2023, DeepLink.
  */
 
-#include "../common/acloprunner.hpp"
+#include "../aclnn/adaptor.hpp"
 
 namespace impl {
 namespace ascend {
 diopiError_t diopiIndexSelect(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, int64_t dim, diopiConstTensorHandle_t index) {
-    std::vector<int64_t> dimVec({dim});
-    diopiSize_t dimInput = vectorToDiopiSize(dimVec);
-    AclOpRunner<3, 1>("GatherV2", ctx).addInput(input).addInput(index).addConstInput(dimInput).setAttr<int64_t>("batch_dims", 0).addOutput(out).run();
+    DIOPI_ASCEND_CALL_ACLNN(aclnnIndexSelect, ctx, input, dim, index, out);
     return diopiSuccess;
 }
 
-diopiError_t diopiIndexSelectBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t grad, diopiSize_t inputSizes,
+diopiError_t diopiIndexSelectBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradInput, diopiConstTensorHandle_t gradOutput, diopiSize_t inputSizes,
                                       int64_t dim, diopiConstTensorHandle_t index) {
-    AscendTensor gradInputAt(gradInput);
-    if (dim < 0) {
-        dim = dim + inputSizes.len;
-    }
-    std::vector<int64_t> dimVec({dim});
-    diopiSize_t dimInput = vectorToDiopiSize(dimVec);
-    diopiScalar_t scalarZero = constructDiopiScalarT(gradInputAt.dtype(), 0);
-    diopiFill(ctx, gradInput, &scalarZero);
-    AclOpRunner<3, 1>("InplaceIndexAdd", ctx).addInput(gradInput).addInput(index).addInput(grad).setAttr<int64_t>("axis", dim).addOutput(gradInput).run();
+    AscendTensor gradInputTensor(gradInput);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceZero, ctx, gradInput);
+    diopiScalar_t one = constructDiopiScalarT(gradInputTensor.dtype(), 1);
+    DIOPI_ASCEND_CALL_ACLNN(aclnnIndexAdd, ctx, gradInput, dim, index, gradOutput, &one, gradInput);
     return diopiSuccess;
 }
 
