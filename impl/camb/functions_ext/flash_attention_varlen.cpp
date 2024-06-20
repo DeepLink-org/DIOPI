dc50f89c2e6eb42a7af6350f5438b1d506114359
POI-WX <wangxing2@sensetime.com>
2024-06-20 21:50:20 +0800

    fix

--- impl/camb/functions_ext/flash_attention_varlen.cpp
+++ impl/camb/functions_ext/flash_attention_varlen.cpp
@@ -3,14 +3,13 @@
  * @author DeepLink
  * @copyright  (c) 2024, DeepLink.
  */
-#include <cnmlrt.h>
+
 #include <diopi/functions_ext.h>
 
 #include <vector>
 
 #include "../cnnl_helper.hpp"
 #include "../common/common.hpp"
-#include "../mlu_helper.hpp"
 
 namespace impl {
 namespace camb {
@@ -122,145 +121,143 @@ diopiError_t diopiFlashAttentionVarLen(diopiContextHandle_t ctx, diopiTensorHand
     return diopiSuccess;
 }
 
-// diopiError_t diopiFlashAttentionVarLenBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradQ, diopiTensorHandle_t gradK, diopiTensorHandle_t gradV,
-//                                                diopiConstTensorHandle_t gradOutput, diopiGeneratorHandle_t gen, diopiConstTensorHandle_t q,
-//                                                diopiConstTensorHandle_t k, diopiConstTensorHandle_t v, diopiConstTensorHandle_t cumSeqQ,
-//                                                diopiConstTensorHandle_t cumSeqKV, diopiConstTensorHandle_t alibiSlopes, diopiConstTensorHandle_t
-//                                                attentionOut, diopiConstTensorHandle_t softmaxLse, int maxSeqLenQ, int maxSeqLenKV, float pDropout, float
-//                                                softmaxScale, bool isCausal, int windowSizeLeft, int windowSizeRight) {
-//     cnnlHandle_t handle = cnnlHandlePool.get(ctx);
+diopiError_t diopiFlashAttentionVarLenBackward(diopiContextHandle_t ctx, diopiTensorHandle_t gradQ, diopiTensorHandle_t gradK, diopiTensorHandle_t gradV,
+                                               diopiConstTensorHandle_t gradOutput, diopiGeneratorHandle_t gen, diopiConstTensorHandle_t q,
+                                               diopiConstTensorHandle_t k, diopiConstTensorHandle_t v, diopiConstTensorHandle_t cumSeqQ,
+                                               diopiConstTensorHandle_t cumSeqKV, diopiConstTensorHandle_t alibiSlopes, diopiConstTensorHandle_t attentionOut,
+                                               diopiConstTensorHandle_t softmaxLse, int maxSeqLenQ, int maxSeqLenKV, float pDropout, float softmaxScale,
+                                               bool isCausal, int windowSizeLeft, int windowSizeRight) {
+    cnnlHandle_t handle = cnnlHandlePool.get(ctx);
 
-//     DIOPI_CHECK(alibiSlopes == nullptr, "For camb, flash attention currently does not support Attention with Linear Biases (ALiBi)!");
+    DIOPI_CHECK(alibiSlopes == nullptr, "For camb, flash attention currently does not support Attention with Linear Biases (ALiBi)!");
 
-//     DiopiTensor qTensor(q);
-//     DiopiTensor kTensor(k);
-//     DiopiTensor vTensor(v);
-//     DiopiTensor cumSeqQTensor(cumSeqQ);
-//     DiopiTensor cumSeqKVTensor(cumSeqKV);
-//     DiopiTensor gradOutTensor(gradOutput);
-//     DiopiTensor attentionOutTensor(attentionOut);
-//     DiopiTensor softmaxLseTensor(softmaxLse);
+    DiopiTensor qTensor(q);
+    DiopiTensor kTensor(k);
+    DiopiTensor vTensor(v);
+    DiopiTensor cumSeqQTensor(cumSeqQ);
+    DiopiTensor cumSeqKVTensor(cumSeqKV);
+    DiopiTensor gradOutTensor(gradOutput);
+    DiopiTensor attentionOutTensor(attentionOut);
+    DiopiTensor softmaxLseTensor(softmaxLse);
 
-//     DiopiTensor gradQTensor(gradQ);
-//     DiopiTensor gradKTensor(gradK);
-//     DiopiTensor gradVTensor(gradV);
+    DiopiTensor gradQTensor(gradQ);
+    DiopiTensor gradKTensor(gradK);
+    DiopiTensor gradVTensor(gradV);
 
-//     // convert dtype
-//     std::vector<DiopiTensor*> qkvTensors{&qTensor, &kTensor, &vTensor, &attentionOutTensor, &gradOutTensor};
-//     std::set<diopiDtype_t> supportedQKVDtypes{diopi_dtype_float16, diopi_dtype_bfloat16};
-//     DIOPI_CALL(autoCastTensorType(ctx, qkvTensors, supportedQKVDtypes));
-//     DIOPI_CALL(autoCastTensorType(ctx, {&cumSeqQTensor, &cumSeqKVTensor}, {diopi_dtype_int32}));
-//     DIOPI_CALL(autoCastTensorType(ctx, {&softmaxLseTensor}, {diopi_dtype_float32}));
+    // convert dtype
+    DIOPI_CALL(autoCastTensorType(ctx, {&qTensor, &kTensor, &vTensor, &attentionOutTensor, &gradOutTensor}, {diopi_dtype_float16, diopi_dtype_bfloat16}));
+    DIOPI_CALL(autoCastTensorType(ctx, {&cumSeqQTensor, &cumSeqKVTensor}, {diopi_dtype_int32}));
+    DIOPI_CALL(autoCastTensorType(ctx, {&softmaxLseTensor}, {diopi_dtype_float32}));
 
-//     DiopiTensor gradQTensorTmp = gradQTensor;
-//     if (gradQTensor.dtype() != qTensor.dtype()) {
-//         gradQTensorTmp = requiresTensor(ctx, gradQTensorTmp.shape(), gradQTensorTmp.stride(), qTensor.dtype());
-//     }
+    DiopiTensor gradQTensorTmp = gradQTensor;
+    if (gradQTensor.dtype() != qTensor.dtype()) {
+        gradQTensorTmp = requiresTensor(ctx, gradQTensor.shape(), gradQTensor.stride(), qTensor.dtype());
+    }
 
-//     DiopiTensor gradKTensorTmp = gradKTensor;
-//     if (gradKTensor.dtype() != qTensor.dtype()) {
-//         gradKTensorTmp = requiresTensor(ctx, gradKTensorTmp.shape(), gradKTensorTmp.stride(), qTensor.dtype());
-//     }
+    DiopiTensor gradKTensorTmp = gradKTensor;
+    if (gradKTensor.dtype() != qTensor.dtype()) {
+        gradKTensorTmp = requiresTensor(ctx, gradKTensor.shape(), gradKTensor.stride(), qTensor.dtype());
+    }
 
-//     DiopiTensor gradVTensorTmp = gradVTensor;
-//     if (gradVTensor.dtype() != qTensor.dtype()) {
-//         gradVTensorTmp = requiresTensor(ctx, gradVTensorTmp.shape(), gradVTensorTmp.stride(), qTensor.dtype());
-//     }
+    DiopiTensor gradVTensorTmp = gradVTensor;
+    if (gradVTensor.dtype() != qTensor.dtype()) {
+        gradVTensorTmp = requiresTensor(ctx, gradVTensor.shape(), gradVTensor.stride(), qTensor.dtype());
+    }
 
-//     // set descriptor
-//     CnnlResourceGuard<cnnlFlashAttentionDescriptor_t, cnnlCreateFlashAttentionDescriptor, cnnlDestroyFlashAttentionDescriptor> flashAttentionDesc;
-//     cnnlAttentionMaskMode_t maskMode = isCausal ? CNNL_ATTN_MASK_CAUSAL : CNNL_ATTN_MASK_NONE;
-//     DIOPI_CALL_CNNL(cnnlSetFlashAttentionBackwardDescriptor(flashAttentionDesc.get(),
-//                                                             CNNL_DTYPE_FLOAT,
-//                                                             CNNL_ACTIVATION_HIGH_PRECISION,
-//                                                             maskMode,
-//                                                             true,
-//                                                             false,
-//                                                             false,
-//                                                             maxSeqLenQ,
-//                                                             maxSeqLenKV,
-//                                                             pDropout,
-//                                                             softmaxScale));
+    // set descriptor
+    CnnlResourceGuard<cnnlFlashAttentionDescriptor_t, cnnlCreateFlashAttentionDescriptor, cnnlDestroyFlashAttentionDescriptor> flashAttentionDesc;
+    cnnlAttentionMaskMode_t maskMode = isCausal ? CNNL_ATTN_MASK_CAUSAL : CNNL_ATTN_MASK_NONE;
+    DIOPI_CALL_CNNL(cnnlSetFlashAttentionBackwardDescriptor(flashAttentionDesc.get(),
+                                                            CNNL_DTYPE_FLOAT,
+                                                            CNNL_ACTIVATION_HIGH_PRECISION,
+                                                            maskMode,
+                                                            true,
+                                                            false,
+                                                            false,
+                                                            maxSeqLenQ,
+                                                            maxSeqLenKV,
+                                                            pDropout,
+                                                            softmaxScale));
 
-//     DIOPI_CALL_CNNL(cnnlSetFlashAttentionSlidingWindowSize(flashAttentionDesc.get(), windowSizeLeft, windowSizeRight, 1));
+    DIOPI_CALL_CNNL(cnnlSetFlashAttentionSlidingWindowSize(flashAttentionDesc.get(), windowSizeLeft, windowSizeRight, 1));
 
-//     CnnlTensorDesc qDesc(qTensor, CNNL_LAYOUT_ARRAY);
-//     CnnlTensorDesc kDesc(kTensor, CNNL_LAYOUT_ARRAY);
-//     CnnlTensorDesc vDesc(vTensor, CNNL_LAYOUT_ARRAY);
-//     CnnlTensorDesc cumSeqQDesc(cumSeqQTensor, CNNL_LAYOUT_ARRAY);
-//     CnnlTensorDesc cumSeqKVDesc(cumSeqKVTensor, CNNL_LAYOUT_ARRAY);
-//     CnnlTensorDesc gradOutDesc(gradOutTensor, CNNL_LAYOUT_ARRAY);
-//     CnnlTensorDesc attentionOutDesc(attentionOutTensor, CNNL_LAYOUT_ARRAY);
-//     CnnlTensorDesc gradQDesc(gradQTensorTmp, CNNL_LAYOUT_ARRAY);
-//     CnnlTensorDesc gradKDesc(gradKTensorTmp, CNNL_LAYOUT_ARRAY);
-//     CnnlTensorDesc gradVDesc(gradVTensorTmp, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc qDesc(qTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc kDesc(kTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc vDesc(vTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc cumSeqQDesc(cumSeqQTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc cumSeqKVDesc(cumSeqKVTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc gradOutDesc(gradOutTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc attentionOutDesc(attentionOutTensor, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc gradQDesc(gradQTensorTmp, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc gradKDesc(gradKTensorTmp, CNNL_LAYOUT_ARRAY);
+    CnnlTensorDesc gradVDesc(gradVTensorTmp, CNNL_LAYOUT_ARRAY);
 
-//     const int64_t totalSeqQ = qTensor.shape()[0];
-//     const int64_t headNum = qTensor.shape()[1];
-//     std::vector<int64_t> softmaxLseShape = {headNum, totalSeqQ};
-//     std::vector<int64_t> softmaxLseStride = calContiguousStride(softmaxLseShape);
-//     CnnlTensorDesc softmaxLseDesc;
-//     softmaxLseDesc.set(softmaxLseTensor.dtype(), softmaxLseShape, softmaxLseStride, CNNL_LAYOUT_ARRAY);
+    const int64_t totalSeqQ = qTensor.shape()[0];
+    const int64_t headNum = qTensor.shape()[1];
+    std::vector<int64_t> softmaxLseShape = {headNum, totalSeqQ};
+    std::vector<int64_t> softmaxLseStride = calContiguousStride(softmaxLseShape);
+    CnnlTensorDesc softmaxLseDesc;
+    softmaxLseDesc.set(softmaxLseTensor.dtype(), softmaxLseShape, softmaxLseStride, CNNL_LAYOUT_ARRAY);
 
-//     // get workspace
-//     size_t workspaceSize = 0;
-//     DIOPI_CALL_CNNL(cnnlGetFlashAttentionBackwardWorkspaceSize(handle, flashAttentionDesc.get(), qDesc.get(), kDesc.get(), vDesc.get(), &workspaceSize));
-//     void* workspace = workspaceSize == 0 ? nullptr : requiresBuffer(ctx, workspaceSize).data();
+    // get workspace
+    size_t workspaceSize = 0;
+    DIOPI_CALL_CNNL(cnnlGetFlashAttentionBackwardWorkspaceSize(handle, flashAttentionDesc.get(), qDesc.get(), kDesc.get(), vDesc.get(), &workspaceSize));
+    void* workspace = workspaceSize == 0 ? nullptr : requiresBuffer(ctx, workspaceSize).data();
 
-//     // get rng state
-//     size_t rngState[2];
-//     rngState[0] = 0;
-//     rngState[1] = 0;
-//     if (pDropout > 0.0) {
-//         DIOPI_CALL(diopiGeneratorGetSeedAndOffset(gen, &(rngState[0]), &(rngState[1])));
-//     }
+    // get rng state
+    size_t rngState[2];
+    rngState[0] = 0;
+    rngState[1] = 0;
+    if (pDropout > 0.0) {
+        DIOPI_CALL(diopiGeneratorGetSeedAndOffset(gen, &(rngState[0]), &(rngState[1])));
+    }
 
-//     DIOPI_CALL_CNNL(cnnlFlashAttentionBackward(handle,
-//                                                flashAttentionDesc.get(),
-//                                                gradOutDesc.get(),
-//                                                gradOutTensor.data(),
-//                                                qDesc.get(),
-//                                                qTensor.data(),
-//                                                kDesc.get(),
-//                                                kTensor.data(),
-//                                                vDesc.get(),
-//                                                vTensor.data(),
-//                                                attentionOutDesc.get(),
-//                                                attentionOutTensor.data(),
-//                                                softmaxLseDesc.get(),
-//                                                softmaxLseTensor.data(),
-//                                                cumSeqQDesc.get(),
-//                                                cumSeqQTensor.data(),
-//                                                cumSeqKVDesc.get(),
-//                                                cumSeqKVTensor.data(),
-//                                                pDropout > 0.0 ? rngState : nullptr,
-//                                                workspace,
-//                                                workspaceSize,
-//                                                gradQDesc.get(),
-//                                                gradQTensorTmp.data(),
-//                                                gradKDesc.get(),
-//                                                gradKTensorTmp.data(),
-//                                                gradVDesc.get(),
-//                                                gradVTensorTmp.data(),
-//                                                nullptr,
-//                                                nullptr,
-//                                                nullptr,
-//                                                nullptr));
+    DIOPI_CALL_CNNL(cnnlFlashAttentionBackward(handle,
+                                               flashAttentionDesc.get(),
+                                               gradOutDesc.get(),
+                                               gradOutTensor.data(),
+                                               qDesc.get(),
+                                               qTensor.data(),
+                                               kDesc.get(),
+                                               kTensor.data(),
+                                               vDesc.get(),
+                                               vTensor.data(),
+                                               attentionOutDesc.get(),
+                                               attentionOutTensor.data(),
+                                               softmaxLseDesc.get(),
+                                               softmaxLseTensor.data(),
+                                               cumSeqQDesc.get(),
+                                               cumSeqQTensor.data(),
+                                               cumSeqKVDesc.get(),
+                                               cumSeqKVTensor.data(),
+                                               pDropout > 0.0 ? rngState : nullptr,
+                                               workspace,
+                                               workspaceSize,
+                                               gradQDesc.get(),
+                                               gradQTensorTmp.data(),
+                                               gradKDesc.get(),
+                                               gradKTensorTmp.data(),
+                                               gradVDesc.get(),
+                                               gradVTensorTmp.data(),
+                                               nullptr,
+                                               nullptr,
+                                               nullptr,
+                                               nullptr));
 
-//     if (gradQTensorTmp.dtype() != gradQTensor.dtype()) {
-//         DIOPI_CALL(dataTypeCast(ctx, gradQTensor, gradQTensorTmp));
-//     }
+    if (gradQTensorTmp.dtype() != gradQTensor.dtype()) {
+        DIOPI_CALL(dataTypeCast(ctx, gradQTensor, gradQTensorTmp));
+    }
 
-//     if (gradKTensorTmp.dtype() != gradKTensor.dtype()) {
-//         DIOPI_CALL(dataTypeCast(ctx, gradKTensor, gradKTensorTmp));
-//     }
+    if (gradKTensorTmp.dtype() != gradKTensor.dtype()) {
+        DIOPI_CALL(dataTypeCast(ctx, gradKTensor, gradKTensorTmp));
+    }
 
-//     if (gradVTensorTmp.dtype() != gradVTensor.dtype()) {
-//         DIOPI_CALL(dataTypeCast(ctx, gradVTensor, gradVTensorTmp));
-//     }
+    if (gradVTensorTmp.dtype() != gradVTensor.dtype()) {
+        DIOPI_CALL(dataTypeCast(ctx, gradVTensor, gradVTensorTmp));
+    }
 
-//     return diopiSuccess;
-// }
+    return diopiSuccess;
+}
 
 }  // namespace camb
 }  // namespace impl
