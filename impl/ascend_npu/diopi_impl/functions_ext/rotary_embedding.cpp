67d9a2b3263fa185edc43caeef7575da308988c7
zhaoguochun1995 <zhaoguochun1995@163.com>
2024-07-04 06:10:43 +0000

    minor change

--- impl/ascend_npu/diopi_impl/functions_ext/rotary_embedding.cpp
+++ impl/ascend_npu/diopi_impl/functions_ext/rotary_embedding.cpp
@@ -67,9 +67,10 @@ DIOPI_API diopiError_t diopiRotaryEmbedding(diopiContextHandle_t ctx, diopiTenso
 
     std::vector<at::Tensor> chunkResult = xView.chunk(2, -1);
     at::Tensor xNew = op_api::cat({chunkResult[1] * (-1), chunkResult[0]}, -1);
-    at::Tensor result = op_api::mul(cosCat, xView) + op_api::mul(sinCat, xNew);
-    outView.copy_(result);
 
+    op_api::mul_out(cosCat, xView, outView);
+    auto result2 = op_api::mul(sinCat, xNew);
+    op_api::add_out(outView, result2, 1.0, outView);
     END_CALL_ACL_OP();
 }
 
