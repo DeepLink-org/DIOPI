32dcf2abe2fbae01dd81cc01fe6304e43f577779
hellozmz <40719054@qq.com>
2024-07-11 11:49:32 +0800

    add diopiBernoulli/diopiBernoulliInp

--- impl/ascend/functions/bernoulli.cpp
+++ impl/ascend/functions/bernoulli.cpp
@@ -9,9 +9,26 @@
 
 namespace impl {
 namespace ascend {
+static const uint64_t philoxDefaultNum = 10;
+
+diopiError_t diopiBernoulli(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiGeneratorHandle_t generator) {
+    const std::pair<uint64_t, uint64_t> gen = getSeedAndOffset(ctx, generator, philoxDefaultNum);
+    const uint64_t seed = gen.first;
+    const uint64_t offset = gen.second;
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceBernoulliTensor, ctx, out, input, seed, offset);
+    return diopiSuccess;
+}
+
+diopiError_t diopiBernoulliInp(diopiContextHandle_t ctx, diopiTensorHandle_t inout, diopiGeneratorHandle_t generator) {
+    const std::pair<uint64_t, uint64_t> gen = getSeedAndOffset(ctx, generator, philoxDefaultNum);
+    const uint64_t seed = gen.first;
+    const uint64_t offset = gen.second;
+    DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceBernoulliTensor, ctx, inout, inout, seed, offset);
+    return diopiSuccess;
+}
 
 diopiError_t diopiBernoulliScalar(diopiContextHandle_t ctx, diopiTensorHandle_t out, double p, diopiGeneratorHandle_t generator) {
-    const std::pair<uint64_t, uint64_t> gen = getSeedAndOffset(ctx, generator, 10);
+    const std::pair<uint64_t, uint64_t> gen = getSeedAndOffset(ctx, generator, philoxDefaultNum);
     const uint64_t seed = gen.first;
     const uint64_t offset = gen.second;
     auto pScalar = constructDiopiScalarT(diopi_dtype_float64, p);
