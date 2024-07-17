0007155f6cad6133ca541cd14fc6239c409d4f3f
hellozmz <40719054@qq.com>
2024-07-17 14:23:07 +0800

    add opapi impl(not ok)

--- impl/ascend/functions/unique.cpp
+++ impl/ascend/functions/unique.cpp
@@ -8,28 +8,29 @@
 #include <iostream>
 #include <vector>
 
+// #include "helper.hpp"
 #include "../aclnn/adaptor.hpp"
 #include "../common/debug.hpp"
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
 
 namespace impl {
 namespace ascend {
 
-diopiError_t diopiUnique(diopiContextHandle_t ctx, diopiTensorHandle_t* out, diopiConstTensorHandle_t input, const int64_t* dim, bool sorted,
+
+diopiError_t diopiUnique1(diopiContextHandle_t ctx, diopiTensorHandle_t* out, diopiConstTensorHandle_t input, const int64_t* dim, bool sorted,
                          bool return_counts, diopiTensorHandle_t indices, diopiTensorHandle_t* counts) {
     bool return_inverse = (indices != nullptr) ? true : false;
-    AscendTensor inputAt(input);
+    AscendTensor inputTmpAt(input), inputAt;
+    contiguous(ctx, inputTmpAt, inputAt);
     if (!inputAt.isContiguous()) {
-        // TODO: need contiguous
-        std::cout << "need contiguous" << std::endl;
+        std::cout << "!!!!!!!!!!!!!!!!!!!!!need contiguous" << std::endl;
     }
     constexpr int64_t NoneN = 1000;
     int64_t dim_value = dim ? (*dim < 0 ? *dim + inputAt.dim() : *dim) : NoneN;
 
     const std::vector<int64_t> inSizeVec = inputAt.shape();
-    std::cout << "inSizeVec=" << std::endl;
-    for (auto i : inSizeVec) {
-        std::cout << i << std::endl;
-    }
     diopiSize_t inSize = {inSizeVec.data(), static_cast<int64_t>(inSizeVec.size())};
 
     diopiTensorHandle_t outTmp = nullptr;
@@ -42,17 +43,21 @@ diopiError_t diopiUnique(diopiContextHandle_t ctx, diopiTensorHandle_t* out, dio
     }
 
     diopiTensorHandle_t inverseTmp = nullptr;
-    if (dim) {
-        std::vector<int64_t> inSizeVec = std::vector<int64_t>{dim_value};
-        diopiSize_t inSize = {inSizeVec.data(), static_cast<int64_t>(inSizeVec.size())};
-        diopiRequireTensor(ctx, &inverseTmp, &inSize, nullptr, diopi_dtype_int64, diopi_device);
-    } else {
-        diopiRequireTensor(ctx, &inverseTmp, &inSize, nullptr, diopi_dtype_int64, diopi_device);
-    }
+    // if (return_inverse) {
+        if (dim) {
+            // std::vector<int64_t> inSizeVec = std::vector<int64_t>{dim_value};
+            std::vector<int64_t> inSizeVec = {inputAt.shape(dim_value)};
+            diopiSize_t inSize = {inSizeVec.data(), static_cast<int64_t>(inSizeVec.size())};
+            diopiRequireTensor(ctx, &inverseTmp, &inSize, nullptr, diopi_dtype_int64, diopi_device);
+        } else {
+            diopiRequireTensor(ctx, &inverseTmp, &inSize, nullptr, diopi_dtype_int64, diopi_device);
+        }
+    // }
 
     diopiTensorHandle_t countsTmp = nullptr;
     if (dim) {
-        std::vector<int64_t> inSizeVec = std::vector<int64_t>{dim_value};
+        // std::vector<int64_t> inSizeVec = std::vector<int64_t>{dim_value};
+        std::vector<int64_t> inSizeVec = {inputAt.shape(dim_value)};
         diopiSize_t inSize = {inSizeVec.data(), static_cast<int64_t>(inSizeVec.size())};
         diopiRequireTensor(ctx, &countsTmp, &inSize, nullptr, diopi_dtype_int64, diopi_device);
     } else {
@@ -63,12 +68,9 @@ diopiError_t diopiUnique(diopiContextHandle_t ctx, diopiTensorHandle_t* out, dio
 
     auto params = ::impl::ascend::aclnn_adaptor::convertParams(input, return_inverse, return_counts, dim_value, outTmp, inverseTmp, countsTmp).params();
 
-    if (dim) {
-        std::cout << "dim=" << *dim << ", change to " << (*dim < 0 ? dim_value : *dim) << std::endl;
-    } else {
-        std::cout << "all dim" << std::endl;
-    }
     DIOPI_ASECND_CALL_ACLNN_TYPE_SYNC(aclnnUniqueConsecutive, ctx, params);
+
+    std::cout << "finish aclnnUniqueConsecutive." << std::endl;
 
     // get true outShape by aclGetViewShape
     int64_t* viewDims = nullptr;
@@ -76,64 +78,62 @@ diopiError_t diopiUnique(diopiContextHandle_t ctx, diopiTensorHandle_t* out, dio
     using aclGetViewShapeFunc = int (*)(const aclTensor* tensor, int64_t** viewDims, uint64_t* viewDimsNum);
     static aclGetViewShapeFunc aclGetViewShape = reinterpret_cast<aclGetViewShapeFunc>(impl::ascend::aclnn_adaptor::getOpApiFuncAddr("aclGetViewShape"));
     int ret = aclGetViewShape(std::get<4>(params), &viewDims, &viewDimNum);
-    ASCEND_CHECK_ABORT(ret == 0, "aclGetViewShape failed");
+    ASCEND_CHECK_ABORT(ret == 0, "get out aclGetViewShape failed");
 
-    std::cout << "viewDimNum=" << viewDimNum << std::endl;
     diopiSize_t outShape{viewDims, static_cast<int64_t>(viewDimNum)};
-    std::vector<int64_t> outShapeVec(viewDims, viewDims + viewDimNum);
-    std::cout << "outShapeVec=" << std::endl;
-    for (auto i : outShapeVec) {
-        std::cout << i << std::endl;
-    }
     // require out tensor from true outShape
     diopiRequireTensor(ctx, out, &outShape, nullptr, inputAt.dtype(), diopi_device);
     // copy outTmp to out
     AscendTensor outAt(*out);
     AscendTensor outTmpAt(outTmp);
-    printContiguousTensor(ctx, outTmpAt, "outTmpAt");
     outTmpAt.view({outShape.data, outShape.data + outShape.len});
     DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceCopy, ctx, outAt, outTmpAt);
+    std::cout << "finish outAt aclnnInplaceCopy." << std::endl;
 
     if (return_inverse) {
-        AscendTensor indicesAt(indices);
         AscendTensor inverseTmpAt(inverseTmp);
+        
+        diopiSize_t inSize = {inverseTmpAt.shape().data(), static_cast<int64_t>(inverseTmpAt.shape().size())};
+        AscendTensor indicesTmpAt(indices);
+        if (indicesTmpAt.shape() != inverseTmpAt.shape()) {
+            diopiRequireTensor(ctx, &indices, &inSize, nullptr, diopi_dtype_int64, diopi_device);
+        }
+        AscendTensor indicesAt(indices);
+        // inverseTmpAt.view(indicesAt.shape());
+        std::cout << dumpTensor(inverseTmpAt, "inverseTmpAt") << std::endl;
+        std::cout << dumpTensor(indicesAt, "indicesAt") << std::endl;
         DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceCopy, ctx, indicesAt, inverseTmpAt);
+        std::cout << "finish indicesAt aclnnInplaceCopy." << std::endl;
     }
 
     if (return_counts) {
-        int ret2 = aclGetViewShape(std::get<6>(params), &viewDims, &viewDimNum);
-        ASCEND_CHECK_ABORT(ret2 == 0, "aclGetViewShape failed");
-        diopiSize_t outShape1{viewDims, static_cast<int64_t>(viewDimNum)};
-        // require out tensor from true outShape
-        diopiRequireTensor(ctx, counts, &outShape1, nullptr, diopi_dtype_int64, diopi_device);
-        // copy outTmp to out
-        AscendTensor countsAt(*counts);
         AscendTensor countsTmpAt(countsTmp);
-        printContiguousTensor(ctx, countsTmpAt, "countsTmpAt");
-        countsTmpAt.view({outShape1.data, outShape1.data + outShape1.len});
-
-        std::vector<int64_t> outSizeVec1(outShape1.data, outShape1.data + outShape1.len);
-        std::cout << "outSizeVec1" << std::endl;
-        for (auto i : outSizeVec1) {
-            std::cout << i << " ";
+        int ret2 = aclGetViewShape(std::get<6>(params), &viewDims, &viewDimNum);
+        ASCEND_CHECK_ABORT(ret2 == 0, "get count aclGetViewShape failed");
+        diopiSize_t countShape{viewDims, static_cast<int64_t>(viewDimNum)};
+        std::vector<int64_t> tmp{countShape.data, countShape.data + countShape.len};        // shape 是错误的。
+        std::cout << "count value=" << std::endl;
+        for (auto i : tmp) {
+            std::cout << i << std::endl;
         }
-        std::cout << "outSizeVec1" << std::endl;
+        std::cout << "finish count value=" << std::endl;
+        std::cout << "tuple_size=" << std::tuple_size<decltype(params)>::value << std::endl;
+        std::cout << dumpTensor(countsTmpAt, "countsTmpAt before resize.") << std::endl;
+        diopiRequireTensor(ctx, counts, &countShape, nullptr, countsTmpAt.dtype(), diopi_device);
+        AscendTensor countsAt(*counts);
+        countsTmpAt.view({countShape.data, countShape.data + countShape.len});
+
+        std::cout << dumpTensor(countsTmpAt, "countsTmpAt") << std::endl;
+        std::cout << dumpTensor(countsAt, "countsAt") << std::endl;
+        printContiguousTensor(ctx, countsAt, "countsAt alllllll");
         DIOPI_ASCEND_CALL_ACLNN(aclnnInplaceCopy, ctx, countsAt, countsTmpAt);
+        std::cout << "finish countsAt aclnnInplaceCopy." << std::endl;
     }
 
-    // if (viewDims) {
-    //     delete viewDims;
-    //     viewDims = nullptr;
-    // }
-
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    std::cout << dumpTensor(*out, "out") << std::endl;
-    std::cout << dumpTensor(indices, "indices") << std::endl;
-    std::cout << dumpTensor(*counts, "counts") << std::endl;
-    printContiguousTensor(ctx, AscendTensor(*out), "out");
-    printContiguousTensor(ctx, AscendTensor(indices), "indices");
-    printContiguousTensor(ctx, AscendTensor(*counts), "counts");
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    if (viewDims) {
+        delete viewDims;
+        viewDims = nullptr;
+    }
 
     return diopiSuccess;
 }
