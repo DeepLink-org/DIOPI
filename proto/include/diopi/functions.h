6d564c9d6c1913f8b9353dd8054ed466dc64f985
Lantian Zhang <1105976166@qq.com>
2024-07-29 19:27:56 +0800

    fix format & fix diopi test config

--- proto/include/diopi/functions.h
+++ proto/include/diopi/functions.h
@@ -1863,7 +1863,8 @@ DIOPI_API diopiError_t diopiSum(diopiContextHandle_t ctx, diopiTensorHandle_t ou
  * @param[in] correction difference between the sample size and sample degrees of freedom.
  * @param[out] out the output tensor depend on dim. type = [float32, float64, float16].
  */
-DIOPI_API diopiError_t diopiStd(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t dim, const diopiScalar_t* correction);
+DIOPI_API diopiError_t diopiStd(diopiContextHandle_t ctx, diopiTensorHandle_t out, diopiConstTensorHandle_t input, diopiSize_t dim,
+                                const diopiScalar_t* correction);
 
 /**
  * @brief Return the minimum value of each row in the input tensor along the given dimension dim.
